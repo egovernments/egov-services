411278f90bf2a9c8f9175a1f4e8858cd6c305a96
venki <venki@egovernments.org>
2017-01-16 16:36:41 +0530

    [PHOENIX-5934] added build and run micro services script.

---
+++ build_and_run_microservices.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SERVICES=(locationassignment employeeassignment persist indexing)
+for d in "${SERVICES[@]}"; do
+ echo "Service name:"$d
+dir_path=egov-pgr-$d
+image_name=egovio/pgr-$d:latest
+cd $dir_path
+ echo "Directory Path:"$dir_path
+ echo "Image name:"$image_name
+mvn clean package
+sudo docker build -t $image_name .
+cd ..
+done
+cd egov-pgrrest/
+sudo docker-compose build
+sudo docker-compose up

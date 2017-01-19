894a94cec49d4d748963cd3acaf51c8cf09200b0
satyamashish <satyam.ashish@egovernments.org>
2017-01-19 13:12:25 +0530

    adding notification in script

--- build_and_run_microservices.sh
+++ build_and_run_microservices.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SERVICES=(notification locationassignment employeeassignment persist indexing)
+SERVICES=(notification pgr-locationassignment pgr-employeeassignment pgr-persist pgr-indexing)
 for d in "${SERVICES[@]}"; do
  echo "Service name:"$d
-dir_path=egov-pgr-$d
+dir_path=egov-$d
 image_name=egovio/pgr-$d:latest
 cd $dir_path
  echo "Directory Path:"$dir_path
@@ -11,4 +11,5 @@ mvn clean package
 sudo docker build -t $image_name .
 cd ..
 done
+pwd
 cd egov-pgrrest/

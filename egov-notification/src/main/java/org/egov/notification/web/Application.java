f7b1b178659e37de57f59102ddb361508c8d865d
Priyank gupta <priyaaank@gmail.com>
2017-01-27 17:22:33 +0530

    Updated notification consumer to implement spring kafka consumer

--- egov-notification/src/main/java/org/egov/notification/web/Application.java
+++ egov-notification/src/main/java/org/egov/notification/web/Application.java
@@ -1,9 +1,5 @@
 package org.egov.notification.web;
 
-import javax.annotation.PostConstruct;
-
-import org.egov.notification.web.consumer.NotificationConsumer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -11,24 +7,9 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-	@Autowired
-	private NotificationConsumer notificationConsumer;
+    public static void main(String[] args) {
 
-	@PostConstruct
-	public void listen() {
-		new Thread(new Runnable() {
+        SpringApplication.run(Application.class, args);
 
-			@Override
-			public void run() {
-				notificationConsumer.consume();
-			}
-		}).start();
-
-	}
-
-	public static void main(String[] args) {
-
-		SpringApplication.run(Application.class, args);
-
-	}
+    }
 }

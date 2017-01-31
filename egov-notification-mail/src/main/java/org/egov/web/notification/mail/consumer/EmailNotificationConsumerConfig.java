d839b002a7b53cde35f61f51ea3cfbb70471921c
venki <venki@egovernments.org>
2017-01-31 18:27:03 +0530

    [PHOENIX-5934] Changed StringDeserializer.

--- egov-notification-mail/src/main/java/org/egov/web/notification/mail/consumer/EmailNotificationConsumerConfig.java
+++ egov-notification-mail/src/main/java/org/egov/web/notification/mail/consumer/EmailNotificationConsumerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.egov.web.notification.mail.transform.EmailRequestDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +15,6 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 
 @Configuration
 @EnableKafka

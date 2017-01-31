7dd0494367c9d8dfcc8957c82d3edfc0d9467978
Priyank gupta <priyaaank@gmail.com>
2017-01-31 14:43:50 +0530

    Externalized the properties for the topics

--- pgr-location-enrichment/src/main/java/org/egov/pgr/consumer/GrievanceLocationEnrichmentListener.java
+++ pgr-location-enrichment/src/main/java/org/egov/pgr/consumer/GrievanceLocationEnrichmentListener.java
@@ -7,11 +7,15 @@ import org.egov.pgr.model.SevaRequest;
 import org.egov.pgr.producer.GrievanceAssignmentProducer;
 import org.egov.pgr.services.BoundaryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 
 import static org.apache.commons.lang.StringUtils.isNotEmpty;
 
 public class GrievanceLocationEnrichmentListener {
+
+    @Value("${kafka.topics.pgr.boundary_enriched.name}")
+    private String locationEnrichedTopicName;
 
     @Autowired
     private GrievanceAssignmentProducer kafkaProducer;
@@ -23,12 +27,12 @@ public class GrievanceLocationEnrichmentListener {
       * Example message
       * {"RequestInfo":{"api_id":"1","ver":"1","ts":null,"action":"create","did":"","key":"","msg_id":"","requester_id":"","auth_token":"ed7fee08-f523-4b30-aa0f-d72b93ba722a"},"ServiceRequest":{"service_request_id":"00029-2017-TK","status":null,"status_details":null,"service_name":null,"service_code":null,"service_id":null,"description":null,"agency_responsible":null,"service_notice":null,"requested_datetime":null,"updated_datetime":null,"expected_datetime":null,"address":null,"address_id":null,"zipcode":null,"lat":12.9188214,"lng":77.6699807,"media_url":null,"first_name":"999999999","last_name":"Jake","phone":null,"email":null,"device_id":null,"account_id":null,"approval_position":null,"approval_comment":null,"values":[]}}
      */
-    @KafkaListener(id = "grievancePersister", topics = "ap.public.mseva.validatedj", group = "grievances")
+    @KafkaListener(id = "${kafka.topics.pgr.validated.id}", topics = "${kafka.topics.pgr.validated.name}", group = "${kafka.topics.pgr.validated.group}")
     public void listen(ConsumerRecord<String, SevaRequest> record) {
         SevaRequest sevaRequest = record.value();
         Long locationId = fetchLocationId(sevaRequest);
         sevaRequest.getServiceRequest().getValues().put("locationId", String.valueOf(locationId));
-        kafkaProducer.sendMessage("ap.public.mseva.locationpopulated", sevaRequest);
+        kafkaProducer.sendMessage(locationEnrichedTopicName, sevaRequest);
     }
 
     private Long fetchLocationId(SevaRequest sevaRequest) {

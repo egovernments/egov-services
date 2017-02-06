1f7edb835217edb42267c7e33c0c11570966901e
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-06 22:19:42 +0530

    Refactoring - Separated out contract and entity class for ComplaintType

---
+++ pgr-rest/src/main/java/org/egov/pgr/PgrRestSpringBootApplication.java
@@ -0,0 +1,21 @@
+package org.egov.pgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class PgrRestSpringBootApplication extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgrRestSpringBootApplication.class, args);
+    }
+}
+
--- pgr-rest/src/main/java/org/egov/pgr/controller/ComplaintTypeController.java
+++ pgr-rest/src/main/java/org/egov/pgr/controller/ComplaintTypeController.java
@@ -1,57 +1,55 @@
 package org.egov.pgr.controller;
 
-import java.util.Date;
-import java.util.List;
-
-import org.egov.pgr.entity.ComplaintType;
+import org.egov.pgr.contract.ComplaintType;
 import org.egov.pgr.model.ResponseInfo;
 import org.egov.pgr.service.ComplaintTypeService;
 import org.egov.pgr.wrapper.ComplaintTypeRequest;
 import org.egov.pgr.wrapper.ComplaintTypeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/complaintType")
 public class ComplaintTypeController {
 
-	@Autowired
-	private ComplaintTypeService complaintTypeService;
+    @Autowired
+    private ComplaintTypeService complaintTypeService;
 
-	@RequestMapping(value = "/complaintTypeByCategory", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public List<ComplaintType> getAllComplaintTypeByCategory(@RequestParam Long categoryId,
-			@RequestParam String tenantId) {
-		return complaintTypeService.findActiveComplaintTypesByCategory(categoryId);
-	}
+    @GetMapping(value = "/complaintTypeByCategory")
+    public List<ComplaintType> getAllComplaintTypeByCategory(@RequestParam Long categoryId,
+                                                             @RequestParam String tenantId) {
+        return complaintTypeService.findActiveComplaintTypesByCategory(categoryId)
+                .stream()
+                .map(ComplaintType::new)
+                .collect(Collectors.toList());
+    }
 
-	@RequestMapping(value = "/getFrequentComplaints", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public List<ComplaintType> getFrequentComplaintsFiled(@RequestParam Integer count, @RequestParam String tenantId) {
-		return complaintTypeService.getFrequentlyFiledComplaints(count);
-	}
+    @GetMapping(value = "/getFrequentComplaints")
+    public List<ComplaintType> getFrequentComplaintsFiled(@RequestParam Integer count, @RequestParam String tenantId) {
+        return complaintTypeService.getFrequentlyFiledComplaints(count)
+                .stream()
+                .map(ComplaintType::new)
+                .collect(Collectors.toList());
+    }
 
-	@GetMapping
-	@ResponseBody
-	public ResponseEntity<?> search(@ModelAttribute ComplaintTypeRequest complaintTypeRequest) {
+    @GetMapping
+    public ResponseEntity<?> search(@ModelAttribute ComplaintTypeRequest complaintTypeRequest) {
 
-		ComplaintTypeResponse complaintTypeResponse = new ComplaintTypeResponse();
-		List<ComplaintType> complaintTypes = complaintTypeService.getComplaintType(complaintTypeRequest);
-		complaintTypeResponse.getComplaintTypes().addAll(complaintTypes);
-		ResponseInfo responseInfo = new ResponseInfo("","",new Date().toString(),"","","");
-		responseInfo.setStatus(HttpStatus.CREATED.toString());
-		complaintTypeResponse.setResponseInfo(responseInfo);
-		return new ResponseEntity<ComplaintTypeResponse>(complaintTypeResponse, HttpStatus.OK);
-	}
+        ComplaintTypeResponse complaintTypeResponse = new ComplaintTypeResponse();
+        List<org.egov.pgr.entity.ComplaintType> complaintTypes = complaintTypeService.getComplaintType
+                (complaintTypeRequest);
+        complaintTypeResponse.getComplaintTypes().addAll(complaintTypes);
+        ResponseInfo responseInfo = new ResponseInfo("", "", new Date().toString(), "", "", "");
+        responseInfo.setStatus(HttpStatus.CREATED.toString());
+        complaintTypeResponse.setResponseInfo(responseInfo);
+        return new ResponseEntity<ComplaintTypeResponse>(complaintTypeResponse, HttpStatus.OK);
+    }
 
 
 }

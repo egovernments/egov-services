bb13212cfa56234dad0eed97868890e8cd40bc6b
parvati <parvati.hiremath@egovernments.org>
2017-02-07 14:24:58 +0530

    [PHOENIX-5934] workflow create

--- egov-workflow/src/main/java/org/egov/workflow/controller/WorkFlowController.java
+++ egov-workflow/src/main/java/org/egov/workflow/controller/WorkFlowController.java
@@ -7,7 +7,7 @@ import org.egov.workflow.model.AssigneeFilterInfo;
 import org.egov.workflow.model.AssigneeRequestInfo;
 import org.egov.workflow.model.PositionResponse;
 import org.egov.workflow.model.ResponseInfo;
-import org.egov.workflow.service.PgrWorkflowImpl;
+import org.egov.workflow.service.WorkflowInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,13 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class WorkFlowController {
 
     @Autowired
-    private PgrWorkflowImpl pgrWorkflowImpl;
+    private WorkflowInterface pgrWorkflowImpl;
     
     @PostMapping(value = "/assignee", produces = MediaType.APPLICATION_JSON_VALUE)
     public PositionResponse getComplaintAssignee(@RequestBody final AssigneeRequestInfo request) {
         final AssigneeFilterInfo assigneeFilterInfo = request.getAssigneeFilterInfo();
 
-        final PositionResponse positionResponse = pgrWorkflowImpl.getAssignee(assigneeFilterInfo.getBoundaryId(),
+        final PositionResponse positionResponse = (PositionResponse) pgrWorkflowImpl.getAssignee(assigneeFilterInfo.getBoundaryId(),
                 assigneeFilterInfo.getComplaintTypeCode(), assigneeFilterInfo.getCurrentAssigneeId());
         if (positionResponse != null)
             positionResponse.setResponseInfo(

e559acde3e684e64f5c7357f3948fee07d1cd343
venki <venki@egovernments.org>
2017-02-08 13:19:47 +0530

    Added new project for pgr web.

---
+++ pgr-web/src/main/java/org/egov/pgr/PgrWebApplication.java
@@ -0,0 +1,12 @@
+package org.egov.pgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgrWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgrWebApplication.class, args);
+	}
+}

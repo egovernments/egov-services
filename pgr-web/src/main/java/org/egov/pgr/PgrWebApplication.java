fcedd5aeae8e04ecb515391599c12f825f6d954d
venki <venki@egovernments.org>
2017-02-08 15:11:48 +0530

    Added javax servlet dependency.

--- pgr-web/src/main/java/org/egov/pgr/PgrWebApplication.java
+++ pgr-web/src/main/java/org/egov/pgr/PgrWebApplication.java
@@ -2,11 +2,18 @@ package org.egov.pgr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PgrWebApplication {
+public class PgrWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PgrWebApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PgrWebApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PgrWebApplication.class, args);
+    }
 }

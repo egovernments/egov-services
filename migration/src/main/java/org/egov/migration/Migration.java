2cefd2a24d92dc9b3126294cbe883c85a8e0ef75
satyamashish <satyam.ashish@egovernments.org>
2017-02-09 10:34:21 +0530

    initial commit for DB migration service

---
+++ migration/src/main/java/org/egov/migration/Migration.java
@@ -0,0 +1,15 @@
+package org.egov.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Migration  {
+
+	
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Migration.class, args);
+	}
+
+}

8d3ddda78c181ed1a33add8dd20328edd98f7d84
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-13 22:08:54 +0530

    Initial commit

---
+++ egov-localization/src/main/java/org/egov/LocalizationServiceApplication.java
@@ -0,0 +1,12 @@
+package org.egov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizationServiceApplication.class, args);
+	}
+}

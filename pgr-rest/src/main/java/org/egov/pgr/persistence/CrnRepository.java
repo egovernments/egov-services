908464f57f429ac468a736dada6033235ca28d78
Renjith G R <grenjithr@gmail.com>
2017-02-16 12:15:20 +0530

    [EGPGR-54]-User @Service instead of @Repository

--- pgr-rest/src/main/java/org/egov/pgr/persistence/CrnRepository.java
+++ pgr-rest/src/main/java/org/egov/pgr/persistence/CrnRepository.java
@@ -3,10 +3,10 @@ package org.egov.pgr.persistence;
 import org.egov.pgr.domain.model.ComplaintRegistrationNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Repository
+@Service
 public class CrnRepository {
 
     private RestTemplate restTemplate;

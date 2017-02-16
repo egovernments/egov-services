8da3a024576c676273f8659583271082c787d3a6
Renjith G R <grenjithr@gmail.com>
2017-02-16 10:52:21 +0530

    [EGPGR-54]-Use crn-generation service in PGR-REST

---
+++ pgr-rest/src/main/java/org/egov/pgr/domain/model/ComplaintRegistrationNumber.java
@@ -0,0 +1,13 @@
+package org.egov.pgr.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ComplaintRegistrationNumber {
+
+    private String value;
+}
---
+++ pgr-rest/src/main/java/org/egov/pgr/persistence/CrnRepository.java
@@ -0,0 +1,26 @@
+package org.egov.pgr.persistence;
+
+import org.egov.pgr.domain.model.ComplaintRegistrationNumber;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+@Repository
+public class CrnRepository {
+
+    private RestTemplate restTemplate;
+    private String crnServiceUrl;
+
+    @Autowired
+    public CrnRepository(RestTemplate restTemplate,
+                         @Value("${crn.service.url}") String crnServiceUrl) {
+
+        this.restTemplate = restTemplate;
+        this.crnServiceUrl = crnServiceUrl;
+    }
+
+    public ComplaintRegistrationNumber getCrn() {
+        return restTemplate.getForObject(crnServiceUrl, ComplaintRegistrationNumber.class);
+    }
+}

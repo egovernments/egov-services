c0dfdf143b427190092065e44af26bff67d8896e
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-16 14:59:01 +0530

    EGPGR-12 | Added correlation id interceptor

--- pgr-crn-generation/src/main/java/org/egov/CrnGenerationApplication.java
+++ pgr-crn-generation/src/main/java/org/egov/CrnGenerationApplication.java
@@ -1,10 +1,36 @@
 package org.egov;
 
+import org.egov.web.interceptor.CorrelationIdInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CrnGenerationApplication {
+
+	@Bean
+	public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+		return new WebMvcConfigurerAdapter() {
+
+			@Override
+			public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+				configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+			}
+
+			@Override
+			public void addInterceptors(InterceptorRegistry registry) {
+				registry.addInterceptor(new CorrelationIdInterceptor());
+			}
+		};
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrnGenerationApplication.class, args);

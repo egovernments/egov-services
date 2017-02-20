7753006329e184f8c963381f67fda87fe89540fd
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-20 14:48:52 +0530

    EGPGR-12 | Ajay, Iffath | Added correlation id

--- pgr-location-enrichment/src/main/java/org/egov/pgr/PgrLocationEnrichmentApplication.java
+++ pgr-location-enrichment/src/main/java/org/egov/pgr/PgrLocationEnrichmentApplication.java
@@ -1,10 +1,18 @@
 package org.egov.pgr;
 
+import org.egov.pgr.interceptor.CorrelationIdAwareRestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PgrLocationEnrichmentApplication {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new CorrelationIdAwareRestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PgrLocationEnrichmentApplication.class, args);

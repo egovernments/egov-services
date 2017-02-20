2ab16a591dd5e76510831c8a4533024dd893d8a5
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-02-20 16:20:45 +0530

    Fixing deployer step

--- jenkins/deployer.groovy
+++ jenkins/deployer.groovy
@@ -2,8 +2,10 @@ def deploy(service_name, commit_id){
     def namespace = "${JOB_NAME}".split("/")[-2]
     def tag = "${BUILD_ID}-${commit_id}"
     def image = "${service_name}:${tag}"
+    def clusters = sh(returnStdout: true, script:"kubectl config get-clusters")
+    println(clusters)
     sh "kubectl set image deployments/${service_name} ${service_name}=${image} --namespace=${namespace}"
-    deployStatus = sh(returnStdout: true, script: "kubectl rollout status deployments/${service_name} --namespace=${namespace}").trim()
+    def deployStatus = sh(returnStdout: true, script: "kubectl rollout status deployments/${service_name} --namespace=${namespace}").trim()
     println(deploystatus)
     if (deployStatus.contains("deployment \"${service_name}\" successfully rolled out")) {
         println("deployment SUCCEDED!!")

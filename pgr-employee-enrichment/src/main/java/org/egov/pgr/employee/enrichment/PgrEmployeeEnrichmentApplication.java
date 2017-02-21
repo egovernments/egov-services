e71bd9adcc3e7c64b1b89ef75552bf1fabc21e08
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-21 10:16:19 +0530

    Refactoring - Globally ignore unknown properties

--- pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/PgrEmployeeEnrichmentApplication.java
+++ pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/PgrEmployeeEnrichmentApplication.java
@@ -1,13 +1,28 @@
 package org.egov.pgr.employee.enrichment;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.egov.pgr.employee.enrichment.http.CorrelationIdAwareRestTemplate;
+import org.egov.pgr.employee.enrichment.json.ObjectMapperFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class PgrEmployeeEnrichmentApplication {
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return ObjectMapperFactory.create();
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jacksonConverter(ObjectMapper objectMapper) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
 
     @Bean
     public RestTemplate restTemplate() {
--- pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/model/WorkflowResponse.java
+++ pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/model/WorkflowResponse.java
@@ -1,11 +1,9 @@
 package org.egov.pgr.employee.enrichment.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @AllArgsConstructor
 public class WorkflowResponse {

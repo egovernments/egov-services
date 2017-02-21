dd2aec6766192809feb0d0e723a7effea6ad4faa
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-02-21 17:20:45 +0530

    Removing old projects

--- eGov/egov/egov-pgr/src/main/resources/db/migration/main/V20170127103339__egi_addcolumn_tenant_filestore.sql
+++
@@ -1,2 +0,0 @@
----Added script in pgr temporarily
-alter table eg_filestoremap add column tenant character varying(50);
--- migration/dock.sh
+++
@@ -1,4 +0,0 @@
-./mvnw clean package
-docker build -t egovio/financials:latest .
-docker-compose build
-docker-compose up

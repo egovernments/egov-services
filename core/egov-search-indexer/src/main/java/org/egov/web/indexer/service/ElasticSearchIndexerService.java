f8153a6a48eec3eed5c77c3d61b6e5fa078f1b96
Ajay Divakaran <ajay_886@hotmail.com>
2017-02-22 16:14:32 +0530

    EGPGR-12 | Upgraded Spring Boot version, added PMD and Jacoco plugins

--- core/egov-search-indexer/src/main/java/org/egov/web/indexer/service/ElasticSearchIndexerService.java
+++ core/egov-search-indexer/src/main/java/org/egov/web/indexer/service/ElasticSearchIndexerService.java
@@ -1,15 +1,13 @@
 package org.egov.web.indexer.service;
 
-import java.util.Map;
-
-import org.egov.web.indexer.config.IndexerPropertiesManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 public class ElasticSearchIndexerService {

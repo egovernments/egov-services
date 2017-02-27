1bc33e9b9fd4093a9843a56ac7ddb4f2aec81386
venki <venki@egovernments.org>
2017-02-27 16:11:22 +0530

    Added some more seed data in localization...

---
+++ core/egov-localization/src/main/resources/db/migration/seed/V20170227155950__locale_message_setup_some_more_data.sql
@@ -0,0 +1,18 @@
+insert into message(id,locale,code,message,tenant_id) values
+(57,'en_IN','sr.in.process','Service Request is received and is under process.','ap.public');
+
+insert into message(id,locale,code,message,tenant_id) values
+(58,'en_IN','ur.service.req.no','Your SRN(Service Request No.)','ap.public');
+
+insert into message(id,locale,code,message,tenant_id) values
+(59,'en_IN','future.references','Please use this for all your future references','ap.public');
+
+insert into message(id,locale,code,message,tenant_id) values
+(60,'mr_IN','sr.in.process','सेवा विनंती प्राप्त झाली आहे आणि प्रक्रिया आहे.','ap.public');
+
+insert into message(id,locale,code,message,tenant_id) values
+(61,'mr_IN','ur.service.req.no','आपल्या SRN (सेवा विनंती क्रमांक)','ap.public');
+
+insert into message(id,locale,code,message,tenant_id) values
+(62,'mr_IN','future.references','कृपया आपल्या भविष्यातील सर्व संदर्भ हे वापरू','ap.public');
+

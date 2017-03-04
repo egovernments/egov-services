09333bcf7dae3337dfa8fecb348323caf98d5498
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-03-05 00:01:42 +0530

    Added jenkins scripts for promotions

---
+++ jenkins/env_snapshot.groovy
@@ -0,0 +1,27 @@
+def takeSnapshot(group, env, ci_image){
+    stage("Snapshot ${group} in ${env} env"){
+        docker.image("${ci_image}").inside {
+            set_kube_credentials(env)
+            withCredentials([string(credentialsId: "${env}-kube-url",
+            variable: "KUBE_SERVER_URL")]){
+            sh "cd jenkins/scripts; python snapshot.py ${group}";
+        }
+    }
+}
+
+def set_kube_credentials(env){
+    sh "rm -rf /kube; mkdir -p /kube"
+    withCredentials([string(credentialsId: "${env}-kube-ca", variable: "CA")]){
+        sh "echo ${CA} >> /kube/ca.pem"
+    }
+    withCredentials([string(credentialsId: "${env}-kube-cert", variable:
+    "CERT")]){
+        sh "echo ${CERT} >> /kube/admin.pem"
+    }
+    withCredentials([string(credentialsId: "${env}-kube-key", variable:
+    "CERT-KEY")]){
+        sh "echo ${CERT-KEY} >> /kube/admin-key.pem"
+    }
+}
+
+return this;
---
+++ jenkins/scripts/snapshot.py
@@ -0,0 +1,69 @@
+import json
+import os
+import sys
+import shlex
+from subprocess import PIPE, Popen
+
+group = sys.argv[1]
+
+
+def get_deployments():
+    get_deployments_cmd = "kubectl get deployments --namespace=egov -l " \
+                          "group={}".format(group)
+    filter_headers_cmd = "grep -v NAME"
+    awk_cmd = "awk {'print $1'}"
+    deployments_verbose = Popen(shlex.split(get_deployments_cmd), stdout=PIPE)
+    deployments_verbose_without_headers = Popen(shlex.split(
+        filter_headers_cmd), stdin=deployments_verbose.stdout, stdout=PIPE)
+    deployment_names = Popen(shlex.split(awk_cmd),
+                             stdin=deployments_verbose_without_headers.stdout,
+                             stdout=PIPE)
+    deployments, error = deployment_names.communicate()
+    if error:
+        raise Exception("Deployments doesn't exist for group {}\nERROR:{}"
+                        .format(group, error))
+    return deployments.strip().split("\n")
+
+
+def get_image_tags(deployments):
+    image_tags = {}
+    for d in deployments:
+        deployment_cmd = "kubectl get deployment {} -o json " \
+                         "--namespace=egov".format(d)
+        deployment_config = Popen(shlex.split(deployment_cmd), stdout=PIPE)
+        deployment_json, error = deployment_config.communicate()
+        if error:
+            raise Exception("Cannot find deployment config for {}\nERROR:{}".
+                            format(d, error))
+        deployment = json.loads(deployment_json)
+        containers = deployment['spec']['template']['spec']['containers']
+        for c in containers:
+            name = str(c['name'])
+            image = str(c['image'])
+            image_tags[name] = image
+    return image_tags
+
+
+def set_kubectl_env():
+    kube_server_url = os.getenv("KUBE_SERVER_URL")
+    if not kube_server_url:
+        raise Exception("KUBE_SERVER_URL env var is not set")
+    set_kubectl_env_cmd = ("kubectl config set-cluster env --server {}"
+                           .format(kube_server_url))
+    out, err = Popen(shlex.split(set_kubectl_env_cmd)).communicate()
+    if err:
+        raise Exception("kube server url could not be set\nERROR:{}"
+                        .format(err))
+
+
+def main():
+    set_kubectl_env()
+    deployments = get_deployments()
+    tags = get_image_tags(deployments)
+    os.remove("image_tags.txt")
+    with open("image_tags.txt", "w") as f:
+        f.write(json.dumps(tags))
+
+
+if __name__ == "__main__":
+    main()

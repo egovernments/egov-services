7d6d522543739a3af5d1b8ec10dca5942cf802ca
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-03-06 07:02:39 +0530

    Testing env promotions via jenkins

--- jenkins/scripts/snapshot.py
+++ jenkins/scripts/snapshot.py
@@ -26,7 +26,7 @@ def get_deployments():
 
 
 def get_image_tags(deployments):
-    get_kube_config_cmd = "cat ~/.kube/config"
+    get_kube_config_cmd = "cat /.kube/config"
     out,err = Popen(shlex.split(get_kube_config_cmd)).communicate()
     print "$$$$$$"
     print out, err

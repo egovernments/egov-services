8027f9962dcb2211d098e86723dad5c06411cb2a
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-08 19:16:33 +0530

    EGPGR-209 | Sending of OTP message to queue is blocking

--- user/user-otp/src/main/java/org/egov/persistence/repository/OtpMessageRepository.java
+++ user/user-otp/src/main/java/org/egov/persistence/repository/OtpMessageRepository.java
@@ -2,18 +2,24 @@ package org.egov.persistence.repository;
 
 import org.egov.domain.model.OtpRequest;
 import org.egov.persistence.contract.SMSRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 import static java.lang.String.format;
 
 @Service
 public class OtpMessageRepository {
     private static final String SMS_OTP_MESSAGE = "Use OTP {0} for portal registration.";
+    private static final String OTP_SEND_FAILURE_MESSAGE = "Sending of User OTP message failed";
     private KafkaTemplate<String, SMSRequest> kafkaTemplate;
     private String smsTopic;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public OtpMessageRepository(KafkaTemplate<String, SMSRequest> kafkaTemplate,
@@ -23,7 +29,12 @@ public class OtpMessageRepository {
     }
 
     public void send(OtpRequest otpRequest, String otpNumber) {
-        kafkaTemplate.send(smsTopic, new SMSRequest(otpRequest.getMobileNumber(), getMessage(otpNumber)));
+        try {
+            kafkaTemplate.send(smsTopic, new SMSRequest(otpRequest.getMobileNumber(), getMessage(otpNumber))).get();
+        } catch (InterruptedException | ExecutionException e) {
+            logger.error(OTP_SEND_FAILURE_MESSAGE, e);
+            throw new RuntimeException(e);
+        }
     }
 
     private String getMessage(String otpNumber) {

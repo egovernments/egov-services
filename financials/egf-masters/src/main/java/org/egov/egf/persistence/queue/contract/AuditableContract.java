17b66715c0b5409cc6b3f683913a8195338ea37b
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2017-03-13 16:20:07 +0530

    Added tenantid column in financial master table and same added in auditable abstract and abstract auditable class

--- financials/egf-masters/src/main/java/org/egov/egf/persistence/queue/contract/AuditableContract.java
+++ financials/egf-masters/src/main/java/org/egov/egf/persistence/queue/contract/AuditableContract.java
@@ -2,21 +2,24 @@ package org.egov.egf.persistence.queue.contract;
 
 import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import lombok.Data;
 
 @Data
-@JsonPropertyOrder({ "createdBy", "createdDate", "lastModifiedBy", "lastModifiedDate" })
+@JsonPropertyOrder({ "createdBy", "createdDate", "lastModifiedBy", "lastModifiedDate", "tenantId" })
 public class AuditableContract {
 
-	private static final long serialVersionUID = 7138056997693406739L;
+    private Long createdBy;
 
-	private Long createdBy;
+    private Date createdDate;
 
-	private Date createdDate;
+    private Long lastModifiedBy;
 
-	private Long lastModifiedBy;
+    private Date lastModifiedDate;
 
-	private Date lastModifiedDate;
+    @NotNull
+    private String tenantId;
 }

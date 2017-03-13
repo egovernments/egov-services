3697c499475e104447ca1e0f39b9055b8600baf7
Elzan Mathew <elzan.mathew@egovernments.org>
2017-03-13 15:16:33 +0530

    Adding attendance type master data

---
+++ hr/hr-attendance/src/main/resources/db/migration/v1_3_egeis_attendancetype_masterdata.sql
@@ -0,0 +1,4 @@
+insert into egeis_attendance_type(id,code,description) values(nextval('seq_egeis_attendance_type'),'P','Present');
+insert into egeis_attendance_type(id,code,description) values(nextval('seq_egeis_attendance_type'),'A','Absent');
+insert into egeis_attendance_type(id,code,description) values(nextval('seq_egeis_attendance_type'),'L','Leave');
+insert into egeis_attendance_type(id,code,description) values(nextval('seq_egeis_attendance_type'),'H','Holiday');

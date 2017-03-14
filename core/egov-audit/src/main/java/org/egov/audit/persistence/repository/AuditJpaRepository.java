b8f3e52aa299e8877e0343bb443ba3287c2da1b0
Seema Nayak <seema.nayak@egovernments.org>
2017-03-14 16:29:28 +0530

    Deleting egov-audit project

--- core/egov-audit/src/main/java/org/egov/audit/persistence/repository/AuditJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package org.egov.audit.persistence.repository;
-
-import org.egov.audit.persistence.entity.Audit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditJpaRepository extends JpaRepository<Audit, Long> {
-
-}
--- core/egov-audit/src/main/java/org/egov/audit/web/controller/AuditController.java
+++
@@ -1,5 +0,0 @@
-package org.egov.audit.web.controller;
-
-public class AuditController {
-
-}

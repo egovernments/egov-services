d5c4fc852981b09fee1d58ee1f4a0940836304cd
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-03-14 12:33:40 +0530

    folders restructured

--- lams/lams/lams-services/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -z "${JAVA_OPTS}" ]];then
-    export JAVA_OPTS="-Xmx64m -Xms64m"
-fi
-
-java ${JAVA_OPTS} -jar /opt/egov/lams.jar
--- lams/lams/lams-web/assets/javascript/search-assets/create-agreement-ack.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $('#close').on("click",function() {
-          window.close();
-    })
-
-
-});

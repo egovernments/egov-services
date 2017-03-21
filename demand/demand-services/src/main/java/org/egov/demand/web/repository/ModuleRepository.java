26cc56eee74dee9235353cdbc21cbcc5cc6061b5
ramki <ramakrishna@egovernments.org>
2017-03-21 18:32:51 +0530

    fixes

--- demand/demand-services/src/main/java/org/egov/demand/web/repository/ModuleRepository.java
+++ demand/demand-services/src/main/java/org/egov/demand/web/repository/ModuleRepository.java
@@ -1,5 +1,7 @@
 package org.egov.demand.web.repository;
 
+import java.util.Date;
+
 import org.egov.demand.web.contract.Module;
 import org.egov.demand.web.contract.ModuleResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +12,26 @@ import org.springframework.web.client.RestTemplate;
 @Repository
 public class ModuleRepository {
 	private final String commonServicesHost;
-	private final String moduleByNameUrl;
+	private final String searchPath;
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	public ModuleRepository(RestTemplate restTemplate,
-			@Value("${egov.services.common-masters.host}") String commonServicesHost,
-			@Value("${egov.services.common-masters.module_by_name}") final String moduleByNameUrl) {
+			@Value("${egov.services.common-masters.hostname}") String commonServicesHost,
+			@Value("${egov.services.common-masters.searchpath}") final String searchPath) {
 		this.restTemplate = restTemplate;
 		this.commonServicesHost = commonServicesHost;
-		this.moduleByNameUrl = moduleByNameUrl;
+		this.searchPath = searchPath;
 	}
 
 	public Module fetchModuleByName(String name) {
-		String url = this.commonServicesHost + moduleByNameUrl;
-		return getModuleServiceResponse(url, name).getModules().get(0);
-	}
-
-	private ModuleResponse getModuleServiceResponse(final String url, String name) {
-		return restTemplate.getForObject(url, ModuleResponse.class, name);
+		String url = this.commonServicesHost + searchPath+"?tenantId=ap.public&name="+name;
+		org.egov.demand.web.contract.RequestInfo requestInfo = new org.egov.demand.web.contract.RequestInfo();
+		requestInfo.setApiId("org.egov.commons.module.search");
+		requestInfo.setVer("1.0");
+		requestInfo.setTs(new Date().toString());
+		ModuleResponse moduleResponse = restTemplate.postForObject(url, requestInfo, ModuleResponse.class);
+		return moduleResponse.getModules().get(0);
 	}
 }

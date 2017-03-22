ba06b3ce73ace61516532840e6d2146aac5fad3c
Seema Nayak <seema.nayak@egovernments.org>
2017-03-22 16:46:05 +0530

    [EGPGR-204] Build Fix

--- pgr/pgr-rest/src/test/java/org/egov/pgr/persistence/repository/ComplaintRepositoryTest.java
+++ pgr/pgr-rest/src/test/java/org/egov/pgr/persistence/repository/ComplaintRepositoryTest.java
@@ -1,5 +1,16 @@
 package org.egov.pgr.persistence.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 import org.egov.pgr.domain.model.ComplaintSearchCriteria;
 import org.egov.pgr.persistence.entity.Complaint;
 import org.egov.pgr.persistence.queue.contract.RequestInfo;
@@ -13,113 +24,83 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.*;
-
 @RunWith(MockitoJUnitRunner.class)
 public class ComplaintRepositoryTest {
 
-    @Mock
-    private ComplaintJpaRepository complaintJpaRepository;
+	@Mock
+	private ComplaintJpaRepository complaintJpaRepository;
 
-    @Mock
-    private ComplaintMessageQueueRepository complaintMessageQueueRepository;
+	@Mock
+	private ComplaintMessageQueueRepository complaintMessageQueueRepository;
 
-    private ComplaintRepository complaintRepository;
+	private ComplaintRepository complaintRepository;
 
-    @Before
-    public void setUp() throws Exception {
-        complaintRepository = new ComplaintRepository(complaintJpaRepository, complaintMessageQueueRepository);
-    }
+	@Before
+	public void setUp() throws Exception {
+		complaintRepository = new ComplaintRepository(complaintJpaRepository, complaintMessageQueueRepository);
+	}
 
-    @Test
-    public void test_complaint_repository_should_call_jpa_repository_to_save_the_complaint() {
-        SevaRequest sevaRequest = mock(SevaRequest.class);
-        RequestInfo requestInfo = mock(RequestInfo.class);
-        when(sevaRequest.getRequestInfo()).thenReturn(requestInfo);
+	@Test
+	public void test_complaint_repository_should_call_jpa_repository_to_save_the_complaint() {
+		SevaRequest sevaRequest = mock(SevaRequest.class);
+		RequestInfo requestInfo = mock(RequestInfo.class);
+		when(sevaRequest.getRequestInfo()).thenReturn(requestInfo);
 
-        complaintRepository.save(sevaRequest);
+		complaintRepository.save(sevaRequest);
 
-        verify(requestInfo).setAction("POST");
-        verify(complaintMessageQueueRepository).save(sevaRequest);
-    }
+		verify(requestInfo).setAction("POST");
+		verify(complaintMessageQueueRepository).save(sevaRequest);
+	}
 
-    @Test
-    public void test_complaint_repository_should_call_jpa_repository_to_update_the_complaint() {
-        SevaRequest sevaRequest = mock(SevaRequest.class);
-        RequestInfo requestInfo = mock(RequestInfo.class);
-        when(sevaRequest.getRequestInfo()).thenReturn(requestInfo);
+	@Test
+	public void test_complaint_repository_should_call_jpa_repository_to_update_the_complaint() {
+		SevaRequest sevaRequest = mock(SevaRequest.class);
+		RequestInfo requestInfo = mock(RequestInfo.class);
+		when(sevaRequest.getRequestInfo()).thenReturn(requestInfo);
 
-        complaintRepository.update(sevaRequest);
+		complaintRepository.update(sevaRequest);
 
-        verify(requestInfo).setAction("PUT");
-        verify(complaintMessageQueueRepository).save(sevaRequest);
-    }
+		verify(requestInfo).setAction("PUT");
+		verify(complaintMessageQueueRepository).save(sevaRequest);
+	}
 
-    @Test
-    public void test_find_all_complaints_using_specification() {
-        final Sort sort = new Sort(Sort.Direction.DESC, "lastModifiedDate");
-        ComplaintSearchCriteria complaintSearchCriteria = mock(ComplaintSearchCriteria.class);
-        Complaint complaintEntityMock1 = mock(Complaint.class);
-        Complaint complaintEntityMock2 = mock(Complaint.class);
-        org.egov.pgr.domain.model.Complaint complaintModelMock1 = mock(org.egov.pgr.domain.model.Complaint.class);
-        org.egov.pgr.domain.model.Complaint complaintModelMock2 = mock(org.egov.pgr.domain.model.Complaint.class);
+	@Test
+	public void test_find_all_complaints_using_specification() {
+		final Sort sort = new Sort(Sort.Direction.DESC, "lastModifiedDate");
+		ComplaintSearchCriteria complaintSearchCriteria = mock(ComplaintSearchCriteria.class);
+		Complaint complaintEntityMock1 = mock(Complaint.class);
+		Complaint complaintEntityMock2 = mock(Complaint.class);
+		org.egov.pgr.domain.model.Complaint complaintModelMock1 = mock(org.egov.pgr.domain.model.Complaint.class);
+		org.egov.pgr.domain.model.Complaint complaintModelMock2 = mock(org.egov.pgr.domain.model.Complaint.class);
 
-        when(complaintEntityMock1.toDomain()).thenReturn(complaintModelMock1);
-        when(complaintEntityMock2.toDomain()).thenReturn(complaintModelMock2);
+		when(complaintEntityMock1.toDomain()).thenReturn(complaintModelMock1);
+		when(complaintEntityMock2.toDomain()).thenReturn(complaintModelMock2);
 
-        when(complaintJpaRepository.findAll(Matchers.<Specification<Complaint>>any(), eq(sort))).thenReturn(
-                Arrays.asList(complaintEntityMock1, complaintEntityMock2)
-        );
+		when(complaintJpaRepository.findAll(Matchers.<Specification<Complaint>> any(), eq(sort)))
+				.thenReturn(Arrays.asList(complaintEntityMock1, complaintEntityMock2));
 
-        List<org.egov.pgr.domain.model.Complaint> actual = complaintRepository.findAll(complaintSearchCriteria);
+		List<org.egov.pgr.domain.model.Complaint> actual = complaintRepository.findAll(complaintSearchCriteria);
 
-        assertThat(Arrays.asList(complaintModelMock1, complaintModelMock2)).isEqualTo(actual);
-    }
+		assertThat(Arrays.asList(complaintModelMock1, complaintModelMock2)).isEqualTo(actual);
+	}
 
-    @Test
-    public void test_complaint_repository_should_get_complaints_for_user_from_jpa_repository() {
-        Complaint complaintEntityMock1 = mock(Complaint.class);
-        Complaint complaintEntityMock2 = mock(Complaint.class);
-        org.egov.pgr.domain.model.Complaint complaintModelMock1 = mock(org.egov.pgr.domain.model.Complaint.class);
-        org.egov.pgr.domain.model.Complaint complaintModelMock2 = mock(org.egov.pgr.domain.model.Complaint.class);
+	@Test
+	public void test_complaint_repository_should_get_complaints_modified_for_a_user_from_jpa_repository() {
+		Date lastAccessedTime = Calendar.getInstance().getTime();
+		Complaint complaintEntityMock1 = mock(Complaint.class);
+		Complaint complaintEntityMock2 = mock(Complaint.class);
+		org.egov.pgr.domain.model.Complaint complaintModelMock1 = mock(org.egov.pgr.domain.model.Complaint.class);
+		org.egov.pgr.domain.model.Complaint complaintModelMock2 = mock(org.egov.pgr.domain.model.Complaint.class);
 
-        when(complaintEntityMock1.toDomain()).thenReturn(complaintModelMock1);
-        when(complaintEntityMock2.toDomain()).thenReturn(complaintModelMock2);
+		when(complaintEntityMock1.toDomain()).thenReturn(complaintModelMock1);
+		when(complaintEntityMock2.toDomain()).thenReturn(complaintModelMock2);
 
-        when(complaintJpaRepository.findByCreatedByOrderByLastAccessedTimeDesc(1L)).thenReturn(
-                Arrays.asList(complaintEntityMock1, complaintEntityMock2)
-        );
+		when(complaintJpaRepository.getAllModifiedComplaintsForCitizen(lastAccessedTime, 1L))
+				.thenReturn(Arrays.asList(complaintEntityMock1, complaintEntityMock2));
 
-        List<org.egov.pgr.domain.model.Complaint> actual = complaintRepository.getAllComplaintsForGivenUser(1L);
+		List<org.egov.pgr.domain.model.Complaint> actual = complaintRepository
+				.getAllModifiedComplaintsForCitizen(lastAccessedTime, 1L);
 
-        assertThat(Arrays.asList(complaintModelMock1, complaintModelMock2)).isEqualTo(actual);
-    }
-
-    @Test
-    public void test_complaint_repository_should_get_complaints_modified_for_a_user_from_jpa_repository() {
-        Date lastAccessedTime = Calendar.getInstance().getTime();
-        Complaint complaintEntityMock1 = mock(Complaint.class);
-        Complaint complaintEntityMock2 = mock(Complaint.class);
-        org.egov.pgr.domain.model.Complaint complaintModelMock1 = mock(org.egov.pgr.domain.model.Complaint.class);
-        org.egov.pgr.domain.model.Complaint complaintModelMock2 = mock(org.egov.pgr.domain.model.Complaint.class);
-
-        when(complaintEntityMock1.toDomain()).thenReturn(complaintModelMock1);
-        when(complaintEntityMock2.toDomain()).thenReturn(complaintModelMock2);
-
-        when(complaintJpaRepository.getAllModifiedComplaintsForCitizen(lastAccessedTime, 1L)).thenReturn(
-                Arrays.asList(complaintEntityMock1, complaintEntityMock2)
-        );
-
-        List<org.egov.pgr.domain.model.Complaint> actual =
-                complaintRepository.getAllModifiedComplaintsForCitizen(lastAccessedTime, 1L);
-
-        assertThat(Arrays.asList(complaintModelMock1, complaintModelMock2)).isEqualTo(actual);
-    }
+		assertThat(Arrays.asList(complaintModelMock1, complaintModelMock2)).isEqualTo(actual);
+	}
 }

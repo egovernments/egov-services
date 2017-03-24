c9e50063618f7c6a88767e6c24cd40a2ffec39ea
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-24 16:34:22 +0530

    EGPGR-294 | Added global objectmapper settings

--- pgr/pgr-notification/src/main/java/org/egov/PgrNotificationApplication.java
+++ pgr/pgr-notification/src/main/java/org/egov/PgrNotificationApplication.java
@@ -1,5 +1,8 @@
 package org.egov;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,23 +10,38 @@ import org.trimou.engine.MustacheEngine;
 import org.trimou.engine.MustacheEngineBuilder;
 import org.trimou.engine.locator.ClassPathTemplateLocator;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PgrNotificationApplication {
 
-	private static final String TEMPLATE_TYPE = "txt";
-	private static final String TEMPLATE_FOLDER = "templates";
-	private static final int PRIORITY = 1;
+    private static final String TEMPLATE_TYPE = "txt";
+    private static final String TEMPLATE_FOLDER = "templates";
+    private static final int PRIORITY = 1;
+    private static final String IST = "Asia/Calcutta";
 
-	public static void main(String[] args) {
-		SpringApplication.run(PgrNotificationApplication.class, args);
-	}
 
-	@Bean
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone(IST));
+        SpringApplication.run(PgrNotificationApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy hh:mm a"));
+        return mapper;
+    }
+
+    @Bean
     public MustacheEngine getMustacheEngine() {
-		ClassPathTemplateLocator classPathTemplateLocator =
-				new ClassPathTemplateLocator(PRIORITY, TEMPLATE_FOLDER, TEMPLATE_TYPE);
-		return MustacheEngineBuilder.newBuilder()
-				.addTemplateLocator(classPathTemplateLocator)
-				.build();
-	}
+        ClassPathTemplateLocator classPathTemplateLocator =
+            new ClassPathTemplateLocator(PRIORITY, TEMPLATE_FOLDER, TEMPLATE_TYPE);
+        return MustacheEngineBuilder.newBuilder()
+            .addTemplateLocator(classPathTemplateLocator)
+            .build();
+    }
 }

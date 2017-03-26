b96f133558d6ea2ee7e8e245f4ff78c6e13dac12
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-26 13:56:24 +0530

    Fixed pgr employee enricher startup issue

--- pgr/pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/PgrEmployeeEnrichmentApplication.java
+++ pgr/pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/PgrEmployeeEnrichmentApplication.java
@@ -2,12 +2,15 @@ package org.egov.pgr.employee.enrichment;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.egov.pgr.employee.enrichment.json.ObjectMapperFactory;
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
+@Import(TracerConfiguration.class)
 public class PgrEmployeeEnrichmentApplication {
 
     @Bean

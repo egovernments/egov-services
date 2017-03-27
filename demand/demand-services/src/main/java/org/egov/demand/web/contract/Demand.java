0917770ce3a8845036c21f8c2b9b29b39625b0cb
ramki <ramakrishna@egovernments.org>
2017-03-27 17:55:53 +0530

    added collectionAmount and amount for demand contract

--- demand/demand-services/src/main/java/org/egov/demand/web/contract/Demand.java
+++ demand/demand-services/src/main/java/org/egov/demand/web/contract/Demand.java
@@ -1,5 +1,6 @@
 package org.egov.demand.web.contract;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -13,8 +14,10 @@ import lombok.Setter;
 @Builder
 public class Demand {
 	private Long id;
+	private BigDecimal taxAmount;
+	private BigDecimal collectionAmount;
 	private String installment;
 	private String moduleName;
 	private List<DemandDetails> demandDetails;
-	private List<PaymentInfo> paymentInfo;
+	private List<PaymentInfo> paymentInfos;
 }

b53d219560607c819f5a6f6f9b38947d276ddb52
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-03-28 13:15:21 +0530

    log update

--- lams/lams-services/src/main/java/org/egov/lams/service/AssetService.java
+++ lams/lams-services/src/main/java/org/egov/lams/service/AssetService.java
@@ -33,31 +33,31 @@ public class AssetService {
 		url = propertiesManager.getAssetServiceHostName() + propertiesManager.getAssetServiceBasePAth()
 				+ propertiesManager.getAssetServiceSearchPath() + "?" + queryString;
 
-		logger.info("the url of asset api call ",url);
+		logger.info("the url of asset api call : "+url,url);
 		try {
 			assetResponse = restTemplate.postForObject(url, requestInfo, AssetResponse.class);
 		} catch (Exception e) {
 			logger.debug("exception in AssetService restTemplate", assetResponse);
 			throw new RuntimeException("check if entered asset API url is correct or the asset service is running");
 		}
-		logger.info("the list of assets from assetresponse obtained by asset api call",assetResponse.getAssets());
+		logger.info("the list of assets from assetresponse obtained by asset api call : "+assetResponse.getAssets(),assetResponse.getAssets());
 		return assetResponse;
 	}
 	
 	public boolean isAssetAvailable(Long assetId) {
 		
 		final String sql = "select id from eglams_agreement where asset="+assetId;
-		logger.info("the url for asset api call to check isAssetAvalable",sql);
+		logger.info("the url for asset api call to check isAssetAvalable : "+sql,sql);
 		//FIXME table name from config 
 		List<Map<String, Object>> resultSet = null;
 		try{
 			resultSet = jdbcTemplate.queryForList(sql);
 		}catch (Exception exception) {
-			logger.debug("aseetService isassetAvailable : ",exception);
+			logger.debug("aseetService isassetAvailable : " + exception,exception);
 			throw exception;
 			// TODO: handle exception
 		}
-		logger.info("the list of assets from assetresponse obtained by asset api call",resultSet);
+		logger.info("the list of assets from assetresponse obtained by asset api call : "+resultSet,resultSet);
 		return resultSet.isEmpty();
 	}
 

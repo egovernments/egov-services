2e38843e72e59b57b222871a39e885c9d3ff21d6
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-28 13:11:50 +0530

    EGPGR-308 | Added migration to create escalation hours table

--- core/egov-workflow/src/main/resources/db/migration/V20170327180828__V1_create_escalation.sql
+++ core/egov-workflow/src/main/resources/db/migration/V20170327180828__V1_create_escalation.sql
@@ -3,9 +3,10 @@ CREATE TABLE egpgr_escalation (
     complaint_type_id bigint,
     designation_id bigint,
     lastmodifiedby bigint,
-    lastmodifieddate timestamp without time zone,
+    tenantid character varying(256) NOT NULL,
+    lastmodifieddate timestamp,
     createdby bigint,
-    createddate timestamp without time zone,
+    createddate timestamp,
     no_of_hrs integer,
     version bigint
 );
@@ -18,16 +19,4 @@ CREATE SEQUENCE seq_egpgr_escalation
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE ONLY egpgr_escalation
-    ADD CONSTRAINT pk_escalation_id PRIMARY KEY (id);
-
-ALTER TABLE ONLY egpgr_escalation
-    ADD CONSTRAINT fk_pgr_escalation_com_type_id FOREIGN KEY (complaint_type_id) REFERENCES egpgr_complainttype(id);
-
-ALTER TABLE ONLY egpgr_escalation
-    ADD CONSTRAINT fk_pgr_escalation_createdby FOREIGN KEY (createdby) REFERENCES eg_user(id);
-ALTER TABLE ONLY egpgr_escalation
-    ADD CONSTRAINT fk_pgr_escalation_desig_id FOREIGN KEY (designation_id) REFERENCES eg_designation(id);
-ALTER TABLE ONLY egpgr_escalation
-    ADD CONSTRAINT fk_pgr_escalation_lastmodifiedby FOREIGN KEY (lastmodifiedby) REFERENCES eg_user(id);
+ALTER TABLE egpgr_escalation ADD CONSTRAINT pk_escalation_id PRIMARY KEY (id);
---
+++ core/egov-workflow/src/test/resources/sql/insertEscalationHours.sql
@@ -0,0 +1,4 @@
+INSERT INTO egpgr_escalation (id, complaint_type_id, designation_id, no_of_hrs, tenantid)
+VALUES (nextval('seq_egpgr_escalation'), 1, 2, 13, 'tenant1');
+INSERT INTO egpgr_escalation (id, complaint_type_id, designation_id, no_of_hrs, tenantid)
+VALUES (nextval('seq_egpgr_escalation'), 1, 3, 15, 'tenant2');

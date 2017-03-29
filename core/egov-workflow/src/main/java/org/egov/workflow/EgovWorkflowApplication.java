cedc2adaca66fdf3b908f7993fb247b9bdf4a218
Ajay Divakaran <ajay_886@hotmail.com>
2017-03-29 12:47:35 +0530

    EGPGR-308 | Registered RestTemplate bean

--- core/egov-workflow/src/main/java/org/egov/workflow/EgovWorkflowApplication.java
+++ core/egov-workflow/src/main/java/org/egov/workflow/EgovWorkflowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -36,13 +37,23 @@ public class EgovWorkflowApplication {
 	}
 
 	@Bean
-	public MappingJackson2HttpMessageConverter jacksonConverter() {
+	public MappingJackson2HttpMessageConverter jacksonConverter(ObjectMapper mapper) {
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-		mapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy hh:mm a"));
 		converter.setObjectMapper(mapper);
 		return converter;
 	}
+
+	@Bean
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.setDateFormat(new SimpleDateFormat("dd/MM/yyyy hh:mm a"));
+        return mapper;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+	    return new RestTemplate();
+    }
 
 }

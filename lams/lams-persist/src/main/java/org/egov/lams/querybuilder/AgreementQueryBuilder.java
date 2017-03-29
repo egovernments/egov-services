6a020aa208a94c62e0cf919e8b4637dafe3fbeda
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-03-30 02:08:03 +0530

    persist repository modified

--- lams/lams-persist/src/main/java/org/egov/lams/querybuilder/AgreementQueryBuilder.java
+++ lams/lams-persist/src/main/java/org/egov/lams/querybuilder/AgreementQueryBuilder.java
@@ -11,9 +11,18 @@ public class AgreementQueryBuilder {
 	}
 
 	public static String updateAgreementQuery() {
-
+		  
 		return "UPDATE eglams_agreement values "
-				+ "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-				+" WHERE acknowledgementNumber=? and tenantid=?";
+				+ "(Id=?,Agreement_Date=?,Agreement_Number=?,"
+				+ "Bank_Guarantee_Amount=?,Bank_Guarantee_Date=?,Case_No=?,Commencement_Date=?,"
+				+ "Council_Date=?,Council_Number=?,Expiry_Date=?,Nature_Of_Allotment=?,"
+				+ "Order_Date=?,Order_Details=?,Order_No=?,Payment_Cycle=?,Registration_Fee=?,"
+				+ "Remarks=?,Rent=?,Rr_Reading_No=?,Security_Deposit=?,Security_Deposit_Date=?,"
+				+ "solvency_certificate_date=?,solvency_certificate_no=?,status=?,tin_number=?,"
+				+ "Tender_Date=?,Tender_Number=?,Trade_license_Number=?,"
+				+ "created_by=?,last_modified_by=?,created_date=?,last_modified_date=?,"
+				+ "allottee=?,asset=?,Rent_Increment_Method=?,"
+				+ "AcknowledgementNumber=?,stateid=?,Tenant_id=?"
+				+" WHERE acknowledgementNumber=? and Tenant_id=?";
 	}
 }

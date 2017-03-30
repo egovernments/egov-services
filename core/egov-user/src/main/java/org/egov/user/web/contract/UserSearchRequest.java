223af839738cc038dfaa78cd6f52208a6c1062b5
sumanth <sumanth@egovernments.org>
2017-03-30 14:52:27 +0530

    [EGPGR-333]-User/M@S : update requestInfo to RequestInfo in egov-user.

--- core/egov-user/src/main/java/org/egov/user/web/contract/UserSearchRequest.java
+++ core/egov-user/src/main/java/org/egov/user/web/contract/UserSearchRequest.java
@@ -1,5 +1,6 @@
 package org.egov.user.web.contract;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserSearchRequest {
 
     @JsonProperty("RequestInfo")

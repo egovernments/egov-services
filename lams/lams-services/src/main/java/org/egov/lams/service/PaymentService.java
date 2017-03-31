13136c5a0e050e1efa3b2b397ad7139eee54bb1a
Iffath <iffath@egovernments.org>
2017-03-31 11:51:48 +0530

    [PHOENIX-6527] : Changes for Collection Integration with LAMS adding loggers

--- lams/lams-services/src/main/java/org/egov/lams/service/PaymentService.java
+++ lams/lams-services/src/main/java/org/egov/lams/service/PaymentService.java
@@ -9,6 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
 import org.egov.lams.model.Agreement;
 import org.egov.lams.model.Demand;
 import org.egov.lams.model.DemandDetails;
@@ -17,8 +18,8 @@ import org.egov.lams.repository.BillRepository;
 import org.egov.lams.repository.DemandRepository;
 import org.egov.lams.web.contract.BillDetailInfo;
 import org.egov.lams.web.contract.BillInfo;
-import org.egov.lams.web.contract.BillReceiptReq;
 import org.egov.lams.web.contract.BillReceiptInfoReq;
+import org.egov.lams.web.contract.BillReceiptReq;
 import org.egov.lams.web.contract.BillSearchCriteria;
 import org.egov.lams.web.contract.DemandSearchCriteria;
 import org.egov.lams.web.contract.LamsConfigurationGetRequest;
@@ -32,6 +33,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
+
+	private static final Logger LOGGER = Logger.getLogger(PaymentService.class);
+
 	@Autowired
 	LamsConfigurationService lamsConfigurationService;
 
@@ -58,33 +62,37 @@ public class PaymentService {
 			billInfo.setIssuedDate(new Date());
 			billInfo.setLastDate(new Date());
 			lamsGetRequest.setName("MODULE_NAME");
+			LOGGER.info("before moduleName>>>>>>>");
+
 			String moduleName = lamsConfigurationService
 					.getLamsConfigurations(lamsGetRequest).get("MODULE_NAME")
 					.get(0);
+			LOGGER.info("after moduleName>>>>>>>" + moduleName);
 			billInfo.setModuleName(moduleName);
 			lamsGetRequest.setTenantId(agreement.getTenantId());
 			lamsGetRequest.setName("FUND_CODE");
 			String fundCode = lamsConfigurationService
-					.getLamsConfigurations(lamsGetRequest).get("FUND_CODE").get(0);
+					.getLamsConfigurations(lamsGetRequest).get("FUND_CODE")
+					.get(0);
 			billInfo.setFundCode(fundCode);
 			lamsGetRequest.setName("FUNCTIONARY_CODE");
 			String functionaryCode = lamsConfigurationService
-					.getLamsConfigurations(lamsGetRequest).get("FUNCTIONARY_CODE")
-					.get(0);
+					.getLamsConfigurations(lamsGetRequest)
+					.get("FUNCTIONARY_CODE").get(0);
 			billInfo.setFunctionaryCode(Long.valueOf(functionaryCode));
 			lamsGetRequest.setName("FUNDSOURCE_CODE");
 			String fundSourceCode = lamsConfigurationService
-					.getLamsConfigurations(lamsGetRequest).get("FUNDSOURCE_CODE")
-					.get(0);
+					.getLamsConfigurations(lamsGetRequest)
+					.get("FUNDSOURCE_CODE").get(0);
 			billInfo.setFundSourceCode(fundSourceCode);
 			lamsGetRequest.setName("DEPARTMENT_CODE");
 			String departmentCode = lamsConfigurationService
-					.getLamsConfigurations(lamsGetRequest).get("DEPARTMENT_CODE")
-					.get(0);
+					.getLamsConfigurations(lamsGetRequest)
+					.get("DEPARTMENT_CODE").get(0);
 			billInfo.setDepartmentCode(departmentCode);
 			billInfo.setCollModesNotAllowed("");
-			billInfo.setBoundaryNumber(agreement.getAsset().getLocationDetails()
-					.getElectionWard());
+			billInfo.setBoundaryNumber(agreement.getAsset()
+					.getLocationDetails().getElectionWard());
 			lamsGetRequest.setName("BOUNDARY_TYPE");
 			String boundaryType = lamsConfigurationService
 					.getLamsConfigurations(lamsGetRequest).get("BOUNDARY_TYPE")
@@ -102,8 +110,9 @@ public class PaymentService {
 							.getAcknowledgementNumber() : agreement
 							.getAgreementNumber()));
 			billInfo.setConsumerCode(StringUtils.isBlank(agreement
-					.getAgreementNumber()) ? agreement.getAcknowledgementNumber()
-					: agreement.getAgreementNumber());
+					.getAgreementNumber()) ? agreement
+					.getAcknowledgementNumber() : agreement
+					.getAgreementNumber());
 			billInfo.setCallbackForApportion('N');
 			billInfo.setEmailId(agreement.getAllottee().getEmailId());
 			billInfo.setConsumerType("Agreement");
@@ -112,9 +121,13 @@ public class PaymentService {
 			DemandSearchCriteria demandSearchCriteria = new DemandSearchCriteria();
 			demandSearchCriteria.setDemandId(1L);
 
+			LOGGER.info("demand before>>>>>>>" + demandSearchCriteria);
+
 			Demand demand = demandRepository
 					.getDemandBySearch(demandSearchCriteria, requestInfo)
 					.getDemands().get(0);
+			LOGGER.info("demand>>>>>>>" + demand);
+
 			billInfo.setDisplayMessage(demand.getModuleName());
 			billInfo.setMinAmountPayable(demand.getMinAmountPayable());
 
@@ -128,16 +141,16 @@ public class PaymentService {
 			for (DemandDetails demandDetail : demand.getDemandDetails()) {
 				orderNo++;
 				totalAmount = totalAmount.add(demandDetail.getTaxAmount());
-				billDetailInfos.addAll(getBilldetails(demandDetail, functionCode,
-						orderNo));
+				billDetailInfos.addAll(getBilldetails(demandDetail,
+						functionCode, orderNo));
 			}
 			billInfo.setTotalAmount(totalAmount.doubleValue());
 			billInfo.setBillAmount(totalAmount.doubleValue());
 			billInfo.setBillDetailInfos(billDetailInfos);
 			billInfos.add(billInfo);
-			final String billXml = billRepository.createBillAndGetXml(billInfos,
-					requestInfo);
-			
+			final String billXml = billRepository.createBillAndGetXml(
+					billInfos, requestInfo);
+
 			try {
 				collectXML = URLEncoder.encode(billXml, "UTF-8");
 			} catch (final UnsupportedEncodingException e) {
@@ -152,23 +165,31 @@ public class PaymentService {
 	public List<BillDetailInfo> getBilldetails(
 			final DemandDetails demandDetail, String functionCode, int orderNo) {
 		final List<BillDetailInfo> billDetails = new ArrayList<>();
-		BillDetailInfo billdetail = new BillDetailInfo();
-		// TODO: Fix me: As per the rules for the order no.
-		billdetail.setOrderNo(orderNo);
-		billdetail.setCreditAmount(demandDetail.getTaxAmount());
-		billdetail.setDebitAmount(BigDecimal.ZERO);
-		billdetail.setGlCode(demandDetail.getGlCode().toString());
-		billdetail.setDescription(demandDetail.getTaxPeriod());
-		billdetail.setPeriod(demandDetail.getTaxPeriod());
-		billdetail.setPurpose(billRepository.getPurpose().get("CURRENT_AMOUNT")
-				.toString());
-		billdetail.setIsActualDemand(demandDetail.getIsActualDemand());
-		billdetail.setFunctionCode(functionCode);
-		billDetails.add(billdetail);
+
+		try {
+			BillDetailInfo billdetail = new BillDetailInfo();
+			// TODO: Fix me: As per the rules for the order no.
+			billdetail.setOrderNo(orderNo);
+			billdetail.setCreditAmount(demandDetail.getTaxAmount());
+			billdetail.setDebitAmount(BigDecimal.ZERO);
+			LOGGER.info("getGlCode before>>>>>>>" + demandDetail.getGlCode());
+			billdetail.setGlCode(demandDetail.getGlCode().toString());
+			LOGGER.info("getGlCode after >>>>>>>" + demandDetail.getGlCode());
+			billdetail.setDescription(demandDetail.getTaxPeriod());
+			billdetail.setPeriod(demandDetail.getTaxPeriod());
+			billdetail.setPurpose(billRepository.getPurpose()
+					.get("CURRENT_AMOUNT").toString());
+			billdetail.setIsActualDemand(demandDetail.getIsActualDemand());
+			billdetail.setFunctionCode(functionCode);
+			billDetails.add(billdetail);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return billDetails;
 	}
 
-	public ResponseEntity<ReceiptAmountInfo> updateDemand(BillReceiptInfoReq billReceiptInfoReq) {
+	public ResponseEntity<ReceiptAmountInfo> updateDemand(
+			BillReceiptInfoReq billReceiptInfoReq) {
 
 		BillSearchCriteria billSearchCriteria = new BillSearchCriteria();
 		BillReceiptReq billReceiptInfo = billReceiptInfoReq
@@ -185,10 +206,10 @@ public class PaymentService {
 		if (currentDemand.getMinAmountPayable() != null
 				&& currentDemand.getMinAmountPayable() > 0)
 			currentDemand.setMinAmountPayable(0d);
-		
-		updateDemandDetailForReceiptCreate(currentDemand, billReceiptInfoReq
-				.getBillReceiptInfo());
-		
+
+		updateDemandDetailForReceiptCreate(currentDemand,
+				billReceiptInfoReq.getBillReceiptInfo());
+
 		currentDemand.setPaymentInfos(setPaymentInfos(billReceiptInfo));
 		demandRepository
 				.updateDemand(Arrays.asList(currentDemand),
@@ -196,19 +217,18 @@ public class PaymentService {
 				.get(0);
 		return receiptAmountBifurcation(billReceiptInfo, billInfo);
 	}
-	
-	
-	private List<PaymentInfo> setPaymentInfos(BillReceiptReq billReceiptInfo)
-	{
-		
+
+	private List<PaymentInfo> setPaymentInfos(BillReceiptReq billReceiptInfo) {
+
 		List<PaymentInfo> paymentInfos = new ArrayList<>();
-		PaymentInfo paymentInfo=  new PaymentInfo();
-		paymentInfo.setReceiptAmount(billReceiptInfo.getTotalAmount().toString());
+		PaymentInfo paymentInfo = new PaymentInfo();
+		paymentInfo.setReceiptAmount(billReceiptInfo.getTotalAmount()
+				.toString());
 		paymentInfo.setReceiptDate(billReceiptInfo.getReceiptDate().toString());
 		paymentInfo.setReceiptNumber(billReceiptInfo.getReceiptNum());
 		paymentInfo.setStatus(billReceiptInfo.getReceiptStatus());
 		return paymentInfos;
-		
+
 	}
 
 	private void updateDemandDetailForReceiptCreate(Demand demand,
@@ -240,7 +260,7 @@ public class PaymentService {
 
 	public ResponseEntity<ReceiptAmountInfo> receiptAmountBifurcation(
 			final BillReceiptReq billReceiptInfo, BillInfo billInfo) {
-        ResponseEntity<ReceiptAmountInfo> receiptAmountInfoResponse = null;
+		ResponseEntity<ReceiptAmountInfo> receiptAmountInfoResponse = null;
 
 		final ReceiptAmountInfo receiptAmountInfo = new ReceiptAmountInfo();
 		BigDecimal currentInstallmentAmount = BigDecimal.ZERO;
@@ -272,7 +292,8 @@ public class PaymentService {
 									.doubleValue());
 					receiptAmountInfo.setArrearsAmount(arrearAmount
 							.doubleValue());
-					receiptAmountInfoResponse =  new ResponseEntity<>(receiptAmountInfo,  HttpStatus.OK);
+					receiptAmountInfoResponse = new ResponseEntity<>(
+							receiptAmountInfo, HttpStatus.OK);
 				}
 			}
 		}

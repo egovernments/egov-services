5d8772ab2dddd9b30dd0c57186eb3bde0ea0f6da
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-04-03 22:32:15 +0530

    user id issues fixed

--- lams/lams-services/src/main/java/org/egov/lams/service/AllotteeService.java
+++ lams/lams-services/src/main/java/org/egov/lams/service/AllotteeService.java
@@ -57,7 +57,10 @@ public class AllotteeService {
 		userSearchRequest.setEmailId(allottee.getEmailId());
 		userSearchRequest.setMobileNumber(allottee.getMobileNumber());
 		logger.info("url for allottee api post call",url);
-		return callAllotteSearch(url,userSearchRequest);
+		AllotteeResponse allotteeResponse = callAllotteSearch(url, userSearchRequest);
+		allottee.setId(allotteeResponse.getAllottee().get(0).getId());
+		System.err.println("the id from alottee ::: " + allotteeResponse.getAllottee().get(0).getId());
+		return allotteeResponse;
 	}
 	
 	public AllotteeResponse createAllottee(Allottee allottee,RequestInfo requestInfo) {

4b86d95f33890c42fb9db88d377163cb49a84f9e
Renjith G R <grenjithr@gmail.com>
2017-04-03 14:30:34 +0530

    Use basic auth when indexing elastic search documents

--- pgr/pgr-search-indexer/src/main/java/org/egov/web/indexer/repository/ElasticSearchRepository.java
+++ pgr/pgr-search-indexer/src/main/java/org/egov/web/indexer/repository/ElasticSearchRepository.java
@@ -1,9 +1,14 @@
 package org.egov.web.indexer.repository;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.Charset;
 import java.util.Map;
 
 @Service
@@ -11,16 +16,31 @@ public class ElasticSearchRepository {
 
     private RestTemplate restTemplate;
     private final String indexServiceHost;
+    private String indexServiceUsername;
+    private String indexServicePassword;
 
     public ElasticSearchRepository(RestTemplate restTemplate,
-                                   @Value("${egov.services.esindexer.host}") String indexServiceHost) {
+                                   @Value("${egov.services.esindexer.host}") String indexServiceHost,
+                                   @Value("${egov.services.esindexer.username}") String indexServiceUsername,
+                                   @Value("${egov.services.esindexer.password}") String indexServicePassword) {
         this.restTemplate = restTemplate;
         this.indexServiceHost = indexServiceHost;
+        this.indexServiceUsername = indexServiceUsername;
+        this.indexServicePassword = indexServicePassword;
     }
 
     public void index(String indexName, String indexId, Object indexObject) {
         String url = this.indexServiceHost + indexName + "/" + indexName + "/" + indexId;
-        restTemplate.postForObject(url, indexObject, Map.class);
+        HttpHeaders headers = new HttpHeaders() {{
+            set("Authorization", getAuthHeaderValue(indexServiceUsername, indexServicePassword));
+        }};
+
+        restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(indexObject, headers), Map.class);
     }
 
+    private String getAuthHeaderValue(String userName, String password) {
+        String authString = String.format("%s:%s", userName, password);
+        byte[] encodedAuthString = Base64.encodeBase64(authString.getBytes(Charset.forName("US-ASCII")));
+        return String.format("Basic %s", new String(encodedAuthString));
+    }
 }
--- pgr/pgr-search-indexer/src/test/java/org/egov/web/indexer/repository/ElasticSearchRepositoryTest.java
+++ pgr/pgr-search-indexer/src/test/java/org/egov/web/indexer/repository/ElasticSearchRepositoryTest.java
@@ -1,10 +1,7 @@
 package org.egov.web.indexer.repository;
 
-import org.egov.web.indexer.repository.ElasticSearchRepository;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
@@ -17,31 +14,41 @@ import static org.springframework.test.web.client.ExpectedCount.once;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ElasticSearchRepositoryTest {
 
-	private MockRestServiceServer server;
+    private MockRestServiceServer server;
 
-	private ElasticSearchRepository elasticSearchRepository;
+    private ElasticSearchRepository elasticSearchRepository;
 
-	@Before
-	public void before() {
-		RestTemplate restTemplate = new RestTemplate();
-		elasticSearchRepository = new ElasticSearchRepository(restTemplate, "http://host/");
-		server = MockRestServiceServer.bindTo(restTemplate).build();
-	}
+    private final String INDEX_SERVICE_URL = "http://host/";
+    private final String INDEX_SERVICE_USERNAME = "username";
+    private final String INDEX_SERVICE_PASSWORD = "password";
+    private final String INDEX_NAME = "indexName";
+    private final String INDEX_ID = "indexId";
 
-	@Test
-	public void test_should_index_object_instance() throws Exception {
-		String indexName = "test";
-		String indexId = "test1";
-		server.expect(once(), requestTo("http://host/test/test/test1")).andExpect(method(HttpMethod.POST))
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andRespond(withSuccess());
-		Map<String, String> stringObj = new HashMap<String, String>();
-		stringObj.put("key", "value");
+    @Before
+    public void before() {
+        RestTemplate restTemplate = new RestTemplate();
+        elasticSearchRepository = new ElasticSearchRepository(
+                restTemplate, INDEX_SERVICE_URL, INDEX_SERVICE_USERNAME, INDEX_SERVICE_PASSWORD);
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+    }
 
-		elasticSearchRepository.index(indexName, indexId, stringObj);
+    @Test
+    public void test_should_index_object_instance() throws Exception {
+        String expectedUri = String.format("%s%s/%s/%s", INDEX_SERVICE_URL, INDEX_NAME, INDEX_NAME, INDEX_ID);
+        server.expect(once(), requestTo(expectedUri))
+                .andExpect(header("Authorization", "Basic dXNlcm5hbWU6cGFzc3dvcmQ="))
+                .andExpect(method(HttpMethod.POST))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().string(new Resources().getFileContents("esIndexRequestBody.json")))
+                .andRespond(withSuccess());
+        Map<String, Object> indexContent = new HashMap<String, Object>() {{
+           put("crn", "CRN");
+        }};
 
-		server.verify();
-	}
+        elasticSearchRepository.index(INDEX_NAME, INDEX_ID, indexContent);
+
+        server.verify();
+    }
 }

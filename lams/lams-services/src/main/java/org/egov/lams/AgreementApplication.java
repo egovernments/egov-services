45806d5d5a281ddc3d28248b01c2cb767b49c721
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-04-04 16:11:07 +0530

    jackson ignore added in app

--- lams/lams-services/src/main/java/org/egov/lams/AgreementApplication.java
+++ lams/lams-services/src/main/java/org/egov/lams/AgreementApplication.java
@@ -3,7 +3,11 @@ package org.egov.lams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class AgreementApplication {
@@ -15,5 +19,15 @@ public class AgreementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgreementApplication.class, args);
+	}
+	
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		//mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH));
+		converter.setObjectMapper(mapper);
+		return converter;
 	}
 }
--- lams/lams-workflow/src/main/java/org/egov/lams/LamsWorkflowApplication.java
+++ lams/lams-workflow/src/main/java/org/egov/lams/LamsWorkflowApplication.java
@@ -3,7 +3,11 @@ package org.egov.lams;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class LamsWorkflowApplication {
@@ -15,5 +19,14 @@ public class LamsWorkflowApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LamsWorkflowApplication.class, args);
+	}
+	
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		converter.setObjectMapper(mapper);
+		return converter;
 	}
 }

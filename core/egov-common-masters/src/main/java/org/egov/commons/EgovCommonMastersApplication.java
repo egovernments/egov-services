88ead41767166b0f92fb90f86dda4bede6e754f5
Rajat Kaushal <rajat.kaushal@riflexions.com>
2017-04-04 18:23:43 +0530

    added FAIL_ON_UNKNOWN_PROPERTIES in main class of 5 services

--- core/egov-common-masters/src/main/java/org/egov/commons/EgovCommonMastersApplication.java
+++ core/egov-common-masters/src/main/java/org/egov/commons/EgovCommonMastersApplication.java
@@ -44,7 +44,11 @@ import org.egov.commons.web.interceptor.CorrelationIdAwareRestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class EgovCommonMastersApplication {
@@ -56,5 +60,15 @@ public class EgovCommonMastersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgovCommonMastersApplication.class, args);
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		//mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH));
+		converter.setObjectMapper(mapper);
+		return converter;
 	}
 }
--- hr/hr-attendance/src/main/java/org/egov/eis/EgovHRAttendanceApplication.java
+++ hr/hr-attendance/src/main/java/org/egov/eis/EgovHRAttendanceApplication.java
@@ -44,7 +44,11 @@ import org.egov.eis.indexer.http.CorrelationIdAwareRestTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class EgovHRAttendanceApplication {
@@ -57,4 +61,14 @@ public class EgovHRAttendanceApplication {
     public static void main(final String[] args) {
         SpringApplication.run(EgovHRAttendanceApplication.class, args);
     }
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		//mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH));
+		converter.setObjectMapper(mapper);
+		return converter;
+	}
 }

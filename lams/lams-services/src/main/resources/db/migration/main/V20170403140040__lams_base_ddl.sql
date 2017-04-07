f703629c1f0f0fe1048ff36754179579934a4966
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-04-07 10:54:21 +0530

    seqeglams demand added, primitives removed

--- lams/lams-services/src/main/resources/db/migration/main/V20170403140040__lams_base_ddl.sql
+++ lams/lams-services/src/main/resources/db/migration/main/V20170403140040__lams_base_ddl.sql
@@ -2,6 +2,7 @@ Create sequence seq_eglams_agreement;
 Create sequence seq_eglams_rentincrement;
 Create sequence seq_eglams_acknowledgementnumber;
 Create sequence seq_eglams_agreementnumber;
+Create sequence seq_eglams_demand;
 
 CREATE TABLE eglams_rentincrementtype
 (

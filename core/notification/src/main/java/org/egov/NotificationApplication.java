b8d212540c5fdd3dd93cf1f183fab04e3e154b09
Renjith G R <grenjithr@gmail.com>
2017-04-12 13:21:12 +0530

    Notification service initial commit.

---
+++ core/notification/src/main/java/org/egov/NotificationApplication.java
@@ -0,0 +1,48 @@
+package org.egov;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class NotificationApplication {
+
+    private static final String IST = "Asia/Calcutta";
+
+    @Bean
+	public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+		return new WebMvcConfigurerAdapter() {
+
+			@Override
+			public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+				configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
+			}
+
+		};
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		converter.setObjectMapper(mapper);
+		return converter;
+	}
+
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(IST));
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+}

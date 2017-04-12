723dcd706fce85cd67302c7551327c578e116bd4
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-04-12 15:28:19 +0530

    logger added

--- lams/lams-services/src/main/java/org/egov/lams/repository/BillRepository.java
+++ lams/lams-services/src/main/java/org/egov/lams/repository/BillRepository.java
@@ -3,12 +3,13 @@ package org.egov.lams.repository;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.egov.lams.config.PropertiesManager;
-import org.egov.lams.web.contract.RequestInfo;
 import org.egov.lams.web.contract.BillInfo;
 import org.egov.lams.web.contract.BillRequest;
 import org.egov.lams.web.contract.BillResponse;
 import org.egov.lams.web.contract.BillSearchCriteria;
+import org.egov.lams.web.contract.RequestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestClientException;
@@ -16,6 +17,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Repository
 public class BillRepository {
+	
+	private static final Logger LOGGER = Logger.getLogger(BillRepository.class);
 
 	@Autowired
 	private RestTemplate restTemplate;
@@ -43,9 +46,18 @@ public class BillRepository {
 	public BillInfo searchBill(BillSearchCriteria billSearchCriteria, RequestInfo requestInfo) {
 		String url = propertiesManager.getDemandServiceHostName()
 				+ propertiesManager.getDemandBillSearchService();
-
-		BillResponse billResponse = restTemplate.postForObject(url, requestInfo,
+			
+		LOGGER.info("The url for search bill API ::: "+url);
+		BillResponse billResponse = null;
+		
+		try{
+			billResponse = restTemplate.postForObject(url, requestInfo,
 				 BillResponse.class,billSearchCriteria);
+		}catch (Exception e) {
+			e.printStackTrace();
+			LOGGER.info("the exception from billsearch API call ::: "+e);
+		}
+		LOGGER.info("the response for bill search API call ::: "+billResponse);
 		return billResponse.getBillInfos().get(0);
 	}
 	

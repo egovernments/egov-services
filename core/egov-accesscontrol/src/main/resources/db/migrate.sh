ff210861ac58ab028b48c3859557ed167ef1be7c
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-04-13 19:06:35 +0530

    Derive flyway locations for env variable

--- core/egov-accesscontrol/src/main/resources/db/migrate.sh
+++ core/egov-accesscontrol/src/main/resources/db/migrate.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=filesystem:/flyway/sql -baselineOnMigrate=true -outOfOrder=true migrate
-
-if [[ "$RUN_FLYWAY_SEED" == "true" ]]; then
-    flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=filesystem:/flyway/seed -baselineOnMigrate=true -outOfOrder=true migrate
-fi
+flyway -url=$DB_URL -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true migrate

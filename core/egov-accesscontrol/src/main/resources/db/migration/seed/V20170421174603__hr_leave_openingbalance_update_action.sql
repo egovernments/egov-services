215ee2c04af57deef0663affefa65514beb1c1c9
venki <venki@egovernments.org>
2017-04-21 19:08:33 +0530

    Fix for build issue.

--- core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170421174603__hr_leave_openingbalance_update_action.sql
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170421174603__hr_leave_openingbalance_update_action.sql
@@ -1,5 +1,5 @@
 update service set parentmodule=(select id from service where name='Leave Management'),displayname='Leave Opening Balance' where name='Leave Opening Balance';
-update eg_action set url='/app/hr/leavemaster/pis.html',queryparams=null where name='CreateLeave Opening Balance';
+update eg_action set url='/app/hr/leavemaster/pis.html',queryparams=null,parentmodule=(select id from service where name='Leave Opening Balance') where name='CreateLeave Opening Balance';
 update eg_action set url='/leaveopeningbalances/_create',queryparams=null,parentmodule=(select id from service where name='HR Leave') where name='CreateLeave Opening Balances';
 update eg_action set url='/app/hr/leavemaster/pis.html',parentmodule=(select id from service where name='Leave Opening Balance'), queryparams='type=update' where name='UpdateLeave Opening Balance';
 update eg_action set url='/leaveopeningbalances/_update',queryparams=null,parentmodule=(select id from service where name='HR Leave') where name='Leave Opening BalanceUpdate';

868c9c4b599e1423dce7d789bfdefd73525640a7
Ajay Divakaran <ajay_886@hotmail.com>
2017-04-21 16:42:30 +0530

    Clean up - Removed pgr-persist since the code has been moved to pgr-rest

--- pgr/pgr-persist/src/test/resources/sql/InsertReceivingModeData.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO egpgr_receivingmode (id, name, code, visible, version) VALUES (1, 'Website','WEBSITE',false,0);
-INSERT INTO egpgr_receivingmode (id, name, code, visible, version) VALUES (2, 'Manual','MANUAL',false,0);
-INSERT INTO egpgr_receivingmode (id, name, code, visible, version) VALUES (3, 'Mobile','MOBILE',false,0);

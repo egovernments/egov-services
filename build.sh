470a359025717b4da983f14df2715c1e0b191653
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-04-25 18:15:10 +0530

    Pointing pgr-rest repository tests to postgresql database instead of h2. Made required ci changes

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ci_image=egovio/ci:0.0.1
+db=`sudo docker run -d postgres:9.4`
+echo "Started db container: $db"
+
+pwd=`pwd`
+ci=`sudo docker run -t -d -v $pwd:/var/lib/egov-services:rw -w /var/lib/egov-services --net=container:${db} ${ci_image} /bin/bash -c "cd $1; mvn clean verify package"`
+echo "Started ci container to run build: $ci"
+
+echo "Attaching to ci container: $ci"
+docker attach $ci
+result=$?
+
+if [[ $result == 0 ]]; then
+    echo "Commiting ci container: $ci"
+    docker commit $ci ${ci_image}
+fi
+
+echo "Stopping db-container: $db"
+docker rm -f $db
+
+echo "Stopping ci-container: $ci"
+docker rm -f $ci
+
+exit $result
+

0429f89f207abf2b3a631c9e70b5b032f3a3a134
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-04-27 12:46:28 +0530

    allottee object altered as per lamsp-services requirement

--- lams/lams-indexer/src/main/java/org/egov/lams/model/Allottee.java
+++ lams/lams-indexer/src/main/java/org/egov/lams/model/Allottee.java
@@ -1,8 +1,12 @@
 package org.egov.lams.model;
 
+import java.util.List;
+
 import org.egov.lams.model.enums.Gender;
 import org.egov.lams.model.enums.UserType;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,7 +30,10 @@ public class Allottee {
 	private String emailId;
 	private String userName;
 	private String password;
-	private boolean active;
+	private Boolean active;
 	private UserType type;
 	private Gender gender;
+	private String tenantId; 
+	@JsonProperty("roles")
+    private List<Role> roles;
 }

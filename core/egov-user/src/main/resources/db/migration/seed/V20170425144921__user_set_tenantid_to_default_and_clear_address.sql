562ee8db9d2628a636e0d6f840255bf9453fb3e8
Elzan Mathew <elzan.mathew@egovernments.org>
2017-04-27 09:30:42 +0530

    Fixing build break- update all roles except CITIZEN since there is already another script for adding CITIZEN for default tenant

--- core/egov-user/src/main/resources/db/migration/seed/V20170425144921__user_set_tenantid_to_default_and_clear_address.sql
+++ core/egov-user/src/main/resources/db/migration/seed/V20170425144921__user_set_tenantid_to_default_and_clear_address.sql
@@ -1,4 +1,4 @@
-update eg_role set tenantid = 'default' where tenantid = 'ap.public';
+update eg_role set tenantid = 'default' where tenantid = 'ap.public' and code !='CITIZEN';
 
 delete from eg_address where id = 1;
 

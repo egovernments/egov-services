f832bfb85062791938aa29f0dee7798fd98e982b
Elzan Mathew <elzan.mathew@egovernments.org>
2017-04-27 10:07:05 +0530

    Fixing build break- update all roles except EMPLOYEE ADMIN since there is already another script for adding EMPLOYEE ADMIN for default tenant

--- core/egov-user/src/main/resources/db/migration/seed/V20170425144921__user_set_tenantid_to_default_and_clear_address.sql
+++ core/egov-user/src/main/resources/db/migration/seed/V20170425144921__user_set_tenantid_to_default_and_clear_address.sql
@@ -1,4 +1,4 @@
-update eg_role set tenantid = 'default' where tenantid = 'ap.public' and code not in('CITIZEN','EMPLOYEE','SUPERUSER');
+update eg_role set tenantid = 'default' where tenantid = 'ap.public' and code not in('CITIZEN','EMPLOYEE','SUPERUSER','EMPLOYEE ADMIN');
 
 delete from eg_address where id = 1;
 

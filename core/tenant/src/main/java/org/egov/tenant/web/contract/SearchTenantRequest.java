155ba9e79c50d99b776dff57bb82f8d752e994dc
Ajay Divakaran <ajay_886@hotmail.com>
2017-04-28 15:03:55 +0530

    Fixed pmd failure - tenant service

--- core/tenant/src/main/java/org/egov/tenant/web/contract/SearchTenantRequest.java
+++ core/tenant/src/main/java/org/egov/tenant/web/contract/SearchTenantRequest.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.egov.common.contract.request.RequestInfo;
 
-import java.util.List;
-
 @Getter
 @AllArgsConstructor
 public class SearchTenantRequest {

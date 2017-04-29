8165a8e7b323d73c81e4bedacc83535dfc68f307
venki <venki@egovernments.org>
2017-04-29 23:14:41 +0530

    Added boundary type in get boundary response.....

--- core/egov-location/src/main/java/org/egov/boundary/web/contract/Boundary.java
+++ core/egov-location/src/main/java/org/egov/boundary/web/contract/Boundary.java
@@ -1,5 +1,6 @@
 package org.egov.boundary.web.contract;
 
+import org.egov.boundary.persistence.entity.BoundaryType;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -20,18 +21,27 @@ public class Boundary {
     @NotEmpty
     @JsonProperty("id")
     private String id;
+    
     @JsonProperty("name")
     private String name;
+    
     @JsonProperty("longitude")
     private Float longitude;
+    
     @JsonProperty("latitude")
     private Float latitude;
+    
     @JsonProperty("boundaryNum")
     private Long boundaryNum;
-
+    
+    @JsonProperty("parent")
     private Boundary parent;
+    
     @JsonProperty("tenantId")
     private String tenantId;
+    
+    @JsonProperty("boundaryType")
+    private BoundaryType boundaryType;
 
     public Boundary(org.egov.boundary.persistence.entity.Boundary entityBoundary) {
         this.id = entityBoundary.getId().toString();
@@ -43,6 +53,7 @@ public class Boundary {
         this.latitude = entityBoundary.getLatitude();
         this.boundaryNum = entityBoundary.getBoundaryNum();
         this.tenantId = entityBoundary.getTenantId();
+        this.boundaryType = entityBoundary.getBoundaryType();
     }
 
 }

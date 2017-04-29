7cf679e8b6faf19909763c4235d984a2c724c319
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-04-29 10:50:12 +0530

    asset alter migration added

--- asset/asset-services/src/main/resources/db/migration/main/V20170429034025__egasset_asset_assetcategory_alter.sql
+++ asset/asset-services/src/main/resources/db/migration/main/V20170429034025__egasset_asset_assetcategory_alter.sql
@@ -1,3 +1,7 @@
+DELETE FROM egasset_assetcategory;
+
+DELETE FROM egasset_asset;
+
 ALTER TABLE egasset_assetcategory ADD column isassetallow boolean,ADD column version character varying(64);
 
 ALTER TABLE egasset_asset ADD column assetRefrance bigint,ADD column version character varying(64);

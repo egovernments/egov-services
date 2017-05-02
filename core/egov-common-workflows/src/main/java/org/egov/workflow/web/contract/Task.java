ba5c8232c547e122b2d6200f5a1a63a6d6523451
Manoj Kulkarni <manoj.kulkarni@egovernments.org>
2017-05-02 18:39:59 +0530

    adding tostring annotation for logging

--- core/egov-common-workflows/src/main/java/org/egov/workflow/web/contract/Task.java
+++ core/egov-common-workflows/src/main/java/org/egov/workflow/web/contract/Task.java
@@ -15,12 +15,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Task {
 
 	/**

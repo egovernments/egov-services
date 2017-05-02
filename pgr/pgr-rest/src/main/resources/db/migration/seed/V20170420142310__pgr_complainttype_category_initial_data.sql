820879746421b16087b9ba1997f95bd32e34c8f4
sumanth <sumanth@egovernments.org>
2017-05-02 16:24:33 +0530

    [EGPGR-434] : Complaint migration / M@S : Fixing pgr migrations.

--- pgr/pgr-rest/src/main/resources/db/migration/seed/V20170420142310__pgr_complainttype_category_initial_data.sql
+++ pgr/pgr-rest/src/main/resources/db/migration/seed/V20170420142310__pgr_complainttype_category_initial_data.sql
@@ -1,12 +1,12 @@
 ----------------- START --------------------
 
 ---------------Category List ---------------
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (1,'Street Lighting','Street Lighting',0,'default');
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (2,'Engineering','Engineering',0,'default');
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (3,'Public Health and Sanitation','Public Health and Sanitation',0,'default');
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (4,'Administration','Administration',0,'default');
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (5,'Town Planning','Town Planning',0,'default');
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (6,'Revenue','Revenue',0,'default');
-insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (7,'Water Supply','Water Supply',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Street Lighting','Street Lighting',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Engineering','Engineering',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Public Health and Sanitation','Public Health and Sanitation',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Administration','Administration',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Town Planning','Town Planning',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Revenue','Revenue',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Water Supply','Water Supply',0,'default');
 
 ----------------- END --------------------

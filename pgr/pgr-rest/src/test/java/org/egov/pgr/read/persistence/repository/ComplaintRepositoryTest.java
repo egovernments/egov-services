6eb0072c392d7868ee4ede618a6953bd653e9c74
mansi <mansi.bansal@egovernments.org>
2017-05-02 13:27:26 +0530

    "fixed receiving mode and receiving center issues"

--- pgr/pgr-rest/src/test/java/org/egov/pgr/read/persistence/repository/ComplaintRepositoryTest.java
+++ pgr/pgr-rest/src/test/java/org/egov/pgr/read/persistence/repository/ComplaintRepositoryTest.java
@@ -3,6 +3,7 @@ import org.egov.common.contract.request.RequestInfo;
 import org.egov.pgr.common.contract.ServiceRequest;
 import org.egov.pgr.common.contract.SevaRequest;
 import org.egov.pgr.common.entity.Complaint;
+import org.egov.pgr.common.repository.ComplaintJpaRepository;
 import org.egov.pgr.read.domain.model.ComplaintSearchCriteria;
 import org.junit.Before;
 import org.junit.Test;

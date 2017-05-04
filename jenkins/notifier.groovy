66205d8d6435456b7ea5bf80753d268af8cbba66
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-05-04 13:39:47 +0530

    Trying to fix build failure notification

--- jenkins/notifier.groovy
+++ jenkins/notifier.groovy
@@ -19,7 +19,8 @@ def notifyBuild(String buildStatus = 'STARTED') {
 
     slackSend (color: colorCode, message: summary)
     emailext (
-      body: '${SCRIPT, template="groovy-html.template"}', recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], replyTo: '$DEFAULT_REPLYTO', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - ${BUILD_STATUS}!', to: '$DEFAULT_RECIPIENTS'
+      body: '${SCRIPT, template="jenkins/template/build-failure.template"}',
+      recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], replyTo: '$DEFAULT_REPLYTO', subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - ${BUILD_STATUS}!', to: '$DEFAULT_RECIPIENTS'
     )
 }
 

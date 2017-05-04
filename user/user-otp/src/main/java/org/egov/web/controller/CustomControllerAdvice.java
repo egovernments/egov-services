5c735e093d38f20fe3bbf3e6ffe177bc13cd7638
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-04 14:07:01 +0530

    EGPGR-422 | Return error response when mobile number is unknown

--- user/user-otp/src/main/java/org/egov/web/controller/CustomControllerAdvice.java
+++ user/user-otp/src/main/java/org/egov/web/controller/CustomControllerAdvice.java
@@ -1,8 +1,10 @@
 package org.egov.web.controller;
 
 import org.egov.domain.exception.InvalidOtpRequestException;
+import org.egov.domain.exception.UserNotFoundException;
 import org.egov.web.contract.ErrorResponse;
 import org.egov.web.error.OtpRequestErrorAdapter;
+import org.egov.web.error.UserNotFoundErrorAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,12 @@ public class CustomControllerAdvice {
     public ErrorResponse handleInvalidOtpRequestException(InvalidOtpRequestException ex) {
         return new OtpRequestErrorAdapter().adapt(ex.getOtpRequest());
     }
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(UserNotFoundException.class)
+	public ErrorResponse handleUserNotFoundException() {
+		return new UserNotFoundErrorAdapter().adapt(null);
+	}
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)

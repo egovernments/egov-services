120ffa6b6eee3e5a4b82329412749db1bf01cf4e
Seema Nayak <seema.nayak@egovernments.org>
2017-05-05 15:16:47 +0530

    hr status master data

---
+++ hr/hr-masters/src/main/resources/db/migration/seed/V20170505151325__hr_status_master_data.sql
@@ -0,0 +1,5 @@
+insert into egeis_hrstatus(id,objectname,code,description,tenantid) values (nextval('seq_egeis_hrStatus'),'Employee Master',
+'SUSPENDED','Employee who is suspended','default');
+
+insert into egeis_hrstatus(id,objectname,code,description,tenantid) values (nextval('seq_egeis_hrStatus'),'Employee Master',
+'TRANSFERRED','Employee who is transferred','default');

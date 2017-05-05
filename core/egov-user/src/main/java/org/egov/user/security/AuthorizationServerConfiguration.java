787000a6204591d6d252f586216f45e734849a0f
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-05 13:07:12 +0530

    EGPGR-421 | Fixed pmd issues

--- core/egov-user/src/main/java/org/egov/user/security/AuthorizationServerConfiguration.java
+++ core/egov-user/src/main/java/org/egov/user/security/AuthorizationServerConfiguration.java
@@ -11,9 +11,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-
 import redis.clients.jedis.JedisShardInfo;
 
 @Configuration

fa67c85ed45242a9aab59a0dc0a206045ffa2d49
sourabh <sourabh.debnath@goodworklabs.com>
2017-05-08 11:20:50 +0530

    Removed- Footer copyright line Added- Code in ack page

--- asset/asset-web/resources/javascript/asset/create-asset-ack.js
+++ asset/asset-web/resources/javascript/asset/create-asset-ack.js
@@ -3,7 +3,15 @@ class CreateAsset extends React.Component {
       // widow.close();
       open(location, '_self').close();
     }
+
     render() {
+      const renderCode = function () {
+        if(getUrlVars()["code"]) {
+          return (
+            <span> & code <b style={{"font-weight": "bold"}}>{getUrlVars()["code"]}</b></span>
+          )
+        }
+      }
 
       return (
           <div>
@@ -14,7 +22,7 @@ class CreateAsset extends React.Component {
                       <center><font color="ass"> Asset {getUrlVars()["type"] ? decodeURIComponent(getUrlVars()["type"]) : ""} {getUrlVars()["value"] == "update" ? "Updated" : "Created"} </font> </center>
                   </h4>
                   <h3>
-                      <center><font color="ass"> Asset {getUrlVars()["type"] ? decodeURIComponent(getUrlVars()["type"]) : ""} with name <strong style={{'fontWeight':700}}> {getUrlVars()["name"] ? decodeURIComponent(getUrlVars()["name"]) : ""}</strong> {getUrlVars()["value"] == "update" ? "updated." : "created."}  </font> </center>
+                      <center><font color="ass"> Asset {getUrlVars()["type"] ? decodeURIComponent(getUrlVars()["type"]) : ""} with name <strong style={{'fontWeight':700}}> {getUrlVars()["name"] ? decodeURIComponent(getUrlVars()["name"]) : ""}</strong> {renderCode()} {getUrlVars()["value"] == "update" ? "updated." : "created."}  </font> </center>
                   </h3>
                   <div className="text-center">
                       <button type="button" className="btn btn-close" onClick={(e)=>{this.close()}}>Close</button>

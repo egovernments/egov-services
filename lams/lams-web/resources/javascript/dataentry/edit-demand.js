965b348fa3179ba2a3f79db67d9f3ba73891a09b
kumareshdas <kummaresh.das@egovernments.org>
2017-05-08 12:22:22 +0530

    edit demand scree

--- lams/lams-web/resources/javascript/dataentry/edit-demand.js
+++ lams/lams-web/resources/javascript/dataentry/edit-demand.js
@@ -2,13 +2,12 @@ class EditDemand extends React.Component {
   constructor(props) {
     super(props);
     this.state={demands:{
-      "arrears":"",
-      "arrearsDemand":"",
-      "paymentCycle":"",
-      "arrearesCollection":"",
-      "current":"",
-      "currentDemand":"",
-      "currentCollection":"",
+      "month":"",
+      "demand":"",
+      "collection":"",
+      "month": "",
+      "year": "",
+
 
 }
 }
@@ -20,7 +19,7 @@ close(){
 
 render() {
   let{demands}=this.state;
-let{arrears,arrearsDemand,paymentCycle,arrearesCollection,current,currentDemand,currentCollection}=demands;
+let{month,demand,collection}=demands;
 
 return (
   <div>
@@ -29,106 +28,23 @@ return (
   <form >
         <div className="form-section-inner">
 
-        <div className="row">
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="arrears">Arrears</label>
-        </div>
-        <div className="col-sm-6">
-                <input type="text" id="arrears" name="arrears" value={arrears}/>
-        </div>
-        </div>
-        </div>
+        <table id="editDemand" className="table table-bordered">
+          <thead>
+          <tr>
+              <th> Month </th>
+              <th>&nbsp; &nbsp; &nbsp; &nbsp; Demand <tr> &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;<input type="checkbox" value="demand" /> </tr></th>
+              <th> &nbsp; &nbsp;&nbsp; &nbsp; Collection <tr> &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;<input type="checkbox" value="collection" /> </tr></th>
 
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="arrearsDemand">Arrears Demand <span>*</span></label>
-        </div>
-        <div className="col-sm-6">
-        <div className="text-no-ui">
-          <span>₹</span>
-                 <input type="number" min="0" name="arrearsDemand" id="arrearsDemand" value={arrearsDemand} required/>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
+          </tr>
+          </thead>
+                    <tbody id="attendanceTableBody">
 
-        <div className="row">
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="arrearesCollection">Arrears Collection <span>*</span></label>
-        </div>
-        <div className="col-sm-6">
-        <div className="text-no-ui">
-          <span>₹</span>
-                 <input type="number" min="0" name="arrearesCollection" id="arrearesCollection" value={arrearesCollection} required/>
-        </div>
-        </div>
-        </div>
-        </div>
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="current">Current</label>
-        </div>
-        <div className="col-sm-6">
-                <input type="text" id="current" name="current" value={current}/>
-        </div>
-        </div>
-        </div>
-        </div>
+          </tbody>
 
 
-        <div className="row">
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="currentDemand">Current Demand <span>*</span></label>
-        </div>
-        <div className="col-sm-6">
-        <div className="text-no-ui">
-          <span>₹</span>
-                 <input type="number" min="0" name="currentDemand" id="currentDemand" value={currentDemand} required/>
-        </div>
-        </div>
-        </div>
-        </div>
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="currentCollection">Current Collection <span>*</span></label>
-        </div>
-        <div className="col-sm-6">
-        <div className="text-no-ui">
-          <span>₹</span>
-                 <input type="number" min="0" name="currentCollection" id="currentCollection" value={currentCollection} required/>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
+          </table>
 
 
-        <div className="row">
-        <div className="col-sm-6">
-        <div className="row">
-        <div className="col-sm-6 label-text">
-                <label for="paymentCycle">Payment Cycle <span>*</span></label>
-        </div>
-        <div className="col-sm-6">
-                         <input type="radio"  name="paymentCycle" id="paymentCycle" value={paymentCycle} required/> Yearly
-                         &nbsp;
-                          <input type="radio"  name="paymentCycle" id="paymentCycle" value={paymentCycle} required/> Monthly
-
-
-        </div>
-        </div>
-        </div>
-        </div>
 
         <div className="text-center">
 

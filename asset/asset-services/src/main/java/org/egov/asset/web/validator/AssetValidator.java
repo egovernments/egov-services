3a2fdd5bc77b4323dc53deb06261c6c51b84177e
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-05-09 17:06:19 +0530

    asset name validation added

--- asset/asset-services/src/main/java/org/egov/asset/web/validator/AssetValidator.java
+++ asset/asset-services/src/main/java/org/egov/asset/web/validator/AssetValidator.java
@@ -3,7 +3,10 @@ package org.egov.asset.web.validator;
 import java.util.List;
 
 import org.egov.asset.contract.AssetRequest;
+import org.egov.asset.model.Asset;
 import org.egov.asset.model.AssetCategory;
+import org.egov.asset.model.AssetCriteria;
+import org.egov.asset.service.AssetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +15,9 @@ public class AssetValidator {
 	
 	@Autowired
 	private AssetCategoryValidator assetCategoryValidator;
+	
+	@Autowired
+	private AssetService assetService;
 	
 	public void validateAsset(AssetRequest assetRequest){
 		findAssetCategory(assetRequest);
@@ -27,6 +33,17 @@ public class AssetValidator {
 			throw new RuntimeException("Invalid asset category");
 		}
 		
+	}
+	
+	public void findAsset(AssetRequest assetRequest){
+		
+		AssetCriteria assetCriteria = new AssetCriteria();
+		assetCriteria.setTenantId(assetRequest.getAsset().getTenantId());
+		assetCriteria.setName(assetRequest.getAsset().getName());
+		List<Asset> assets = assetService.getAssets(assetCriteria);
+		
+		if(assets.get(0).getName().equalsIgnoreCase(assetRequest.getAsset().getName()))
+			throw new RuntimeException("Duplicate asset name asset already exists");
 	}
 
 

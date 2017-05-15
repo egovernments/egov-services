3aff5235f8f3fcc7a1e711aa546be526710075b8
Rajat Kaushal <rajat.kaushal@riflexions.com>
2017-05-15 16:15:19 +0530

    fixed egf-account-details-consumer service - removed exceptions

--- financials/egf-account-details-consumer/src/main/java/org/egov/egf/listner/factory/RequestInfoFactory.java
+++ financials/egf-account-details-consumer/src/main/java/org/egov/egf/listner/factory/RequestInfoFactory.java
@@ -1,7 +1,5 @@
 package org.egov.egf.listner.factory;
 
-import static org.springframework.util.ObjectUtils.isEmpty;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -23,22 +21,22 @@ public class RequestInfoFactory {
 
 		Date ts = null;
 		try {
-			ts = isEmpty(requestInfoJSONObject.getString("ts")) ? null : sdf.parse(requestInfoJSONObject.getString("ts"));
+			ts = requestInfoJSONObject.isNull("ts") ? null : sdf.parse(requestInfoJSONObject.getString("ts"));
 		} catch (ParseException e) {
 			e.printStackTrace();
 			LOGGER.debug("Parse exception while parsing date : " + requestInfoJSONObject.getString("ts"));
 		}
 
 		RequestInfo requestInfo = RequestInfo.builder()
-				.apiId(isEmpty(requestInfoJSONObject.getString("apiId")) ? null : requestInfoJSONObject.getString("apiId"))
-				.ver(isEmpty(requestInfoJSONObject.getString("ver")) ? null : requestInfoJSONObject.getString("ver"))
+				.apiId(requestInfoJSONObject.isNull("apiId") ? null : requestInfoJSONObject.getString("apiId"))
+				.ver(requestInfoJSONObject.isNull("ver") ? null : requestInfoJSONObject.getString("ver"))
 				.ts(ts)
-				.action(isEmpty(requestInfoJSONObject.getString("action")) ? null : requestInfoJSONObject.getString("action"))
-				.did(isEmpty(requestInfoJSONObject.getString("did")) ? null : requestInfoJSONObject.getString("did"))
-				.key(isEmpty(requestInfoJSONObject.getString("key")) ? null : requestInfoJSONObject.getString("key"))
-				.msgId(isEmpty(requestInfoJSONObject.getString("msgId")) ? null : requestInfoJSONObject.getString("msgId"))
-				.requesterId(isEmpty(requestInfoJSONObject.getString("requesterId")) ? null : requestInfoJSONObject.getString("requesterId"))
-				.authToken(isEmpty(requestInfoJSONObject.getString("authToken")) ? null : requestInfoJSONObject.getString("authToken"))
+				.action(requestInfoJSONObject.isNull("action") ? null : requestInfoJSONObject.getString("action"))
+				.did(requestInfoJSONObject.isNull("did") ? null : requestInfoJSONObject.getString("did"))
+				.key(requestInfoJSONObject.isNull("key") ? null : requestInfoJSONObject.getString("key"))
+				.msgId(requestInfoJSONObject.isNull("msgId") ? null : requestInfoJSONObject.getString("msgId"))
+				.requesterId(requestInfoJSONObject.isNull("requesterId") ? null : requestInfoJSONObject.getString("requesterId"))
+				.authToken(requestInfoJSONObject.isNull("authToken") ? null : requestInfoJSONObject.getString("authToken"))
 				.tenantId(tenantId)
 				.build();
 

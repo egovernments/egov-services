3fa96b90bdb77a75599eaca12973f06f20e9eeaa
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-05-16 12:17:20 +0530

    Rectified db migration scripts in data-sync-task

--- hybrid-data-sync/data-sync-task/src/main/resources/db/migration/seed/V20170510172301__update_epoch_time.sql
+++
@@ -1,4 +0,0 @@
-DELETE  FROM data_sync_epoch;
-
-UPDATE data_sync_epoch set epoch = '2017-05-01 00:00:00' where profile = 'ml-to-ms';
-UPDATE data_sync_epoch set epoch = '2017-05-01 00:00:00' where profile = 'ms-to-ml';

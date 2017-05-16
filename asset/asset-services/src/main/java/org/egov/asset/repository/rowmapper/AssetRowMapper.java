710c0500ff47700d8144c59717fde3328f899f0c
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-05-16 12:39:38 +0530

    rowmapper modified

--- asset/asset-services/src/main/java/org/egov/asset/repository/rowmapper/AssetRowMapper.java
+++ asset/asset-services/src/main/java/org/egov/asset/repository/rowmapper/AssetRowMapper.java
@@ -88,8 +88,17 @@ public class AssetRowMapper implements RowMapper<Asset> {
 			asset.setLength(rs.getString("length"));
 			asset.setWidth(rs.getString("width"));
 			asset.setTotalArea(rs.getString("totalArea"));
-			asset.setAccumulatedDepreciation(rs.getDouble("accumulateddepreciation"));
-			asset.setGrossValue(rs.getDouble("grossvalue"));
+			Double accumulatedDepreciation = rs.getDouble("accumulateddepreciation");
+			if (accumulatedDepreciation == 0)
+				asset.setAccumulatedDepreciation(null);
+			else
+				asset.setAccumulatedDepreciation(accumulatedDepreciation);
+
+			Double grossValue = rs.getDouble("grossvalue");
+			if (grossValue == 0)
+				asset.setGrossValue(null);
+			else
+				asset.setGrossValue(accumulatedDepreciation);
 			asset.setAssetReference((Long) rs.getObject("assetreference"));
 			asset.setVersion(rs.getString("version"));
 

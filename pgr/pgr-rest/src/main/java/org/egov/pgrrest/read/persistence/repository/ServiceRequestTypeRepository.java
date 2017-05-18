3bd6e2024b2fbb626512f37a7c77fb26506ba861
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-18 14:55:54 +0530

    EGSVC-146 | Associate keywords for a service type

--- pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/persistence/repository/ServiceRequestTypeRepository.java
+++ pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/persistence/repository/ServiceRequestTypeRepository.java
@@ -1,8 +1,10 @@
 package org.egov.pgrrest.read.persistence.repository;
 
 import org.egov.pgrrest.common.entity.ServiceType;
+import org.egov.pgrrest.common.entity.ServiceTypeKeyword;
 import org.egov.pgrrest.common.entity.Submission;
 import org.egov.pgrrest.common.repository.ServiceRequestTypeJpaRepository;
+import org.egov.pgrrest.common.repository.ServiceTypeKeywordJpaRepository;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
@@ -15,27 +17,85 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceRequestTypeRepository {
 
     private ServiceRequestTypeJpaRepository serviceRequestTypeJpaRepository;
+    private ServiceTypeKeywordJpaRepository serviceTypeKeywordJpaRepository;
     @PersistenceContext
     private EntityManager entityManager;
 
     @Autowired
-    public ServiceRequestTypeRepository(ServiceRequestTypeJpaRepository serviceRequestTypeJpaRepository) {
+    public ServiceRequestTypeRepository(ServiceRequestTypeJpaRepository serviceRequestTypeJpaRepository,
+                                        ServiceTypeKeywordJpaRepository serviceTypeKeywordJpaRepository) {
         this.serviceRequestTypeJpaRepository = serviceRequestTypeJpaRepository;
+        this.serviceTypeKeywordJpaRepository = serviceTypeKeywordJpaRepository;
     }
 
     public List<ServiceType> findActiveServiceRequestTypesByCategoryAndTenantId(Long categoryId, String tenantId) {
-        return serviceRequestTypeJpaRepository.findActiveServiceTypes(categoryId, tenantId);
+        final List<ServiceType> serviceTypes = serviceRequestTypeJpaRepository
+            .findActiveServiceTypes(categoryId, tenantId);
+        enrichWithKeywords(serviceTypes, tenantId);
+        return serviceTypes;
     }
 
     public List<ServiceType> getFrequentlyFiledServiceRequests(Integer count, String tenantId) {
         final List<String> serviceCodes = getFrequentlyUsedServiceCodes(count, tenantId);
-        return serviceRequestTypeJpaRepository.findActiveServiceTypes(serviceCodes, tenantId);
+        final List<ServiceType> serviceTypes = serviceRequestTypeJpaRepository
+            .findActiveServiceTypes(serviceCodes, tenantId);
+        enrichWithKeywords(serviceTypes, tenantId);
+        return serviceTypes;
+    }
+
+    public ServiceType getServiceRequestType(String serviceTypeCode, String tenantId) {
+        final ServiceType serviceType = serviceRequestTypeJpaRepository
+            .findByCodeAndTenantId(serviceTypeCode, tenantId);
+        if (serviceType != null) {
+            enrichWithKeywords(Collections.singletonList(serviceType), tenantId);
+            return serviceType;
+        }
+        return null;
+    }
+
+    public List<ServiceType> getAllServiceTypes(String tenantId) {
+        final List<ServiceType> serviceTypes = serviceRequestTypeJpaRepository.findByTenantId(tenantId);
+        enrichWithKeywords(serviceTypes, tenantId);
+        return serviceTypes;
+    }
+
+    private void enrichWithKeywords(List<ServiceType> serviceTypes, String tenantId) {
+        final List<String> serviceCodes = getServiceCodes(serviceTypes);
+        final Map<String, List<ServiceTypeKeyword>> serviceCodeToKeywordMap =
+            getServiceCodeToKeywordMap(serviceCodes, tenantId);
+        serviceTypes.forEach(serviceType -> {
+            final List<ServiceTypeKeyword> matchingEntityKeywords = serviceCodeToKeywordMap
+                .getOrDefault(serviceType.getCode(), Collections.emptyList());
+            serviceType.setKeywords(getKeywords(matchingEntityKeywords));
+        });
+    }
+
+    private List<String> getKeywords(List<ServiceTypeKeyword> keywordEntities) {
+        return keywordEntities.stream()
+            .map(ServiceTypeKeyword::getKeyword)
+            .collect(Collectors.toList());
+    }
+
+    private List<String> getServiceCodes(List<ServiceType> serviceTypes) {
+        return serviceTypes.stream()
+            .map(ServiceType::getCode)
+            .collect(Collectors.toList());
+    }
+
+    private Map<String, List<ServiceTypeKeyword>> getServiceCodeToKeywordMap(List<String> serviceCodes,
+                                                                             String tenantId) {
+        final List<ServiceTypeKeyword> keywords = serviceTypeKeywordJpaRepository
+            .findByServiceCodeInAndTenantId(serviceCodes, tenantId);
+        return keywords.stream().collect(Collectors.groupingBy(ServiceTypeKeyword::getServiceCode));
     }
 
     private List<String> getFrequentlyUsedServiceCodes(Integer count, String tenantId) {
@@ -60,13 +120,5 @@ public class ServiceRequestTypeRepository {
             serviceCodes.add((String) columns[0]);
         }
         return serviceCodes;
-    }
-
-    public ServiceType getServiceRequestType(String complaintTypeCode, String tenantId) {
-        return serviceRequestTypeJpaRepository.findByCodeAndTenantId(complaintTypeCode, tenantId);
-    }
-
-    public List<ServiceType> getAllServiceTypes() {
-        return serviceRequestTypeJpaRepository.findAll();
     }
 }

5426d49d2ede5500c7a34c131550ea6854ef26bf
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-19 10:53:01 +0530

    Added utility method to create/update attribute entry

--- libraries/pgr-common/src/main/java/org/egov/pgr/common/contract/AttributeValues.java
+++ libraries/pgr-common/src/main/java/org/egov/pgr/common/contract/AttributeValues.java
@@ -27,4 +27,29 @@ public class AttributeValues {
             .map(AttributeEntry::getName)
             .collect(Collectors.toList());
     }
+
+    public static void createOrUpdateAttributeEntry(List<AttributeEntry> attributeValues, String key, String name) {
+        if (isAttributeKeyPresent(attributeValues, key)) {
+            updateAttributeEntry(attributeValues, key, name);
+        } else {
+            createAttributeEntry(attributeValues, key, name);
+        }
+    }
+
+    private static boolean isAttributeKeyPresent(List<AttributeEntry> attributeValues, String key) {
+        return attributeValues.stream()
+            .anyMatch(attribute -> key.equals(attribute.getKey()));
+    }
+
+    private static void createAttributeEntry(List<AttributeEntry> attributeValues, String key, String name) {
+        attributeValues.add(new AttributeEntry(key, name));
+    }
+
+    private static void updateAttributeEntry(List<AttributeEntry> attributeValues, String key, String name) {
+        final AttributeEntry matchingEntry = attributeValues.stream()
+            .filter(attribute -> key.equals(attribute.getKey()))
+            .findFirst()
+            .orElse(null);
+        matchingEntry.setName(name);
+    }
 }

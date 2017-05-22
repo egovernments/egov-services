8fe46cfa9edc8a0d9b76ecc20a24195aff58acca
Raman Pandey <raman.pandey@egovernments.org>
2017-05-22 18:45:08 +0530

    [EGSVC-153]-added search criteria based on tenant id for account code purpose

--- financials/egf-masters/src/main/java/org/egov/egf/persistence/specification/AccountCodePurposeSpecification.java
+++ financials/egf-masters/src/main/java/org/egov/egf/persistence/specification/AccountCodePurposeSpecification.java
@@ -9,6 +9,7 @@ import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.egov.egf.persistence.entity.AbstractAuditable_;
 import org.egov.egf.persistence.entity.AccountCodePurpose;
 import org.egov.egf.persistence.entity.AccountCodePurpose_;
 import org.egov.egf.persistence.queue.contract.AccountCodePurposeContract;
@@ -26,12 +27,16 @@ public class AccountCodePurposeSpecification implements Specification<AccountCod
 			final CriteriaBuilder criteriaBuilder) {
 		final Path<Long> id = root.get(AccountCodePurpose_.id);
 		final Path<String> name = root.get(AccountCodePurpose_.name);
+		final Path<String> tenantId = root.get(AbstractAuditable_.tenantId);
 		final List<Predicate> predicates = new ArrayList<>();
 		if (criteria.getId() != null)
 			predicates.add(criteriaBuilder.equal(id, criteria.getId()));
 
 		if (criteria.getName() != null)
 			predicates.add(criteriaBuilder.equal(name, criteria.getName()));
+
+		if (criteria.getTenantId() != null)
+			predicates.add(criteriaBuilder.equal(tenantId, criteria.getTenantId()));
 
 		return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
 	}

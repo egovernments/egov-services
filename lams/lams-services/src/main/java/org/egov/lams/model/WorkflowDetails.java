d50632bd6ab55514d91dcc7a01bf9680cacbc199
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-05-25 12:07:45 +0530

    @not null added to assigne in workflowdetails

--- lams/lams-services/src/main/java/org/egov/lams/model/WorkflowDetails.java
+++ lams/lams-services/src/main/java/org/egov/lams/model/WorkflowDetails.java
@@ -1,5 +1,7 @@
 package org.egov.lams.model;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,14 +10,11 @@ import lombok.NoArgsConstructor;
 public class WorkflowDetails {
 	
   private String department;
-
   private String designation;
 
+  @NotNull
   private Long assignee;
-
   private String action;
-  
   private String status;
-  
   private Long initiatorPosition;
 }

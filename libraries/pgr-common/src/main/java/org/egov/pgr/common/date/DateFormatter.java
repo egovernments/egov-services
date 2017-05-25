16b6a912d11a7701833dd5a885a7c0690b2fdb6c
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-25 23:06:07 +0530

    Added null check to date convertor

--- libraries/pgr-common/src/main/java/org/egov/pgr/common/date/DateFormatter.java
+++ libraries/pgr-common/src/main/java/org/egov/pgr/common/date/DateFormatter.java
@@ -3,14 +3,22 @@ package org.egov.pgr.common.date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import static org.springframework.util.StringUtils.isEmpty;
+
 public class DateFormatter {
     private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
     public String toString(java.util.Date date) {
+        if (date == null) {
+            return null;
+        }
         return DATE_TIME_FORMAT.format(date);
     }
 
     public java.util.Date toDate(String dateTime) {
+        if (isEmpty(dateTime)) {
+            return null;
+        }
         try {
             return DATE_TIME_FORMAT.parse(dateTime);
         } catch (ParseException e) {

fd0394846cf63af4c227509b15102fed7db5ab16
sanjeevnvp <sanjeev.p@egovernments.org>
2017-05-30 01:27:10 +0530

    [EGSVC-214]:Sample data scripts for pgr ,access control service

---
+++ core/egov-localization/src/main/resources/db/migration/seed/V20170529221733__service_definition_messagedata.sql
@@ -0,0 +1,29 @@
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.lastpaid.property','Last paid property tax receipt','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.halfyear.property','Whether property tax is paid till the current half year','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.own.property','Do you own the property','default');
+
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.bride.name','Bride Name','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.bride.address','Bride address','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.bride.age','Bride age','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.bride.religion','Bride Religion','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.groom.name','Groom name','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.groom.address','Groom address','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.groom.age','Groom age','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.groom.religion','Groom Religion','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.witness1.name','Witness 1 name','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.witness1.age','Witness 1 age','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.witness1.address','Witness1 address','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.witness2.name','Witness2 name','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.witness2.age','Witness2 age','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.witness2.address','Witness2 address','default');
+
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.isbridegroom.citizen','Is the bride and groom are indian citizens','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.marriage.published','Whether the intent for marriage was published on the notice board','default');
+
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.ptax.paid','Whether property tax is paid till the current half year','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.sewarage.tax','Whether Sewarage charges are paid till date','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.water.tax','Whether water charges is paid till date','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.licenses.ondate','Whether licences for  trades established in this premises are renewed as on date','default');
+insert into message (id,locale,code,message,tenantid) values(nextval('SEQ_MESSAGE'),'en_IN','core.tt.dept','Whether titl transfer is approved in registration department','default');
+
+
---
+++ pgr/pgr-rest/src/main/resources/db/migration/dev/V20170529235424__panavel_receiving_mode.sql
@@ -0,0 +1,12 @@
+INSERT INTO egpgr_receivingmode (id, name, code, visible, version, tenantid)
+VALUES (nextval('seq_egpgr_receivingmode'), 'Website', 'WEBSITE', false, 0, 'panavel');
+INSERT INTO egpgr_receivingmode (id, name, code, visible, version, tenantid)
+VALUES (nextval('seq_egpgr_receivingmode'), 'SMS', 'SMS', false, 0, 'panavel');
+INSERT INTO egpgr_receivingmode (id, name, code, visible, version, tenantid)
+VALUES (nextval('seq_egpgr_receivingmode'), 'Call', 'CALL', true, 0, 'panavel');
+INSERT INTO egpgr_receivingmode (id, name, code, visible, version, tenantid)
+VALUES (nextval('seq_egpgr_receivingmode'), 'Email', 'EMAIL', true, 0, 'panavel');
+INSERT INTO egpgr_receivingmode (id, name, code, visible, version, tenantid)
+VALUES (nextval('seq_egpgr_receivingmode'), 'Manual', 'MANUAL', true, 0, 'panavel');
+INSERT INTO egpgr_receivingmode (id, name, code, visible, version, tenantid)
+VALUES (nextval('seq_egpgr_receivingmode'), 'Mobile', 'MOBILE', false, 0, 'panavel');

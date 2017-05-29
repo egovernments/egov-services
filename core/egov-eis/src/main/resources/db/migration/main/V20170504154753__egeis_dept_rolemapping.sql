a749d0d1527cb53bd0111368f070f1ec6df7e6c1
Ajay Divakaran <ajay_886@hotmail.com>
2017-05-29 23:24:32 +0530

    Deleted eis project given Citizen services has started consuming hr & employee service

--- core/egov-eis/src/main/resources/db/migration/main/V20170504154753__egeis_dept_rolemapping.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE egeis_desig_rolemapping (
-    designationid bigint NOT NULL,
-    roleid bigint NOT NULL,
-    tenantid character varying (256)
-);
--- core/egov-eis/src/main/resources/db/migration/seed/V20170509130016__eis_grade_master_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO egeis_grade_mstr (grade_id, grade_value, age, order_no, tenantId) VALUES (nextval('EGPIMS_GRADE_MSTR_SEQ'), 'A', 60, NULL,'default');
-INSERT INTO egeis_grade_mstr (grade_id, grade_value, age, order_no, tenantId) VALUES (nextval('EGPIMS_GRADE_MSTR_SEQ'), 'B', 60, NULL,'default');
-INSERT INTO egeis_grade_mstr (grade_id, grade_value, age, order_no, tenantId) VALUES (nextval('EGPIMS_GRADE_MSTR_SEQ'), 'C', 60, NULL,'default');
-INSERT INTO egeis_grade_mstr (grade_id, grade_value, age, order_no, tenantId) VALUES (nextval('EGPIMS_GRADE_MSTR_SEQ'), 'D', 60, NULL,'default');

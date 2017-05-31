c56788592cc3aedfe6de56f9dd25a8bdfeb7c3fb
ramki <ramakrishna@egovernments.org>
2017-05-31 08:04:05 +0530

    Added tax period from and to dates for demanddetails contract

--- demand/demand-services/src/main/java/org/egov/demand/web/contract/DemandDetails.java
+++ demand/demand-services/src/main/java/org/egov/demand/web/contract/DemandDetails.java
@@ -1,6 +1,9 @@
 package org.egov.demand.web.contract;
 
 import java.math.BigDecimal;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +21,10 @@ public class DemandDetails {
 	private BigDecimal rebateAmount;
 	private String taxReason;
 	private String taxPeriod;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "IST")
+	private Date periodStartDate;
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", timezone = "IST")
+	private Date periodEndDate;
 	private Long glCode;
 	private Integer isActualDemand;
 	private String tenantId;

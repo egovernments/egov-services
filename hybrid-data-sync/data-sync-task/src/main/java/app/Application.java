1993c6494e5fb3714555209f8286bd7002588428
Sathish P <sathish.p@egovernments.org>
2017-06-06 15:16:35 +0530

    Set default time zone

--- hybrid-data-sync/data-sync-task/src/main/java/app/Application.java
+++ hybrid-data-sync/data-sync-task/src/main/java/app/Application.java
@@ -1,8 +1,21 @@
 package app;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,4 +24,13 @@ public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class);
     }
+    
+    @Value("${app.timezone}")
+	private String timeZone;
+    
+    @PostConstruct
+	public void initialize() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+	}
+      
 }

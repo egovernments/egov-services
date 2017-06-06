defb6a21b84b47b07816a20dc8a063fea421934e
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-06-06 16:44:01 +0530

    timezone default to ist added

--- lams/lams-services/src/main/java/org/egov/lams/AgreementApplication.java
+++ lams/lams-services/src/main/java/org/egov/lams/AgreementApplication.java
@@ -1,5 +1,10 @@
 package org.egov.lams;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -7,10 +12,19 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class AgreementApplication {
+
+	@Value("${app.timezone}")
+	private String timeZone;
+
+	@PostConstruct
+	public void initialize() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+	}
 
 	@Bean
 	public RestTemplate getRestTemplate() {
@@ -20,12 +34,15 @@ public class AgreementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgreementApplication.class, args);
 	}
-	
+
 	@Bean
 	public MappingJackson2HttpMessageConverter jacksonConverter() {
 		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		// mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT,Locale.ENGLISH));
+		mapper.setTimeZone(TimeZone.getTimeZone(timeZone));
 		converter.setObjectMapper(mapper);
 		return converter;
 	}

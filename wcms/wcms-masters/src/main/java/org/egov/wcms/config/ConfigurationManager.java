910585268fced017da9c2eef05fdd4ff9574a23d
Darshan Nagesh <darshan.nagesh@tarento.com>
2017-06-07 11:22:32 +0530

    Fixing the Configuration Manager issue

--- wcms/wcms-masters/src/main/java/org/egov/wcms/config/ConfigurationManager.java
+++ wcms/wcms-masters/src/main/java/org/egov/wcms/config/ConfigurationManager.java
@@ -2,6 +2,8 @@ package org.egov.wcms.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 
 import lombok.Getter;
 import lombok.ToString;
@@ -9,6 +11,8 @@ import lombok.ToString;
 @Getter
 @ToString
 @Configuration
+@PropertySource(value = { "classpath:config/application-config.properties" }, ignoreResourceNotFound = true)
+@Order(0)
 public class ConfigurationManager {
 
 	@Value("${egov.services.workflow_service.hostname}")
@@ -35,10 +39,10 @@ public class ConfigurationManager {
 	@Value("${kafka.topics.update.workflow}")
 	private String kafkaUpdateworkflowTopic;
 	
-	@Value("${kafka.topics.save.agreement}")
+	@Value("${kafka.topics.save.wcms}")
 	private String kafkaSaveWaterConnectionTopic;
 	
-	@Value("${kafka.topics.update.agreement}")
+	@Value("${kafka.topics.update.wcms}")
 	private String kafkaUpdateWaterConnectionTopic;
 }
 

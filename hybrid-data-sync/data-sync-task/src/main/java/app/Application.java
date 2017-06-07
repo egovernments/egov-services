7f5c082ac07e0a6a9f578df1355df6dad98af072
Sathish P <sathish.p@egovernments.org>
2017-06-07 18:56:38 +0530

    Compilation issue fix

--- hybrid-data-sync/data-sync-task/src/main/java/app/Application.java
+++ hybrid-data-sync/data-sync-task/src/main/java/app/Application.java
@@ -1,7 +1,5 @@
 package app;
 
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
@@ -9,13 +7,8 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @EnableScheduling

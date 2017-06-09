a60ac6563284be54836d68b53ed457899dcd72a3
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-06-09 14:17:04 +0530

    url issue fixed

--- lams/lams-indexer/src/main/java/org/egov/lams/repository/BoundaryRepository.java
+++ lams/lams-indexer/src/main/java/org/egov/lams/repository/BoundaryRepository.java
@@ -21,10 +21,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Service
 public class BoundaryRepository {
 

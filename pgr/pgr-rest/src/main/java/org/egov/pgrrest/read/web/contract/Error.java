c87ba8bc22e282a52c3361b8e9ab9d50da0d9d05
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-06-09 19:00:08 +0530

    Cleaning pgr-rest to remove master package which was not needed

--- pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/web/contract/Error.java
+++ pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/web/contract/Error.java
@@ -1,19 +1,12 @@
 package org.egov.pgrrest.read.web.contract;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.List;
 
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode
--- pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/web/contract/ErrorResponse.java
+++ pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/web/contract/ErrorResponse.java
@@ -1,21 +1,13 @@
 package org.egov.pgrrest.read.web.contract;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
+import lombok.*;
 import org.egov.common.contract.response.ResponseInfo;
-
 
 import java.util.List;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
 @ToString
 @EqualsAndHashCode

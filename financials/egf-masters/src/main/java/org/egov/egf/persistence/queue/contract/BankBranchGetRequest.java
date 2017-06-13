550edbbc62b0a8c6bed27b64a7db8e21da1cf1e1
venki <venki@egovernments.org>
2017-06-13 17:17:09 +0530

    Added bankbranch service test.

--- financials/egf-masters/src/main/java/org/egov/egf/persistence/queue/contract/BankBranchGetRequest.java
+++ financials/egf-masters/src/main/java/org/egov/egf/persistence/queue/contract/BankBranchGetRequest.java
@@ -48,12 +48,14 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @Getter
 @Setter
 @NoArgsConstructor

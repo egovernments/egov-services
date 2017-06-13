3040f383b044211b5833bd7cd100afb634ef34a5
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-06-13 12:44:58 +0530

    Create BillingServiceApplication.java

--- billingservices/billing-service/src/main/java/org/egov/demand/BillingServiceApplication.java
+++ billingservices/billing-service/src/main/java/org/egov/demand/BillingServiceApplication.java
@@ -1,17 +1,7 @@
 package org.egov.demand;
 
-import java.util.TimeZone;
-
-import javax.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class BillingServiceApplication {

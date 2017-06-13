4b2d405e0772abe8293cc317738ca1ed54a80b37
Sathish P <sathish.p@egovernments.org>
2017-06-13 17:38:48 +0530

    Updated epoch time to the date M@S deployed on production

---
+++ hybrid-data-sync/data-sync-task/src/main/resources/db/migration/seed/V20170613173610__update_epoch_time.sql
@@ -0,0 +1,4 @@
+
+update data_sync_epoch set epoch='2017-06-11 00:00:00' where profile='ml-to-ms';
+update data_sync_epoch set epoch='2017-06-11 00:00:00' where profile='ms-to-ml';
+update data_sync_epoch set epoch='2017-06-11 00:00:00' where profile='ms-to-ml-employee';

00847958897847c3f8295ba3e7336ff1c9101c3f
Ghanshyam <ghanshyam.rawat@riflexions.com>
2017-06-13 15:49:56 +0530

    Billing services common kafka

--- billingservices/billing-service/src/main/java/org/egov/demand/BillingServiceApplication.java
+++ billingservices/billing-service/src/main/java/org/egov/demand/BillingServiceApplication.java
@@ -1,12 +1,43 @@
 package org.egov.demand;
 
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
+@Import({TracerConfiguration.class})
 public class BillingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BillingServiceApplication.class, args);
 	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter jacksonConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		//mapper.setDateFormat(new SimpleDateFormat(DATE_FORMAT, Locale.ENGLISH));
+		//mapper.setTimeZone(TimeZone.getTimeZone(timeZone));
+		converter.setObjectMapper(mapper);
+		return converter;
+	}
+	
+	
+	@Bean
+	public ObjectMapper getObjectMapper(){
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		return objectMapper;
+	}
+
 }
--- billingservices/billing-service/src/main/java/org/egov/demand/web/controller/BillController.java
+++ billingservices/billing-service/src/main/java/org/egov/demand/web/controller/BillController.java
@@ -1,22 +1,58 @@
 package org.egov.demand.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.egov.demand.model.BillInfo;
+import org.egov.demand.service.BillService;
+import org.egov.demand.web.contract.BillRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class BillController {
 	
-
+	@Autowired
+	private BillService billService;
 	
-	public ResponseEntity<?> create(@RequestBody BillInfo billInfo, BindingResult bindingResult){
+	@PostMapping("_create")
+	@ResponseBody
+	public ResponseEntity<?> create(@RequestBody BillRequest billRequest, BindingResult bindingResult){
 		
 		/*if (bindingResult.hasErrors()) {
 			ErrorResponse errorResponse = populateErrors(bindingResult);
 			return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 		}*/
+		System.out.println("billInfo:"+billRequest);
+		billService.createAsync(billRequest);
 		
 		return null;
+	}
+	
+	@GetMapping("_search")
+	@ResponseBody
+	public ResponseEntity<?> get(){
+		
+		/*if (bindingResult.hasErrors()) {
+			ErrorResponse errorResponse = populateErrors(bindingResult);
+			return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		}*/
+		BillInfo billInfo =new BillInfo();
+		BillRequest billRequest =new BillRequest();
+		List<BillInfo> billInfos = new ArrayList<>();
+		billInfos.add(billInfo);
+		billRequest.setBillInfos(billInfos);
+		System.out.println("billInfo:"+billInfo);
+		//billService.createAsync(billInfo);
+		
+		return new ResponseEntity<>(billRequest, HttpStatus.OK);
 	}
 	
 	/*private ErrorResponse populateErrors(BindingResult errors) {

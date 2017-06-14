8cc44e963142466001defeacbe6896f0710f317c
muralimgwl <murali.m@goodworklabs.com>
2017-06-14 11:04:58 +0530

    Updated common-web

--- web/common-web/src/components/App.js
+++ web/common-web/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
@@ -11,8 +12,61 @@ import Footer from './common/Footer';
 
 import router from "../router";
 
+//api import
+import api from "../api/commonAPIS"
+
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+      // this.getOtp = this.getOtp.bind(this);
+      // this.validateOtp = this.validateOtp.bind(this);
+      // this.callLogin = this.callLogin.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+      if (nextProps.redirectTo) {
+          this.context.router.replace(nextProps.redirectTo);
+          this.props.onRedirect();
+      }
+  }
+
+  componentWillMount() {
+      // this.props.setLabels(agent.labels.getLabels());
+      // const token = window.localStorage.getItem('jwt');
+      // const userId = window.localStorage.getItem('userId');
+      // const type = window.localStorage.getItem('type');
+      //
+      // let currentUser = window.localStorage.getItem('currentUser');
+      //
+      // if(currentUser) {
+      //     currentUser = JSON.parse(currentUser);
+      // }
+      //
+      // if (token) {
+      //     // agent.setToken(token);
+      //     // agent.setUserId(userId);
+      //     // agent.setType(type);
+      // }
+
+      // api.commonApiPost("user/oauth/token",{tenantId:"default",
+      //     username:"narasappa",
+      //     password:"demo",
+      //     grant_type:"password",
+      //     scope:"read"}).then((response)=>{
+      //       console.log(response);
+      //     },(err)=> {
+      //     console.log(err);
+      //   });
+
+
+
+      // this.props.onLoad(!currentUser
+      //     ? agent.Auth.login((this.props.auth.userName || "9999999999"), (this.props.auth.password || "demo"))
+      //     : {UserRequest: currentUser}, token);
+  }
+
   render() {
     var {toggleDailogAndSetText,isDialogOpen,msg}=this.props;
     const actions = [
@@ -45,14 +99,51 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+    labels: state.labels,
+    appLoaded: state.common.appLoaded,
+    appName: state.common.appName,
+    currentUser: state.common.currentUser,
+    redirectTo: state.common.redirectTo,
+    auth:state.common.token,
+    pleaseWait: state.common.pleaseWait,
+    isDialogOpen: state.form.dialogOpen,
+    msg: state.form.msg
+});
 
-const mapStateToProps = state => ({isDialogOpen: state.form.dialogOpen, msg: state.form.msg});
+// this.props.appLoaded
 
 const mapDispatchToProps = dispatch => ({
-  toggleDailogAndSetText: (dailogState,msg) => {
-    dispatch({type: "TOGGLE_DAILOG_AND_SET_TEXT", dailogState,msg});
-  }
+    onLoad: (payload, token) => dispatch({type: 'APP_LOAD', payload, token, skipTracking: true}),
+    onRedirect: () => dispatch({type: 'REDIRECT'}),
+    setLabels: payload => dispatch({type: 'LABELS', payload}),
+    // onUpdateAuth: (value, key) => dispatch({type: 'UPDATE_FIELD_AUTH', key, value}),
+    // onLogin: (username, password) => {
+    //     dispatch({
+    //         type: 'LOGIN',
+    //         payload: []//agent.Auth.login(username, password)
+    //     })
+    // },
+    updateError: (error) =>
+        dispatch({
+            type: 'UPDATE_ERROR',
+            error
+        }),
+    setPleaseWait: (pleaseWait) =>
+        dispatch({
+            type: 'PLEASE_WAIT',
+            pleaseWait
+        }),
+   toggleDailogAndSetText: (dailogState,msg) => {
+          dispatch({type: "TOGGLE_DAILOG_AND_SET_TEXT", dailogState,msg});
+        }
 });
+
+
+App.contextTypes = {
+    router: React.PropTypes.object.isRequired
+};
+
 
 
 

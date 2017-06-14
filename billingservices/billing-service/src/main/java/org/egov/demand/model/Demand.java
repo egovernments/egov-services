bea9b85dc81f72dd05acda13b42af01864bcf8e4
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-06-14 18:32:13 +0530

    demand model modified

--- billingservices/billing-service/src/main/java/org/egov/demand/model/Demand.java
+++ billingservices/billing-service/src/main/java/org/egov/demand/model/Demand.java
@@ -25,7 +25,7 @@ public class Demand {
 
 	private List<DemandDetail> demandDetails = new ArrayList<>();
 
-	private AuditDetail auditDetails;
+	private AuditDetail auditDetail;
 
 	private String tenantId;
 

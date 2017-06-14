2606acde98b0a908853f5412b70552f8c5a49b4a
Ghanshyam <ghanshyam.rawat@riflexions.com>
2017-06-14 13:32:57 +0530

    Read topic name

--- billingservices/billing-service/src/main/java/org/egov/demand/consumer/DemandConsumer.java
+++ billingservices/billing-service/src/main/java/org/egov/demand/consumer/DemandConsumer.java
@@ -2,15 +2,19 @@ package org.egov.demand.consumer;
 
 import java.util.HashMap;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DemandConsumer {
 	
 	@KafkaListener(topics = "test-sp-kaf")
-    public void processMessage(ConsumerRecord<String, HashMap<String, Object>> consumerRecord) {
-		
+    public void processMessage(HashMap<String, Object> consumerRecord,
+    		@Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+	
+		System.out.println("consumerRecord.topic>>>>"+topic+","+"consumerRecord.value"+consumerRecord);
+       
     }
 }

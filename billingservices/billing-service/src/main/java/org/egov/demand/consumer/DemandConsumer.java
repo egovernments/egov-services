a7c556e44a30493e6d317a648a3359c14d630b83
Ghanshyam <ghanshyam.rawat@riflexions.com>
2017-06-15 09:36:25 +0530

    Mearge conflicts

--- billingservices/billing-service/src/main/java/org/egov/demand/consumer/DemandConsumer.java
+++ billingservices/billing-service/src/main/java/org/egov/demand/consumer/DemandConsumer.java
@@ -13,11 +13,7 @@ public class DemandConsumer {
 	@KafkaListener(topics = "test-sp-kaf")
     public void processMessage(HashMap<String, Object> consumerRecord,
     		@Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-<<<<<<< Updated upstream
 
-=======
-		
->>>>>>> Stashed changes
 		System.out.println("consumerRecord.topic>>>>"+topic+","+"consumerRecord.value"+consumerRecord);
        
     }

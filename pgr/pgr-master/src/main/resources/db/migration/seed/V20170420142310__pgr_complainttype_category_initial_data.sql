9e159bf7671fd8b8a23e094cffd3a4b13296c914
Sumanth Nagadavalli Suresh <nsready@gmail.com>
2017-06-16 15:49:56 +0530

    Reverting V20170420142310__pgr_complainttype_category_initial_data.sql to the state where code was not existing

--- pgr/pgr-master/src/main/resources/db/migration/seed/V20170420142310__pgr_complainttype_category_initial_data.sql
+++ pgr/pgr-master/src/main/resources/db/migration/seed/V20170420142310__pgr_complainttype_category_initial_data.sql
@@ -1,12 +1,12 @@
 ----------------- START --------------------
 
 ---------------Category List ---------------
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'SL','Street Lighting','Street Lighting',0,'default');
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'ENG','Engineering','Engineering',0,'default');
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'PBS','Public Health and Sanitation','Public Health and Sanitation',0,'default');
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'ADMIN','Administration','Administration',0,'default');
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'TP','Town Planning','Town Planning',0,'default');
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'RVN','Revenue','Revenue',0,'default');
-insert into egpgr_complainttype_category (id,code,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'WS','Water Supply','Water Supply',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Street Lighting','Street Lighting',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Engineering','Engineering',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Public Health and Sanitation','Public Health and Sanitation',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Administration','Administration',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Town Planning','Town Planning',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Revenue','Revenue',0,'default');
+insert into egpgr_complainttype_category (id,name,description,version,tenantid) values (nextval('seq_egpgr_complainttype_category'),'Water Supply','Water Supply',0,'default');
 
 ----------------- END --------------------

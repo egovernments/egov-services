c1b6885f900549fe2354a8aa4b7a73a5ab5e5dcc
kumareshdas <kummaresh.das@egovernments.org>
2017-06-20 16:46:18 +0530

    water soruce type master create screen with api

--- web/wcms-web/src/components/contents/masters/WaterSupplyType.js
+++ web/wcms-web/src/components/contents/masters/WaterSupplyType.js
@@ -80,7 +80,7 @@ class WaterSupplyType extends Component {
             let response=Api.commonApiPost("wcms-masters", "supplytype", "_update/"+id, {},{}).then((res)=>
            {
               this.setState({
-                list: res.supplytypes
+                list: res.SupplyTypes
             });
 
         },  (err)=> {

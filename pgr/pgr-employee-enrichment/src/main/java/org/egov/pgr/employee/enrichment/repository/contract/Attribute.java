7566fb80d99f9a2f20d36ab924c99da7eb496db9
Ajay Divakaran <ajay_886@hotmail.com>
2017-06-21 16:06:28 +0530

    Fixed build failure

--- pgr/pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/repository/contract/Attribute.java
+++ pgr/pgr-employee-enrichment/src/main/java/org/egov/pgr/employee/enrichment/repository/contract/Attribute.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;

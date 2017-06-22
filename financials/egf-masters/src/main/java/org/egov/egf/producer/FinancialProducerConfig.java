4be9fba87ba862280fe308a7127ae42ee3a86a51
venki <venki@egovernments.org>
2017-06-22 11:19:46 +0530

    Referring producer conf properties from application.properties.

--- financials/egf-masters/src/main/java/org/egov/egf/producer/FinancialProducerConfig.java
+++ financials/egf-masters/src/main/java/org/egov/egf/producer/FinancialProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,6 +17,22 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 @EnableKafka
 public class FinancialProducerConfig {
+
+	@Value("${kafka.config.bootstrap_server_config}")
+	private String serverConfig;
+
+	@Value("${kafka.producer.config.retries_config}")
+	private Integer retriesConfig;
+
+	@Value("${kafka.producer.config.batch_size_config}")
+	private Integer batchSizeConfig;
+
+	@Value("${kafka.producer.config.linger_ms_config}")
+	private Integer lingerMsConfig;
+
+	@Value("${kafka.producer.config.buffer_memory_config}")
+	private Integer bufferMemoryConfig;
+
 	@Bean
 	public ProducerFactory<String, Object> producerFactory() {
 		return new DefaultKafkaProducerFactory<>(producerConfigs());
@@ -24,11 +41,11 @@ public class FinancialProducerConfig {
 	@Bean
 	public Map<String, Object> producerConfigs() {
 		Map<String, Object> props = new HashMap<>();
-		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(ProducerConfig.RETRIES_CONFIG, 0);
-		props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-		props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverConfig);
+		props.put(ProducerConfig.RETRIES_CONFIG, retriesConfig);
+		props.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSizeConfig);
+		props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMsConfig);
+		props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, bufferMemoryConfig);
 		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return props;

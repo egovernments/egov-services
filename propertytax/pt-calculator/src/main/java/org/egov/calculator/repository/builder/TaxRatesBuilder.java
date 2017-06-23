e9dc54413f836014c00e1a84f6ac35ee8ff97414
Yosadhara <yosadhara.pradhan@walkingtree.in>
2017-06-23 16:02:55 +0530

    Search query modification

--- propertytax/pt-calculator/src/main/java/org/egov/calculator/repository/builder/TaxRatesBuilder.java
+++ propertytax/pt-calculator/src/main/java/org/egov/calculator/repository/builder/TaxRatesBuilder.java
@@ -7,29 +7,31 @@ package org.egov.calculator.repository.builder;
  *
  */
 public class TaxRatesBuilder {
-	
+
 	public static final String INSERT_TAXRATES_QUERY = "INSERT INTO egpt_mstr_taxrates"
 			+ " (tenantid, taxHead, dependentTaxHead, fromdate, todate,"
 			+ " fromValue, toValue, ratePercentage, taxFlatValue, createdby,"
-			+ " lastmodifiedby, createdtime, lastmodifiedtime)"
+			+ " lastmodifiedby, createdtime, lastmodifiedtime)" 
 			+ " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
 	public static final String UPDATE_TAXRATES_QUERY = "UPDATE egpt_mstr_taxrates"
 			+ " SET tenantid = ?, taxHead = ?, dependentTaxHead = ?, fromdate = ?,"
 			+ " todate = ?, fromValue = ?, toValue = ?, ratePercentage = ?,"
-			+ " taxFlatValue = ?, lastmodifiedby = ?, lastmodifiedtime = ?"
+			+ " taxFlatValue = ?, lastmodifiedby = ?, lastmodifiedtime = ?" 
 			+ " WHERE id = ?";
 
-	public static String getTaxRatesSearchQuery(String tenantId, String taxHead,
-			String validDate, Double validARVAmount, String parentTaxHead) {
+	public static String getTaxRatesSearchQuery(String tenantId, String taxHead, String validDate,
+			Double validARVAmount, String parentTaxHead) {
 
-		String selectQuery = "SELECT * FROM egpt_mstr_taxrates "
-				+ " WHERE tenantId = '" + tenantId + "'" + " AND taxHead = '"
-				+ taxHead + "'" + " AND '" + validDate
-				+ "' BETWEEN fromdate AND  todate" + " AND " + validARVAmount
-				+ " BETWEEN fromValue AND toValue" + " OR dependentTaxHead = '"
-				+ parentTaxHead + "'";
+		StringBuffer selectQuery = new StringBuffer();
+		selectQuery.append("SELECT * FROM egpt_mstr_taxrates").append(" WHERE tenantId = '" + tenantId + "'")
+				.append(" AND taxHead = '" + taxHead + "'")
+				.append(" AND '" + validDate + "' BETWEEN fromdate AND  todate")
+				.append(" AND " + validARVAmount + " BETWEEN fromValue AND toValue");
 
-		return selectQuery;
+		if (parentTaxHead != null && !parentTaxHead.isEmpty())
+			selectQuery.append(" AND dependentTaxHead = '" + parentTaxHead + "'");
+
+		return selectQuery.toString();
 	}
 }

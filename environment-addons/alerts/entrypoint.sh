76ad8f455640bed27b475f0403946f6cf2c9bf1f
Ajay Divakaran <ajay_886@hotmail.com>
2017-06-28 09:10:19 +0530

    Spike - application exception alerting

---
+++ environment-addons/alerts/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+envtpl < config.yaml.tmpl > config.yaml
+elastalert-create-index --index "${ELASTALERT_META_INDEX:-elastalert_status}" --old-index "${ELASTALERT_OLD_INDEX:-''}" --config ./config.yaml
+elastalert --debug

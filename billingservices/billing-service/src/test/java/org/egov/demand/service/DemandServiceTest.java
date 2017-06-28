c7c5ba3cbc86f045f5b0b9f3d48803e558eb1002
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-06-28 10:41:25 +0530

    demand test cases fixed

--- billingservices/billing-service/src/test/java/org/egov/demand/service/DemandServiceTest.java
+++ billingservices/billing-service/src/test/java/org/egov/demand/service/DemandServiceTest.java
@@ -115,15 +115,11 @@ public class DemandServiceTest {
 		
 		demandDetail.setTaxAmount(BigDecimal.valueOf(100d));
 		demandDetail.setCollectionAmount(BigDecimal.ZERO);
-		TaxHeadMaster taxHeadMaster = new TaxHeadMaster();
-		taxHeadMaster.setCode("0002");
-	//	demandDetail.setTaxHeadMaster(taxHeadMaster);
+		demandDetail.setTaxHeadMasterCode("0002");
 		DemandDetail demandDetail1 = new DemandDetail();
 		demandDetail1.setTaxAmount(BigDecimal.valueOf(200d));
 		demandDetail1.setCollectionAmount(BigDecimal.ZERO);
-		TaxHeadMaster taxHeadMaster1 = new TaxHeadMaster();
-		taxHeadMaster1.setCode("0003");
-	//	demandDetail1.setTaxHeadMaster(taxHeadMaster1);
+		demandDetail1.setTaxHeadMasterCode("0003");
 		demandDetails.add(demandDetail);
 		demandDetails.add(demandDetail1);
 		return demandDetails;

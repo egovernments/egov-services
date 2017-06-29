eae9505b16065b2965d5d6fcd2f47b3d57a30e0c
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-06-29 12:12:54 +0530

    committing the changes of the bugeGov-Services/EGSVC-424

--- propertytax/pt-property/src/main/java/org/egov/property/exception/ValidationUrlNotFoundException.java
+++ propertytax/pt-property/src/main/java/org/egov/property/exception/ValidationUrlNotFoundException.java
@@ -7,23 +7,21 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * Description : InvalidPropertyBoundary custom exception class
+ * Description : ValidationUrlNotFound custom exception class
  * 
  * @author Narendra
  *
  */
-
 @AllArgsConstructor
 @Getter
 @Setter
-public class InvalidPropertyBoundaryException extends RuntimeException {
+public class ValidationUrlNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
 	private String customMsg;
-	
+
 	private String msgDetails;
 
 	private RequestInfo requestInfo;
-
 }

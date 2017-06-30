8f944ec3eb31dee424ce3e3cd4bfa72a56757276
muralimgwl <murali.m@goodworklabs.com>
2017-06-30 11:37:04 +0530

    Updated projects

--- web/common-web/src/router.js
+++ web/common-web/src/router.js
@@ -4,17 +4,17 @@ import {Switch,Route} from 'react-router-dom';
 import PropertyTaxSearch from './components/contents/PropertyTaxSearch';
 import Login from './components/contents/Login';
 import Dashboard from './components/contents/Dashboard';
-import PropertyTaxCreate from './components/contents/CruProperty';
+import PropertyTaxCreate from '/property-tax-web/src/components/contents/CruProperty';
 
 
 const Main = () => (
   <main>
     <Switch>
-      <Route path='/propertyCreate' component={PropertyTaxCreate}/>
 
       <Route path='/propertyTaxSearch' component={PropertyTaxSearch}/>
       <Route exact path='/' component={Login}/>
       <Route exact path='/dashboard' component={Dashboard}/>
+
     </Switch>
   </main>
 )

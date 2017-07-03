fe378d507a73bf2f54f6a171bd38e7e3d76a6efb
Vishal Mahuli <vishalmahuli8@gmail.com>
2017-07-03 16:55:06 +0530

    minor change

--- collections/collection-services/src/main/resources/db/migration/main/V20170703163913__egcl_config_tables_ddl.sql
+++ collections/collection-services/src/main/resources/db/migration/main/V20170703163913__egcl_config_tables_ddl.sql
@@ -1,7 +1,7 @@
-CREATE SEQUENCE seq_egcl_clconfigurationValues;
-CREATE SEQUENCE seq_egcl_clconfiguration;
+CREATE SEQUENCE seq_egcl_configurationvalues;
+CREATE SEQUENCE seq_egcl_configuration;
 
-CREATE TABLE egcl_clconfiguration (
+CREATE TABLE egcl_configuration (
 	id BIGINT NOT NULL,
 	keyName CHARACTER VARYING(50) NOT NULL,
 	description CHARACTER VARYING(250),
@@ -11,10 +11,10 @@ CREATE TABLE egcl_clconfiguration (
 	lastModifiedDate DATE,
 	tenantId CHARACTER VARYING(250) NOT NULL,
 
-	CONSTRAINT pk_egcl_clconfiguration PRIMARY KEY (id)
+	CONSTRAINT pk_egcl_configuration PRIMARY KEY (id)
 );
 
-CREATE TABLE egcl_clconfigurationvalues (
+CREATE TABLE egcl_configurationvalues (
 	id BIGINT NOT NULL,
 	keyId BIGINT NOT NULL,
 	value CHARACTER VARYING(1000) NOT NULL,
@@ -25,5 +25,5 @@ CREATE TABLE egcl_clconfigurationvalues (
 	lastModifiedDate DATE,
 	tenantId CHARACTER VARYING(250) NOT NULL,
 
-	CONSTRAINT pk_egcl_clconfigurationvalues PRIMARY KEY (id)
+	CONSTRAINT pk_egcl_configurationvalues PRIMARY KEY (id)
 );

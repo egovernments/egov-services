1bdd21cf465342ef2cf7ff7c41efc04291c6e50c
sumanth <sumanth@egovernments.org>
2017-07-05 15:23:01 +0530

    [EGSVC-461] initial commit.

---
+++ pgr/thirdpartypoc/src/main/java/org/egov/poc/ThirdPartyPocApplication.java
@@ -0,0 +1,12 @@
+package org.egov.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdPartyPocApplication.class, args);
+	}
+}
---
+++ pgr/thirdpartypoc/src/test/java/org/egov/poc/ThirdPartyPocApplicationTests.java
@@ -0,0 +1,15 @@
+package org.egov.poc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThirdPartyPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}

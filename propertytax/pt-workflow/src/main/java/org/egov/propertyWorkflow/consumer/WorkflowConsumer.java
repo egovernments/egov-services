214d4783178b59d591302258775cced5ae4513cf
prasadkhandagale <prasad.khandagale@walkingtree.tech>
2017-07-06 12:57:21 +0530

    Initial commit of workflow & notification

---
+++ propertytax/pt-workflow/src/main/java/org/egov/propertyWorkflow/consumer/WorkflowConsumer.java
@@ -0,0 +1,96 @@
+package org.egov.propertyWorkflow.consumer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.egov.models.ProcessInstanceRequest;
+import org.egov.models.Property;
+import org.egov.models.PropertyRequest;
+import org.egov.models.WorkFlowDetails;
+import org.egov.models.WorkflowDetailsRequestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.stereotype.Service;
+
+/**
+ * 
+ * @author Debabrata
+ *
+ */
+
+@EnableKafka
+@Service
+public class WorkflowConsumer {
+	@Autowired
+	private WorkFlowUtil workflowUtil;
+
+	@Autowired
+	Environment environment;
+
+	@Bean
+	public Map<String, Object> consumerConfig() {
+		Map<String, Object> consumerProperties = new HashMap<String, Object>();
+		consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, environment.getProperty("auto.offset.reset"));
+		consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+				environment.getProperty("bootstrap.server.config"));
+		consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+		consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("consumer.groupId"));
+		return consumerProperties;
+	}
+
+	@Bean
+	public ConsumerFactory<String, PropertyRequest> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerConfig(), new StringDeserializer(),
+				new JsonDeserializer<>(PropertyRequest.class));
+
+	}
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, PropertyRequest> kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, PropertyRequest> factory = new ConcurrentKafkaListenerContainerFactory<String, PropertyRequest>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
+	}
+
+	@KafkaListener(topics = { "#{environment.getProperty('property.start.workflow')}",
+			"#{environment.getProperty('property.update.workflow')}" })
+	public void listen(ConsumerRecord<String, PropertyRequest> record) throws Exception {
+
+		PropertyRequest propertyRequest = record.value();
+
+		if (record.topic().equalsIgnoreCase(environment.getProperty("property.start.workflow"))) {
+
+			for (Property property : propertyRequest.getProperties()) {
+				WorkFlowDetails workflowDetails = property.getPropertyDetail().getWorkFlowDetails();
+				WorkflowDetailsRequestInfo workflowDetailsRequestInfo = new WorkflowDetailsRequestInfo();
+				workflowDetailsRequestInfo.setRequestInfo(propertyRequest.getRequestInfo());
+				workflowDetailsRequestInfo.setTenantId(property.getTenantId());
+				workflowDetailsRequestInfo.setWorkflowDetails(workflowDetails);
+				ProcessInstanceRequest processInstanceRequest = new ProcessInstanceRequest();
+				workflowUtil.startWorkflow(processInstanceRequest);
+			}
+		} else if (record.topic().equals(environment.getProperty("property.update.workflow"))) {
+			for (Property property : propertyRequest.getProperties()) {
+				WorkFlowDetails workflowDetails = property.getPropertyDetail().getWorkFlowDetails();
+				WorkflowDetailsRequestInfo workflowDetailsRequestInfo = new WorkflowDetailsRequestInfo();
+				workflowDetailsRequestInfo.setTenantId(property.getTenantId());
+				workflowDetailsRequestInfo.setWorkflowDetails(workflowDetails);
+				workflowDetailsRequestInfo.setRequestInfo(propertyRequest.getRequestInfo());
+				workflowUtil.updateWorkflow(workflowDetailsRequestInfo);
+			}
+		}
+
+	}
+}
---
+++ propertytax/pt-workflow/src/test/java/org/egov/processworkflow/service/ProcessWorkflowServiceTest.java
@@ -0,0 +1,175 @@
+package org.egov.processworkflow.service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.egov.models.Position;
+import org.egov.models.ProcessInstance;
+import org.egov.models.ProcessInstanceRequest;
+import org.egov.models.RequestInfo;
+import org.egov.models.RequestInfoWrapper;
+import org.egov.models.TaskResponse;
+import org.egov.models.UserAuthResponseInfo;
+import org.egov.models.WorkFlowDetails;
+import org.egov.models.WorkflowDetailsRequestInfo;
+import org.egov.propertyWorkflow.PtWorkflowApplication;
+import org.egov.propertyWorkflow.consumer.WorkFlowUtil;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = { PtWorkflowApplication.class })
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ProcessWorkflowServiceTest {
+
+	@Autowired
+	WorkFlowUtil workFlowUtil;
+
+	@Autowired
+	Environment env;
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@Test
+	public void startWorkflowTest() {
+
+		ProcessInstanceRequest processInstanceRequest = new ProcessInstanceRequest();
+		RequestInfoWrapper requestInfoWrapper = new RequestInfoWrapper();
+		RequestInfo requestInfo = new RequestInfo();
+		ProcessInstance processInstance = new ProcessInstance();
+		Position position = new Position();
+
+		position.setId(1l);
+
+		requestInfo.setAction("create");
+		requestInfo.setDid("1");
+		requestInfo.setMsgId("20170310130900");
+		requestInfo.setRequesterId("yosadhara");
+
+		requestInfo.setAuthToken("602a0808-fe7f-410f-b223-039616a385dd");
+		requestInfo.setApiId("");
+		requestInfo.setVer("1.0");
+		requestInfo.setTs("27-06-2017 10:30:12");
+		requestInfo.setKey("abcdkey");
+		requestInfo.setTenantId("default");
+
+		requestInfoWrapper.setRequestInfo(requestInfo);
+
+		processInstance.setBusinessKey("Property");
+		processInstance.setType("Property");
+		processInstance.setAssignee(position);
+		processInstance.setSenderName("manas");
+		processInstance.setComments("test");
+		processInstance.setTenantId("default");
+
+		requestInfoWrapper.setRequestInfo(requestInfo);
+
+		processInstanceRequest.setRequestInfo(requestInfoWrapper.getRequestInfo());
+		processInstanceRequest.setProcessInstance(processInstance);
+
+		try {
+
+			requestInfo.setAuthToken(getAuthToken());
+			processInstance = workFlowUtil.startWorkflow(processInstanceRequest);
+			if (processInstance == null)
+				assertTrue(false);
+
+			assertTrue(true);
+
+		} catch (Exception e) {
+			assertTrue(false);
+		}
+	}
+
+	@Test
+	public void updateWorkflowTest() {
+
+		String tenantId = "default";
+		RequestInfo requestInfo = getRequestInfoObject();
+		WorkflowDetailsRequestInfo workflowDetailsRequestInfo = new WorkflowDetailsRequestInfo();
+		WorkFlowDetails workflowDetails = new WorkFlowDetails();
+		workflowDetails.setDepartment("egov");
+		workflowDetails.setDesignation("test");
+		workflowDetails.setAssignee(1l);
+		workflowDetails.setAction("forward");
+		workflowDetails.setStatus("Assistant Approved");
+		workflowDetailsRequestInfo.setTenantId(tenantId);
+		workflowDetailsRequestInfo.setRequestInfo(requestInfo);
+		workflowDetailsRequestInfo.setWorkflowDetails(workflowDetails);
+
+		try {
+
+			TaskResponse taskResponse = workFlowUtil.updateWorkflow(workflowDetailsRequestInfo);
+			if (taskResponse == null)
+				assertTrue(false);
+
+			assertTrue(true);
+
+		} catch (Exception e) {
+			System.out.println("UPDATE Workflow Test: " + e.toString());
+			assertTrue(false);
+		}
+
+	}
+
+	private RequestInfo getRequestInfoObject() {
+
+		RequestInfo requestInfo = new RequestInfo();
+		requestInfo.setAction("create");
+		requestInfo.setDid("1");
+		requestInfo.setMsgId("20170310130900");
+		requestInfo.setRequesterId("yosadhara");
+		try {
+			requestInfo.setAuthToken(getAuthToken());
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		requestInfo.setApiId("");
+		requestInfo.setVer("1.0");
+		requestInfo.setTs("27-06-2017 10:30:12");
+		requestInfo.setKey("abcdkey");
+		requestInfo.setTenantId("default");
+
+		return requestInfo;
+	}
+
+	private String getAuthToken() throws Exception {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		headers.add("Authorization", env.getProperty("authkey"));
+
+		MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+		map.add("username", env.getProperty("oauth.username"));
+		map.add("password", env.getProperty("password"));
+		map.add("grant_type", env.getProperty("grant_type"));
+		map.add("scope", env.getProperty("scope"));
+		map.add("tenantId", "default");
+
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(map,
+				headers);
+
+		UserAuthResponseInfo userAuthResponseInfo = restTemplate.postForObject(env.getProperty("user.auth"),
+				requestEntity, UserAuthResponseInfo.class);
+
+		if (userAuthResponseInfo != null) {
+			return userAuthResponseInfo.getAccess_token();
+		}
+
+		return "";
+	}
+}

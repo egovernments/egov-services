2f3ac7a495a5097361917c7b0883141a6d36e590
ramki <ramakrishna@egovernments.org>
2017-07-06 19:01:04 +0530

    added context root and removed space in starting for the service folder name

---
+++ propertytax/pt-user-enrichment/src/main/java/org/egov/propertyUser/PtUserValidatorApplication.java
@@ -0,0 +1,12 @@
+package org.egov.propertyUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtUserValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtUserValidatorApplication.class, args);
+	}
+}

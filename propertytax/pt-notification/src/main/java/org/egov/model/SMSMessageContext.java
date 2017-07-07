df7334f280dca4406869a7f2bcbce5cfe15c4f3a
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-07-07 17:38:22 +0530

    committing the changes of the pt-notification service

---
+++ propertytax/pt-notification/src/main/java/org/egov/model/SMSMessageContext.java
@@ -0,0 +1,26 @@
+package org.egov.model;
+
+import java.util.Map;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+/**
+ * 
+ * @author Yosadhara
+ *
+ */
+public class SMSMessageContext {
+
+	private String templateName;
+
+	private Map<Object, Object> templateValues;
+}
---
+++ propertytax/pt-notification/src/main/java/org/egov/model/SmsMessage.java
@@ -0,0 +1,20 @@
+package org.egov.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+/**
+ * 
+ * @author Yosadhara
+ *
+ */
+public class SmsMessage {
+
+	private String message;
+
+	private String mobileNumber;
+}

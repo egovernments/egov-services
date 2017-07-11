b514784ceaa866863ed5f80352431e20f4c87f82
Senthil <senthil.kalimuthu@tarento.com>
2017-07-11 12:20:54 +0530

    report module will exit after it pushes to nexus

--- jenkins/code_builder.groovy
+++ jenkins/code_builder.groovy
@@ -1,8 +1,11 @@
 def build(path, ci_image){
     def build_workflow_exists = fileExists("${path}/build.wkflo");
+    def docker_file_exists = fileExists("${path}/Dockerfile")
     if (build_workflow_exists) {
         build_wkflo = load("${path}/build.wkflo")
         build_wkflo.build(path, ci_image)
+    } else if (docker_file_exists) {
+        return this
     } else {
         defaultMavenBuild(path, ci_image)
     }

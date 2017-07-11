10039e48f4bd07e23dc3a48c3d34ba609c8ae2e3
narendrabandhamneni <narendra.bandhamneni@walkingtree.tech>
2017-07-11 15:53:27 +0530

    tittle transfer producer

---
+++ propertytax/pt-property/src/main/java/org/egov/property/consumer/TitleTransferProducer.java
@@ -0,0 +1,82 @@
+package org.egov.property.consumer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.egov.models.TitleTransferRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * This class will use for sending  title transfer object to kafka server
+ * 
+ * @author S Anilkumar
+ */
+
+public class TitleTransferProducer {
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    KafkaTemplate<String, TitleTransferRequest> kafkaTemplate;
+
+    /**
+     * This method will return map object for producer configuration
+     */
+    @Bean
+    public Map<String, Object> producerConfig() {
+            Map<String, Object> producerProperties = new HashMap<String, Object>();
+            producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("kafka.config.bootstrap_server_config"));
+            producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+            return producerProperties;
+    }
+
+    /**
+     * This method will return producer factory bean based on producer
+     * configuration
+     */
+    @Bean
+    public ProducerFactory<String, TitleTransferRequest> producerFactory() {
+            return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    /**
+     * This method will return kafka template bean based on producer factory
+     * bean
+     */
+
+    @Bean
+    public KafkaTemplate<String, TitleTransferRequest> kafkaTemplate() {
+            return new KafkaTemplate<>(producerFactory());
+    }
+
+    /**
+     * This will be create rest template and returns rest object
+     * 
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+            return new RestTemplate();
+    }
+
+  /**
+   * This method will send titleTransfer object to kafka server
+   * @param topic
+   * @param titleTransferRequest
+   */
+
+    public void send(String topic, TitleTransferRequest titleTransferRequest) {
+            kafkaTemplate.send(topic, titleTransferRequest);
+    }
+    
+
+}
---
+++ propertytax/pt-property/src/main/java/org/egov/property/exception/PropertyUnderWorkflowException.java
@@ -0,0 +1,25 @@
+package org.egov.property.exception;
+
+import org.egov.models.RequestInfo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PropertyUnderWorkflowException  extends Exception{
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private String customMsg;
+    
+    private RequestInfo requestInfo;
+
+}

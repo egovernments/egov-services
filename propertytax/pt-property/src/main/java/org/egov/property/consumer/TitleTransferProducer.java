bbaded6166df60941eac2fe59208c2f321faf3ca
narendrabandhamneni <narendra.bandhamneni@walkingtree.tech>
2017-07-11 15:58:38 +0530

    added annotations for producer

--- propertytax/pt-property/src/main/java/org/egov/property/consumer/TitleTransferProducer.java
+++ propertytax/pt-property/src/main/java/org/egov/property/consumer/TitleTransferProducer.java
@@ -8,11 +8,13 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.egov.models.TitleTransferRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -21,6 +23,8 @@ import org.springframework.web.client.RestTemplate;
  * @author S Anilkumar
  */
 
+@Configuration
+@Service
 public class TitleTransferProducer {
     @Autowired
     private Environment environment;

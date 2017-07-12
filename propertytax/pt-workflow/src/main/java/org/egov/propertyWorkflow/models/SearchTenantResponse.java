a36d13ccfaa576eaccd2d01252277b98447445f1
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-07-12 18:43:48 +0530

    committing the upic no generation changes

---
+++ propertytax/pt-workflow/src/main/java/org/egov/propertyWorkflow/models/SearchTenantResponse.java
@@ -0,0 +1,18 @@
+package org.egov.propertyWorkflow.models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SearchTenantResponse {
+
+    private ResponseInfo responseInfo;
+    private List<Tenant> tenant;
+}

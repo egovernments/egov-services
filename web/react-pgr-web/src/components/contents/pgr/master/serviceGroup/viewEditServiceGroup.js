875b2d4d3cfee6d5c594dd9a64064be49d85d0a5
sourabh <sourabh.debnath@goodworklabs.com>
2017-07-12 12:59:28 +0530

    Fixed- Navigation from grievance type

--- web/react-pgr-web/src/components/contents/pgr/master/serviceGroup/viewEditServiceGroup.js
+++ web/react-pgr-web/src/components/contents/pgr/master/serviceGroup/viewEditServiceGroup.js
@@ -181,7 +181,7 @@ class ViewEditServiceGroup extends Component {
                         if(url == '/pgr/serviceGroup/view'){
                           this.props.history.push('/pgr/viewServiceGroup/'+e.id);
                         } else {
-                          this.props.history.push('/pgr/createServiceGroup/'+e.id);
+                          this.props.history.push('/pgr/updateServiceGroup/'+e.id);
                         }
                       }}>
                                               <td>{e.id}</td>

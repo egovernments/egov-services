ed8d15f01d7b78dbb239c84d694df059f1610b59
shashi <shashikanth.g@egovernments.org>
2017-07-12 18:44:20 +0530

    Added missing labels

---
+++ core/egov-localization/src/main/resources/db/migration/seed/V20170712183119__adding_missed_labels.sql
@@ -0,0 +1,11 @@
+    insert into message (id,locale,code,message,tenantid,module,createdby) 
+    values(nextval('SEQ_MESSAGE'),'en_IN','reports.pgr.position.name','Position Name','default','PGR',1);
+
+    insert into message (id,locale,code,message,tenantid,module,createdby) 
+    values(nextval('SEQ_MESSAGE'),'en_IN','reports.pgr.position1.name','Position1 Name','default','PGR',1);
+
+    insert into message (id,locale,code,message,tenantid,module,createdby) 
+    values(nextval('SEQ_MESSAGE'),'en_IN','reports.pgr.position2.name','Position2 Name','default','PGR',1);
+
+    insert into message (id,locale,code,message,tenantid,module,createdby) 
+    values(nextval('SEQ_MESSAGE'),'en_IN','reports.pgr.position3.name','Position3 Name','default','PGR',1);

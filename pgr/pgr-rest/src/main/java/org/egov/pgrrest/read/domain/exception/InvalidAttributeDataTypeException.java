0eafad2ecb89170b5a1a36289ad90c48580a67ee
Ajay Divakaran <ajay_886@hotmail.com>
2017-07-13 09:15:15 +0530

    Added message to custom exception

--- pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/domain/exception/InvalidAttributeDataTypeException.java
+++ pgr/pgr-rest/src/main/java/org/egov/pgrrest/read/domain/exception/InvalidAttributeDataTypeException.java
@@ -3,12 +3,14 @@ package org.egov.pgrrest.read.domain.exception;
 import lombok.Getter;
 
 public class InvalidAttributeDataTypeException extends RuntimeException {
+    private static final String EXCEPTION_MESSAGE = "Invalid data type '%s' for attribute definition '%s'";
     @Getter
     private final String invalidDataType;
     @Getter
     private final String attributeCode;
 
     public InvalidAttributeDataTypeException(String invalidDataType, String attributeCode) {
+        super(String.format(EXCEPTION_MESSAGE, invalidDataType, attributeCode));
         this.invalidDataType = invalidDataType;
         this.attributeCode = attributeCode;
     }

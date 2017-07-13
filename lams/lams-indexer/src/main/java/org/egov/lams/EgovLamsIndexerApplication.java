0e84e25b8bfd5ee150e3984cc85f9f0926896351
rajesh.m@riflexions.com <rajesh.m@riflexions.com>
2017-07-13 12:18:57 +0530

    tracer library update to lams-indexer

--- lams/lams-indexer/src/main/java/org/egov/lams/EgovLamsIndexerApplication.java
+++ lams/lams-indexer/src/main/java/org/egov/lams/EgovLamsIndexerApplication.java
@@ -4,27 +4,24 @@ import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
+@Import({ TracerConfiguration.class })
 public class EgovLamsIndexerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgovLamsIndexerApplication.class, args);
-	}
-
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
 	}
 
 	@Value("${app.timezone}")

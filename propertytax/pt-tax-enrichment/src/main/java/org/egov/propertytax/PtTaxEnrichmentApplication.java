ff05a5615e0746968e3cb0d7bd6fdab3238c9c71
Manasa Niranjan <manasa.niranjan@egovernments.org>
2017-07-17 19:10:17 +0530

    Demand integration for Property Tax

---
+++ propertytax/pt-tax-enrichment/src/main/java/org/egov/propertytax/PtTaxEnrichmentApplication.java
@@ -0,0 +1,30 @@
+package org.egov.propertytax;
+
+import org.egov.models.ResponseInfoFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class PtTaxEnrichmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtTaxEnrichmentApplication.class, args);
+	}
+
+	@Bean
+	public ResponseInfoFactory responseInfoFactory(){
+		return new ResponseInfoFactory();
+	}
+
+	/**
+	 * This method will create rest template object
+	 */
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}

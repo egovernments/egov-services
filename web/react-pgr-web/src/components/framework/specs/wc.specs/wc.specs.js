7193504e011784fbd28426b4cad60eda329ed0ac
sourabh <sourabh.debnath@goodworklabs.com>
2017-07-17 20:11:07 +0530

    Added- Wc specs

---
+++ web/react-pgr-web/src/components/framework/specs/wc.specs/wc.specs.js
@@ -0,0 +1,23 @@
+var dat = {
+	"wc.create": {
+		"numCols": 3,
+		"groups": [
+			{	
+				"label": "wc.create.applicantDetails.title"
+				"applicantDetails": {
+					"fields": [
+						{
+							"name": "AssessmentNumber",
+							"jsonPath": "connection.property.propertyIdentifier",
+							"label": "",
+							"pattern": "",
+							"type": "text"
+						}
+					] 
+				}
+			}
+		]
+	}
+}
+
+export default dat;

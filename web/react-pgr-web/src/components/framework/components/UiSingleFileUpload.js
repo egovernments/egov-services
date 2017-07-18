819535bc47ff53849beb1c126ef7e58b4ebc8f9d
sourabh <sourabh.debnath@goodworklabs.com>
2017-07-18 12:54:18 +0530

    Added- Single and multi file upload

---
+++ web/react-pgr-web/src/components/framework/components/UiSingleFileUpload.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import TextField from 'material-ui/TextField';
+
+export default class UiSingleFileUpload extends Component {
+	constructor(props) {
+       super(props);
+   	}
+
+	renderSingleFileUpload = (item) => {
+		switch (this.props.ui) {
+			case 'google': 
+				return (
+					<TextField 
+						fullWidth={true} 
+						type="file"
+						floatingLabelText={item.label + (item.isRequired ? " *" : "")} 
+						value={eval(item.jsonpath)}
+						disabled={item.isDisabled}
+						errorText={this.props.fieldErrors[eval(item.jsonpath)]}
+						onChange={(e) => this.props.handler(e, eval(item.jsonpath), item.isRequired ? true : false, '', item.requiredErrMsg, item.patternErrMsg)} />
+				);
+		}
+	}
+
+	render () {
+		return (
+	      <div>
+	        {this.renderSingleFileUpload(this.props.item)}
+	      </div>
+	    );
+	}
+}

a298d38ea15a5eac843d8ebd2784ad11f9948721
aslam <aslam.rahman@egovernments.org>
2017-07-19 18:32:21 +0530

    fetched address from latitude and longitude

--- web/react-pgr-web/src/components/common/viewSRN.js
+++ web/react-pgr-web/src/components/common/viewSRN.js
@@ -4,6 +4,7 @@ import {Card, CardHeader, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import FileDownload from 'material-ui/svg-icons/action/get-app';
 import {translate} from './common';
+var axios = require('axios');
 
 const styles = {
   headerStyle : {
@@ -18,10 +19,20 @@ const styles = {
   }
 };
 
-const viewsrn = ({srn}) => {
-  const filesUploaded = () =>{
-    if(srn.files != undefined){
-      return srn.files.map((files, index) => {
+class viewsrn extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      address : undefined
+    }
+  }
+  componentWillReceiveProps(nextprops){
+    if((nextprops.srn.lat !== 0 && nextprops.srn.lng !== 0) && (nextprops.srn.lat && nextprops.srn.lng))
+      this.getAddress(nextprops.srn.lat, nextprops.srn.lng);
+  }
+  filesUploaded = () =>{
+    if(this.props.srn.files != undefined){
+      return this.props.srn.files.map((files, index) => {
         return (
           <Col xs={6} md={3} key={index}>
             <RaisedButton
@@ -39,131 +50,143 @@ const viewsrn = ({srn}) => {
       });
     }
   }
-  return (
-    <Grid style={{width:'100%'}}>
-      <Card style={{margin:'15px 0'}}>
-        <CardHeader style={{paddingBottom:0}} title={< div style = {styles.headerStyle} >
-           {translate('pgr.lbl.srn')} : {srn.serviceRequestId}
-         < /div>}/>
-         <CardText style={{padding:'8px 16px 0'}}>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.add.name')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.firstName}
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.mobilenumber')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.phone ? srn.phone : 'N/A'}
-              </Col>
-            </Row>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.email.compulsory')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.email ? srn.email : 'N/A'}
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.address')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.requesterAddress ? srn.requesterAddress : 'N/A'}
-              </Col>
-            </Row>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.enter.aadharcard.number')}
-              </Col>
-              <Col xs={6} md={3}>
-                N/A
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.description')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.description}
-              </Col>
-            </Row>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('pgr.lbl.grievance.type')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.serviceName}
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.department')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.departmentName}
-              </Col>
-            </Row>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('pgr.lbl.registered.date')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.requestedDatetime}
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('pgr.lbl.nextescalation.date')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.expectedDatetime}
-              </Col>
-            </Row>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('pgr.lbl.filedvia')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.receivingMode}
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('pgr.lbl.receivingcenter')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.receivingCenterName ? srn.receivingCenterName :'N/A'}
-              </Col>
-            </Row>
-            <Row style={styles.addBorderBottom}>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.location')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.childLocationName ? srn.childLocationName + " - " + srn.locationName : srn.locationName}
-              </Col>
-              <Col xs={6} md={3}>
-                {translate('core.lbl.landmark')}
-              </Col>
-              <Col xs={6} md={3}>
-                {srn.address ? srn.address : 'N/A'}
-              </Col>
-            </Row>
-            {srn.externalCRN ?
-              <Row style={{padding:'10px'}}>
+  getAddress = (lat, lng) => {
+    var _this = this;
+    axios.post('https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&sensor=true')
+    .then(function (response) {
+      if(response.data.results.length > 0)
+        _this.setState({address : response.data.results[0].formatted_address});
+    });
+  }
+  render(){
+    return (
+      <Grid style={{width:'100%'}}>
+        <Card style={{margin:'15px 0'}}>
+          <CardHeader style={{paddingBottom:0}} title={< div style = {styles.headerStyle} >
+             {translate('pgr.lbl.srn')} : {this.props.srn.serviceRequestId}
+           < /div>}/>
+           <CardText style={{padding:'8px 16px 0'}}>
+              <Row style={styles.addBorderBottom}>
                 <Col xs={6} md={3}>
-                  External CRN
+                  {translate('core.lbl.add.name')}
                 </Col>
                 <Col xs={6} md={3}>
-                  {srn.externalCRN}
+                  {this.props.srn.firstName}
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.mobilenumber')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.phone ? this.props.srn.phone : 'N/A'}
                 </Col>
               </Row>
-            : ''}
-            <Row style={{padding:'10px'}}>
-              <Col xs={6} md={3}>
-                Files
-              </Col>
-              {filesUploaded()}
-            </Row>
-         </CardText>
-      </Card>
-    </Grid>
-  )
+              <Row style={styles.addBorderBottom}>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.email.compulsory')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.email ? this.props.srn.email : 'N/A'}
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.address')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.requesterAddress ? this.props.srn.requesterAddress : 'N/A'}
+                </Col>
+              </Row>
+              <Row style={styles.addBorderBottom}>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.enter.aadharcard.number')}
+                </Col>
+                <Col xs={6} md={3}>
+                  N/A
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.description')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.description}
+                </Col>
+              </Row>
+              <Row style={styles.addBorderBottom}>
+                <Col xs={6} md={3}>
+                  {translate('pgr.lbl.grievance.type')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.serviceName}
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.department')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.departmentName}
+                </Col>
+              </Row>
+              <Row style={styles.addBorderBottom}>
+                <Col xs={6} md={3}>
+                  {translate('pgr.lbl.registered.date')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.requestedDatetime}
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('pgr.lbl.nextescalation.date')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.expectedDatetime}
+                </Col>
+              </Row>
+              <Row style={styles.addBorderBottom}>
+                <Col xs={6} md={3}>
+                  {translate('pgr.lbl.filedvia')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.receivingMode}
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('pgr.lbl.receivingcenter')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.receivingCenterName ? this.props.srn.receivingCenterName :'N/A'}
+                </Col>
+              </Row>
+              <Row style={styles.addBorderBottom}>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.location')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.state.address ? this.state.address :
+                  this.props.srn.childLocationName ? this.props.srn.childLocationName + " - " + this.props.srn.locationName : this.props.srn.locationName
+                  }
+                </Col>
+                <Col xs={6} md={3}>
+                  {translate('core.lbl.landmark')}
+                </Col>
+                <Col xs={6} md={3}>
+                  {this.props.srn.address ? this.props.srn.address : 'N/A'}
+                </Col>
+              </Row>
+              {this.props.srn.externalCRN ?
+                <Row style={{padding:'10px'}}>
+                  <Col xs={6} md={3}>
+                    External CRN
+                  </Col>
+                  <Col xs={6} md={3}>
+                    {this.props.srn.externalCRN}
+                  </Col>
+                </Row>
+              : ''}
+              <Row style={{padding:'10px'}}>
+                <Col xs={6} md={3}>
+                  Files
+                </Col>
+                {this.filesUploaded()}
+              </Row>
+           </CardText>
+        </Card>
+      </Grid>
+    )
+  }
 }
 
 export default viewsrn;

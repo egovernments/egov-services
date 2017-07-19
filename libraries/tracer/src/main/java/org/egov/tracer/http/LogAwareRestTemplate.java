6baadade55861d5cd3c14c6d1a1ebed82987389a
Ajay Divakaran <ajay_886@hotmail.com>
2017-07-19 12:14:12 +0530

    Tracer - Log RestTemplate request/response only when body is json compatible.

--- libraries/tracer/src/main/java/org/egov/tracer/http/LogAwareRestTemplate.java
+++ libraries/tracer/src/main/java/org/egov/tracer/http/LogAwareRestTemplate.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.egov.tracer.config.TracerProperties;
 import org.egov.tracer.model.RequestContext;
+import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
@@ -12,7 +14,9 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 public class LogAwareRestTemplate extends RestTemplate {
@@ -25,7 +29,9 @@ public class LogAwareRestTemplate extends RestTemplate {
     private static final String UTF_8 = "UTF-8";
     private static final String RESPONSE_BODY_ERROR_MESSAGE = "Error reading response body";
     private static final String RESPONSE_CODE_ERROR_MESSAGE = "Error reading response code";
-    private static final String EMPTY_BODY = "<EMPTY BODY>";
+    private static final String EMPTY_BODY = "<NOT-AVAILABLE>";
+    private static final List<String> JSON_MEDIA_TYPES =
+        Arrays.asList(MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_JSON_VALUE);
     private TracerProperties tracerProperties;
 
     public LogAwareRestTemplate(TracerProperties tracerProperties) {
@@ -48,18 +54,23 @@ public class LogAwareRestTemplate extends RestTemplate {
     private ClientHttpResponse executeAndLogResponse(HttpRequest httpRequest, byte[] body, ClientHttpRequestExecution
         clientHttpRequestExecution) throws IOException {
         try {
-			final ClientHttpResponse rawResponse = clientHttpRequestExecution.execute(httpRequest, body);
-			if (tracerProperties.isDetailedTracingDisabled()) {
-				log.info(RESPONSE_MESSAGE, httpRequest.getURI());
-				return rawResponse;
-			}
-			final CacheableSimpleClientHttpResponse response = new CacheableSimpleClientHttpResponse(rawResponse);
-			logResponse(response, httpRequest.getURI());
-			return response;
-		} catch (Exception e) {
-			log.error(String.format(FAILED_RESPONSE_MESSAGE, httpRequest.getURI()), e);
-			throw e;
-		}
+            final ClientHttpResponse rawResponse = clientHttpRequestExecution.execute(httpRequest, body);
+            if (tracerProperties.isDetailedTracingDisabled() || isBodyNotCompatibleForParsing(httpRequest)) {
+                log.info(RESPONSE_MESSAGE, httpRequest.getURI());
+                return rawResponse;
+            }
+            final CacheableSimpleClientHttpResponse response = new CacheableSimpleClientHttpResponse(rawResponse);
+            logResponse(response, httpRequest.getURI());
+            return response;
+        } catch (Exception e) {
+            log.error(String.format(FAILED_RESPONSE_MESSAGE, httpRequest.getURI()), e);
+            throw e;
+        }
+    }
+
+    private boolean isBodyNotCompatibleForParsing(HttpMessage httpMessage) {
+        final MediaType contentType = httpMessage.getHeaders().getContentType();
+        return contentType == null || !JSON_MEDIA_TYPES.contains(contentType.getType());
     }
 
     private void logResponse(ClientHttpResponse response, URI uri) {
@@ -85,10 +96,10 @@ public class LogAwareRestTemplate extends RestTemplate {
     }
 
     private void logRequest(HttpRequest httpRequest, byte[] body) {
-        if (tracerProperties.isDetailedTracingEnabled()) {
-            log.info(REQUEST_MESSAGE_WITH_BODY, httpRequest.getURI(), httpRequest.getMethod().name(), getBody(body));
-        } else {
+        if (tracerProperties.isDetailedTracingDisabled() || isBodyNotCompatibleForParsing(httpRequest)) {
             log.info(REQUEST_MESSAGE, httpRequest.getURI(), httpRequest.getMethod().name());
+        } else {
+            log.info(REQUEST_MESSAGE_WITH_BODY, httpRequest.getURI(), httpRequest.getMethod().name(), getBody(body));
         }
     }
 

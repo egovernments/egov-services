c5f1b1d3aff9f1059c0ce18a15d8c4651c3aac1c
Manasa Niranjan <manasa.niranjan@egovernments.org>
2017-07-25 17:53:06 +0530

    Added loggers for dates

--- propertytax/pt-tax-enrichment/src/main/java/org/egov/propertytax/repository/BillingServiceRepository.java
+++ propertytax/pt-tax-enrichment/src/main/java/org/egov/propertytax/repository/BillingServiceRepository.java
@@ -14,6 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 @Repository
 public class BillingServiceRepository {

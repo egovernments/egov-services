c93b9381867a69e0ef9ea627c9b401cf5a3bd2a0
sanjeevnvp <sanjeev.p@egovernments.org>
2017-07-26 14:01:55 +0530

    [EGSVC-610]:removed not completed status and fix for actions in attribute definition

--- pgr/pgr-rest/src/main/java/org/egov/pgrrest/common/persistence/entity/AttributeActionsDefinitionKey.java
+++ pgr/pgr-rest/src/main/java/org/egov/pgrrest/common/persistence/entity/AttributeActionsDefinitionKey.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
 @EqualsAndHashCode
 public class AttributeActionsDefinitionKey implements Serializable {
 
+    private static final long serialVersionUID = 5033468783850139270L;
     @Column(name = "attributecode", nullable = false)
     private String attributeCode;
 
@@ -22,5 +23,8 @@ public class AttributeActionsDefinitionKey implements Serializable {
 
     @Column(name = "servicecode")
     private String serviceCode;
+
+    @Column(name = "name")
+    private String name;
 }
 

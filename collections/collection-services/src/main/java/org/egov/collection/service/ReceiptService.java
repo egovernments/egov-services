43b16ff89aeebe52647d320b87c7049ff9dc4b57
vishalmahuli <vishal.mahuli@tarento.com>
2017-07-28 21:28:17 +0530

    build failure fix

--- collections/collection-services/src/main/java/org/egov/collection/service/ReceiptService.java
+++ collections/collection-services/src/main/java/org/egov/collection/service/ReceiptService.java
@@ -60,7 +60,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
+//import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.*;
 

02c70a5d49b84ac70a91827053c3d048888186e3
aslam <aslam.rahman@egovernments.org>
2017-07-28 11:04:24 +0530

    OTP popup for anonymous issue fixed. Datetime datatype field supported

--- web/react-pgr-web/src/components/common/Fields.js
+++ web/react-pgr-web/src/components/common/Fields.js
@@ -4,12 +4,16 @@ import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
 import MaterialDateTimePicker from 'material-datetime-picker';
 import {translate} from './common';
 
 export default class Fields extends Component{
+  constructor(props){
+    super(props);
+    this.state={};
+  }
   renderFields = (obj) =>{
-    //console.log('came to renderfields',obj.code);
     let des = translate(obj.description);
     let mandatory = (obj.required == true) ? " *" : ""
     let description = des + mandatory;
@@ -18,25 +22,26 @@ export default class Fields extends Component{
         case "string":
           return (
             <Col xs={12} md={3}>
-              <TextField fullWidth={true} ref={obj.code} floatingLabelText={description} onChange={(e,newValue) => this.props.handler(newValue, obj.code, obj.required, '')} errorText={this.props.error ? this.props.error : ""}/>
+              <TextField fullWidth={true} ref={obj.code} floatingLabelText={description} value={this.props.value} onChange={(e,newValue) => this.props.handler(newValue, obj.code, obj.required, '')} errorText={this.props.error ? this.props.error : ""}/>
             </Col>
           );
         case "integer":
           return (
             <Col xs={12} md={3}>
-              <TextField fullWidth={true} ref={obj.code} floatingLabelText={description} onChange={(e,newValue) => this.props.handler(newValue, obj.code, obj.required, /^[+-]?\d+$/)} errorText={this.props.error ? this.props.error : ""} />
+              <TextField fullWidth={true} ref={obj.code} floatingLabelText={description} value={this.props.value} onChange={(e,newValue) => this.props.handler(newValue, obj.code, obj.required, /^[+-]?\d+$/)} errorText={this.props.error ? this.props.error : ""} />
             </Col>
           );
         case "double":
           return (
             <Col xs={12} md={3}>
-              <TextField fullWidth={true} ref={obj.code} floatingLabelText={description} onChange={(e,newValue) => this.props.handler(newValue, obj.code, obj.required, /^[+-]?\d+(\.\d+)?$/)} errorText={this.props.error ? this.props.error : ""}/>
+              <TextField fullWidth={true} ref={obj.code} floatingLabelText={description} value={this.props.value} onChange={(e,newValue) => this.props.handler(newValue, obj.code, obj.required, /^[+-]?\d+(\.\d+)?$/)} errorText={this.props.error ? this.props.error : ""}/>
             </Col>
           );
         case "date":
           return(
             <Col xs={12} md={3}>
-              <DatePicker fullWidth={true} ref={obj.code} floatingLabelText={description} formatDate={(date)=>{
+              <DatePicker fullWidth={true} ref={obj.code} floatingLabelText={description} value={this.props.value ? new Date(this.props.value.split('-')[2], this.props.value.split('-')[1]-1, this.props.value.split('-')[0]) : ''}
+              formatDate={(date)=>{
                 let dateObj = new Date(date);
                 let year = dateObj.getFullYear();
                 let month = dateObj.getMonth()+1;
@@ -58,8 +63,35 @@ export default class Fields extends Component{
             </Col>
           );
         case "datetime":
+        //console.log(this.props.value, this.props.value.split(' '));
+          let date = this.props.value.split(' ')[0];
+          let time = this.props.value.split(' ')[1];
+          //console.log((this.props.value && date && time) ? new Date(date.split('-')[2], date.split('-')[1]-1, date.split('-')[0], time.split(':')[0], time.split(':')[1], time.split(':')[2]) : '');
           return (
             <Col xs={12} md={3}>
+              <DatePicker fullWidth={true} ref={obj.code} floatingLabelText={description} value={(this.props.value && date && time) ? new Date(date.split('-')[2], date.split('-')[1]-1, date.split('-')[0], time.split(':')[0], time.split(':')[1], time.split(':')[2]) : ''}
+              formatDate={(date)=>{
+                let dateObj = new Date(date);
+                let year = dateObj.getFullYear();
+                let month = dateObj.getMonth()+1;
+                let dt = dateObj.getDate();
+                dt =  dt < 10 ? '0' + dt : dt;
+                month = month < 10 ? '0' + month : month;
+                return dt + '-' + month + '-' + year;
+              }}
+              onChange={(nothing,date) => {
+                let dateObj = new Date(date);
+                let year = dateObj.getFullYear();
+                let month = dateObj.getMonth()+1;
+                let dt = dateObj.getDate();
+                dt =  dt < 10 ? '0' + dt : dt;
+                month = month < 10 ? '0' + month : month;
+                this.props.handler(dt + '-' + month + '-' + year, obj.code, obj.required, '');
+                this.refs[obj.code+'timepicker'].openDialog();
+              }}/>
+              <TimePicker ref={obj.code+'timepicker'} format="24hr" style={{display: 'none'}} onChange={(nothing, time)=>{
+                this.props.handler(this.props.value+' '+time.getHours()+':'+time.getMinutes()+':00', obj.code, obj.required, '');
+              }}/>
             </Col>
           );
         case "singlevaluelist":
--- web/react-pgr-web/src/components/contents/pgr/grievanceCreate.js
+++ web/react-pgr-web/src/components/contents/pgr/grievanceCreate.js
@@ -412,7 +412,7 @@ class grievanceCreate extends Component {
     //console.log(JSON.stringify(request));
     var currentThis = this;
 
-    if(localStorage.getItem('type') === null){
+    if(localStorage.getItem('type') === null && this.state.otpEnabled){
 
       let mob = this.props.grievanceCreate.phone;
       let tenant =  localStorage.getItem('tenantId') ? localStorage.getItem('tenantId') : 'default';

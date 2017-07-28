827b882c2c9c372fa415bbe06ee99b6644c338b0
bharathtn <bharathtn12@gmail.com>
2017-07-28 12:34:42 +0530

    Added new masters

--- web/react-pgr-web/src/components/framework/specs/wc/master/categoryType.js
+++ web/react-pgr-web/src/components/framework/specs/wc/master/categoryType.js
@@ -81,6 +81,53 @@ var dat = {
 				]
 			}
 		]
+	},
+	"wc.view": {
+		"numCols": 12/3,
+		"url": "/wcms/masters/categorytype/_create",
+		"tenantIdRequired": true,
+		"objectName": "CategoryType",
+		"groups": [
+			{
+				"label": "wc.create.categorytype.title",
+				"name": "createCategoryType",
+				"fields": [
+						{
+							"name": "name",
+							"jsonPath": "CategoryType.name",
+							"label": "Category Type",
+							"pattern": "",
+							"type": "text",
+							"isRequired": true,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						},
+						{
+							"name": "description",
+							"jsonPath": "CategoryType.description",
+							"label": "Description",
+							"pattern": "",
+							"type": "text",
+							"isRequired": false,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						},
+						{
+							"name": "Active",
+							"jsonPath": "CategoryType.active",
+							"label": "Active",
+							"pattern": "",
+							"type": "checkbox",
+							"isRequired": false,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						}
+				]
+			}
+		]
 	}
 }
 

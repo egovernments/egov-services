d58e28fa744705061883dd4b20e31b11ac74aab2
Manoj Kulkarni <manoj.kulkarni@egovernments.org>
2017-07-28 16:54:46 +0530

    hr employee actions corrected

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170728165029__hr_employee_updating_action_scripts.sql
@@ -0,0 +1,3 @@
+update eg_action set url = '/hr-web/app/hr/employee/create.html' where name = 'CreateEmployee';
+update eg_action set url = '/hr-web/app/hr/common/employee-search.html' where name = 'UpdateEmployee';
+update eg_action set url = '/hr-web/app/hr/common/employee-search.html' where name = 'ViewEmployee';

8dbd03f9ed4cc7ebdd4af165756e8889a054bdef
Darshan Nagesh <darshan.nagesh@tarento.com>
2017-07-29 19:34:24 +0530

    Changes in the Role Action File

--- core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170729190913__wcms_connection_search.sql
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170729190913__wcms_connection_search.sql
@@ -1,6 +1,6 @@
 INSERT INTO eg_action (id, name, url, servicecode, queryparams, parentmodule, displayname, enabled, createdby, 
 createddate, lastmodifiedby, lastmodifieddate) VALUES (NEXTVAL('SEQ_EG_ACTION'),'Search Water Connection',
-'/wcms-connection/connection/_search','tenantId=&acknowledgementNumber=' 'WCMS','WCMS','Search Water Connection', false, 1, now() ,1 ,now()); 
+'/wcms-connection/connection/_search','tenantId=&acknowledgementNumber=','WCMS','WCMS','Search Water Connection', false, 1, now() ,1 ,now()); 
 
 INSERT INTO eg_roleaction (rolecode, actionid, tenantid) VALUES 
 ('SUPERUSER',(SELECT id FROM eg_action WHERE url='/wcms-connection/connection/_search'),'default');

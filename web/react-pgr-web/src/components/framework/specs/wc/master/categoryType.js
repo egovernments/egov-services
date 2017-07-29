3099e99dd010f2b88a08e56b3219ce88212876f6
bharathtn <bharathtn12@gmail.com>
2017-07-29 12:43:42 +0530

    Local Labels

--- web/react-pgr-web/src/components/framework/specs/wc/master/categoryType.js
+++ web/react-pgr-web/src/components/framework/specs/wc/master/categoryType.js
@@ -133,6 +133,55 @@ var dat = {
 	"wc.update": {
 		"numCols": 12/3,
 		"searchUrl": "/wcms/masters/categorytype/_search?id={id}",
+		"url":"/wcms/masters/categorytype/{CategoryTypes[0].code}/_update",
+		"tenantIdRequired": true,
+		"useTimestamp": true,
+		"objectName": "CategoryTypes",
+		"groups": [
+			{
+				"label": "wc.create.categorytype.title",
+				"name": "createCategoryType",
+				"fields": [
+						{
+							"name": "name",
+							"jsonPath": "CategoryTypes[0].name",
+							"label": "Category Type",
+							"pattern": "",
+							"type": "text",
+							"isRequired": true,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						},
+						{
+							"name": "description",
+							"jsonPath": "CategoryTypes[0].description",
+							"label": "Description",
+							"pattern": "",
+							"type": "text",
+							"isRequired": false,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						},
+						{
+							"name": "Active",
+							"jsonPath": "CategoryTypes[0].active",
+							"label": "Active",
+							"pattern": "",
+							"type": "checkbox",
+							"isRequired": false,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						}
+				]
+			}
+		]
+	},
+	"wc.search": {
+		"numCols": 12/3,
+		"searchUrl": "/wcms/masters/categorytype/_search?id={id}",
 		"tenantIdRequired": true,
 		"useTimestamp": true,
 		"objectName": "CategoryType",

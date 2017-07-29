51bfe390fff2ba90306b7e99ab4308a33a2ce763
Elzan Mathew <elzan.mathew@egovernments.org>
2017-07-29 23:41:26 +0530

    Updated the employee urls after removing context

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170729234008__hr_updateurl.sql
@@ -0,0 +1,3 @@
+update  eg_action set url='/app/hr/common/employee-search.html' where name='UpdateEmployee';
+update  eg_action set url='/app/hr/common/employee-search.html' where name='ViewEmployee';
+update eg_action set url='/app/hr/employee/create.html' where name='CreateEmployee';

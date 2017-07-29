bbcda51e87427eefc36736bf2caab387e64ea8c8
malathi <malathi@egovernments.org>
2017-07-29 16:45:42 -0700

    Escalation Master menutree displayname change.

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170729164318__pgr_escalationmaster_egactionupdate.sql
@@ -0,0 +1,4 @@
+UPDATE eg_action SET displayname = 'Create/Update Escalation' WHERE name='Create Escalation' and servicecode='ESCL' and url='/pgr-master/escalation/_create';
+
+UPDATE eg_action SET displayname = 'Create/Update Bulk Escalation' WHERE name='Search Escalation' and servicecode='ESCL' and url='/pgr-master/escalation/_search';
+

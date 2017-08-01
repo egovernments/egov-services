1e0bde8b2afae66fca979d9b6c4418b0339b0cc9
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2017-08-01 18:04:05 +0530

    Pushing data to ES for instrument and budget and added json for the same

---
+++ financials/egf-masters-indexer/mapping/budgetreappropriation/update_budgetreappropriation_mapping.sh
@@ -0,0 +1,1 @@
+curl -X PUT 'localhost:9200/budgetreappropriation/_mapping/budgetreappropriation?pretty' -H 'Content-Type: application/json' -d @update_budgetreappropriation_mapping.json
--- financials/egf-masters-indexer/src/main/java/org/egov/egf/master/index/consumer/IndexerListener.java
+++ financials/egf-masters-indexer/src/main/java/org/egov/egf/master/index/consumer/IndexerListener.java
@@ -3,6 +3,18 @@ package org.egov.egf.master.index.consumer;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
+import org.egov.egf.budget.web.contract.BudgetContract;
+import org.egov.egf.budget.web.contract.BudgetDetailContract;
+import org.egov.egf.budget.web.contract.BudgetDetailRequest;
+import org.egov.egf.budget.web.contract.BudgetReAppropriationContract;
+import org.egov.egf.budget.web.contract.BudgetReAppropriationRequest;
+import org.egov.egf.budget.web.contract.BudgetRequest;
+import org.egov.egf.instrument.web.contract.InstrumentAccountCodeContract;
+import org.egov.egf.instrument.web.contract.InstrumentContract;
+import org.egov.egf.instrument.web.contract.SurrenderReasonContract;
+import org.egov.egf.instrument.web.requests.InstrumentAccountCodeRequest;
+import org.egov.egf.instrument.web.requests.InstrumentRequest;
+import org.egov.egf.instrument.web.requests.SurrenderReasonRequest;
 import org.egov.egf.master.index.persistence.repository.ElasticSearchRepository;
 import org.egov.egf.master.web.contract.AccountCodePurposeContract;
 import org.egov.egf.master.web.contract.AccountDetailKeyContract;
@@ -76,6 +88,9 @@ public class IndexerListener {
     private static final String BUDGET_OBJECT_TYPE = "budget";
     private static final String BUDGETDETAIL_OBJECT_TYPE = "budgetdetail";
     private static final String BUDGETREAPPROPRIATION_OBJECT_TYPE = "budgetreappropriation";
+    private static final String INSTRUMENT_OBJECT_TYPE = "instrument";
+    private static final String INSTRUMENTACCOUNTCODE_OBJECT_TYPE = "instrumentaccountcode";
+    private static final String SURRENDERREASON_OBJECT_TYPE = "surrenderreason";
 
     private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
@@ -85,7 +100,9 @@ public class IndexerListener {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @KafkaListener(id = "${kafka.topics.egov.index.id}", topics = "${kafka.topics.egov.index.name}", group = "${kafka.topics.egov.index.group}")
+    @KafkaListener(id = "${kafka.topics.egov.index.id}", topics = { "${kafka.topics.egov.index.name}",
+            "${kafka.topics.egf.budget.completed.topic}",
+            "${kafka.topics.egf.instrument.completed.topic}" }, group = "${kafka.topics.egov.index.group}")
     public void listen(final HashMap<String, Object> financialContractRequestMap) {
 
         if (financialContractRequestMap.get("fund_persisted") != null) {
@@ -381,6 +398,291 @@ public class IndexerListener {
                 }
         }
 
+        if (financialContractRequestMap.get("budget_persisted") != null) {
+
+            final FinancialStatusRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("budget_persisted"),
+                    FinancialStatusRequest.class);
+
+            if (request.getFinancialStatuses() != null && !request.getFinancialStatuses().isEmpty())
+                for (final FinancialStatusContract financialStatusContract : request.getFinancialStatuses()) {
+                    final HashMap<String, Object> indexObj = getFinancialStatusContractIndexObject(financialStatusContract);
+                    elasticSearchRepository.index(FINANCIALSTATUS_OBJECT_TYPE,
+                            financialStatusContract.getTenantId() + "-" + financialStatusContract.getCode(), indexObj);
+                }
+        }
+
+        if (financialContractRequestMap.get("budgetreappropriation_persisted") != null) {
+
+            final BudgetReAppropriationRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("budgetreappropriation_persisted"),
+                    BudgetReAppropriationRequest.class);
+
+            if (request.getBudgetReAppropriations() != null && !request.getBudgetReAppropriations().isEmpty())
+                for (final BudgetReAppropriationContract budgetReAppropriationContract : request.getBudgetReAppropriations()) {
+                    final HashMap<String, Object> indexObj = getBudgetReAppropriationContractIndexObject(
+                            budgetReAppropriationContract);
+                    elasticSearchRepository.index(BUDGETREAPPROPRIATION_OBJECT_TYPE,
+                            budgetReAppropriationContract.getTenantId() + "-"
+                                    + budgetReAppropriationContract.getBudgetDetail().getBudget().getName(),
+                            indexObj);
+                }
+        }
+
+        if (financialContractRequestMap.get("budgetdetail_persisted") != null) {
+
+            final BudgetDetailRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("budgetdetail_persisted"),
+                    BudgetDetailRequest.class);
+
+            if (request.getBudgetDetails() != null && !request.getBudgetDetails().isEmpty())
+                for (final BudgetDetailContract budgetDetailContract : request.getBudgetDetails()) {
+                    final HashMap<String, Object> indexObj = getBudgetDetailContractIndexObject(budgetDetailContract);
+                    elasticSearchRepository.index(BUDGETDETAIL_OBJECT_TYPE,
+                            budgetDetailContract.getTenantId() + "-" + budgetDetailContract.getBudget().getName(), indexObj);
+                }
+        }
+
+        if (financialContractRequestMap.get("budget_persisted") != null) {
+
+            final BudgetRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("budget_persisted"),
+                    BudgetRequest.class);
+
+            if (request.getBudgets() != null && !request.getBudgets().isEmpty())
+                for (final BudgetContract budgetContract : request.getBudgets()) {
+                    final HashMap<String, Object> indexObj = getBudgetContractIndexObject(budgetContract);
+                    elasticSearchRepository.index(BUDGET_OBJECT_TYPE,
+                            budgetContract.getTenantId() + "-" + budgetContract.getName(), indexObj);
+                }
+        }
+
+        if (financialContractRequestMap.get("instrumentaccountcode_persisted") != null) {
+
+            final InstrumentAccountCodeRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("instrumentaccountcode_persisted"),
+                    InstrumentAccountCodeRequest.class);
+
+            if (request.getInstrumentAccountCodes() != null && !request.getInstrumentAccountCodes().isEmpty())
+                for (final InstrumentAccountCodeContract instrumentAccountCodeContract : request.getInstrumentAccountCodes()) {
+                    final HashMap<String, Object> indexObj = getInstrumentAccountContractIndexObject(
+                            instrumentAccountCodeContract);
+                    elasticSearchRepository.index(INSTRUMENTACCOUNTCODE_OBJECT_TYPE,
+                            instrumentAccountCodeContract.getTenantId() + "-"
+                                    + instrumentAccountCodeContract.getInstrumentType().getId(),
+                            indexObj);
+                }
+        }
+
+        if (financialContractRequestMap.get("instrument_persisted") != null) {
+
+            final InstrumentRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("instrument_persisted"),
+                    InstrumentRequest.class);
+
+            if (request.getInstruments() != null && !request.getInstruments().isEmpty())
+                for (final InstrumentContract instrumentContract : request.getInstruments()) {
+                    final HashMap<String, Object> indexObj = getInstrumentContractIndexObject(instrumentContract);
+                    elasticSearchRepository.index(INSTRUMENT_OBJECT_TYPE,
+                            instrumentContract.getTenantId() + "-" + instrumentContract.getTransactionNumber(), indexObj);
+                }
+        }
+
+        if (financialContractRequestMap.get("surrenderreason_persisted") != null) {
+
+            final SurrenderReasonRequest request = objectMapper.convertValue(
+                    financialContractRequestMap.get("surrenderreason_persisted"),
+                    SurrenderReasonRequest.class);
+
+            if (request.getSurrenderReasons() != null && !request.getSurrenderReasons().isEmpty())
+                for (final SurrenderReasonContract surrenderReasonContract : request.getSurrenderReasons()) {
+                    final HashMap<String, Object> indexObj = getSurrenderReasonContractIndexObject(surrenderReasonContract);
+                    elasticSearchRepository.index(SURRENDERREASON_OBJECT_TYPE,
+                            surrenderReasonContract.getTenantId() + "-" + surrenderReasonContract.getName(), indexObj);
+                }
+        }
+
+    }
+
+    private HashMap<String, Object> getInstrumentAccountContractIndexObject(
+            final InstrumentAccountCodeContract instrumentAccountCodeContract) {
+
+        final HashMap<String, Object> indexObj = new HashMap<String, Object>();
+        indexObj.put("id", instrumentAccountCodeContract.getId());
+        indexObj.put("instrumentType", instrumentAccountCodeContract.getInstrumentType());
+        indexObj.put("accountCode", instrumentAccountCodeContract.getAccountCode());
+        indexObj.put("tenantId", instrumentAccountCodeContract.getTenantId());
+        indexObj.put("createdBy", instrumentAccountCodeContract.getCreatedBy());
+        indexObj.put("lastModifiedBy", instrumentAccountCodeContract.getLastModifiedBy());
+
+        if (instrumentAccountCodeContract.getCreatedDate() != null)
+            indexObj.put("createdDate", formatter.format(instrumentAccountCodeContract.getCreatedDate()));
+        else
+            indexObj.put("createdDate", null);
+        if (instrumentAccountCodeContract.getLastModifiedDate() != null)
+            indexObj.put("lastModifiedDate", formatter.format(instrumentAccountCodeContract.getLastModifiedDate()));
+        else
+            indexObj.put("lastModifiedDate", null);
+
+        return indexObj;
+    }
+
+    private HashMap<String, Object> getInstrumentContractIndexObject(final InstrumentContract instrumentContract) {
+
+        final HashMap<String, Object> indexObj = new HashMap<String, Object>();
+        indexObj.put("id", instrumentContract.getId());
+        indexObj.put("transactionNumber", instrumentContract.getTransactionNumber());
+        if (instrumentContract.getTransactionDate() != null)
+            indexObj.put("transactionDate", formatter.format(instrumentContract.getTransactionDate()));
+        else
+            indexObj.put("transactionDate", null);
+        indexObj.put("amount", instrumentContract.getAmount());
+        indexObj.put("instrumentType", instrumentContract.getInstrumentType());
+        indexObj.put("bank", instrumentContract.getBank());
+        indexObj.put("branchName", instrumentContract.getBranchName());
+        indexObj.put("bankAccount", instrumentContract.getBankAccount());
+        indexObj.put("financialStatus", instrumentContract.getFinancialStatus());
+        indexObj.put("transactionType", instrumentContract.getTransactionType());
+        indexObj.put("payee", instrumentContract.getPayee());
+        indexObj.put("drawer", instrumentContract.getDrawer());
+        indexObj.put("surrenderReason", instrumentContract.getSurrenderReason());
+        indexObj.put("serialNo", instrumentContract.getSerialNo());
+        indexObj.put("instrumentVouchers", instrumentContract.getInstrumentVouchers());
+
+        indexObj.put("createdBy", instrumentContract.getCreatedBy());
+        indexObj.put("lastModifiedBy", instrumentContract.getLastModifiedBy());
+        indexObj.put("tenantId", instrumentContract.getTenantId());
+        if (instrumentContract.getCreatedDate() != null)
+            indexObj.put("createdDate", formatter.format(instrumentContract.getCreatedDate()));
+        else
+            indexObj.put("createdDate", null);
+        if (instrumentContract.getLastModifiedDate() != null)
+            indexObj.put("lastModifiedDate", formatter.format(instrumentContract.getLastModifiedDate()));
+        else
+            indexObj.put("lastModifiedDate", null);
+
+        return indexObj;
+    }
+
+    private HashMap<String, Object> getSurrenderReasonContractIndexObject(final SurrenderReasonContract surrenderReasonContract) {
+
+        final HashMap<String, Object> indexObj = new HashMap<String, Object>();
+        indexObj.put("id", surrenderReasonContract.getId());
+        indexObj.put("name", surrenderReasonContract.getName());
+        indexObj.put("description", surrenderReasonContract.getDescription());
+        indexObj.put("createdBy", surrenderReasonContract.getCreatedBy());
+        indexObj.put("lastModifiedBy", surrenderReasonContract.getLastModifiedBy());
+        indexObj.put("tenantId", surrenderReasonContract.getTenantId());
+        if (surrenderReasonContract.getCreatedDate() != null)
+            indexObj.put("createdDate", formatter.format(surrenderReasonContract.getCreatedDate()));
+        else
+            indexObj.put("createdDate", null);
+        if (surrenderReasonContract.getLastModifiedDate() != null)
+            indexObj.put("lastModifiedDate", formatter.format(surrenderReasonContract.getLastModifiedDate()));
+        else
+            indexObj.put("lastModifiedDate", null);
+
+        return indexObj;
+    }
+
+    private HashMap<String, Object> getBudgetReAppropriationContractIndexObject(
+            final BudgetReAppropriationContract budgetReAppropriationContract) {
+
+        final HashMap<String, Object> indexObj = new HashMap<String, Object>();
+        indexObj.put("id", budgetReAppropriationContract.getId());
+        indexObj.put("budgetDetail", budgetReAppropriationContract.getBudgetDetail());
+        indexObj.put("additionAmount", budgetReAppropriationContract.getAdditionAmount());
+        indexObj.put("asOnDate", budgetReAppropriationContract.getAsOnDate());
+        indexObj.put("deductionAmount", budgetReAppropriationContract.getDeductionAmount());
+        indexObj.put("originalAdditionAmount", budgetReAppropriationContract.getOriginalAdditionAmount());
+        indexObj.put("originalDeductionAmount", budgetReAppropriationContract.getOriginalDeductionAmount());
+        indexObj.put("anticipatoryAmount", budgetReAppropriationContract.getAnticipatoryAmount());
+        indexObj.put("status", budgetReAppropriationContract.getStatus());
+        indexObj.put("tenantId", budgetReAppropriationContract.getTenantId());
+        indexObj.put("createdBy", budgetReAppropriationContract.getCreatedBy());
+        indexObj.put("lastModifiedBy", budgetReAppropriationContract.getLastModifiedBy());
+
+        if (budgetReAppropriationContract.getCreatedDate() != null)
+            indexObj.put("createdDate", formatter.format(budgetReAppropriationContract.getCreatedDate()));
+        else
+            indexObj.put("createdDate", null);
+        if (budgetReAppropriationContract.getLastModifiedDate() != null)
+            indexObj.put("lastModifiedDate", formatter.format(budgetReAppropriationContract.getLastModifiedDate()));
+        else
+            indexObj.put("lastModifiedDate", null);
+
+        return indexObj;
+    }
+
+    private HashMap<String, Object> getBudgetDetailContractIndexObject(final BudgetDetailContract budgetDetailContract) {
+
+        final HashMap<String, Object> indexObj = new HashMap<String, Object>();
+        indexObj.put("id", budgetDetailContract.getId());
+        indexObj.put("budget", budgetDetailContract.getBudget());
+        indexObj.put("budgetGroup", budgetDetailContract.getBudgetGroup());
+        indexObj.put("usingDepartment", budgetDetailContract.getUsingDepartment());
+        indexObj.put("createdBy", budgetDetailContract.getCreatedBy());
+        indexObj.put("executingDepartment", budgetDetailContract.getExecutingDepartment());
+        indexObj.put("fund", budgetDetailContract.getFund());
+        indexObj.put("lastModifiedBy", budgetDetailContract.getLastModifiedBy());
+        indexObj.put("function", budgetDetailContract.getFunction());
+        indexObj.put("scheme", budgetDetailContract.getScheme());
+        indexObj.put("subScheme", budgetDetailContract.getSubScheme());
+        indexObj.put("functionary", budgetDetailContract.getFunctionary());
+        indexObj.put("boundary", budgetDetailContract.getBoundary());
+        indexObj.put("anticipatoryAmount", budgetDetailContract.getAnticipatoryAmount());
+        indexObj.put("originalAmount", budgetDetailContract.getOriginalAmount());
+        indexObj.put("approvedAmount", budgetDetailContract.getApprovedAmount());
+        indexObj.put("planningPercent", budgetDetailContract.getPlanningPercent());
+        indexObj.put("budgetAvailable", budgetDetailContract.getBudgetAvailable());
+        indexObj.put("status", budgetDetailContract.getStatus());
+        indexObj.put("documentNumber", budgetDetailContract.getDocumentNumber());
+        indexObj.put("uniqueNo", budgetDetailContract.getUniqueNo());
+        indexObj.put("materializedPath", budgetDetailContract.getMaterializedPath());
+
+        indexObj.put("tenantId", budgetDetailContract.getTenantId());
+
+        if (budgetDetailContract.getCreatedDate() != null)
+            indexObj.put("createdDate", formatter.format(budgetDetailContract.getCreatedDate()));
+        else
+            indexObj.put("createdDate", null);
+        if (budgetDetailContract.getLastModifiedDate() != null)
+            indexObj.put("lastModifiedDate", formatter.format(budgetDetailContract.getLastModifiedDate()));
+        else
+            indexObj.put("lastModifiedDate", null);
+
+        return indexObj;
+    }
+
+    private HashMap<String, Object> getBudgetContractIndexObject(final BudgetContract budgetContract) {
+
+        final HashMap<String, Object> indexObj = new HashMap<String, Object>();
+        indexObj.put("id", budgetContract.getId());
+        indexObj.put("name", budgetContract.getName());
+        indexObj.put("financialYear", budgetContract.getFinancialYear());
+        indexObj.put("estimationType", budgetContract.getEstimationType());
+        indexObj.put("createdBy", budgetContract.getCreatedBy());
+        indexObj.put("parent", budgetContract.getParent());
+        indexObj.put("primaryBudget", budgetContract.getPrimaryBudget());
+        indexObj.put("lastModifiedBy", budgetContract.getLastModifiedBy());
+        indexObj.put("active", budgetContract.getActive());
+        indexObj.put("referenceBudget", budgetContract.getReferenceBudget());
+        indexObj.put("tenantId", budgetContract.getTenantId());
+        indexObj.put("documentNumber", budgetContract.getDocumentNumber());
+        indexObj.put("status", budgetContract.getStatus());
+        indexObj.put("materializedPath", budgetContract.getMaterializedPath());
+        indexObj.put("description", budgetContract.getDescription());
+
+        if (budgetContract.getCreatedDate() != null)
+            indexObj.put("createdDate", formatter.format(budgetContract.getCreatedDate()));
+        else
+            indexObj.put("createdDate", null);
+        if (budgetContract.getLastModifiedDate() != null)
+            indexObj.put("lastModifiedDate", formatter.format(budgetContract.getLastModifiedDate()));
+        else
+            indexObj.put("lastModifiedDate", null);
+
+        return indexObj;
     }
 
     private HashMap<String, Object> getFundIndexObject(final FundContract fundContract) {

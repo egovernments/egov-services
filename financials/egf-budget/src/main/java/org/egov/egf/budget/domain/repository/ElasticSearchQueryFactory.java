d84fbcb4f62771edfe9f87981b03fbfa726a4696
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2017-08-07 16:21:24 +0530

    adding method to contract repository to fetch data from for search

---
+++ financials/egf-budget/src/main/java/org/egov/egf/budget/domain/repository/ElasticSearchQueryFactory.java
@@ -0,0 +1,48 @@
+package org.egov.egf.budget.domain.repository;
+
+import org.egov.egf.budget.web.contract.BudgetSearchContract;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
+
+@Service
+public class ElasticSearchQueryFactory {
+
+    @Autowired
+    private ElasticSearchUtils elasticSearchUtils;
+
+    public BoolQueryBuilder searchBudget(BudgetSearchContract budgetSearchContract) {
+        BoolQueryBuilder boolQueryBuilder = boolQuery();
+        elasticSearchUtils.add(budgetSearchContract.getDescription(), "description", boolQueryBuilder);
+        return boolQueryBuilder;
+    }
+
+
+    public List<String> prepareOrderBys(String sortBy) {
+        List<String> orderByList = new ArrayList<String>();
+        List<String> sortByList = new ArrayList<String>();
+        if (sortBy.contains(",")) {
+                sortByList = Arrays.asList(sortBy.split(","));
+        } else {
+                sortByList = Arrays.asList(sortBy);
+        }
+        for (String s : sortByList) {
+                if (s.contains(" ") && (s.toLowerCase().trim().endsWith("asc") || s.toLowerCase().trim().endsWith("desc"))) {
+                    orderByList.add(s.trim());
+                }
+                else {
+                    
+                    orderByList.add(s.trim() + " asc");
+                }
+        }
+        
+        return orderByList;
+    }
+
+}
--- financials/egf-master/src/main/java/org/egov/egf/master/web/repository/FinancialConfigurationContractRepository.java
+++ financials/egf-master/src/main/java/org/egov/egf/master/web/repository/FinancialConfigurationContractRepository.java
@@ -9,36 +9,44 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class FinancialConfigurationContractRepository {
 
-	private RestTemplate restTemplate;
-	private String hostUrl;
-	public static final String SEARCH_URL = "/egf-master/financialconfigurations/_search?";
+    private RestTemplate restTemplate;
+    private String hostUrl;
+    public static final String SEARCH_URL = "/egf-master/financialconfigurations/_search?";
 
-	public FinancialConfigurationContractRepository(@Value("${egf.master.host.url}") String hostUrl,
-			RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-		this.hostUrl = hostUrl;
-	}
+    @Value("${fetch_data_from}")
+    private String fetchDataFrom;
 
-	public FinancialConfigurationContract findById(FinancialConfigurationContract financialConfigurationContract) {
+    public FinancialConfigurationContractRepository(@Value("${egf.master.host.url}") String hostUrl,
+            RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+        this.hostUrl = hostUrl;
+    }
 
-		String url = String.format("%s%s", hostUrl, SEARCH_URL);
-		StringBuffer content = new StringBuffer();
-		if (financialConfigurationContract.getId() != null) {
-			content.append("id=" + financialConfigurationContract.getId());
-		}
+    public FinancialConfigurationContract findById(FinancialConfigurationContract financialConfigurationContract) {
 
-		if (financialConfigurationContract.getTenantId() != null) {
-			content.append("&tenantId=" + financialConfigurationContract.getTenantId());
-		}
-		url = url + content.toString();
-		FinancialConfigurationResponse result = restTemplate.postForObject(url, null,
-				FinancialConfigurationResponse.class);
+        String url = String.format("%s%s", hostUrl, SEARCH_URL);
+        StringBuffer content = new StringBuffer();
+        if (financialConfigurationContract.getId() != null) {
+            content.append("id=" + financialConfigurationContract.getId());
+        }
 
-		if (result.getFinancialConfigurations() != null && result.getFinancialConfigurations().size() == 1) {
-			return result.getFinancialConfigurations().get(0);
-		} else {
-			return null;
-		}
+        if (financialConfigurationContract.getTenantId() != null) {
+            content.append("&tenantId=" + financialConfigurationContract.getTenantId());
+        }
+        url = url + content.toString();
+        FinancialConfigurationResponse result = restTemplate.postForObject(url, null,
+                FinancialConfigurationResponse.class);
 
-	}
+        if (result.getFinancialConfigurations() != null && result.getFinancialConfigurations().size() == 1) {
+            return result.getFinancialConfigurations().get(0);
+        } else {
+            return null;
+        }
+
+    }
+
+    public String fetchDataFrom() {
+        return fetchDataFrom;
+    }
+
 }

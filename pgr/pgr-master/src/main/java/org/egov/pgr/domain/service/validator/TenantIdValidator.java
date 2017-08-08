a52f433a49a5ed5b0a00710f79b7af47af31a36c
shashi <shashikanth.g@egovernments.org>
2017-08-08 14:45:17 +0530

    [EGSVC-656]:ServiceType configuration search endpoint;

--- pgr/pgr-master/src/main/java/org/egov/pgr/domain/service/validator/TenantIdValidator.java
+++ pgr/pgr-master/src/main/java/org/egov/pgr/domain/service/validator/TenantIdValidator.java
@@ -3,6 +3,7 @@ package org.egov.pgr.domain.service.validator;
 import org.egov.pgr.domain.exception.ServiceCodeMandatoryException;
 import org.egov.pgr.domain.exception.TenantIdMandatoryException;
 import org.egov.pgr.domain.model.ServiceTypeConfiguration;
+import org.egov.pgr.domain.model.ServiceTypeConfigurationSearchCriteria;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,6 +17,18 @@ public class TenantIdValidator implements ServiceTypeConfigurationValidator {
     @Override
     public void validate(ServiceTypeConfiguration serviceTypeConfiguration) {
         if(serviceTypeConfiguration.isTenantIdAbsent()){
+            throw new TenantIdMandatoryException();
+        }
+    }
+    
+    @Override
+    public boolean canValidater(ServiceTypeConfigurationSearchCriteria serviceTypeConfigurationSearchCriteria) {
+        return true;
+    }
+
+    @Override
+    public void validater(ServiceTypeConfigurationSearchCriteria serviceTypeConfigurationSearchCriteria) {
+        if(serviceTypeConfigurationSearchCriteria.getTenantId().isEmpty() && serviceTypeConfigurationSearchCriteria.getTenantId()== null){
             throw new TenantIdMandatoryException();
         }
     }

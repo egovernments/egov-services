a0420fe2f8cd460aa0bcaac0e95e97cce4804c95
mansi <mansi.bansal@egovernments.org>
2017-08-09 17:05:26 +0530

    "deleted collection report data script"

--- core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170809165004__update_collection_report_data.sql
+++
@@ -1,4 +0,0 @@
-update eg_action set enabled=true where name='GetReportData';
-update eg_action set enabled=true where name='GetReportMetadata';
-update eg_action set enabled=true where name='ReloadReport';
-

6f6401e33c4db4a63eae55001b715e33546bb32c
sanjeevnvp <sanjeev.p@egovernments.org>
2017-08-09 19:49:52 +0530

    build failure fix

--- core/egov-workflow/src/main/java/org/egov/workflow/domain/service/PgrWorkflow.java
+++ core/egov-workflow/src/main/java/org/egov/workflow/domain/service/PgrWorkflow.java
@@ -100,7 +100,13 @@ public class PgrWorkflow implements Workflow {
     private Long resolveAssignee(final ProcessInstance processInstance) {
         final String complaintTypeCode = processInstance.getValueForKey("complaintTypeCode");
         String boundaryIdValue = processInstance.getValueForKey("boundaryId");
-        Long boundaryId = (processInstance.getValueForKey("boundaryId") == "" || processInstance.getValueForKey("boundaryId") == null) ? null : Long.valueOf(boundaryIdValue);
+        Long boundaryId;
+        if (processInstance.getValueForKey("boundaryId") == "" || processInstance.getValueForKey("boundaryId") == null) {
+            boundaryId = null;
+        }
+        else {
+            boundaryId = Long.valueOf(boundaryIdValue);
+        }
         final Long firstTimeAssignee = null;
         final PositionResponse response = complaintRouterService.getAssignee(boundaryId, complaintTypeCode,
             firstTimeAssignee, processInstance.getTenantId());

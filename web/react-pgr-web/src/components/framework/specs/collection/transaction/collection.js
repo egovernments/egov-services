f4592b95c3379cc7e8d61f9cad36af0475b45e14
Murali <muralim4242@users.noreply.github.com>
2017-08-10 17:47:00 +0530

    Added menu links and updated collection screen

--- web/react-pgr-web/src/components/framework/specs/collection/transaction/collection.js
+++ web/react-pgr-web/src/components/framework/specs/collection/transaction/collection.js
@@ -9,21 +9,20 @@ var cashOrMops = {
     "multiple": false, //If true, its an array
     "children": [],
     "fields": [{
-        "name": "paidBy",
-        "jsonPath": "collection.modeOfPayment.paidBy",
-        "label": "Paid By",
-        "pattern": "",
-        "type": "text",
-        "isRequired": true,
-        "isDisabled": false,
-        "requiredErrMsg": "", //Remove required messages
-        "patternErrMsg": ""
-      }
-    ]
+      "name": "paidBy",
+      "jsonPath": "collection.modeOfPayment.paidBy",
+      "label": "Paid By",
+      "pattern": "",
+      "type": "text",
+      "isRequired": true,
+      "isDisabled": false,
+      "requiredErrMsg": "", //Remove required messages
+      "patternErrMsg": ""
+    }]
   }]
 }
 
-var chequeOrDD={
+var chequeOrDD = {
   "name": "chequeOrDD",
   "version": "v1", //Maps to parent version
   "level": 1,
@@ -33,6 +32,7 @@ var chequeOrDD={
     "name": "chequeOrDD",
     "multiple": false, //If true, its an array
     "children": [],
+    "hide": false,
     "fields": [{
         "name": "chequeOrDDNumber",
         "jsonPath": "Receipt[0].instrument.transactionNumber",
@@ -44,42 +44,40 @@ var chequeOrDD={
         "requiredErrMsg": "",
         "patternErrMsg": ""
       },
-			{
-	        "name": "chequeOrDDDate",
-	        "jsonPath": "Receipt[0].instrument.transactionDate",
-	        "label": "Cheque/DD Date",
-	        "pattern": "",
-	        "type": "datePicker",
-	        "isRequired": true,
-	        "isDisabled": false,
-	        "requiredErrMsg": "",
-	        "patternErrMsg": ""
-	      }
-			,
-			{
-					"name": "chequeOrDDBankName",
-					"jsonPath": "Receipt[0].instrument.bank.id",
-					"label": "Cheque/DD Bank Name",
-					"pattern": "",
-					"type": "autoCompelete",
-					"isRequired": true,
-					"isDisabled": false,
-					"requiredErrMsg": "",
-					"patternErrMsg": "",
-          "url":"egf-masters/banks/_search?|$..id|$..name"
-				}
-				,
-				{
-						"name": "chequeOrDDBranchName",
-						"jsonPath": "Receipt[0].instrument.branchName",
-						"label": "Cheque/DD Branch Name",
-						"pattern": "",
-						"type": "text",
-						"isRequired": false,
-						"isDisabled": false,
-						"requiredErrMsg": "",
-						"patternErrMsg": ""
-					}
+      {
+        "name": "chequeOrDDDate",
+        "jsonPath": "Receipt[0].instrument.transactionDate",
+        "label": "Cheque/DD Date",
+        "pattern": "",
+        "type": "datePicker",
+        "isRequired": true,
+        "isDisabled": false,
+        "requiredErrMsg": "",
+        "patternErrMsg": ""
+      },
+      {
+        "name": "chequeOrDDBankName",
+        "jsonPath": "Receipt[0].instrument.bank.id",
+        "label": "Cheque/DD Bank Name",
+        "pattern": "",
+        "type": "autoCompelete",
+        "isRequired": true,
+        "isDisabled": false,
+        "requiredErrMsg": "",
+        "patternErrMsg": "",
+        "url": "egf-masters/banks/_search?|$..id|$..name"
+      },
+      {
+        "name": "chequeOrDDBranchName",
+        "jsonPath": "Receipt[0].instrument.branchName",
+        "label": "Cheque/DD Branch Name",
+        "pattern": "",
+        "type": "text",
+        "isRequired": false,
+        "isDisabled": false,
+        "requiredErrMsg": "",
+        "patternErrMsg": ""
+      }
     ]
   }]
 }
@@ -214,70 +212,70 @@ var dat = {
       ]
     }],
     "result": {
-      "header": [
-      {
-        "name": "businessService",
-        "jsonPath": "businessService",
-        "label": "Biller Service Name",
-        "pattern": "",
-        "type": "label",
-        "isRequired": true,
-        "isDisabled": false,
-        "requiredErrMsg": "",
-        "patternErrMsg": "",
-        "isLabel":false
-      },
-      {
-        "name": "consumerCode",
-        "jsonPath": "consumerCode",
-        "label": "Consumer code",
-        "pattern": "",
-        "type": "label",
-        "isRequired": true,
-        "isDisabled": false,
-        "requiredErrMsg": "",
-        "patternErrMsg": "",
-        "isLabel":false
-      },
-      {
-        "name": "totalAmount",
-        "jsonPath": "totalAmount",
-        "label": "Amount Due",
-        "pattern": "",
-        "type": "label",
-        "isRequired": true,
-        "isDisabled": false,
-        "requiredErrMsg": "",
-        "patternErrMsg": "",
-        "isLabel":false
-      },
-      {
-        "name": "minimumAmount",
-        "jsonPath": "minimumAmount",
-        "label": "Minimum amount payable",
-        "pattern": "",
-        "type": "label",
-        "isRequired": true,
-        "isDisabled": false,
-        "requiredErrMsg": "",
-        "patternErrMsg": "",
-        "isLabel":false
-      },
-      {
-        "name": "amountPaid",
-        "jsonPath": "amountPaid",
-        "label": "Amount paid(Rs)",
-        "pattern": "",
-        "type": "text",
-        "isRequired": true,
-        "isDisabled": false,
-        "requiredErrMsg": "",
-        "patternErrMsg": "",
-        "isLabel":false
-      }],
+      "header": [{
+          "name": "businessService",
+          "jsonPath": "businessService",
+          "label": "Biller Service Name",
+          "pattern": "",
+          "type": "label",
+          "isRequired": true,
+          "isDisabled": false,
+          "requiredErrMsg": "",
+          "patternErrMsg": "",
+          "isLabel": false
+        },
+        {
+          "name": "consumerCode",
+          "jsonPath": "consumerCode",
+          "label": "Consumer code",
+          "pattern": "",
+          "type": "label",
+          "isRequired": true,
+          "isDisabled": false,
+          "requiredErrMsg": "",
+          "patternErrMsg": "",
+          "isLabel": false
+        },
+        {
+          "name": "totalAmount",
+          "jsonPath": "totalAmount",
+          "label": "Amount Due",
+          "pattern": "",
+          "type": "label",
+          "isRequired": true,
+          "isDisabled": false,
+          "requiredErrMsg": "",
+          "patternErrMsg": "",
+          "isLabel": false
+        },
+        {
+          "name": "minimumAmount",
+          "jsonPath": "minimumAmount",
+          "label": "Minimum amount payable",
+          "pattern": "",
+          "type": "label",
+          "isRequired": true,
+          "isDisabled": false,
+          "requiredErrMsg": "",
+          "patternErrMsg": "",
+          "isLabel": false
+        },
+        {
+          "name": "amountPaid",
+          "jsonPath": "amountPaid",
+          "label": "Amount paid(Rs)",
+          "pattern": "^\\d+(\\.\\d+)?$",
+          "type": "text",
+          "isRequired": true,
+          "isDisabled": false,
+          "requiredErrMsg": "",
+          "patternErrMsg": "",
+          "isLabel": false
+        }
+      ],
       // "values": ["businessService", "consumerCode", "totalAmount","minimumAmount","bill"],
       "resultPath": "Bill[0].billDetails",
-      "tableResultPath":"Receipt[0].Bill[0].billDetails",
+      "tableResultPath": "Receipt[0].Bill[0].billDetails",
 
       // "rowClickUrlUpdate": "/update/wc/pipeSize/{id}",
       // "rowClickUrlView": "/view/wc/pipeSize/{id}"
@@ -287,63 +285,73 @@ var dat = {
       "name": "paymentMode",
       "children": [chequeOrDD],
       "fields": [
-				// {
-				// 	"name": "totalAmountPaid",
-				// 	"jsonPath": "Receipt[0].instrument.amount",
-				// 	"label": "Total Amount Paid",
-				// 	"pattern": "",
-				// 	"type": "label",
-				// 	"isRequired": false,
-				// 	"isDisabled": false,
-				// 	"isHidden": false,
-				// 	"defaultValue": "",
-				// 	"requiredErrMsg": "",
-				// 	"patternErrMsg": ""
-				// },
-				{
-        "name": "modeOfPayment",
-        "jsonPath": "Receipt[0].instrument.instrumentType.name",
-        "label": "Mode Of Payment",
-        "pattern": "",
-        "type": "singleValueList",
-        "isRequired": false,
-        "isDisabled": false,
-        "url": "",
-        "requiredErrMsg": "",
-        "patternErrMsg": "",
-        "defaultValue": [{
-            "key": "",
-            "value": null
-          },
-          {
-            "key": "1",
-            "value": "Cash"
-          },
-          {
-            "key": "2",
-            "value": "Check"
-          },
-          {
-            "key": "3",
-            "value": "DD"
-          },
-          // {
-          //   "key": "4",
-          //   "value": "Credit/Debit Card"
-          // },
-          // {
-          //   "key": "5",
-          //   "value": "Direct Bank"
-          // },
-          // {
-          //   "key": "6",
-          //   "value": "SBI MOPS Bank Callan"
-          // }
-        ]
-      },
-      {
+        // {
+        // 	"name": "totalAmountPaid",
+        // 	"jsonPath": "Receipt[0].instrument.amount",
+        // 	"label": "Total Amount Paid",
+        // 	"pattern": "",
+        // 	"type": "label",
+        // 	"isRequired": false,
+        // 	"isDisabled": false,
+        // 	"isHidden": false,
+        // 	"defaultValue": "",
+        // 	"requiredErrMsg": "",
+        // 	"patternErrMsg": ""
+        // },
+        {
+          "name": "modeOfPayment",
+          "jsonPath": "Receipt[0].instrument.instrumentType.name",
+          "label": "Mode Of Payment",
+          "pattern": "",
+          "type": "singleValueList",
+          "isRequired": false,
+          "isDisabled": false,
+          "url": "",
+          "requiredErrMsg": "",
+          "patternErrMsg": "",
+          "showHideFields": [{
+            "ifValue": "Cheque",
+            "hide": [],
+            "show": [{
+              "name": "chequeOrDD",
+              "isGroup": true,
+              "isField": false
+            }]
+          }],
+
+          "defaultValue": [{
+              "key": "",
+              "value": null
+            },
+            {
+              "key": "Cash",
+              "value": "Cash"
+            },
+            {
+              "key": "Cheque",
+              "value": "Cheque"
+            },
+            {
+              "key": "DD",
+              "value": "DD"
+            },
+            // {
+            //   "key": "4",
+            //   "value": "Credit/Debit Card"
+            // },
+            // {
+            //   "key": "5",
+            //   "value": "Direct Bank"
+            // },
+            // {
+            //   "key": "6",
+            //   "value": "SBI MOPS Bank Callan"
+            // }
+          ]
+        },
+        {
           "name": "paidBy",
-          "jsonPath": "Receipt[0].Bill[0].payeeName",
+          "jsonPath": "Receipt[0].Bill[0].paidBy",
           "label": "Paid By",
           "pattern": "",
           "type": "text",
@@ -352,7 +360,7 @@ var dat = {
           "requiredErrMsg": "", //Remove required messages
           "patternErrMsg": ""
         }
-    ]
+      ]
     }]
   }
 }

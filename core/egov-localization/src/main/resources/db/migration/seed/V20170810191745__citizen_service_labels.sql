dcadb0699994b1effb24ab40355d615d5d976630
sanjeevnvp <sanjeev.p@egovernments.org>
2017-08-10 19:23:13 +0530

    added citizen service labels

---
+++ core/egov-localization/src/main/resources/db/migration/seed/V20170810191745__citizen_service_labels.sql
@@ -0,0 +1,8 @@
+insert into message (id,locale,code,message,tenantid,module,createdby)
+values(nextval('SEQ_MESSAGE'),'en_IN','csv.lbl.myrequest','My Request','default','CSV',1);
+
+insert into message (id,locale,code,message,tenantid,module,createdby)
+values(nextval('SEQ_MESSAGE'),'en_IN','csv.lbl.services','Services','default','CSV',1);
+
+insert into message (id,locale,code,message,tenantid,module,createdby)
+values(nextval('SEQ_MESSAGE'),'en_IN','csv.lbl.underprocess','Your application is received and is under process','default','CSV',1);

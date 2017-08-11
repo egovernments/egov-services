d18fe72f4aa223afcd763dda942e53fa24ddf575
mansi <mansi.bansal@egovernments.org>
2017-08-11 11:20:52 +0530

    "updated collection roleaction data for reports"

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20170811111913__update_collection_report_roleaction.sql
@@ -0,0 +1,3 @@
+update service set name='Collection Transactions' where name='Collection-Transactions' and tenantId='default';
+update service set name='Collection Masters' where name='Collection-Masters' and tenantId='default';
+update service set name='Collection Reports' where name='Collection-Reports' and tenantId='default';

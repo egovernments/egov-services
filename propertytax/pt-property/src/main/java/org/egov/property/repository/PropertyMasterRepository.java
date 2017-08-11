b65d13db54a07f0c02ce10120556e814e192533c
shwetha <shwetha.dc@egovernments.org>
2017-08-11 16:00:25 +0530

    [EGSVC-409] reverted file

--- propertytax/pt-property/src/main/java/org/egov/property/repository/PropertyMasterRepository.java
+++ propertytax/pt-property/src/main/java/org/egov/property/repository/PropertyMasterRepository.java
@@ -391,9 +391,6 @@ public class PropertyMasterRepository {
 			propertyType.setNameLocal(propertyData.getNameLocal());
 			propertyType.setActive(propertyData.getActive());
 			propertyType.setOrderNumber(propertyData.getOrderNumber());
-
-
-
 		}
 		return propertyTypes;
 	}

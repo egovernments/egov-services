7f1e13211d1358e14ec4344f999e96770dea4c94
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-08-13 02:42:44 +0530

    committed FeeMatrix Controller RequestMapping changes

--- tradelicense/tl-masters/src/main/java/org/egov/tradelicense/web/controller/FeeMatrixController.java
+++ tradelicense/tl-masters/src/main/java/org/egov/tradelicense/web/controller/FeeMatrixController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 @RestController
-@RequestMapping(path = "/feematrix")
+@RequestMapping(path = "/feematrix/v1")
 public class FeeMatrixController {
 
 	@Autowired

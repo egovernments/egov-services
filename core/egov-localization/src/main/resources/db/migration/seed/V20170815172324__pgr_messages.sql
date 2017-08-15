a23f4e9368d16df02d5cb812ba6506e014b1ef48
sanjeevnvp <sanjeev.p@egovernments.org>
2017-08-15 17:30:53 +0530

    [EGSVC-864] : added labels create grievance

---
+++ core/egov-localization/src/main/resources/db/migration/seed/V20170815172324__pgr_messages.sql
@@ -0,0 +1,8 @@
+insert into message (id,locale,code,message,tenantid,module,createdby)
+values(nextval('SEQ_MESSAGE'),'en_IN','pgr.lbl.invalidcrn','Invalid CRN','default','PGR',1);
+
+insert into message (id,locale,code,message,tenantid,module,createdby)
+values(nextval('SEQ_MESSAGE'),'en_IN','csv.lbl.loading','loading....','default','CSV',1);
+
+insert into message (id,locale,code,message,tenantid,module,createdby)
+values(nextval('SEQ_MESSAGE'),'en_IN','csv.lbl.noservices','No Services Found','default','CSV',1);

7fb0aefd5c9c0bd9e92d286d7ebca8a36e705a7b
Manoj Kulkarni <manoj.kulkarni@egovernments.org>
2017-08-16 12:38:44 +0530

    TL Number generation logic

---
+++ tradelicense/tl-services/src/main/java/org/egov/tradelicense/domain/service/TradeLicenseNumberGeneratorService.java
@@ -0,0 +1,16 @@
+package org.egov.tradelicense.domain.service;
+
+/**
+ * Generator class for Trade License Number
+ * 
+ * @author Manoj Kulkarni
+ *
+ */
+public interface TradeLicenseNumberGeneratorService {
+    /**
+     * Method to generate Trade License Number
+     * 
+     * @return generated number
+     */
+    public String generate();
+}

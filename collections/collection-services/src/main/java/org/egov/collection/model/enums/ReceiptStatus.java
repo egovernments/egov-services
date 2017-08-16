ca1d5fe5aa0eccbac3cf01951592d9c2c35c0e14
vishalmahuli <vishal.mahuli@tarento.com>
2017-08-16 17:38:50 +0530

    minor changew

--- collections/collection-services/src/main/java/org/egov/collection/model/enums/ReceiptStatus.java
+++ collections/collection-services/src/main/java/org/egov/collection/model/enums/ReceiptStatus.java
@@ -44,8 +44,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ReceiptStatus {
-    SUBMITTED("Submitted"),APPROVED("Approved"),REMITTED("Remitted"),
-    REJECTED("Rejected"),CANCELLED("Cancelled"),TOBESUBMITTED("TO BE SUBMITTED");
+    APPROVED("Approved"),REMITTED("Remitted"), APPROVALPENDING("Approval Pending"),
+    REJECTED("Rejected"),CANCELLED("Cancelled"),TOBESUBMITTED("To be Submitted");
 
     
 	private String value;

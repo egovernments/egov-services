593062abb6c77cedc6572a2ce97b0dbf70e20027
Senthil <senthil.kalimuthu@tarento.com>
2017-08-16 16:06:21 +0530

    Adding build config for tl-indexer

---
+++ tradelicense/tl-indexer/start.sh
@@ -0,0 +1,7 @@
+/Users/senthil/eGov/source/egov-services/tradelicense/tl-indexer/start.sh#!/bin/sh
+
+if [[ -z "${JAVA_OPTS}" ]];then
+    export JAVA_OPTS="-Xmx64m -Xms64m"
+fi
+
+java ${JAVA_OPTS} -jar /opt/egov/tl-indexer.jar

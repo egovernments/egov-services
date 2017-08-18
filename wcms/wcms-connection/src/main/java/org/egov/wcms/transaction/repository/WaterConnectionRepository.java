d564c08e7e6425f4ac65582b3c15f660fe2c4170
Darshan Nagesh <darshan.nagesh@tarento.com>
2017-08-18 17:41:57 +0530

    Adding Validations for Location Insert in Water Connection without property

--- wcms/wcms-connection/src/main/java/org/egov/wcms/transaction/repository/WaterConnectionRepository.java
+++ wcms/wcms-connection/src/main/java/org/egov/wcms/transaction/repository/WaterConnectionRepository.java
@@ -265,23 +265,23 @@ public class WaterConnectionRepository {
 					PreparedStatement statement = connection.prepareStatement(persistConnectionAddressQuery,
 							returnValColumn);
 					statement.setString(1, conn.getTenantId());
-					statement.setDouble(2, conn.getAddress().getLatitude()); 
-					statement.setDouble(3, conn.getAddress().getLongitude());
-					statement.setString(4, conn.getAddress().getAddressId());
-					statement.setString(5, conn.getAddress().getAddressNumber());
-					statement.setString(6, conn.getAddress().getAddressLine1());
-					statement.setString(7, conn.getAddress().getAddressLine2());
-					statement.setString(8, conn.getAddress().getLandMark());
-					statement.setString(9, conn.getAddress().getDoorNo());
-					statement.setString(10, conn.getAddress().getCity());
-					statement.setString(11, conn.getAddress().getPinCode());
-					statement.setString(12, conn.getAddress().getDetail());
-					statement.setString(13, conn.getAddress().getRoute());
-					statement.setString(14, conn.getAddress().getStreet());
-					statement.setString(15, conn.getAddress().getArea());
-					statement.setString(16, conn.getAddress().getRoadName());
+					statement.setDouble(2, (null!=conn.getAddress().getLatitude() && conn.getAddress().getLatitude()>0)? conn.getAddress().getLatitude() : 0); 
+					statement.setDouble(3, (null!=conn.getAddress().getLongitude() && conn.getAddress().getLongitude()>0)? conn.getAddress().getLongitude() : 0);
+					statement.setString(4, (null!=conn.getAddress().getAddressId() && !conn.getAddress().getAddressId().isEmpty())? conn.getAddress().getAddressId() : "" );
+					statement.setString(5, (null!=conn.getAddress().getAddressNumber() && !conn.getAddress().getAddressNumber().isEmpty())? conn.getAddress().getAddressNumber() : "" );
+					statement.setString(6, (null!=conn.getAddress().getAddressLine1() && !conn.getAddress().getAddressLine1().isEmpty())? conn.getAddress().getAddressLine1() : "" );
+					statement.setString(7, (null!=conn.getAddress().getAddressLine2() && !conn.getAddress().getAddressLine2().isEmpty())? conn.getAddress().getAddressLine2() : "" );
+					statement.setString(8, (null!=conn.getAddress().getLandMark() && !conn.getAddress().getLandMark().isEmpty())? conn.getAddress().getLandMark() : "" );
+					statement.setString(9, (null!=conn.getAddress().getDoorNo() && !conn.getAddress().getDoorNo().isEmpty())? conn.getAddress().getDoorNo() : "" );
+					statement.setString(10, (null!=conn.getAddress().getCity() && !conn.getAddress().getCity().isEmpty())? conn.getAddress().getCity() : "" );
+					statement.setString(11, (null!=conn.getAddress().getPinCode() && !conn.getAddress().getPinCode().isEmpty())? conn.getAddress().getPinCode() : "" );
+					statement.setString(12, (null!=conn.getAddress().getDetail() && !conn.getAddress().getDetail().isEmpty())? conn.getAddress().getDetail() : "" );
+					statement.setString(13, (null!=conn.getAddress().getRoute() && !conn.getAddress().getRoute().isEmpty())? conn.getAddress().getRoute() : "" );
+					statement.setString(14, (null!=conn.getAddress().getStreet() && !conn.getAddress().getStreet().isEmpty())? conn.getAddress().getStreet() : "" );
+					statement.setString(15, (null!=conn.getAddress().getArea() && !conn.getAddress().getArea().isEmpty())? conn.getAddress().getArea() : "" );
+					statement.setString(16, (null!=conn.getAddress().getRoadName() && !conn.getAddress().getRoadName().isEmpty())? conn.getAddress().getRoadName() : "" );
 					statement.setLong(17, waterConnectionReq.getRequestInfo().getUserInfo().getId());
-					statement.setString(18, String.valueOf(new Date(new java.util.Date().getTime()).getTime()));
+					statement.setDate(18, new Date(new java.util.Date().getTime()));
 					return statement;
 				}
 			}, keyHolder);
@@ -308,7 +308,7 @@ public class WaterConnectionRepository {
 					statement.setLong(2, conn.getConnectionLocation().getLocationBoundary().getId()); 
 					statement.setLong(3, conn.getConnectionLocation().getAdminBoundary().getId());
 					statement.setLong(4, waterConnectionReq.getRequestInfo().getUserInfo().getId());
-					statement.setString(5, String.valueOf(new Date(new java.util.Date().getTime()).getTime()));
+					statement.setDate(5, new Date(new java.util.Date().getTime()));
 					return statement;
 				}
 			}, keyHolder);

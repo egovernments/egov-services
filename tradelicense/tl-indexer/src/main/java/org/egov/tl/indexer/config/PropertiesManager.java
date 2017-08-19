ee1441fc02c7466c6c7804ca8cf579b437871daf
PavanWTC-PC\PavanWTC <pavan.kumar@walkingtree.tech>
2017-08-19 17:15:32 +0530

    committing the tl-indexer search mapping of ids with names changes

--- tradelicense/tl-indexer/src/main/java/org/egov/tl/indexer/config/PropertiesManager.java
+++ tradelicense/tl-indexer/src/main/java/org/egov/tl/indexer/config/PropertiesManager.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
@@ -24,34 +23,53 @@ public class PropertiesManager {
 
 	@Value("${egov.services.tl-services.create.legacy.tradelicense.validated}")
 	private String createLegacyTradeValidated;
-	
+
 	@Value("${es.host}")
 	private String esHost;
-	
+
 	@Value("${es.port}")
 	private String esPort;
 
 	@Value("${es.multiThread.indicator}")
 	private String isMultithreaded;
-	
+
 	@Value("${es.timeout}")
 	private String timeOut;
-	
+
 	@Value("${tradelicense.es.index.name}")
 	private String esIndex;
-	
+
 	@Value("${tradelicense.es.index.type}")
 	private String esIndexType;
-	
+
 	@Value("${auto.offset.reset.config}")
 	private String kafkaOffsetConfig;
-	
+
 	@Value("${spring.kafka.bootstrap.servers}")
 	private String kafkaServerConfig;
 
+	@Value("${egov.services.tl-masters_v1.hostname}")
+	private String tradeLicenseMasterServiceHostName;
 
-	
-	
+	@Value("${egov.services.tl-masters_v1.basepath}")
+	private String tradeLicenseMasterServiceBasePath;
 
+	@Value("${egov.services.tl-masters_v1.category.searchpath}")
+	private String categoryServiceSearchPath;
+
+	@Value("${egov.services.tl-masters_v1.uom.searchpath}")
+	private String uomServiceSearchPath;
+
+	@Value("${egov.services.tl-masters_v1.status.searchpath}")
+	private String statusServiceSearchPath;
+
+	@Value("${egov.services.egov-location.hostname}")
+	private String locationServiceHostName;
+
+	@Value("${egov.services.egov-location.basepath}")
+	private String locationServiceBasePath;
+
+	@Value("${egov.services.egov-location.searchpath}")
+	private String locationServiceSearchPath;
 
 }
---
+++ tradelicense/tl-indexer/src/main/java/org/egov/tl/indexer/web/repository/CategoryContractRepository.java
@@ -0,0 +1,143 @@
+package org.egov.tl.indexer.web.repository;
+
+import java.util.Date;
+
+import org.egov.tl.commons.web.requests.CategoryResponse;
+import org.egov.tl.commons.web.requests.LicenseStatusResponse;
+import org.egov.tl.commons.web.requests.RequestInfoWrapper;
+import org.egov.tl.commons.web.requests.UOMResponse;
+import org.egov.tl.indexer.config.PropertiesManager;
+import org.modelmapper.ModelMapper;
+import org.rgov.tl.indexer.web.requests.TlMasterRequestInfo;
+import org.rgov.tl.indexer.web.requests.TlMasterRequestInfoWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class CategoryContractRepository {
+
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private PropertiesManager propertiesManger;
+
+	public CategoryContractRepository(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+
+	}
+
+	public CategoryResponse findByCategoryIds(String tenantId, String ids, RequestInfoWrapper requestInfoWrapper) {
+		String hostUrl = propertiesManger.getTradeLicenseMasterServiceHostName()
+				+ propertiesManger.getTradeLicenseMasterServiceBasePath();
+		String searchUrl = propertiesManger.getCategoryServiceSearchPath();
+		String url = String.format("%s%s", hostUrl, searchUrl);
+		StringBuffer content = new StringBuffer();
+		if (ids != null) {
+			content.append("ids=" + ids);
+		}
+
+		if (tenantId != null) {
+			content.append("&tenantId=" + tenantId);
+		}
+		url = url + content.toString();
+		TlMasterRequestInfoWrapper tlMasterRequestInfoWrapper = getTlMasterRequestInfoWrapper(requestInfoWrapper);
+		CategoryResponse categoryResponse = null;
+
+		try {
+
+			categoryResponse = restTemplate.postForObject(url, tlMasterRequestInfoWrapper, CategoryResponse.class);
+		} catch (Exception e) {
+			log.error("Error while connecting to the category end point");
+		}
+		if (categoryResponse != null && categoryResponse.getCategories() != null
+				&& categoryResponse.getCategories().size() > 0) {
+			return categoryResponse;
+		} else {
+			return null;
+		}
+
+	}
+
+	public UOMResponse findByUomIds(String tenantId, String ids, RequestInfoWrapper requestInfoWrapper) {
+
+		String hostUrl = propertiesManger.getTradeLicenseMasterServiceHostName()
+				+ propertiesManger.getTradeLicenseMasterServiceBasePath();
+		String searchUrl = propertiesManger.getUomServiceSearchPath();
+		String url = String.format("%s%s", hostUrl, searchUrl);
+		StringBuffer content = new StringBuffer();
+		if (ids != null) {
+			content.append("ids=" + ids);
+		}
+
+		if (tenantId != null) {
+			content.append("&tenantId=" + tenantId);
+		}
+		url = url + content.toString();
+		TlMasterRequestInfoWrapper tlMasterRequestInfoWrapper = getTlMasterRequestInfoWrapper(requestInfoWrapper);
+		UOMResponse uomResponse = null;
+
+		try {
+
+			uomResponse = restTemplate.postForObject(url, tlMasterRequestInfoWrapper, UOMResponse.class);
+		} catch (Exception e) {
+			log.error("Error while connecting to the category end point");
+		}
+		if (uomResponse != null && uomResponse.getUoms() != null && uomResponse.getUoms().size() > 0) {
+			return uomResponse;
+		} else {
+			return null;
+		}
+
+	}
+
+	public LicenseStatusResponse findByStatusIds(String tenantId, String ids, RequestInfoWrapper requestInfoWrapper) {
+
+		String hostUrl = propertiesManger.getTradeLicenseMasterServiceHostName()
+				+ propertiesManger.getTradeLicenseMasterServiceBasePath();
+		String searchUrl = propertiesManger.getStatusServiceSearchPath();
+		String url = String.format("%s%s", hostUrl, searchUrl);
+		StringBuffer content = new StringBuffer();
+		if (ids != null) {
+			content.append("ids=" + ids);
+		}
+
+		if (tenantId != null) {
+			content.append("&tenantId=" + tenantId);
+		}
+		url = url + content.toString();
+		TlMasterRequestInfoWrapper tlMasterRequestInfoWrapper = getTlMasterRequestInfoWrapper(requestInfoWrapper);
+		LicenseStatusResponse licenseStatusResponse = null;
+
+		try {
+
+			licenseStatusResponse = restTemplate.postForObject(url, tlMasterRequestInfoWrapper,
+					LicenseStatusResponse.class);
+		} catch (Exception e) {
+			log.error("Error while connecting to the category end point");
+		}
+		if (licenseStatusResponse != null && licenseStatusResponse.getLicenseStatuses() != null
+				&& licenseStatusResponse.getLicenseStatuses().size() > 0) {
+			return licenseStatusResponse;
+		} else {
+			return null;
+		}
+
+	}
+
+	public TlMasterRequestInfoWrapper getTlMasterRequestInfoWrapper(RequestInfoWrapper requestInfoWrapper) {
+
+		TlMasterRequestInfoWrapper tlMasterRequestInfoWrapper = new TlMasterRequestInfoWrapper();
+		TlMasterRequestInfo tlMasterRequestInfo = new TlMasterRequestInfo();
+		ModelMapper mapper = new ModelMapper();
+		mapper.map(requestInfoWrapper.getRequestInfo(), tlMasterRequestInfo);
+		tlMasterRequestInfo.setTs(new Date().getTime());
+		tlMasterRequestInfoWrapper.setRequestInfo(tlMasterRequestInfo);
+
+		return tlMasterRequestInfoWrapper;
+	}
+
+}

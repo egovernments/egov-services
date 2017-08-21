2543b6cf371f9dee8f59e76fe13da773d426a458
Raman Pandey <raman.pandey@egovernments.org>
2017-08-21 18:29:47 +0530

    added test cases for depreciation and revaluation

--- asset/asset-services/src/main/java/org/egov/asset/contract/DepreciationResponse.java
+++ asset/asset-services/src/main/java/org/egov/asset/contract/DepreciationResponse.java
@@ -7,14 +7,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
+@ToString
 public class DepreciationResponse {
-	
-	private ResponseInfo responseInfo;
-	
-	private Depreciation depreciation;
+
+    private ResponseInfo responseInfo;
+
+    private Depreciation depreciation;
 }

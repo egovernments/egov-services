9ff6c0986e6b2f0e40b7eef72adb7128f7234c44
vishalmahuli <vishal.mahuli@tarento.com>
2017-08-21 12:42:28 +0530

    dml additions

--- collections/collection-services/src/main/resources/db/migration/main/V20170819154001__alter_colltype_configvalues_ddl.sql
+++ collections/collection-services/src/main/resources/db/migration/main/V20170819154001__alter_colltype_configvalues_ddl.sql
@@ -1,3 +1,1 @@
-alter table egcl_receiptheader alter column collectiontype set not null; 
-
-update egcl_configurationvalues set tenantId = 'inactivetenant' where value = 'PREAPPROVED';
+alter table egcl_receiptheader alter column collectiontype set not null;

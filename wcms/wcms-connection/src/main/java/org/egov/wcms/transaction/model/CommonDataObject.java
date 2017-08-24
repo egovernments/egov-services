0d47b15c67ca19e67f3cb431d5a2c257d2fa8709
Darshan Nagesh <darshan.nagesh@tarento.com>
2017-08-24 23:44:33 +0530

    Adding the Model object

---
+++ wcms/wcms-connection/src/main/java/org/egov/wcms/transaction/model/CommonDataObject.java
@@ -0,0 +1,69 @@
+package org.egov.wcms.transaction.model;
+
+public class CommonDataObject {
+
+	private String code;
+	private String data;
+	private String name;
+	private String active;
+	private Long parent;
+	private String nameLocal;
+	private String description;
+	private Long orderNumber;
+	private AuditDetails auditDetails;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+	public Long getParent() {
+		return parent;
+	}
+	public void setParent(Long parent) {
+		this.parent = parent;
+	}
+	public String getNameLocal() {
+		return nameLocal;
+	}
+	public void setNameLocal(String nameLocal) {
+		this.nameLocal = nameLocal;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(Long orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public AuditDetails getAuditDetails() {
+		return auditDetails;
+	}
+	public void setAuditDetails(AuditDetails auditDetails) {
+		this.auditDetails = auditDetails;
+	}
+
+}

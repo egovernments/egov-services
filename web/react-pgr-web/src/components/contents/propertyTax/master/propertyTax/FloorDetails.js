56181b0620b195ae300040e95bd6879444050f3a
vineetm30 <vineet.mishra@goodworklabs.com>
2017-08-27 13:29:22 +0530

    property level occupancy date issue handled

--- web/react-pgr-web/src/components/contents/propertyTax/master/propertyTax/FloorDetails.js
+++ web/react-pgr-web/src/components/contents/propertyTax/master/propertyTax/FloorDetails.js
@@ -1122,7 +1122,7 @@ handleUsage = (value) => {
 																		  value: val
 																	  }
 																	}
-																	 this.handleAge(e.target.value);
+																handleAge(e.target.value);
 																handleChangeFloor(e,"floor" ,"constCompletionDate", true,  /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g)}}
 														  floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
 														  underlineStyle={styles.underlineStyle}
@@ -1373,6 +1373,7 @@ handleUsage = (value) => {
 																  
 																  setTimeout(()=>{
 																	_this.createFloorObject();
+																	_this.getFloors();
 																	}, 300);
 																}
 															}/>

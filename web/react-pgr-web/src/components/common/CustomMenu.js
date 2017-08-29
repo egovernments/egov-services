bea56a232e256cfcf7dd97c3134c4279a6950cc2
Murali <muralim4242@users.noreply.github.com>
2017-08-29 19:06:34 +0530

    Added convention for water charge report

--- web/react-pgr-web/src/components/common/CustomMenu.js
+++ web/react-pgr-web/src/components/common/CustomMenu.js
@@ -146,9 +146,12 @@ const menuConvention={
   "Trade License.License Search.SearchLicense": "/search/tl/CreateLegacyLicense/view",
   "Trade License.License Reports.License Register Report":"/report/tradelicense/licenseRegisterReport",
 
+  "Water Charge.Wcms Reports.WCOutstandingReport":"/report/wcms-connection/OutstandingRegister",
+  "Water Charge.Wcms Reports.WCDemandRegister":"/report/wcms-connection/DemandRegister",
+  "Water Charge.Wcms Reports.WCDCBReport":"/report/wcms-connection/DCBReport",
 
 
- 
+
   "Water Charge.Water Transactions.LegacyCreateNewConnectionAPI":"/create/wc/legacy",
 
   //employee Master,

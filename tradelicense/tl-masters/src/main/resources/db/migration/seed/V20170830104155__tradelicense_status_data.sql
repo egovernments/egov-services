d0e87a005969ac64038f3bb1ce993f6ace9c0269
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2017-08-30 10:52:12 +0530

    [EGSVC-1149]-Adding data for trade license status for default tenant

---
+++ tradelicense/tl-masters/src/main/resources/db/migration/seed/V20170830104155__tradelicense_status_data.sql
@@ -0,0 +1,12 @@
+--Default
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Acknowledged', 'ACKNOWLEDGED', 'true', 1, 1, 1503386127924, 1503386127924,'NEW LICENSE');
+
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Application Fee paid', 'APPLICATIONFEEPAID', 'true', 1, 1, 1503386127924, 1503386127924,'NEW LICENSE');
+
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Scrutiny Completed', 'SCRUTINYCOMPLETED', 'true', 1, 1, 1503386127924, 1503386127924,'NEW LICENSE');
+
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Inspection Completed', 'INSPECTIONCOMPLETED', 'true', 1, 1, 1503386127924, 1503386127924,'NEW LICENSE');
+
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Final approval Completed', 'APPROVALCOMPLETED', 'true', 1, 1, 1503386127924, 1503386127924,'NEW LICENSE');
+
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'License Issued', 'LICENSEISSUED', 'true', 1, 1, 1503386127924, 1503386127924,'NEW LICENSE');

56a5a98067c674ee98e8446f067ab9c860dd400b
PradeepNaikR <pradeep.naik@riflexions.com>
2017-09-01 17:41:41 +0530

    Modified testcase for marriageRegnRepository in MR

--- mr/marriage-regn/src/test/java/org/egov/mr/repository/MarriageRegnRepositoryTest.java
+++ mr/marriage-regn/src/test/java/org/egov/mr/repository/MarriageRegnRepositoryTest.java
@@ -1,7 +1,7 @@
 package org.egov.mr.repository;
 
-
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
@@ -38,7 +38,9 @@ import org.egov.mr.web.contract.MarriageRegnRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -65,41 +67,65 @@ public class MarriageRegnRepositoryTest {
 
 	@Test
 	public void testFindForCriteria() {
-		List<MarriageRegn> marriageRegns=getMarriageRegnFromDB();
+		List<MarriageRegn> marriageRegns = getMarriageRegnFromDB();
 
 		when(marriageRegnQueryBuilder.getQueryForListOfMarriageRegnIds(any(MarriageRegnCriteria.class),
 				any(List.class))).thenReturn(StringUtils.EMPTY);
-		when(jdbcTemplate.query(any(String.class), any(Object[].class), any(MarriageRegnIdsRowMapper.class))).thenReturn( getApplicationnumbers());
-		when(marriageRegnQueryBuilder.getQuery(any(MarriageRegnCriteria.class),any(List.class), any(List.class))).thenReturn(StringUtils.EMPTY);
-		when(jdbcTemplate.query(any(String.class), any(Object[].class), any(MarriageRegnRowMapper.class))).thenReturn(marriageRegns);
-		 assertTrue(marriageRegns.equals(marriageRegnRepository.findForCriteria(new MarriageRegnCriteria())));
+		when(jdbcTemplate.query(any(String.class), any(Object[].class), any(MarriageRegnIdsRowMapper.class)))
+				.thenReturn(getApplicationnumbers());
+		when(marriageRegnQueryBuilder.getQuery(any(MarriageRegnCriteria.class), any(List.class), any(List.class)))
+				.thenReturn(StringUtils.EMPTY);
+		when(jdbcTemplate.query(any(String.class), any(Object[].class), any(MarriageRegnRowMapper.class)))
+				.thenReturn(marriageRegns);
+		assertTrue(marriageRegns.equals(marriageRegnRepository.findForCriteria(new MarriageRegnCriteria())));
 	}
 
 	@Test
 	public void testSave() {
-		MarriageRegnRequest  marriageRegnRequest=new MarriageRegnRequest();
-		RequestInfo requestInfo=new RequestInfo();
-		List<MarriageRegn> marriageRegns= getMarriageRegnFromDB();
+		MarriageRegnRequest marriageRegnRequest = new MarriageRegnRequest();
+		RequestInfo requestInfo = new RequestInfo();
+		List<MarriageRegn> marriageRegns = getMarriageRegnFromDB();
 		marriageRegnRequest.setMarriageRegn(marriageRegns.get(0));
 		marriageRegnRequest.setRequestInfo(requestInfo);
 		when(jdbcTemplate.update(any(String.class), any(Object[].class))).thenReturn(1);
-	//Mockdonwhen(witnessRepository.save(Matchers.anyString(), Matchers.anyString(), Matchers.any(Witness.class)))getClass().;
-		
-		
+		Mockito.doNothing().when(witnessRepository).save(Matchers.anyString(), Matchers.anyString(),
+				Matchers.any(Witness.class));
+		marriageRegnRepository.save(marriageRegns.get(0));
 	}
-	
+
 	@Test
 	public void testUpdate() {
-		MarriageRegnRequest  marriageRegnRequest=new MarriageRegnRequest();
-		RequestInfo requestInfo=new RequestInfo();
-		List<MarriageRegn> marriageRegns= getMarriageRegnFromDB();
+		MarriageRegnRequest marriageRegnRequest = new MarriageRegnRequest();
+		RequestInfo requestInfo = new RequestInfo();
+		List<MarriageRegn> marriageRegns = getMarriageRegnFromDB();
 		marriageRegnRequest.setMarriageRegn(marriageRegns.get(0));
 		marriageRegnRequest.setRequestInfo(requestInfo);
 		when(jdbcTemplate.update(any(String.class), any(Object[].class))).thenReturn(1);
-		}
-	
+	}
 
-	
+	@Test
+	public void testUpdateForWitnessNull() {
+		MarriageRegnRequest marriageRegnRequest = new MarriageRegnRequest();
+		RequestInfo requestInfo = new RequestInfo();
+		List<MarriageRegn> marriageRegns = getMarriageRegnFromDB();
+		marriageRegns.get(0).setWitnesses(null);
+		marriageRegnRequest.setMarriageRegn(marriageRegns.get(0));
+		marriageRegnRequest.setRequestInfo(requestInfo);
+		when(jdbcTemplate.update(any(String.class), any(Object[].class))).thenReturn(1);
+		Mockito.doNothing().when(witnessRepository).update(Matchers.anyString(), Matchers.anyString(),
+				Matchers.any(Witness.class));
+		marriageRegnRepository.update(marriageRegns.get(0));
+
+	}
+
+	@Test
+	public void testGenerateApplicationnumbers() {
+		when(jdbcTemplate.queryForObject(Matchers.any(String.class), Matchers.<Class<String>>any()))
+				.thenReturn("00015");
+		String applicationNumber = marriageRegnRepository.generateApplicationNumber();
+		assertEquals("00015", applicationNumber);
+	}
+
 	public List<MarriageRegn> getMarriageRegnFromDB() {
 		AuditDetails auditDetails = AuditDetails.builder().createdBy("asish").createdTime(1503056473884l)
 				.lastModifiedBy("asish").lastModifiedTime(1503056473884l).build();
@@ -127,9 +153,12 @@ public class MarriageRegnRepositoryTest {
 				.relatedTo(RelatedTo.BRIDEGROOM).relationForIdentification("uncle").witnessNo(1).build();
 		List<Witness> witnesses = new ArrayList<>();
 		witnesses.add(witness);
+
+		Demand demand = new Demand();
+		demand.setBusinessService("MarriageRegistration");
 		List<Demand> demands = new ArrayList<>();
-	
-       
+		demands.add(demand);
+
 		List<String> actions = new ArrayList<>();
 		actions.add("action");
 
@@ -155,25 +184,25 @@ public class MarriageRegnRepositoryTest {
 
 		MarriageRegn marriageRegn = MarriageRegn.builder().applicationNumber("8").approvalDetails(approvalDetails)
 				.auditDetails(auditDetails).bride(bride).bridegroom(bridegroom).certificates(marriageCertificates)
-				.city("bangalore").demands(demands).documents(documentsList).fee(Fee.builder().fee(new BigDecimal(50)).build()).isActive(true)
-				.locality("locality").actions(Action.CREATE).marriageDate(214335l).marriagePhoto("photo")
-				.placeOfMarriage("RESIDENCE").placeOfMarriage("bangalore").priest(priest).street("street")
-				.regnDate(121231l).regnNumber("regnno").regnUnit(regnunit).rejectionReason("inappropriate document")
-				.remarks("remarks").serialNo("serino").source(Source.SYSTEM).tenantId("ap.kurnool")
-				.status(ApplicationStatus.APPROVED).stateId("stateid").venue(Venue.FUNCTION_HALL).volumeNo("volno")
-				.witnesses(witnesses).remarks("remarks").build();
+				.city("bangalore").demands(demands).documents(documentsList)
+				.fee(Fee.builder().fee(new BigDecimal(50)).build()).isActive(true).locality("locality")
+				.actions(Action.CREATE).marriageDate(214335l).marriagePhoto("photo").placeOfMarriage("RESIDENCE")
+				.placeOfMarriage("bangalore").priest(priest).street("street").regnDate(121231l).regnNumber("regnno")
+				.regnUnit(regnunit).rejectionReason("inappropriate document").remarks("remarks").serialNo("serino")
+				.source(Source.SYSTEM).tenantId("ap.kurnool").status(ApplicationStatus.APPROVED).stateId("stateid")
+				.venue(Venue.FUNCTION_HALL).volumeNo("volno").witnesses(witnesses).remarks("remarks").build();
 		List<MarriageRegn> marriageRegns = new ArrayList<>();
 		marriageRegns.add(marriageRegn);
 		return marriageRegns;
 
 	}
-	
-	public List<String> getApplicationnumbers(){
-		
-		String applnno="12345";
-		List<String> applnNoList= new ArrayList<>();
+
+	public List<String> getApplicationnumbers() {
+
+		String applnno = "12345";
+		List<String> applnNoList = new ArrayList<>();
 		applnNoList.add(applnno);
 		return applnNoList;
-		
+
 	}
 }

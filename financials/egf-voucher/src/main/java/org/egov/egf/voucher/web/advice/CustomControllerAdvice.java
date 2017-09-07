c40689262c209855cc59cb7e5f0e63ab89c9237d
venki <venki@egovernments.org>
2017-09-07 15:26:30 +0530

    Deleted egf voucher subtype setup data.

--- financials/egf-voucher/src/main/java/org/egov/egf/voucher/web/advice/CustomControllerAdvice.java
+++ financials/egf-voucher/src/main/java/org/egov/egf/voucher/web/advice/CustomControllerAdvice.java
@@ -1,15 +1,18 @@
 package org.egov.egf.voucher.web.advice;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.egov.common.contract.response.Error;
-import org.egov.common.contract.response.ErrorField;
 import org.egov.common.contract.response.ErrorResponse;
 import org.egov.common.contract.response.ResponseInfo;
 import org.egov.common.domain.exception.CustomBindException;
+import org.egov.common.domain.exception.ErrorCode;
 import org.egov.common.domain.exception.InvalidDataException;
 import org.egov.common.domain.exception.UnauthorizedAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -22,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomControllerAdvice {
 
+	private static final Logger LOG = LoggerFactory.getLogger(CustomControllerAdvice.class);
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MissingServletRequestParameterException.class)
 	public String handleMissingParamsError(Exception ex) {
@@ -30,49 +35,109 @@ public class CustomControllerAdvice {
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(CustomBindException.class)
-	public ErrorResponse handleBindingErrors(CustomBindException ex) {
-		ErrorResponse errRes = new ErrorResponse();
+	public org.egov.common.web.contract.ErrorResponse handleBindingErrors(CustomBindException ex) {
+		org.egov.common.web.contract.ErrorResponse errRes = new org.egov.common.web.contract.ErrorResponse();
+		// errRes.setErrors(new ArrayList<>());
 		BindingResult errors = ex.getErrors();
 		ResponseInfo responseInfo = new ResponseInfo();
 		responseInfo.setStatus(HttpStatus.BAD_REQUEST.toString());
 		errRes.setResponseInfo(responseInfo);
-		Error error = new Error();
+		org.egov.common.web.contract.Error error = new org.egov.common.web.contract.Error();
 		if (errors.getGlobalError() != null) {
-			error.setCode(Integer.valueOf(errors.getGlobalError().getCode()));
-			error.setMessage(errors.getGlobalError().getObjectName());
-			error.setDescription(errors.getGlobalError().getDefaultMessage());
-		} else {
-			if (errors.getFieldErrorCount() > 0) {
-				error.setDescription("Missing fields");
+			if (errors.getGlobalError().getCode() != null) {
+				error.setCode(errors.getGlobalError().getCode());
+				error.setMessage(errors.getGlobalError().getObjectName());
+				error.setDescription(errors.getGlobalError().getDefaultMessage());
+			} else if (ErrorCode.getError(errors.getGlobalError().getCode()) != null) {
+				error.setCode(ErrorCode.getError(errors.getGlobalError().getDefaultMessage()).getCode());
+				String message = MessageFormat.format(
+						ErrorCode.getError(errors.getGlobalError().getDefaultMessage()).getMessage(),
+						errors.getGlobalError().getObjectName());
+				error.setMessage(message);
+				String desc = MessageFormat.format(
+						ErrorCode.getError(errors.getGlobalError().getDefaultMessage()).getDescription(),
+						errors.getGlobalError().getObjectName());
+				error.setDescription(desc);
 			}
+			errRes.getErrors().add(error);
+
 		}
+		/*
+		 * if (errors.getFieldErrorCount() > 0) { error.setDescription(
+		 * "Validation errors"); }
+		 */
+
 		if (errors.hasFieldErrors()) {
 			List<org.springframework.validation.FieldError> fieldErrors = errors.getFieldErrors();
 			for (org.springframework.validation.FieldError errs : fieldErrors) {
-				ErrorField f = new ErrorField(errs.getCode(), errs.getDefaultMessage(), errs.getField());
-				error.setFields(new ArrayList<>());
-				error.getFields().add(f);
-
+				org.egov.common.web.contract.Error err = new org.egov.common.web.contract.Error();
+				if (ErrorCode.getError(errs.getCode()) != null) {
+					err.setCode(errs.getCode());
+					String message = MessageFormat.format(ErrorCode.getError(errs.getCode()).getMessage(),
+							errs.getField(), errs.getRejectedValue());
+					err.setMessage(message);
+					String desc = MessageFormat.format(ErrorCode.getError(errs.getCode()).getDescription(),
+							errs.getField(), errs.getRejectedValue());
+					err.setDescription(desc);
+					err.getParams().add(errs.getField());
+					err.getParams().add((String) errs.getRejectedValue());
+				} else {
+					err.setCode(errs.getCode());
+					err.setMessage(errs.getDefaultMessage());
+					err.setDescription(errs.getField());
+				}
+				errRes.getErrors().add(err);
 			}
+
 		}
-		errRes.setError(error);
+
 		return errRes;
 	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(InvalidDataException.class)
-	public ErrorResponse handleBindingErrors(InvalidDataException ex) {
-		ErrorResponse errRes = new ErrorResponse();
+	public org.egov.common.web.contract.ErrorResponse handleBindingErrors(InvalidDataException ex) {
+		org.egov.common.web.contract.ErrorResponse errRes = new org.egov.common.web.contract.ErrorResponse();
 
 		ResponseInfo responseInfo = new ResponseInfo();
 		responseInfo.setStatus(HttpStatus.BAD_REQUEST.toString());
 		errRes.setResponseInfo(responseInfo);
-		Error error = new Error();
-		error.setCode(Integer.valueOf(InvalidDataException.code));
-		error.setMessage(ex.getFieldName());
-		error.setDescription(ex.getMessage());
-		errRes.setError(error);
+		org.egov.common.web.contract.Error error = new org.egov.common.web.contract.Error();
+		error.setCode(ex.getMessageKey());
+		System.out.println(ErrorCode.getError("non.unique.value"));
+		if (ErrorCode.getError(ex.getMessageKey()) != null) {
+			String message = MessageFormat.format(ErrorCode.getError(ex.getMessageKey()).getMessage(),
+					ex.getFieldName(), ex.getFieldValue());
+			error.setMessage(message);
+			String desc = MessageFormat.format(ErrorCode.getError(ex.getMessageKey()).getDescription(),
+					ex.getFieldName(), ex.getFieldValue());
+			error.setDescription(desc);
+		} else {
+			LOG.warn("error code is not defined for " + ex.getMessageKey());
+		}
+		List<org.egov.common.web.contract.Error> errors = new ArrayList<org.egov.common.web.contract.Error>();
+		errors.add(error);
+		errRes.setErrors(errors);
+		return errRes;
+	}
 
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(org.apache.kafka.common.errors.TimeoutException.class)
+	public org.egov.common.web.contract.ErrorResponse handleThrowable(
+			org.apache.kafka.common.errors.TimeoutException ex) {
+		org.egov.common.web.contract.ErrorResponse errRes = new org.egov.common.web.contract.ErrorResponse();
+		ex.printStackTrace();
+		ResponseInfo responseInfo = new ResponseInfo();
+		responseInfo.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.toString());
+		errRes.setResponseInfo(responseInfo);
+		org.egov.common.web.contract.Error error = new org.egov.common.web.contract.Error();
+
+		error.setCode(ErrorCode.KAFKA_TIMEOUT_ERROR.getCode());
+		error.setMessage(ErrorCode.KAFKA_TIMEOUT_ERROR.getMessage());
+		error.setDescription(ErrorCode.KAFKA_TIMEOUT_ERROR.getDescription());
+		List<org.egov.common.web.contract.Error> errors = new ArrayList<org.egov.common.web.contract.Error>();
+		errors.add(error);
+		errRes.setErrors(errors);
 		return errRes;
 	}
 
@@ -82,7 +147,7 @@ public class CustomControllerAdvice {
 		ErrorResponse errRes = new ErrorResponse();
 		ex.printStackTrace();
 		ResponseInfo responseInfo = new ResponseInfo();
-		responseInfo.setStatus(HttpStatus.BAD_REQUEST.toString());
+		responseInfo.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.toString());
 		errRes.setResponseInfo(responseInfo);
 		Error error = new Error();
 

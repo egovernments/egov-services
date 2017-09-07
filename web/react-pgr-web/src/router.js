1e52810ba3148b4582811a71d4e429f9844518f9
Murali <muralim4242@users.noreply.github.com>
2017-09-07 11:49:45 +0530

    fireNoc view added payment gateway

--- web/react-pgr-web/src/router.js
+++ web/react-pgr-web/src/router.js
@@ -264,7 +264,7 @@ const Main = () => {
       <Route exact path={base + '/non-framework/citizenServices/create/:status/:id/:paymentGateWayRes?'} component={CS_WaterConnection}/>
       <Route exact path={base + '/non-framework/citizenServices/view/:status/:id/:ackNo/:paymentGateWayRes?'} component={CS_VIEW_WaterConnection}/>
       <Route exact path={base + '/non-framework/citizenServices/fireNoc/:status/:id/:paymentGateWayRes?'} component={CS_FireNoc}/>
-      <Route exact path={base + '/non-framework/citizenServices/fireNoc/view/:type/:ackNo'} component={CS_VIEW_FireNoc}/>
+      <Route exact path={base + '/non-framework/citizenServices/fireNoc/:status/:id/:ackNo/:paymentGateWayRes?'} component={CS_VIEW_FireNoc}/>
       <Route exact path= {base + '/payment/response/redirect/:msg'} component={Payment}/>
       <Route exact path= {base + '/receipt/:page/:type/:cc/:sid'} component={ReceiptDownload}/>
 

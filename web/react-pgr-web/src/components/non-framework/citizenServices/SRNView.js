a92130868bd3f808d000ed6fae81791fe3e524b1
sourabh <sourabh.debnath@goodworklabs.com>
2017-09-12 17:22:49 +0530

    Fixed- Filtering documents based on role

--- web/react-pgr-web/src/components/non-framework/citizenServices/SRNView.js
+++ web/react-pgr-web/src/components/non-framework/citizenServices/SRNView.js
@@ -34,8 +34,7 @@ class CertificateView extends Component {
 			open: false,
 			ServiceRequest: {},
 			statuses: ["APPROVED", "SANCTIONED"],
-			serviceCodes: ["TL_NEWCONN", "WATER_NEWCONN", "BPA_FIRE_NOC"],
-			documents: []
+			serviceCodes: ["TL_NEWCONN", "WATER_NEWCONN", "BPA_FIRE_NOC"]
 		};
 	}
 
@@ -47,8 +46,7 @@ class CertificateView extends Component {
 			self.props.setLoadingStatus('hide');
 			if(res && res.serviceReq && res.serviceReq.length && self.state.serviceCodes.indexOf(res.serviceReq[0].serviceCode) > -1) {
 				self.setState({
-					ServiceRequest: res.serviceReq[0],
-					documents: res.documents || []
+					ServiceRequest: res.serviceReq[0]
 				})
 			} else {
 				self.setState({
@@ -69,7 +67,7 @@ class CertificateView extends Component {
 
 	render () {
 		let self = this;
-		let { open, ServiceRequest, documents } = this.state;
+		let { open, ServiceRequest } = this.state;
 		let { handleClose } = this;
 
 		return (
@@ -124,17 +122,19 @@ class CertificateView extends Component {
 	                          </thead>
 	                          <tbody>
 	                            {
-	                              documents && 
-	                              documents.length ? 
-	                              documents.map(function(v, i) {
-	                              	return (
-		                                  <tr key={i}>
-		                                    <td>{v.from}</td>
-		                                    <td>{getFullDate(v.timeStamp)}</td>
-		                                    <td>{v.name}</td>
-		                                    <td><a target="_blank" href={"/filestore/v1/files/id?tenantId=" + localStorage.getItem("tenantId") + "&fileStoreId=" + v.filePath}>Download</a></td>
-		                                  </tr>
-		                                )
+	                              ServiceRequest && 
+	                              ServiceRequest.documents && 
+	                              ServiceRequest.documents.length ? 
+	                              ServiceRequest.documents.map(function(v, i) {
+	                              	if(v.uploadedbyrole == "EMPLOYEE")
+		                              	return (
+			                                  <tr key={i}>
+			                                    <td>{v.from}</td>
+			                                    <td>{getFullDate(v.timeStamp)}</td>
+			                                    <td>{v.name}</td>
+			                                    <td><a target="_blank" href={"/filestore/v1/files/id?tenantId=" + localStorage.getItem("tenantId") + "&fileStoreId=" + v.filePath}>Download</a></td>
+			                                  </tr>
+			                                )
 	                              }) : <tr><td style={{"textAlign": "center"}} colSpan={4}>No documents uploaded!</td></tr>
 	                            }
 	                          </tbody>

f91312b34bce8f153f9cd13483ee3549564244be
sourabh <sourabh.debnath@goodworklabs.com>
2017-09-12 15:36:16 +0530

    Fixed- UI issues

--- web/react-pgr-web/src/components/non-framework/citizenServices/tl/create.js
+++ web/react-pgr-web/src/components/non-framework/citizenServices/tl/create.js
@@ -340,7 +340,6 @@ this.setState({openLicense: false});
       var _date = _val.split("-");
       return new Date(_date[0], (Number(_date[1])-1), _date[2]);
     }
-
     return typeof _val != "undefined" ? _val : "";
   }
 
@@ -1540,7 +1539,7 @@ if(property == "licenses[0].categoryId"){
                                       </tr>
                                   </tbody>
                               </Table>
-                              <span style={{textAlign:"right"}}>{translate("This is computer generated receipt no authorised signature required")}</span>
+                              <span style={{textAlign:"right"}}>{ translate("This is computer generated receipt no authorised signature required") }</span>
                         </CardText>
                       </Card> : ""}
                       <br/>

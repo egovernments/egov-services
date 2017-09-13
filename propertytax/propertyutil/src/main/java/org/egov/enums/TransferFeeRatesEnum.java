8e8bd83486f5299e275ec18e1d41e650b25bb69a
Yosadhara <yosadhara.pradhan@walkingtree.in>
2017-09-13 14:59:23 +0530

    Added models for TransferFeeRate master and TransferFeeCal

---
+++ propertytax/propertyutil/src/main/java/org/egov/enums/TransferFeeRatesEnum.java
@@ -0,0 +1,43 @@
+package org.egov.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * FeeFactor enum: The fee factor based on which the fee will be calculated
+ * 
+ * @author Yosadhara
+ *
+ */
+public enum TransferFeeRatesEnum {
+
+	PROPERTYTAX("propertytax"),
+
+	DOCUMENTVALUE("documentvalue"),
+
+	MARKETVALUE("marketvalue"),
+
+	FLATRATE("flatrate");
+
+	private String value;
+
+	TransferFeeRatesEnum(String value) {
+		this.value = value;
+	}
+
+	@Override
+	@JsonValue
+	public String toString() {
+		return String.valueOf(value);
+	}
+
+	@JsonCreator
+	public static TransferFeeRatesEnum fromValue(String text) {
+		for (TransferFeeRatesEnum transferFeeRates : TransferFeeRatesEnum.values()) {
+			if (String.valueOf(transferFeeRates.value).equals(text)) {
+				return transferFeeRates;
+			}
+		}
+		return null;
+	}
+}

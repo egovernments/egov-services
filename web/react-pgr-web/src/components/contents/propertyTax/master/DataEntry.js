71849ac3f814e16c667c02299e7b4634f67e6290
vineetm30 <vineet.mishra@goodworklabs.com>
2017-09-13 13:07:52 +0530

    extent of site made non mandatory

--- web/react-pgr-web/src/components/contents/propertyTax/master/DataEntry.js
+++ web/react-pgr-web/src/components/contents/propertyTax/master/DataEntry.js
@@ -706,7 +706,7 @@ const mapDispatchToProps = dispatch => ({
       validationData: {
         required: {
           current: [],
-          required: ['reasonForCreation','propertyType','pin' ,'usage','extentOfSite','doorNo', 'zoneNo', 'wardNo', 'sequenceNo', 'oldUpicNumber', 'totalFloors', 'currentAssessmentDate', 'firstAssessmentDate', 'lastAssessmentDate']
+          required: ['reasonForCreation','propertyType','pin' ,'usage','doorNo', 'zoneNo', 'wardNo', 'sequenceNo', 'oldUpicNumber', 'totalFloors', 'currentAssessmentDate', 'firstAssessmentDate', 'lastAssessmentDate']
         },
         pattern: {
           current: [],

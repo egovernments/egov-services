e694c2e397dd37e4a4363caa40cf7556fc781e92
subhash.bose@egovernments.org <subhash.bose@egovernments.org>
2017-09-14 16:25:32 +0530

    Test cases for notice query builder

--- lams/lams-services/src/main/java/org/egov/lams/repository/NoticeRepository.java
+++ lams/lams-services/src/main/java/org/egov/lams/repository/NoticeRepository.java
@@ -1,9 +1,5 @@
 package org.egov.lams.repository;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import org.egov.lams.model.Notice;
 import org.egov.lams.model.NoticeCriteria;
 import org.egov.lams.repository.builder.NoticeQueryBuilder;
@@ -15,68 +11,76 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class NoticeRepository {
-	
-	public static final Logger LOGGER = LoggerFactory.getLogger(NoticeRepository.class);
-	
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	
-	public Notice createNotice(NoticeRequest noticeRequest) {
-		
-		RequestInfo requestInfo = noticeRequest.getRequestInfo();
-		Notice notice= noticeRequest.getNotice();
-		notice.setNoticeNo(getNoticeNo());
-		
-		Object[] obj = new Object[] { notice.getNoticeNo(), notice.getNoticeDate(),notice.getAgreementNumber(),
-				notice.getAssetCategory(), notice.getAcknowledgementNumber(),notice.getAssetNo(), notice.getAllotteeName(), 
-				notice.getAllotteeAddress(),notice.getAllotteeMobileNumber(),notice.getAgreementPeriod(),notice.getCommencementDate(),
-				notice.getTemplateVersion(),notice.getExpiryDate(),notice.getRent(),notice.getSecurityDeposit(),notice.getCommissionerName(),
-				notice.getZone(),notice.getWard(),notice.getStreet(),notice.getElectionward(),notice.getLocality(),notice.getBlock(),
-				requestInfo.getRequesterId(), new Date(), requestInfo.getRequesterId(), new Date(),
-				notice.getTenantId(),notice.getRentInWord(),notice.getFileStore()};
-		
-		try{
-			jdbcTemplate.update(NoticeQueryBuilder.INSERT_NOTICE_QUERY, obj);
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		
-		return notice;
-	}
-	
-	//public List<Notice> get
-	
-	private String getNoticeNo() {
-		
-		Integer result = jdbcTemplate.queryForObject(NoticeQueryBuilder.SEQ_NOTICE_NO,Integer.class);
-		StringBuilder noticeNo=null;
-		
-		try{
-			noticeNo = new StringBuilder(String.format("%09d", result));
-		}catch(Exception ex){
-			LOGGER.info("the exception from notice repo getnoticeno :: "+ex);
-			ex.printStackTrace();
-		}
-		
-		return noticeNo.toString();
-	}	
-	
-	public List<Notice> getNotices(NoticeCriteria noticeCriteria){
-		
-		List<Notice> notices = null;
-		List<Object> preparedStatementValues = new ArrayList<>();
-		String queryString = NoticeQueryBuilder.getNoticeQuery(noticeCriteria, preparedStatementValues);
-		try {
-			notices = jdbcTemplate.query(queryString, preparedStatementValues.toArray(),new NoticeRowMapper());
-		} catch (DataAccessException e) {
-			LOGGER.info("the exception from notice repo query :: "+e);
-			throw new RuntimeException(e.getMessage());
-		}
-		return notices;
-	}
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(NoticeRepository.class);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public Notice createNotice(NoticeRequest noticeRequest) {
+
+        RequestInfo requestInfo = noticeRequest.getRequestInfo();
+        Notice notice = noticeRequest.getNotice();
+        notice.setNoticeNo(getNoticeNo());
+
+        Object[] obj = new Object[]{notice.getNoticeNo(), notice.getNoticeDate(), notice.getAgreementNumber(),
+                notice.getAssetCategory(), notice.getAcknowledgementNumber(), notice.getAssetNo(), notice.getAllotteeName(),
+                notice.getAllotteeAddress(), notice.getAllotteeMobileNumber(), notice.getAgreementPeriod(), notice.getCommencementDate(),
+                notice.getTemplateVersion(), notice.getExpiryDate(), notice.getRent(), notice.getSecurityDeposit(), notice.getCommissionerName(),
+                notice.getZone(), notice.getWard(), notice.getStreet(), notice.getElectionward(), notice.getLocality(), notice.getBlock(),
+                requestInfo.getRequesterId(), new Date(), requestInfo.getRequesterId(), new Date(),
+                notice.getTenantId(), notice.getRentInWord(), notice.getFileStore()};
+
+        try {
+            jdbcTemplate.update(NoticeQueryBuilder.INSERT_NOTICE_QUERY, obj);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return notice;
+    }
+
+    //public List<Notice> get
+
+    private String getNoticeNo() {
+
+        Integer result = jdbcTemplate.queryForObject(NoticeQueryBuilder.SEQ_NOTICE_NO, Integer.class);
+        StringBuilder noticeNo = null;
+
+        try {
+            noticeNo = new StringBuilder(String.format("%09d", result));
+        } catch (Exception ex) {
+            LOGGER.info("the exception from notice repo getnoticeno :: " + ex);
+            ex.printStackTrace();
+        }
+
+        return noticeNo.toString();
+    }
+
+    public List<Notice> getNotices(NoticeCriteria noticeCriteria) {
+
+        List<Notice> notices = null;
+        Map params = new HashMap<>();
+        String queryString = NoticeQueryBuilder.getNoticeQuery(noticeCriteria, params);
+        try {
+            notices = namedParameterJdbcTemplate.query(queryString, params, new NoticeRowMapper());
+        } catch (DataAccessException e) {
+            LOGGER.info("the exception from notice repo query :: " + e);
+            throw new RuntimeException(e.getMessage());
+        }
+        return notices;
+    }
 
 }

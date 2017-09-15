c310f3c5bb690436e6f21b3d80fc6829fe5952a5
Manoj Kulkarni <manoj.kulkarni@egovernments.org>
2017-09-15 18:24:37 +0530

    [EGSVC-1401] - rejection scenario for trade license

---
+++ tradelicense/tl-masters/src/main/resources/db/migration/seed/V20170915180455__tradelicense_mstr_rejected_status.sql
@@ -0,0 +1,2 @@
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Rejected', 'REJECTED', 'true', 1, 1, 1505436237000, 1505436237000,'NEW LICENSE');
+INSERT INTO egtl_mstr_status(id, tenantid, name, code, active, createdby, lastmodifiedby, createdtime, lastmodifiedtime, moduletype)VALUES (nextval('seq_egtl_mstr_status'), 'default', 'Cancelled', 'CANCELLED', 'true', 1, 1, 1505436237000, 1505436237000,'NEW LICENSE');

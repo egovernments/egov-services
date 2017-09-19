e645c8d20c1b6f9eba2e3ab23e945ddea711c539
Barath Vutukuri <barath.vutukuri@goodworklabs.com>
2017-09-19 13:46:56 +0530

    BillChecklist test cases

---
+++ financials/egf-bill/src/test/resources/sql/billchecklist/insertbillchecklistdata.sql
@@ -0,0 +1,2 @@
+insert into egf_billchecklist (id,billid,checklistid,checklistvalue,createdby,createddate,lastmodifiedby,lastmodifieddate,tenantid)
+values('6','29','4','newValue',1,now(),1,now(),'default');

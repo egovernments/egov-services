0903ac5884d56b19b7c332eb8132a07c8c8c2ec4
anilkumarsandrapati <anil.sandrapati@walkingtree.tech>
2017-09-20 14:42:30 +0530

    removed param

--- propertytax/pt-property/src/main/java/org/egov/property/repository/PropertyMasterRepository.java
+++ propertytax/pt-property/src/main/java/org/egov/property/repository/PropertyMasterRepository.java
@@ -1292,7 +1292,7 @@ public class PropertyMasterRepository {
 	 * @param code
 	 * @return {@link Boolean} True /False if code exists/doesn't Exists
 	 */
-	public Boolean checkUniqueCodeForMutation(String code, Double totalTax) {
+	public Boolean checkUniqueCodeForMutation(String code) {
 		Boolean isExists = Boolean.TRUE;
 
 		String query = MutationMasterBuilder.CHECK_UNIQUE_CODE;

7ae3ea4bc18a604e3c826d6723c4e5f72b3760b3
venki <venki@egovernments.org>
2017-09-25 17:22:46 +0530

    Initial setup for egf-payment service.

---
+++ financials/egf-payment/src/main/java/org/egov/egf/payment/EgfPaymentApplication.java
@@ -0,0 +1,12 @@
+package org.egov.egf.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgfPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgfPaymentApplication.class, args);
+	}
+}

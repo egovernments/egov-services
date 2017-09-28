e7faa12f6c4aa657744da7577d311a15278046f0
vishalmahuli <vishal.mahuli@tarento.com>
2017-09-28 15:25:35 +0530

    ES down handling with consumer orchestration

--- citizen/citizen-indexer/src/main/java/org/egov/infra/indexer/consumer/IndexerMessageListener.java
+++ citizen/citizen-indexer/src/main/java/org/egov/infra/indexer/consumer/IndexerMessageListener.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.listener.MessageListener;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Service
 public class IndexerMessageListener implements MessageListener<String, String> {
 	
@@ -20,18 +18,9 @@ public class IndexerMessageListener implements MessageListener<String, String> {
 	
 	@Override
 	public void onMessage(ConsumerRecord<String, String> data) {
-        logger.info("record received: "+data);
         logger.info("Topic: "+data.topic());
         logger.info("Value: "+data.value());
-		ObjectMapper mapper = new ObjectMapper();
-	//	IndexerService indexerService = new IndexerService();
-		String recordString = null;
-		try{
-			recordString = mapper.writeValueAsString(data.value());
-		}catch(Exception e){
-			logger.error("Exception while parsing map to json", e);
-		}
-		logger.info("recordString: "+recordString);
+
 		indexerService.elasticIndexer(data.topic(), data.value());     
 	}
 

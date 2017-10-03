432489713b7d11de065ab693567299705809a90e
Manikanta <manikanta@egovernments.org>
2017-10-03 21:23:36 +0530

    adding validator service

---
+++ egov-masters-validate/src/main/java/org/egov/masters/EgovMastersValidateApplication.java
@@ -0,0 +1,12 @@
+package org.egov.masters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgovMastersValidateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgovMastersValidateApplication.class, args);
+	}
+}

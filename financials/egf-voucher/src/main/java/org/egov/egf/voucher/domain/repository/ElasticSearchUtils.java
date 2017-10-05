f0fa03bcae3fcd8803c473a14d450b66d38dd4a8
venki <venki@egovernments.org>
2017-10-05 12:41:00 +0530

    Added filter conditions for vouchernumbers,names,glcodes and statuses.

--- financials/egf-voucher/src/main/java/org/egov/egf/voucher/domain/repository/ElasticSearchUtils.java
+++ financials/egf-voucher/src/main/java/org/egov/egf/voucher/domain/repository/ElasticSearchUtils.java
@@ -1,45 +1,42 @@
 package org.egov.egf.voucher.domain.repository;
 
+import static org.elasticsearch.index.query.QueryBuilders.termsQuery;
+
+import java.util.List;
+
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.stereotype.Service;
 
-import static org.elasticsearch.index.query.QueryBuilders.termsQuery;
-
 @Service
 public class ElasticSearchUtils {
 
-    void add(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
-        if (fieldValue != null) {
-            boolQueryBuilder = boolQueryBuilder.filter(termsQuery(field, fieldValue));
-        }
+	void add(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
+		if (fieldValue != null) {
+			boolQueryBuilder = boolQueryBuilder.filter(termsQuery(field, fieldValue));
+		}
 
-    }
+	}
 
-    void gte(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
-        if (fieldValue != null) {
-            boolQueryBuilder = boolQueryBuilder
-                    .filter(QueryBuilders.rangeQuery(field)
-                            .from(fieldValue));
-        }
+	void gte(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
+		if (fieldValue != null) {
+			boolQueryBuilder = boolQueryBuilder.filter(QueryBuilders.rangeQuery(field).from(fieldValue));
+		}
 
-    }
+	}
 
-    void lte(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
-        if (fieldValue != null) {
-            boolQueryBuilder = boolQueryBuilder
-                    .filter(QueryBuilders.rangeQuery(field)
-                            .to(fieldValue));
-        }
+	void lte(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
+		if (fieldValue != null) {
+			boolQueryBuilder = boolQueryBuilder.filter(QueryBuilders.rangeQuery(field).to(fieldValue));
+		}
 
-    }
+	}
 
-    void in(Object fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
-        if (fieldValue != null) {
-            boolQueryBuilder = boolQueryBuilder
-                    .should(QueryBuilders.multiMatchQuery(fieldValue, field));
-        }
+	void in(List fieldValue, String field, BoolQueryBuilder boolQueryBuilder) {
+		if (fieldValue != null) {
+			boolQueryBuilder.filter(QueryBuilders.termsQuery(field, fieldValue));
+		}
 
-    }
+	}
 
 }

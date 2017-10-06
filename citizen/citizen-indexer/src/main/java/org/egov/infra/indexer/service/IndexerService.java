6d0b37b7387f976363381bf2e8b2e29c8d1d81f5
vishalmahuli <vishal.mahuli@tarento.com>
2017-10-06 11:47:30 +0530

    minor refacotring

--- citizen/citizen-indexer/src/main/java/org/egov/infra/indexer/service/IndexerService.java
+++ citizen/citizen-indexer/src/main/java/org/egov/infra/indexer/service/IndexerService.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;

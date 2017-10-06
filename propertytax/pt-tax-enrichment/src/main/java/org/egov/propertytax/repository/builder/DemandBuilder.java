ba71ec0b173e996b6f76889314e76fc882135aae
prasadkhandagale <prasad.khandagale@walkingtree.tech>
2017-10-06 16:32:23 +0530

    Changing the upic number to application no to get the demands

--- propertytax/pt-tax-enrichment/src/main/java/org/egov/propertytax/repository/builder/DemandBuilder.java
+++ propertytax/pt-tax-enrichment/src/main/java/org/egov/propertytax/repository/builder/DemandBuilder.java
@@ -3,7 +3,7 @@ package org.egov.propertytax.repository.builder;
 public class DemandBuilder {
 	
 	
-	public static final String GET_property_Id = "select id from egpt_property where upicnumber=?";
+	public static final String GET_property_Id = "select property from egpt_propertydetails 	where applicationno=?";
 	
 	
 	public static final String GET_TAX_CALC_FOR_PROPERTY= "select taxcalculations from egpt_propertydetails where property=?";

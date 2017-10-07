30e64b1b2151545f3601b8ba8f6212632132bafd
aslam <aslam.rahman@egovernments.org>
2017-10-07 13:06:27 +0530

    [EGSVC-1414] Export to excel and pdf issue fixed

--- web/react-pgr-web/src/components/contents/reports/reportResult.js
+++ web/react-pgr-web/src/components/contents/reports/reportResult.js
@@ -8,12 +8,14 @@ import {translate} from '../../common/common';
 import _ from 'lodash';
 
 import $ from 'jquery';
-import 'datatables.net-buttons/js/buttons.html5.js';// HTML 5 file export
-import 'datatables.net-buttons/js/buttons.flash.js';// Flash file export
-import jszip from 'jszip/dist/jszip';
+import JSZip from 'jszip/dist/jszip';
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import 'datatables.net-buttons/js/buttons.html5.js';// HTML 5 file export
+import 'datatables.net-buttons/js/buttons.flash.js';// Flash file export
+import {fonts} from '../../common/pdf-generation/PdfConfig';
+pdfMake.fonts = fonts;
+window.JSZip = JSZip;
 
 var sumColumn = [];
 var footerexist = false;
--- web/react-pgr-web/src/components/non-framework/tl/search/NoticeSearchResult.js
+++ web/react-pgr-web/src/components/non-framework/tl/search/NoticeSearchResult.js
@@ -9,12 +9,14 @@ import Api from '../../../../api/api';
 import styles from '../../../../styles/material-ui';
 
 import $ from 'jquery';
-import 'datatables.net-buttons/js/buttons.html5.js';// HTML 5 file export
-import 'datatables.net-buttons/js/buttons.flash.js';// Flash file export
-import jszip from 'jszip/dist/jszip';
+import JSZip from 'jszip/dist/jszip';
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import 'datatables.net-buttons/js/buttons.html5.js';// HTML 5 file export
+import 'datatables.net-buttons/js/buttons.flash.js';// Flash file export
+import {fonts} from '../../../common/pdf-generation/PdfConfig';
+pdfMake.fonts = fonts;
+window.JSZip = JSZip;
 
 export default class NoticeSearchResult extends Component{
   constructor(){
@@ -68,7 +70,7 @@ export default class NoticeSearchResult extends Component{
             orientation: 'landscape',
             pageSize: 'TABLOID',
             footer : true
-        },
+        }
       ],
       bDestroy: true,
       order:[]
@@ -122,7 +124,7 @@ export default class NoticeSearchResult extends Component{
       )
     }else if(contentType === 'image/jpeg'){
       return (
-          <img src={this.state.iframe_src} style={{width:'100%'}}/>
+          <img src={this.state.iframe_src} style={{width:'100%'}} alt=""/>
       )
     }else{
       return (
@@ -132,7 +134,7 @@ export default class NoticeSearchResult extends Component{
   }
   render(){
     const actions = [
-      <FlatButton style={styles.marginStyle} href={this.state.iframe_src} download label={translate('tl.download')} download primary={true}/>,
+      <FlatButton style={styles.marginStyle} href={this.state.iframe_src} label={translate('tl.download')} download primary={true}/>,
       <FlatButton
         label={translate('core.lbl.cancel')}
         primary={true}

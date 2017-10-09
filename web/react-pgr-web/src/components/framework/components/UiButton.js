b3991c0b4a8996f481eb5cb12442ed6b998ba26e
sourabh <sourabh.debnath@goodworklabs.com>
2017-10-09 09:57:39 +0530

    Added- Reset button in search screens Fixed- TenantId changes to default issue on session timeout

--- web/react-pgr-web/src/components/framework/components/UiButton.js
+++ web/react-pgr-web/src/components/framework/components/UiButton.js
@@ -10,7 +10,7 @@ export default class UiButton extends Component {
 		switch (this.props.ui) {
 			case 'google':
 				return (
-					<RaisedButton id={item.label.split(".").join("-")} type={item.uiType || "button"} label={item.label} primary={true} onClick={this.props.handler || function(){}} disabled={item.isDisabled ? true : false}/>
+					<RaisedButton id={item.label.split(".").join("-")} type={item.uiType || "button"} label={item.label} primary={typeof item.primary != 'undefined' ? item.primary : true} secondary={item.secondary || false} onClick={this.props.handler || function(){}} disabled={item.isDisabled ? true : false}/>
 				);
 		}
 	}

1a604df8171ee4262a617ae328a1035f41868c46
Ghanshyam <ghanshyam.rawat@riflexions.com>
2017-10-10 17:13:54 +0530

    add PGobject for json data

--- citizen/citizen-persister/src/main/java/org/egov/infra/persist/service/PersistService.java
+++ citizen/citizen-persister/src/main/java/org/egov/infra/persist/service/PersistService.java
@@ -8,6 +8,7 @@ import org.egov.infra.persist.web.contract.JsonMap;
 import org.egov.infra.persist.web.contract.Mapping;
 import org.egov.infra.persist.web.contract.QueryMap;
 import org.egov.infra.persist.web.contract.TopicMap;
+import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,44 +19,40 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PersistService {
 
-	
 	@Autowired
 	private TopicMap topicMap;
-	
+
 	@Autowired
 	private PersistRepository persistRepository;
-	
+
 	@Transactional
-	public void persist(String topic, String json){
-		
+	public void persist(String topic, String json) {
+
 		Map<String, Mapping> map = topicMap.getTopicMap();
 		Mapping mapping = map.get(topic);
-		log.info("PersistService mapping:"+mapping);
-		
+		log.info("PersistService mapping:" + mapping);
+
 		List<QueryMap> queryMaps = mapping.getQueryMaps();
-		for(QueryMap queryMap : queryMaps){
+		for (QueryMap queryMap : queryMaps) {
 			String query = queryMap.getQuery();
 			List<JsonMap> jsonMaps = queryMap.getJsonMaps();
 			String basePath = queryMap.getBasePath();
 			persistRepository.persist(query, jsonMaps, json, basePath);
-			
+
 		}
-		
-		
-	/*	for(Mapping mapping : mappings){
-			log.info("mapping.getFromTopic():"+mapping.getFromTopic()+","+"topic:"+topic);
-			if(mapping.getFromTopic().equals(topic)){
-				List<QueryMap> queryMaps = mapping.getQueryMaps();
-				for(QueryMap queryMap : queryMaps){
-					String query = queryMap.getQuery();
-					List<JsonMap> jsonMaps = queryMap.getJsonMaps();
-					String basePath = queryMap.getBasePath();
-					persistRepository.persist(query, jsonMaps, json, basePath);
-					
-				}
-			}
-		}*/
-		
+
+		/*
+		 * for(Mapping mapping : mappings){
+		 * log.info("mapping.getFromTopic():"+mapping.getFromTopic()+","+"topic:"+topic)
+		 * ; if(mapping.getFromTopic().equals(topic)){ List<QueryMap> queryMaps =
+		 * mapping.getQueryMaps(); for(QueryMap queryMap : queryMaps){ String query =
+		 * queryMap.getQuery(); List<JsonMap> jsonMaps = queryMap.getJsonMaps(); String
+		 * basePath = queryMap.getBasePath(); persistRepository.persist(query, jsonMaps,
+		 * json, basePath);
+		 * 
+		 * } } }
+		 */
+
 	}
-	
+
 }

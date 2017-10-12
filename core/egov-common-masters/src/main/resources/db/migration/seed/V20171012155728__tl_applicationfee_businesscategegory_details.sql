d44a734d28d8ba2c6f9ee8f05571885019ce4cd2
Barath Vutukuri <barath.vutukuri@goodworklabs.com>
2017-10-12 16:11:33 +0530

    EGSVC-1709 modifying business category name for tradelicense new application

--- core/egov-common-masters/src/main/resources/db/migration/seed/V20171012155728__tl_applicationfee_businesscategegory_details.sql
+++ core/egov-common-masters/src/main/resources/db/migration/seed/V20171012155728__tl_applicationfee_businesscategegory_details.sql
@@ -1,7 +1,7 @@
 INSERT INTO eg_businesscategory(
           id, name, code, active, tenantid, version, createdby, createddate, 
           lastmodifiedby, lastmodifieddate)
-  VALUES (nextval('seq_eg_businesscategory'),'Trade Application Fee','NEWTRADEAPPLICATIONFEE',true,'default',0,1,
+  VALUES (nextval('seq_eg_businesscategory'),'Trade Application Fee','TLAPPLNFEE',true,'default',0,1,
   (select extract ('epoch' from (select * from now()))*1000), 
   1, (select extract ('epoch' from (select * from now()))*1000));
 
@@ -11,6 +11,6 @@ INSERT INTO eg_businessdetails(
           isvoucherapproved, vouchercutoffdate, createddate, lastmodifieddate, 
           createdby, lastmodifiedby, ordernumber, version, tenantid, callbackforapportioning)
   VALUES (nextval('seq_eg_businessdetails'), 'Trade Application Fee', '/receipts/receipt-create.action', 
-  true, 'NEWTRADEAPPLICATIONFEE','B','01','101500',null,null,'HD',false,(select id from eg_businesscategory where code='NEWTRADEAPPLICATIONFEE' 
+  true, 'TLAPPLNFEE','B','01','101500',null,null,'HD',false,(select id from eg_businesscategory where code='TLAPPLNFEE' 
   and tenantid='default'),false,null,(select extract ('epoch' from (select * from now()))*1000),(select extract ('epoch' from (select * 
   from now()))*1000),1,1,1,0,'default',false);

0ef09f4fe7684f6f8e43d64c2e5781f53be17538
Yosadhara <yosadhara.pradhan@walkingtree.in>
2017-10-13 20:28:35 +0530

    Added changes in workflow

--- propertytax/pt-workflow/src/main/java/org/egov/propertyWorkflow/consumer/WorkFlowUtil.java
+++ propertytax/pt-workflow/src/main/java/org/egov/propertyWorkflow/consumer/WorkFlowUtil.java
@@ -159,8 +159,9 @@ public class WorkFlowUtil {
 		task.setAction(workflowDetails.getAction());
 		task.setStatus(workflowDetails.getStatus());
 		task.setTenantId(workflowDetailsRequest.getTenantId());
-		if ( !workflowDetails.getAction().equalsIgnoreCase(propertiesManager.getSpecialNoticeAction())){
-		assignee.setId((long) workflowDetails.getAssignee());
+		if (!workflowDetails.getAction().equalsIgnoreCase(propertiesManager.getSpecialNoticeAction())
+				&& !workflowDetails.getAction().equalsIgnoreCase(propertiesManager.getCancel())) {
+			assignee.setId((long) workflowDetails.getAssignee());
 		}
 		else{
 			assignee.setId(null);

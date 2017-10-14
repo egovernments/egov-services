ddcec6797801b62d92ae5268b6de377f76a0fab5
prasadkhandagale <prasad.khandagale@walkingtree.tech>
2017-10-14 13:24:52 +0530

    Adding the demolition model's

---
+++ propertytax/propertyutil/src/main/java/org/egov/models/Demolition.java
@@ -0,0 +1,78 @@
+package org.egov.models;
+
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 
+ * @author Prasad
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Demolition {
+	
+	@JsonProperty("id")
+	private Long id = null;
+	
+	@JsonProperty("tenantId")
+	@NotNull
+	@Size(min = 4, max = 128)
+	private String tenantId =null;
+	
+	@JsonProperty("upicNumber")
+	@NotNull
+	@Size(min = 6, max = 128)
+	private String upicNumber = null;
+	
+	@JsonProperty("propertySubType")
+	private String propertySubType =null;
+	
+	@JsonProperty("usageType")
+	private String usageType =null;
+	
+	@JsonProperty("usageSubType")
+	private String usageSubType =null;
+	
+	@JsonProperty("totalArea")
+	private Double totalArea =null;
+	
+	@JsonProperty("sequenceNo")
+	private Long sequenceNo =null;
+	
+	@JsonProperty("isLegal")
+	private Boolean isLegal =null;
+	
+	@JsonProperty("demolitionReason")
+	private String demolitionReason =null;
+	
+	@JsonProperty("documents")
+	private List<Document> documents=null;
+	
+	@JsonProperty("comments")
+	private String comments =null;
+	
+	@JsonProperty("stateId")
+	private String stateId = null;
+	
+	@JsonProperty("auditDetails")
+	private AuditDetails auditDetails=null;
+	
+	@JsonProperty("workFlowDetails")
+	private WorkFlowDetails workFlowDetails;
+	
+
+}

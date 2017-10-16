72da531d894a61769a7647647651c942279e643c
saurabh297 <saurabh91verma@gmail.com>
2017-10-16 18:07:21 +0530

    asset screen Ui is done

---
+++ web/react-pgr-web/src/components/framework/specs/asset/master/assetMaster.js
@@ -0,0 +1,633 @@
+var dat = {
+	"asset.create": {
+		"numCols": 12/3,
+		"url": "",
+		"tenantIdRequired": true,
+		"idJsonPath": "",
+		"objectName": "",
+		"groups": [
+			{
+				"label": "Header Details",
+				"name": "createAsset",
+				"fields": [
+						{
+							"name": "NewReferenceNumber",
+							"jsonPath": "",
+							"label": "Asset ID No",
+							"pattern": "^[\s.]*([^\s.][\s.]*){0,100}$",
+							"type": "text",
+							"isRequired": false,
+							"isDisabled": false,
+							"requiredErrMsg": "",
+							"patternErrMsg": ""
+						},
+            {
+  						"name": "OldReferenceNumber",
+  						"jsonPath": "",
+  						"label": "Old Asset ID No",
+  						"pattern": "",
+  						"type": "text",
+  						"url": "",
+  						"isRequired": false,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+              "name": "Date",
+              "jsonPath": "",
+              "label": "Date",
+              "pattern": "",
+              "type": "date",
+              "isRequired": true,
+              "isDisabled": false,
+              "requiredErrMsg": "",
+              "patternErrMsg": ""
+            },
+            {
+  						"name": "RefNoOfWIP",
+  						"jsonPath": "",
+  						"label": "Ref. no of WIP Register",
+  						"pattern": "",
+  						"type": "singleValueList",
+  						"url": "",
+  						"isRequired": true,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+  						"name": "AssetName",
+  						"jsonPath": "",
+  						"label": "Asset Name",
+  						"pattern": "",
+  						"type": "text",
+  						"url": "",
+  						"isRequired": true,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+  						"name": "AssetCategoryType",
+  						"jsonPath": "",
+  						"label": "Asset Sub Category Name",
+  						"pattern": "",
+  						"type": "singleValueList",
+  						"url": "",
+  						"isRequired": true,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+  						"name": "Department",
+  						"jsonPath": "",
+  						"label": "Department",
+  						"pattern": "",
+  						"type": "singleValueList",
+  						"url": "",
+  						"isRequired": true,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+  						"name": "NoOfOrder",
+  						"jsonPath": "",
+  						"label": "No Of Order",
+  						"pattern": "",
+  						"type": "number",
+  						"url": "",
+  						"isRequired": false,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+              "name": "DateOfOrder",
+              "jsonPath": "",
+              "label": "Date Of Order",
+              "pattern": "",
+              "type": "date",
+              "isRequired": false,
+              "isDisabled": false,
+              "requiredErrMsg": "",
+              "patternErrMsg": ""
+            },
+            {
+              "name": "DateOfAcquisition",
+              "jsonPath": "",
+              "label": "Date Of Acquisition",
+              "pattern": "",
+              "type": "date",
+              "isRequired": true,
+              "isDisabled": false,
+              "requiredErrMsg": "",
+              "patternErrMsg": ""
+            },
+            {
+  						"name": "ModeofAcquisition",
+  						"jsonPath": "",
+  						"label": "Mode of Acquisition",
+  						"pattern": "",
+  						"type": "singleValueList",
+  						"url": "",
+  						"isRequired": false,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+            {
+  						"name": "LandAssetID",
+  						"jsonPath": "",
+  						"label": "Land Asset ID",
+  						"pattern": "",
+  						"type": "autoCompelete",
+  						"url": "",
+  						"isRequired": false,
+  						"isDisabled": false,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": "",
+              "autoCompleteDependancy": {
+								"autoCompleteUrl": "/egf-masters/chartofaccounts/_search?id={value}",
+								"autoFillFields": {
+									"BusinessDetails[0].accountDetails[0].chartOfAccounts": "chartOfAccounts[0].glcode"
+								 }
+							 }
+  					},
+            {
+  						"name": "SurveyNoOfLandOnWhichStructureIsLocated ",
+  						"jsonPath": "BusinessDetails[0].accountDetails[0].chartOfAccounts",
+  						"label": "Survey no of land on which structure is located ",
+  						"pattern": "",
+  						"type": "number",
+  						"url": "",
+  						"isRequired": false,
+  						"isDisabled": true,
+  						"requiredErrMsg": "",
+  						"patternErrMsg": ""
+  					},
+				]
+			},
+      {
+				"label": "Location Details",
+				"name": "LocationField",
+        "multiple":false,
+        "jsonPath":"",
+				"fields": [
+          {
+            "name": "Location",
+            "jsonPath": "",
+            "label": "Location",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "Longitude",
+            "jsonPath": "",
+            "label": "Longitude",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": true,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "Latitude",
+            "jsonPath": "",
+            "label": "Latitude",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": true,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "Address",
+            "jsonPath": "",
+            "label": "Address",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "ElectionWard",
+            "jsonPath": "",
+            "label": "Election Ward",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "No of Floors",
+            "jsonPath": "",
+            "label": "No of Floors",
+            "pattern": "",
+            "type": "number",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "PlinthArea",
+            "jsonPath": "",
+            "label": "Plinth Area",
+            "pattern": "",
+            "type": "number",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "CubicContents",
+            "jsonPath": "",
+            "label": "Cubic Contents",
+            "pattern": "",
+            "type": "number",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "DimensionOfStructure(L,B,H)",
+            "jsonPath": "",
+            "label": "Dimension of Structure (L,B,H)",
+            "pattern": "",
+            "type": "number",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "Usage",
+            "jsonPath": "",
+            "label": "Usage",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+
+				]
+			},
+      {
+				"label": "Asset Details",
+				"name": "AssetField",
+        "multiple":false,
+        "jsonPath":"",
+				"fields": [
+          {
+            "name": "AnticipatedLifeOfAsset",
+            "jsonPath": "",
+            "label": "Anticipated life of Asset",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": true,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "OriginalValueOfAsset",
+            "jsonPath": "",
+            "label": "Original Value of Asset",
+            "pattern": "^[1-9]\\d{0,3}(\\.\\d{0,3})*(,\\d+)?$",
+            "type": "number",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "FromWhomAcquired",
+            "jsonPath": "",
+            "label": "From whom acquired",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "TitleDocumentsAvailable",
+            "jsonPath": "",
+            "label": "Title documents available",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "SourceOfFunds",
+            "jsonPath": "",
+            "label": "Source of funds",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "Warranty",
+            "jsonPath": "",
+            "label": "Warranty",
+            "pattern": "",
+            "type": "radio",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": "",
+            "values": [{"label":"Yes", "value":true},{"label":"No", "value":false}],
+            "defaultValue":true
+          },
+          {
+            "name": "WarrantyExpiryDate",
+            "jsonPath": "",
+            "label": "Warranty expiry date",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "DefectLiabilityPeriod",
+            "jsonPath": "",
+            "label": "Defect liability Period",
+            "pattern": "",
+            "type": "number",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "SecurityDepositRetained",
+            "jsonPath": "",
+            "label": "Security deposit retained",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": true,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "SecurityDepositRealized",
+            "jsonPath": "",
+            "label": "Security deposit realized",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": true,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "AssetDescription",
+            "jsonPath": "",
+            "label": "Asset description",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "AssetAcountCode",
+            "jsonPath": "",
+            "label": "Asset account code",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "AccumulatedDepreciationAccount",
+            "jsonPath": "",
+            "label": "Accumulated Depreciation Account",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "RevaluationReserveAccount",
+            "jsonPath": "",
+            "label": "Revaluation Reserve Account",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "DepreciationExpensesAccount ",
+            "jsonPath": "",
+            "label": "Depreciation Expenses Account ",
+            "pattern": "",
+            "type": "singleValueList",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+
+				]
+			},
+      {
+				"name": "TableField",
+        "jsonPath":"",
+				"fields": [
+          // {
+          //   "name": "AnticipatedLifeOfAsset",
+          //   "label": "Anticipated life of Asset",
+          //   "type": "dynamicTable"
+          //   "resultList": {
+          //     "resultHeader": [{"label": "asset.create.test"}],
+          //     "resultValues": [[{}, ], [], []]
+          //   }
+          // },
+          {
+            "name": "OpeningDate",
+            "jsonPath": "",
+            "label": "Opening date",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "OpeningWrittenDownValue",
+            "jsonPath": "",
+            "label": "Opening Written down Value",
+            "pattern": "^[1-9]\\d{0,3}(\\.\\d{0,3})*(,\\d+)?$",
+            "type": "number",
+            "url": "",
+            "isRequired": true,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "AdditionDate",
+            "jsonPath": "",
+            "label": "Addition Date",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "AdditionValue",
+            "jsonPath": "",
+            "label": "Addition Value",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "RevaluationDate",
+            "jsonPath": "",
+            "label": "Revaluation date",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "RevaluationValue",
+            "jsonPath": "",
+            "label": "Revaluation Value",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "DepreciationDate",
+            "jsonPath": "",
+            "label": "Depreciation Date",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "DepreciationValue",
+            "jsonPath": "",
+            "label": "Depreciation Value",
+            "pattern": "",
+            "type": "text",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "Closing date",
+            "jsonPath": "",
+            "label": "Closing Date",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          },
+          {
+            "name": "ClosingValue",
+            "jsonPath": "",
+            "label": "Closing written Down Value",
+            "pattern": "",
+            "type": "date",
+            "url": "",
+            "isRequired": false,
+            "isDisabled": false,
+            "requiredErrMsg": "",
+            "patternErrMsg": ""
+          }
+
+				]
+			}
+		]
+	}
+}
+
+export default dat;

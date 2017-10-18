96a83f1de73b20eb28267a80661c3f76534789e9
prasadkhandagale <prasad.khandagale@walkingtree.tech>
2017-10-18 19:59:33 +0530

    EGSVC-1615 Demolition of property

---
+++ propertytax/propertyutil/src/main/java/org/egov/models/DemolitionReasonResponse.java
@@ -0,0 +1,19 @@
+package org.egov.models;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DemolitionReasonResponse {
+	
+	private ResponseInfo responseInfo;
+
+	private List<DemolitionReason> demolitionReason;
+}

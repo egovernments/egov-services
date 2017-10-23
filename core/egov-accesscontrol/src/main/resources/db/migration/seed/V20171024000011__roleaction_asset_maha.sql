926d3e4ebbc900ca1c35c718d27b53366615be93
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-10-24 01:09:50 +0530

    url updated

--- core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171024000011__roleaction_asset_maha.sql
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171024000011__roleaction_asset_maha.sql
@@ -1,12 +1,13 @@
 update eg_action set displayname='View Asset Service Maha' where name='ViewAssetServiceMaha' and displayname='View Asset Service';
 update eg_action set displayname='Create Asset Service Maha' where name='CreateAssetServiceMaha' and displayname='Create Asset Service';
 
+update eg_action set url='/asset-services-maha/assets/_create' where name='CreateAssetServiceMaha' and displayname='Create Asset Service Maha';
+update eg_action set url='/asset-services-maha/assets/_search' where name='ViewAssetServiceMaha' and displayname='View Asset Service Maha';
+insert into eg_roleaction(roleCode, actionid, tenantId) 
+
+values ('SUPERUSER', (select id from eg_action where name='ViewAssetServiceMaha' and url='/asset-services-maha/assets/_search' and displayname='View Asset Service Maha'), 'default');
 
 insert into eg_roleaction(roleCode, actionid, tenantId) 
 
-values ('SUPERUSER', (select id from eg_action where name='ViewAssetServiceMaha' and url='/assets/_search' and displayname='View Asset Service Maha'), 'default');
-
-insert into eg_roleaction(roleCode, actionid, tenantId) 
-
-values ('SUPERUSER', (select id from eg_action where name='CreateAssetServiceMaha' and url='/assets/_create' and displayname='Create Asset Service Maha'), 'default');
+values ('SUPERUSER', (select id from eg_action where name='CreateAssetServiceMaha' and url='/asset-services-maha/assets/_create' and displayname='Create Asset Service Maha'), 'default');
 

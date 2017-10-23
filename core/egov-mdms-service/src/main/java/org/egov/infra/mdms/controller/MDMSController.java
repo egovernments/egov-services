20257968a562c4b23a38ee8f4cd9f00bb7fb278e
Ghanshyam <ghanshyam.rawat@riflexions.com>
2017-10-23 13:12:42 +0530

    Add get path in mdms

--- core/egov-mdms-service/src/main/java/org/egov/infra/mdms/controller/MDMSController.java
+++ core/egov-mdms-service/src/main/java/org/egov/infra/mdms/controller/MDMSController.java
@@ -1,19 +1,25 @@
 package org.egov.infra.mdms.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
 
+import org.egov.common.contract.request.RequestInfo;
 import org.egov.infra.mdms.service.MDMSService;
+import org.egov.mdms.model.MasterDetail;
+import org.egov.mdms.model.MdmsCriteria;
 import org.egov.mdms.model.MdmsCriteriaReq;
 import org.egov.mdms.model.MdmsResponse;
+import org.egov.mdms.model.ModuleDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,5 +47,49 @@ public class MDMSController {
 		mdmsResponse.setMdmsRes(response);
 		return new ResponseEntity<>(mdmsResponse, HttpStatus.OK);
 
+		
 	}
+	
+
+	@PostMapping("_get")
+	@ResponseBody
+	private ResponseEntity<?> search(@RequestParam("moduleName") String module,
+									 @RequestParam("masterName") String master,
+									 @RequestParam(value = "filter", required = false) String filter,
+									 @RequestParam("tenantId") String tenantId,
+									 @RequestBody RequestInfo requestInfo){
+
+    	log.info("MDMSController mdmsCriteriaReq [" + module + ", " + master + ", " + filter + "]");
+    	/*if(bindingResult.hasErrors()) {
+    		throw new CustomBindingResultExceprion(bindingResult);
+    	}*/
+
+    	MdmsCriteriaReq mdmsCriteriaReq = new MdmsCriteriaReq();
+    	mdmsCriteriaReq.setRequestInfo(requestInfo);
+		MdmsCriteria criteria = new MdmsCriteria();
+		criteria.setTenantId(tenantId);
+
+		ModuleDetail detail = new ModuleDetail();
+		detail.setModuleName(module);
+
+		MasterDetail masterDetail = new MasterDetail();
+		masterDetail.setName(master);
+		masterDetail.setFilter(filter);
+		ArrayList<MasterDetail> masterList = new ArrayList<>();
+		masterList.add(masterDetail);
+		detail.setMasterDetails(masterList);
+
+		ArrayList<ModuleDetail> moduleList = new ArrayList<>();
+		moduleList.add(detail);
+
+		criteria.setModuleDetails(moduleList);
+		mdmsCriteriaReq.setMdmsCriteria(criteria);
+
+		Map<String, List<Map<String, JSONArray>>> response = mdmsService.getMaster(mdmsCriteriaReq);
+		MdmsResponse mdmsResponse = new MdmsResponse();
+		mdmsResponse.setMdmsRes(response);
+		return new ResponseEntity<>(mdmsResponse ,HttpStatus.OK);
+
+	}
+	
 }

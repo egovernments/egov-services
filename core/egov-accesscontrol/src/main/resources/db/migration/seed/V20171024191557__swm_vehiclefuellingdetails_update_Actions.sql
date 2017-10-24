22b75b3f5479c7cad7dd811bde082ae611598dab
venki <venki@egovernments.org>
2017-10-24 19:20:15 +0530

    Updating swm vehicle fuelling details actions whth new urls.

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171024191557__swm_vehiclefuellingdetails_update_Actions.sql
@@ -0,0 +1,5 @@
+update eg_action set url = '/swm-services/vehiclefuellingdetails/_create' where name = 'VehicleFuellingDetails Create';
+
+update eg_action set url = '/swm-services/vehiclefuellingdetails/_update' where name = 'VehicleFuellingDetails Update';
+
+update eg_action set url = '/swm-services/vehiclefuellingdetails/_search' where name = 'VehicleFuellingDetails Search';

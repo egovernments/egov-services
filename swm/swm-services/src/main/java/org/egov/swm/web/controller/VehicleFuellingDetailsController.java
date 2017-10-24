6ad54cc37776b2bacba6bd2df6585e359da71c3e
venki <venki@egovernments.org>
2017-10-24 17:54:26 +0530

    Updated request mapping in vehicle fuelling details.

--- swm/swm-services/src/main/java/org/egov/swm/web/controller/VehicleFuellingDetailsController.java
+++ swm/swm-services/src/main/java/org/egov/swm/web/controller/VehicleFuellingDetailsController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/vehiclefuellingdetailses")
+@RequestMapping("/vehiclefuellingdetails")
 public class VehicleFuellingDetailsController {
 
 	@Autowired

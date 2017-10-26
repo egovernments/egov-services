4907f9ff4dc84b1407c23dd814b6a49d21251ed3
sourabh <sourabh.debnath@goodworklabs.com>
2017-10-26 12:14:17 +0530

    Fixed- Deletion in array text/number component

--- web/react-pgr-web/src/components/framework/components/UiArrayField.js
+++ web/react-pgr-web/src/components/framework/components/UiArrayField.js
@@ -20,34 +20,121 @@ export default class UiArrayField extends Component {
 		switch (this.props.ui) {
 			case 'google':
 				return (
-						<Row>
-							<Col xs={12} md={6}>
-								<TextField
-						            className = "cutustom-form-controll-for-textfield"
-						            id = {item.jsonPath.split(".").join("-")}
-									floatingLabelStyle = {{"color": "#A9A9A9", "fontSize": "20px", "white-space": "nowrap"}}
-									inputStyle = {{"color": "#5F5C57"}}
-									floatingLabelFixed = {true}
-									maxLength = {item.maxLength || ""}
-									style = {{"display": (item.hide ? 'none' : 'inline-block')}}
-									errorStyle = {{"float":"left"}}
-									fullWidth = {true}
-									floatingLabelText = {<span>{item.label} <span style={{"color": "#FF0000"}}>{item.isRequired ? " *" : ""}</span></span>}
-									value = {this.state.valueList.join(", ")}
-									disabled = {true}
-								/>
-							</Col>
-							<Col xs={12} md={6}>
-								<FloatingActionButton 
-									style={{marginTop: 39}} 
-									mini={true}
-									onClick={()=>{
-										this.handleOpen()
-									}}>
-									<i className="material-icons">add</i>
-								</FloatingActionButton>
-							</Col>
-						</Row>
+						<div>
+							<Row>
+								<Col xs={12} md={6}>
+									<TextField
+							            className = "cutustom-form-controll-for-textfield"
+							            id = {item.jsonPath.split(".").join("-")}
+										floatingLabelStyle = {{"color": "#A9A9A9", "fontSize": "20px", "white-space": "nowrap"}}
+										inputStyle = {{"color": "#5F5C57"}}
+										floatingLabelFixed = {true}
+										maxLength = {item.maxLength || ""}
+										style = {{"display": (item.hide ? 'none' : 'inline-block')}}
+										errorStyle = {{"float":"left"}}
+										fullWidth = {true}
+										floatingLabelText = {<span>{item.label} <span style={{"color": "#FF0000"}}>{item.isRequired ? " *" : ""}</span></span>}
+										value = {this.state.valueList.join(", ")}
+										disabled = {true}
+									/>
+								</Col>
+								<Col xs={12} md={6}>
+									<FloatingActionButton 
+										style={{marginTop: 39}} 
+										mini={true}
+										onClick={()=>{
+											this.handleOpen()
+										}}>
+										<i className="material-icons">add</i>
+									</FloatingActionButton>
+								</Col>
+							</Row>
+							<Dialog
+					          title={this.props.item.label}
+					          actions={
+					          	<FlatButton
+							        label = {translate("pt.create.button.viewdcb.close")}
+							        primary = {true}
+							        onClick = {this.handleClose}
+							    />
+					          }
+					          modal={false}
+					          open={this.state.open}
+					          onRequestClose={this.handleClose}
+					        >
+					        	<Row>
+									<Col xs={12} md={8}>
+										<TextField 
+							         		className = "cutustom-form-controll-for-textfield"
+							         		floatingLabelStyle = {{"color": "#696969", "fontSize": "20px", "white-space": "nowrap"}}
+											inputStyle = {{"color": "#5F5C57"}}
+											floatingLabelFixed = {true}
+											value = {this.state.currentValue}
+											maxLength = {this.props.item.maxLength || ""}
+											errorStyle = {{"float":"left"}}
+											fullWidth = {true}
+											floatingLabelText = {<span>{this.props.item.label}</span>}
+											errorText = {this.state.currentValue && this.props.item.pattern && !new RegExp(this.props.item.pattern).test(this.state.currentValue) ? (this.props.item.patternErrMsg || "Invalid Pattern") : ""}
+											onChange = {(e) => {
+												if(e.target.value) {
+													e.target.value = e.target.value.replace(/^\s*/, "");
+													if(e.target.value[e.target.value.length-1] == " " && e.target.value[e.target.value.length-2] == " ")
+														return;
+												}
+
+												if(this.props.item.type == "arrayNumber" && e.target.value && !/^\d.*$/.test(e.target.value)) return;
+												this.setState({ currentValue: e.target.value })
+											}} />
+									</Col>
+									<Col xs={12} md={4}>
+										<FlatButton
+											label = {translate("pt.create.groups.ownerDetails.fields.add")}
+											disabled = {!this.state.currentValue || (this.state.currentValue && this.props.item.pattern && !new RegExp(this.props.item.pattern).test(this.state.currentValue))}
+											secondary = {true}
+											style = {{"marginTop": 39}}
+											onClick = {(e) => {
+												let list = [...this.state.valueList];
+												list.push(this.state.currentValue);
+												this.setState({ 
+													valueList: list,
+													currentValue: ''
+												}, () => {
+													this.props.handler({target: { value: this.state.valueList }}, this.props.item.jsonPath, this.props.item.isRequired ? true : false, '', this.props.item.requiredErrMsg, this.props.item.patternErrMsg)
+												});
+
+											}}
+										/>
+									</Col>
+								</Row>
+								<br/>
+								<Table className="table table-striped table-bordered" responsive>
+									<thead>
+										<tr>
+											<th>#</th>
+											<th>{translate("collection.pay.value")}</th>
+											<th>{translate("reports.common.action")}</th>
+										</tr>
+									</thead>
+									<tbody>
+										{
+											this.state.valueList.map((v, i) => {
+												return (
+													<tr key={i}>
+														<td>{i+1}</td>
+														<td>{v}</td>
+														<td>
+															<div className="material-icons" onClick={()=>{
+																this.valueFromList(i)
+															}}>delete</div>
+														</td>
+													</tr>
+												)
+											})
+										}
+									</tbody>
+								</Table>
+					        </Dialog>
+						</div>
 					);
 		}
 	}
@@ -69,6 +156,8 @@ export default class UiArrayField extends Component {
 		list.splice(index, 1);
 		this.setState({
 			valueList: list
+		}, () => {
+			this.props.handler({target: { value: this.state.valueList.length ? this.state.valueList : "" }}, this.props.item.jsonPath, this.props.item.isRequired ? true : false, '', this.props.item.requiredErrMsg, this.props.item.patternErrMsg);
 		})
 	}
 
@@ -76,91 +165,6 @@ export default class UiArrayField extends Component {
 		return (
 			<div>
 				{this.renderArrayField(this.props.item)}
-				<Dialog
-		          title={this.props.item.label}
-		          actions={
-		          	<FlatButton
-				        label = {translate("pt.create.button.viewdcb.close")}
-				        primary = {true}
-				        onClick = {this.handleClose}
-				    />
-		          }
-		          modal={false}
-		          open={this.state.open}
-		          onRequestClose={this.handleClose}
-		        >
-		        	<Row>
-						<Col xs={12} md={8}>
-							<TextField 
-				         		className = "cutustom-form-controll-for-textfield"
-				         		floatingLabelStyle = {{"color": "#696969", "fontSize": "20px", "white-space": "nowrap"}}
-								inputStyle = {{"color": "#5F5C57"}}
-								floatingLabelFixed = {true}
-								value = {this.state.currentValue}
-								maxLength = {this.props.item.maxLength || ""}
-								errorStyle = {{"float":"left"}}
-								fullWidth = {true}
-								floatingLabelText = {<span>{this.props.item.label}</span>}
-								errorText = {this.state.currentValue && this.props.item.pattern && !new RegExp(this.props.item.pattern).test(this.state.currentValue) ? (this.props.item.patternErrMsg || "Invalid Pattern") : ""}
-								onChange = {(e) => {
-									if(e.target.value) {
-										e.target.value = e.target.value.replace(/^\s*/, "");
-										if(e.target.value[e.target.value.length-1] == " " && e.target.value[e.target.value.length-2] == " ")
-											return;
-									}
-
-									if(this.props.item.type == "arrayNumber" && e.target.value && !/^\d.*$/.test(e.target.value)) return;
-									this.setState({ currentValue: e.target.value })
-								}} />
-						</Col>
-						<Col xs={12} md={4}>
-							<FlatButton
-								label = {translate("pt.create.groups.ownerDetails.fields.add")}
-								disabled = {!this.state.currentValue || (this.state.currentValue && this.props.item.pattern && !new RegExp(this.props.item.pattern).test(this.state.currentValue))}
-								secondary = {true}
-								style = {{"marginTop": 39}}
-								onClick = {(e) => {
-									let list = [...this.state.valueList];
-									list.push(this.state.currentValue);
-									this.setState({ 
-										valueList: list,
-										currentValue: ''
-									}, () => {
-										this.props.handler({target: { value: this.state.valueList }}, this.props.item.jsonPath, this.props.item.isRequired ? true : false, '', this.props.item.requiredErrMsg, this.props.item.patternErrMsg)
-									});
-
-								}}
-							/>
-						</Col>
-					</Row>
-					<br/>
-					<Table className="table table-striped table-bordered" responsive>
-						<thead>
-							<tr>
-								<th>#</th>
-								<th>{translate("collection.pay.value")}</th>
-								<th>{translate("reports.common.action")}</th>
-							</tr>
-						</thead>
-						<tbody>
-							{
-								this.state.valueList.map((v, i) => {
-									return (
-										<tr key={i}>
-											<td>{i+1}</td>
-											<td>{v}</td>
-											<td>
-												<div className="material-icons" onClick={()=>{
-													this.valueFromList(i)
-												}}>delete</div>
-											</td>
-										</tr>
-									)
-								})
-							}
-						</tbody>
-					</Table>
-		        </Dialog>
 			</div>
 	    );
 	}

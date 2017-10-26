96d4f72326b8bd7b86e2fd2927a2ecbd531f288f
shashi <shashikanth.g@egovernments.org>
2017-10-26 12:18:23 +0530

    Removed old search end point in land acquisition service.

--- lams-maha/lams-services/src/main/java/org/egov/lams/services/web/controller/LandAcquisitionController.java
+++ lams-maha/lams-services/src/main/java/org/egov/lams/services/web/controller/LandAcquisitionController.java
@@ -35,70 +35,40 @@ public class LandAcquisitionController {
 	@PostMapping("_create")
 	public ResponseEntity<?> create(@RequestBody @Valid final LandAcquisitionRequest landAcquisitionRequest,
 			final BindingResult bindingResult) {
-		
+
 		if (bindingResult.hasErrors())
-			return new ResponseEntity<>(responseFactory.getErrorResponse(bindingResult, landAcquisitionRequest.getRequestInfo()),HttpStatus.BAD_REQUEST);
-		
-		return new ResponseEntity<>(landAcquisitionService.create(landAcquisitionRequest),HttpStatus.CREATED);
+			return new ResponseEntity<>(
+					responseFactory.getErrorResponse(bindingResult, landAcquisitionRequest.getRequestInfo()),
+					HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(landAcquisitionService.create(landAcquisitionRequest), HttpStatus.CREATED);
 	}
 
 	@PostMapping("_update")
 	public ResponseEntity<?> update(@RequestBody @Valid final LandAcquisitionRequest landAcquisitionRequest,
 			final BindingResult bindingResult) {
-		
-		if (bindingResult.hasErrors())
-			return new ResponseEntity<>(responseFactory.getErrorResponse(bindingResult, landAcquisitionRequest.getRequestInfo()),HttpStatus.BAD_REQUEST);
 
-		return new ResponseEntity<>(landAcquisitionService.update(landAcquisitionRequest),HttpStatus.OK);	
-		}
-	
+		if (bindingResult.hasErrors())
+			return new ResponseEntity<>(
+					responseFactory.getErrorResponse(bindingResult, landAcquisitionRequest.getRequestInfo()),
+					HttpStatus.BAD_REQUEST);
+
+		return new ResponseEntity<>(landAcquisitionService.update(landAcquisitionRequest), HttpStatus.OK);
+	}
+
 	@PostMapping("_search")
 	@ResponseBody
 	public ResponseEntity<?> search(@RequestBody @Valid final RequestInfoWrapper requestInfoWrapper,
 			@ModelAttribute @Valid final LandAcquisitionSearchCriteria landAcquisitionSearchCriteria,
 			final BindingResult bindingResult) {
-		
+
 		if (bindingResult.hasErrors()) {
-			final ErrorResponse errorResponse = responseFactory.getErrorResponse(bindingResult, requestInfoWrapper.getRequestInfo());
+			final ErrorResponse errorResponse = responseFactory.getErrorResponse(bindingResult,
+					requestInfoWrapper.getRequestInfo());
 			return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 		}
-		final LandAcquisitionResponse landAcquisitionResponse = landAcquisitionService.search(landAcquisitionSearchCriteria,
-				requestInfoWrapper.getRequestInfo());
+		final LandAcquisitionResponse landAcquisitionResponse = landAcquisitionService
+				.search(landAcquisitionSearchCriteria, requestInfoWrapper.getRequestInfo());
 		return new ResponseEntity<>(landAcquisitionResponse, HttpStatus.OK);
 	}
-	/*@PostMapping("/_search")
-    public  ResponseEntity<?>  search(@RequestParam(value = "tenantId", defaultValue = "default") String tenantId,
-                                    @RequestParam(value = "landAcquisitionId", required = false) String landAcquisitionId,
-                                    @RequestParam(value = "landAquisitionNumber", required = false) String landAquisitionNumber,
-                                    @RequestParam(value = "landOwnerName", required = false) String landOwnerName,
-                                    @RequestParam(value = "surveyNo", required = false) String surveyNo,
-                                    @RequestParam(value = "ctsNumber", required = false) String ctsNumber,
-                                    @RequestParam(value = "advocateName", required = false) String advocateName,
-                                    @RequestParam(value = "landType", required = false) String landType,
-                                    @RequestParam(value = "organizationName", required = false) String organizationName,
-                                    @RequestParam(value = "pageSize", required = false ) int pageSize,
-                                    @RequestParam(value = "offSet", required = false ) int pageNumber,
-                                    @RequestParam(value = "sortBy", required = false ) int pageNumber,
-                                    @RequestBody RequestInfo requestInfo){
-
-		LandAcquisitionSearchCriteria landAcquisitionSearchCriteria = LandAcquisitionSearchCriteria.builder()
-                .tenantId(tenantId)
-                .landAcquisitionId(landAcquisitionId)
-                .surveyNo(surveyNo)
-                .landOwnerName(landOwnerName)
-                .landType(landType)
-                .organizationName(organizationName)
-                .landAcquisitionNumber(landAquisitionNumber)
-                .ctsNumber(ctsNumber)
-                .pageSize(pageSize)
-                .offSet(offset)
-                .sortBy(sortBy)
-                .build();
-		
-		Pagination<La> materialList = materialService
-                .search(materialSearchRequest);
-		
-		return new ResponseEntity<>(landAcquisitionService.search(landAcquisitionSearchCriteria),HttpStatus.OK);
-
-    }*/
 }

a70a237e0d1bd2c713ec41db4da6ef8b194d3b92
kusumaRS <kusuma.rs@riflexions.com>
2017-10-26 16:58:02 +0530

    updated url

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171026164603__roleaction_assetcurrentvalue_maha.sql
@@ -0,0 +1,4 @@
+ update eg_action set url='/asset-services-maha/assets/currentvalues/_search' where url='asset-services-maha/assets/currentvalues/_search' and name='ViewAsseturrentValueServiceMaha';
+
+
+ update eg_action set url='/asset-services-maha/assets/currentvalues/_create' where url='asset-services-maha/assets/currentvalues/_create' and name='CreateAssetCurrentValueServiceMaha';

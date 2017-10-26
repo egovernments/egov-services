f93667606d8d38cfbfb5e03dea3eedcb9c21f0a6
venki <venki@egovernments.org>
2017-10-26 18:45:23 +0530

    Added actions for vehicle master.

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171026184404__swm_vehicle_actions_dml.sql
@@ -0,0 +1,17 @@
+insert into service (id, code, name, enabled, displayname, ordernumber, parentmodule, tenantId) values (nextval('SEQ_SERVICE'), 'SWM Masters', 'Masters', true, 'Masters', 1, (select id from service where code = 'SWM' and tenantId='default'), 'default');
+
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'Vehicle Create','/swm-services/vehicles/_create','SWM Masters',null,(select id from service where code='SWM Masters' and tenantid='default'),0,'Vehicle Create',true,1,now(),1,now());
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'Vehicle Update','/swm-services/vehicles/_update','SWM Masters',null,(select id from service where code='SWM Masters' and tenantid='default'),0,'Vehicle Update',true,1,now(),1,now());
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'Vehicle Search','/swm-services/vehicles/_search','SWM Masters',null,(select id from service where code='SWM Masters' and tenantid='default'),0,'Vehicle Search',true,1,now(),1,now());
+
+
+insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'Vehicle Create' ),'default');
+insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'Vehicle Update' ),'default');
+insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'Vehicle Search' ),'default');
+

b5792756aef9a2905595443c03ca59d67824564e
sourabh <sourabh.debnath@goodworklabs.com>
2017-10-28 16:59:24 +0530

    Added- Uiutility for Ui templates

--- web/ui-auto-gen/templates/create.js
+++ web/ui-auto-gen/templates/create.js
@@ -1,18 +1,43 @@
 var utilities = require('../utilities/utility');
-var uiUtilities = require('../utilities/uiUtility');
 const getType = utilities.getType;
 const getTitleCase = utilities.getTitleCase;
 const setLabels = utilities.setLabels;
 const getQuery = utilities.getQuery;
-const getFieldsFromInnerObject = uiUtilities.getFieldsFromInnerObject;
-const addUrls = uiUtilities.addUrls;
-const addDependents = uiUtilities.addDependents;
-const addAutoFills = uiUtilities.addAutoFills;
-const addRadios = uiUtilities.addRadios;
-const addGroups = uiUtilities.addGroups;
 
 let localeFields = {};
 let errors = {};
+let getFieldsFromInnerObject = function(reference, fields, definition, module, jPath, isArray) {
+    if (definition[reference])
+        for (let key in definition[reference].properties) {
+            if (["id", "tenantId", "auditDetails", "assigner"].indexOf(key) > -1) continue;
+
+            if (definition[reference].properties[key].type == "array") {
+                let refSplitArr = definition[reference].properties[key].items.$ref.split("/");
+                getFieldsFromInnerObject(refSplitArr[refSplitArr.length - 1], fields, definition, module, (isArray ? (jPath + "[0]") : jPath) + "." + key, true);
+            } else if (definition[reference].properties[key].$ref) {
+                let refSplitArr = definition[reference].properties[key].$ref.split("/");
+                getFieldsFromInnerObject(refSplitArr[refSplitArr.length - 1], fields, definition, module, (isArray ? (jPath + "[0]") : jPath) + "." + key);
+            } else {
+                localeFields[module + ".create." + key] = getTitleCase(key);
+                fields[(isArray ? (jPath + "[0]") : jPath) + "." + key] = {
+                    "name": key,
+                    "jsonPath": (isArray ? (jPath + "[0]") : jPath) + "." + key,
+                    "label": module + ".create." + key,
+                    "pattern": definition[reference].properties[key].pattern,
+                    "type": definition[reference].properties[key].enum ? "singleValueList" : definition[reference].properties[key].format && ["number", "integer", "double", "long", "float"].indexOf(definition[reference].properties[key].type) == -1 ? getType(definition[reference].properties[key].format) :  getType(definition[reference].properties[key].type),
+                    "isRequired": (definition[reference].properties[key].required || (definition[reference].required && definition[reference].required.constructor == Array && definition[reference].required.indexOf(key) > -1) ? true : false),
+                    "isDisabled": definition[reference].properties[key].readOnly ? true : false,
+                    "defaultValue": definition[reference].properties[key].default,
+                    "maxLength": definition[reference].properties[key].maxLength,
+                    "minLength": definition[reference].properties[key].minLength,
+                    "patternErrorMsg": definition[reference].properties[key].pattern ? (module + ".create.field.message." + key) : ""
+                };
+
+                if(fields[(isArray ? (jPath + "[0]") : jPath) + "." + key].type == "text" && definition[reference].properties[key].maxLength && definition[reference].properties[key].maxLength > 256)
+                    fields[(isArray ? (jPath + "[0]") : jPath) + "." + key].type = "textarea";
+            }
+        }
+}
 
 let createTemplate = function(module, numCols, path, config, definition, basePath, uiInfoDef) {
     let specifications = {
@@ -52,40 +77,98 @@ let createTemplate = function(module, numCols, path, config, definition, basePat
         }
     }
 
-    let fieldsData = getFieldsFromInnerObject(reference, fields, definition, module, isArr ? (specifications.objectName + "[0]") : specifications.objectName);
-    fields = fieldsData.fields;
-    errors = Object.assign({}, errors, fieldsData.errors);
-    localeFields = Object.assign({}, localeFields, fieldsData.localeFields);
-
+    getFieldsFromInnerObject(reference, fields, definition, module, isArr ? (specifications.objectName + "[0]") : specifications.objectName);
     //=======================CUSTOM FILE LOGIC==========================>>
     if (uiInfoDef.externalData && typeof uiInfoDef.externalData == "object" && uiInfoDef.externalData.length) {
-        fieldsData = addUrls(fields, uiInfoDef);
-        fields = fieldsData.fields;
-        errors = Object.assign({}, errors, fieldsData.errors);
+        for(var i=0; i<uiInfoDef.externalData.length; i++) {
+            if(fields[uiInfoDef.externalData[i].fieldName]) {
+                if(fields[uiInfoDef.externalData[i].fieldName].type == "autoCompelete")
+                    fields[uiInfoDef.externalData[i].fieldName].autoCompleteUrl = uiInfoDef.externalData[i].url + getQuery(uiInfoDef.externalData[i].url, uiInfoDef.externalData[i].keyPath, uiInfoDef.externalData[i].valPath);
+                else
+                    fields[uiInfoDef.externalData[i].fieldName].url = uiInfoDef.externalData[i].url + getQuery(uiInfoDef.externalData[i].url, uiInfoDef.externalData[i].keyPath, uiInfoDef.externalData[i].valPath);
+            } else {
+                errors[uiInfoDef.externalData[i].fieldName] = "Field exists in x-ui-info externalData section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+            }
+        }
     }
 
     if (uiInfoDef.dependents && uiInfoDef.dependents.length) {
-        fieldsData = addDependents(fields, uiInfoDef);
-        fields = fieldsData.fields;
-        errors = Object.assign({}, errors, fieldsData.errors);
+        for (let i = 0; i < uiInfoDef.dependents.length; i++) {
+            if (fields[uiInfoDef.dependents[i].onChangeField]) {
+                fields[uiInfoDef.dependents[i].onChangeField].depedants = [];
+                for (let key in uiInfoDef.dependents[i].affectedFields) {
+                    fields[uiInfoDef.dependents[i].onChangeField].depedants.push({
+                        "jsonPath": key,
+                        "type": uiInfoDef.dependents[i].affectedFields[key].type,
+                        "pattern": uiInfoDef.dependents[i].affectedFields[key].pattern
+                    })
+                }
+            }
+        }
     }
 
     if(uiInfoDef.autoFills && uiInfoDef.autoFills.length) {
-        fieldsData = addAutoFills(fields, uiInfoDef);
-        fields = fieldsData.fields;
-        errors = Object.assign({}, errors, fieldsData.errors);
+        for(var i=0; i< uiInfoDef.autoFills.length; i++) {
+            if(fields[uiInfoDef.autoFills[i].onChangeField]) {
+                fields[uiInfoDef.autoFills[i].onChangeField].type = "textSearch";
+                fields[uiInfoDef.autoFills[i].onChangeField].autoCompleteDependancy = {
+                    autoCompleteUrl: uiInfoDef.autoFills[i].url,
+                    autoFillFields: {}
+                };
+
+                for(var j=0; j< uiInfoDef.autoFills[i].affectedFields.length; j++) {
+                    fields[uiInfoDef.autoFills[i].onChangeField].autoCompleteDependancy.autoFillFields[uiInfoDef.autoFills[i].affectedFields[j]] = uiInfoDef.autoFills[i].affectJSONPath[j];
+                }
+            } else {
+                errors[uiInfoDef.autoFills[i].onChangeField] = "Field exists in x-ui-info AutoFills section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+            }
+        }
+    }
+
+    if(uiInfoDef.checkboxes && uiInfoDef.checkboxes.length) {
+        for(var i=0; i<uiInfoDef.checkboxes.length; i++) {
+            if(fields[uiInfoDef.checkboxes[i]]) {
+                fields[uiInfoDef.checkboxes[i]].type = "checkbox";
+            } else {
+                errors[uiInfoDef.checkboxes[i]] = "Field exists in x-ui-info checkboxes section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;   
+            }
+        }
     }
 
     if(uiInfoDef.radios && uiInfoDef.radios.length) {
-        fieldsData = addRadios(fields, uiInfoDef);
-        errors = Object.assign({}, errors, fieldsData.errors);
-        localeFields = Object.assign({}, localeFields, fieldsData.localeFields);
+        for(var i=0; i<uiInfoDef.radios.length; i++) {
+            if(fields[uiInfoDef.radios[i].jsonPath]) {
+                fields[uiInfoDef.radios[i].jsonPath].type = "radio";
+                localeFields[module + ".create." + uiInfoDef.radios[i].trueLabel] = getTitleCase(uiInfoDef.radios[i].trueLabel);
+                localeFields[module + ".create." + uiInfoDef.radios[i].falseLabel] = getTitleCase(uiInfoDef.radios[i].falseLabel);
+                fields[uiInfoDef.radios[i].jsonPath].values = [{
+                    label: module + ".create." + uiInfoDef.radios[i].trueLabel,
+                    value: true
+                }, {
+                    label: module + ".create." + uiInfoDef.radios[i].falseLabel,
+                    value: false
+                }];
+            } else {
+                errors[uiInfoDef.radios[i].jsonPath] = "Field exists in x-ui-info radios section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;   
+            }
+        }
     }
 
-    fieldsData = addGroups(specifications, fields, uiInfoDef);
-    specifications = fieldsData.specifications;
-    errors = Object.assign({}, errors, fieldsData.errors);
-    localeFields = Object.assign({}, localeFields, fieldsData.localeFields);
+    for (var key in uiInfoDef.groups) {
+        localeFields[module + ".create.group.title." + key] = getTitleCase(key);
+        let group = {
+            name: key,
+            label: module + ".create.group.title." + key,
+            fields: []
+        };
+        for (var i = 0; i < uiInfoDef.groups[key].fields.length; i++) {
+            if (fields[uiInfoDef.groups[key].fields[i]])
+                group.fields.push(fields[uiInfoDef.groups[key].fields[i]]);
+            else
+                errors[uiInfoDef.groups[key].fields[i]] = "Field exists in x-ui-info groups section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+        }
+        specifications.groups.push(group);
+    }
 
     setLabels(localeFields, "./output/" + module);
     if(Object.keys(errors).length) {
---
+++ web/ui-auto-gen/utilities/uiUtility.js
@@ -0,0 +1,166 @@
+var utilities = require('./utility');
+const getTitleCase = utilities.getTitleCase;
+
+let getFieldsFromInnerObject = function(reference, fields, definition, module, jPath, isArray) {
+    let errors = {};
+    let localeFields = {};
+    if (definition[reference])
+        for (let key in definition[reference].properties) {
+            if (["id", "tenantId", "auditDetails", "assigner"].indexOf(key) > -1) continue;
+
+            if (definition[reference].properties[key].type == "array") {
+                let refSplitArr = definition[reference].properties[key].items.$ref.split("/");
+                getFieldsFromInnerObject(refSplitArr[refSplitArr.length - 1], fields, definition, module, (isArray ? (jPath + "[0]") : jPath) + "." + key, true);
+            } else if (definition[reference].properties[key].$ref) {
+                let refSplitArr = definition[reference].properties[key].$ref.split("/");
+                getFieldsFromInnerObject(refSplitArr[refSplitArr.length - 1], fields, definition, module, (isArray ? (jPath + "[0]") : jPath) + "." + key);
+            } else {
+                localeFields[module + ".create." + key] = getTitleCase(key);
+                fields[(isArray ? (jPath + "[0]") : jPath) + "." + key] = {
+                    "name": key,
+                    "jsonPath": (isArray ? (jPath + "[0]") : jPath) + "." + key,
+                    "label": module + ".create." + key,
+                    "pattern": definition[reference].properties[key].pattern,
+                    "type": definition[reference].properties[key].enum ? "singleValueList" : definition[reference].properties[key].format && ["number", "integer", "double", "long", "float"].indexOf(definition[reference].properties[key].type) == -1 ? getType(definition[reference].properties[key].format) : getType(definition[reference].properties[key].type),
+                    "isRequired": (definition[reference].properties[key].required || (definition[reference].required && definition[reference].required.constructor == Array && definition[reference].required.indexOf(key) > -1) ? true : false),
+                    "isDisabled": definition[reference].properties[key].readOnly ? true : false,
+                    "defaultValue": definition[reference].properties[key].default,
+                    "maxLength": definition[reference].properties[key].maxLength,
+                    "minLength": definition[reference].properties[key].minLength,
+                    "patternErrorMsg": definition[reference].properties[key].pattern ? (module + ".create.field.message." + key) : ""
+                };
+
+                if (fields[(isArray ? (jPath + "[0]") : jPath) + "." + key].type == "text" && definition[reference].properties[key].maxLength && definition[reference].properties[key].maxLength > 256)
+                    fields[(isArray ? (jPath + "[0]") : jPath) + "." + key].type = "textarea";
+            }
+        }
+    return {
+        fields,
+        errors,
+        localeFields
+    };
+}
+
+let addUrls = function(fields, uiInfoDef) {
+    let errors = {};
+    for (var i = 0; i < uiInfoDef.externalData.length; i++) {
+        if (fields[uiInfoDef.externalData[i].fieldName]) {
+            if (fields[uiInfoDef.externalData[i].fieldName].type == "autoCompelete")
+                fields[uiInfoDef.externalData[i].fieldName].autoCompleteUrl = uiInfoDef.externalData[i].url + getQuery(uiInfoDef.externalData[i].url, uiInfoDef.externalData[i].keyPath, uiInfoDef.externalData[i].valPath);
+            else
+                fields[uiInfoDef.externalData[i].fieldName].url = uiInfoDef.externalData[i].url + getQuery(uiInfoDef.externalData[i].url, uiInfoDef.externalData[i].keyPath, uiInfoDef.externalData[i].valPath);
+        } else {
+            errors[uiInfoDef.externalData[i].fieldName] = "Field exists in x-ui-info externalData section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+        }
+    }
+
+    return {
+        fields,
+        errors
+    }
+}
+
+let addDependents = function(fields, uiInfoDef) {
+    let errors = {};
+    for (let i = 0; i < uiInfoDef.dependents.length; i++) {
+        if (fields[uiInfoDef.dependents[i].onChangeField]) {
+            fields[uiInfoDef.dependents[i].onChangeField].depedants = [];
+            for (let key in uiInfoDef.dependents[i].affectedFields) {
+                fields[uiInfoDef.dependents[i].onChangeField].depedants.push({
+                    "jsonPath": key,
+                    "type": uiInfoDef.dependents[i].affectedFields[key].type,
+                    "pattern": uiInfoDef.dependents[i].affectedFields[key].pattern
+                })
+            }
+        }
+    }
+
+    return {
+        fields,
+        errors
+    };
+}
+
+let addAutoFills = function(fields, uiInfoDef) {
+    let errors = {};
+    for (var i = 0; i < uiInfoDef.autoFills.length; i++) {
+        if (fields[uiInfoDef.autoFills[i].onChangeField]) {
+            fields[uiInfoDef.autoFills[i].onChangeField].type = "textSearch";
+            fields[uiInfoDef.autoFills[i].onChangeField].autoCompleteDependancy = {
+                autoCompleteUrl: uiInfoDef.autoFills[i].url,
+                autoFillFields: {}
+            };
+
+            for (var j = 0; j < uiInfoDef.autoFills[i].affectedFields.length; j++) {
+                fields[uiInfoDef.autoFills[i].onChangeField].autoCompleteDependancy.autoFillFields[uiInfoDef.autoFills[i].affectedFields[j]] = uiInfoDef.autoFills[i].affectJSONPath[j];
+            }
+        } else {
+            errors[uiInfoDef.autoFills[i].onChangeField] = "Field exists in x-ui-info AutoFills section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+        }
+    }
+
+    return {
+        fields,
+        errors
+    };
+}
+
+let addRadios = function(fields, uiInfoDef) {
+    let errors = {};
+    let localeFields = {};
+    for (var i = 0; i < uiInfoDef.radios.length; i++) {
+        if (fields[uiInfoDef.radios[i].jsonPath]) {
+            fields[uiInfoDef.radios[i].jsonPath].type = "radio";
+            localeFields[module + ".create." + uiInfoDef.radios[i].trueLabel] = getTitleCase(uiInfoDef.radios[i].trueLabel);
+            localeFields[module + ".create." + uiInfoDef.radios[i].falseLabel] = getTitleCase(uiInfoDef.radios[i].falseLabel);
+            fields[uiInfoDef.radios[i].jsonPath].values = [{
+                label: module + ".create." + uiInfoDef.radios[i].trueLabel,
+                value: true
+            }, {
+                label: module + ".create." + uiInfoDef.radios[i].falseLabel,
+                value: false
+            }];
+        } else {
+            errors[uiInfoDef.radios[i].jsonPath] = "Field exists in x-ui-info radios section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+        }
+    }
+
+    return {
+        fields,
+        errors,
+        localeFields
+    };
+}
+
+let addGroups = function(specifications, fields, uiInfoDef) {
+    let errors = {};
+    let localeFields = {};
+    for (var key in uiInfoDef.groups) {
+        localeFields[module + ".create.group.title." + key] = getTitleCase(key);
+        let group = {
+            name: key,
+            label: module + ".create.group.title." + key,
+            fields: []
+        };
+        for (var i = 0; i < uiInfoDef.groups[key].fields.length; i++) {
+            if (fields[uiInfoDef.groups[key].fields[i]])
+                group.fields.push(fields[uiInfoDef.groups[key].fields[i]]);
+            else
+                errors[uiInfoDef.groups[key].fields[i]] = "Field exists in x-ui-info groups section but not present in API specifications. REFERENCE PATH: " + uiInfoDef.referencePath;
+        }
+        specifications.groups.push(group);
+    }
+
+    return {
+        specifications,
+        errors,
+        localeFields
+    }
+}
+
+exports.getFieldsFromInnerObject = getFieldsFromInnerObject;
+exports.addUrls = addUrls;
+exports.addDependents = addDependents;
+exports.addAutoFills = addAutoFills;
+exports.addRadios = addRadios;
+exports.addGroups = addGroups;

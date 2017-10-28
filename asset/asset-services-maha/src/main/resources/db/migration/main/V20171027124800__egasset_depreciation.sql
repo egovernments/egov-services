933e6dab8fee11dac4ec6d6c32547b329339a674
kusumaRS <kusuma.rs@riflexions.com>
2017-10-28 12:51:51 +0530

    adding depreciation createscript

---
+++ asset/asset-services-maha/src/main/resources/db/migration/main/V20171027124800__egasset_depreciation.sql
@@ -0,0 +1,23 @@
+CREATE SEQUENCE seq_egasset_depreciation;
+
+CREATE TABLE egasset_depreciation (
+id bigint NOT NULL,
+assetid bigint NOT NULL,
+financialyear character varying(16) ,
+fromdate bigint,
+todate bigint,
+voucherreference bigint,
+status character varying(64) NOT NULL,
+valuebeforedepreciation numeric(12,2) NOT NULL,
+depreciationvalue  numeric(12,2) ,
+valueafterdepreciation numeric(12,2) ,
+tenantid character varying(64) NOT NULL,
+createdby character varying(64) NOT NULL,
+createdtime bigint NOT NULL,
+lastmodifiedby character varying(64),
+lastmodifiedtime  bigint,
+reasonforfailure character varying(256),
+CONSTRAINT pk_depreciation PRIMARY KEY(id,tenantid)
+);
+
+

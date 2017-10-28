859abc42ba7753f27cde65e69799bf2bf8c19b5a
Yosadhara <yosadhara.pradhan@walkingtree.in>
2017-10-28 21:19:34 +0530

    comitting the buider and repository

---
+++ lcms/lcms-services/src/main/java/org/egov/lcms/repository/AdvocateRepository.java
@@ -0,0 +1,50 @@
+package org.egov.lcms.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.egov.lcms.config.PropertiesManager;
+import org.egov.lcms.models.Advocate;
+import org.egov.lcms.models.AdvocateSearchCriteria;
+import org.egov.lcms.repository.builder.AdvocateBuilders;
+import org.egov.lcms.rowmapper.AdvocateRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Repository
+@Slf4j
+public class AdvocateRepository {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	PropertiesManager propertiesManager;
+	
+	@Autowired
+	AdvocateRowMapper advocateRowMapper;
+	
+	public List<Advocate> search(AdvocateSearchCriteria advocateSearchCriteria) {
+		
+		if (advocateSearchCriteria.getPageNumber() == null || advocateSearchCriteria.getPageNumber() == 0)
+			advocateSearchCriteria.setPageNumber(Integer.valueOf(propertiesManager.getDefaultPageNumber().trim()));
+		
+		if (advocateSearchCriteria.getPageSize() == null) 
+			advocateSearchCriteria.setPageSize(Integer.valueOf(propertiesManager.getDefaultPageSize().trim()));
+		
+		final List<Object> preparedStatementValues = new ArrayList<Object>();
+		final String selectQuery = AdvocateBuilders.getSearchQuery(advocateSearchCriteria, preparedStatementValues);
+
+		List<Advocate> advocates = new ArrayList<Advocate>();
+		try {
+			advocates = jdbcTemplate.query(selectQuery, preparedStatementValues.toArray(), advocateRowMapper);
+		} catch (final Exception exception) {
+			log.info("the exception in advocate search :" + exception);
+		}
+
+		return advocates;
+	}
+}
---
+++ lcms/lcms-services/src/main/java/org/egov/lcms/repository/builder/AdvocateBuilders.java
@@ -0,0 +1,129 @@
+package org.egov.lcms.repository.builder;
+
+import java.util.List;
+import org.egov.lcms.config.PropertiesManager;
+import org.egov.lcms.models.AdvocateSearchCriteria;
+
+public class AdvocateBuilders {
+	
+	static
+	PropertiesManager propertiesManager;
+	
+	private static final String BASE_QUERY = "SELECT * from egov_lcms_advocate ";
+	
+	public static String getSearchQuery(AdvocateSearchCriteria advocateSearchCriteria,
+			List<Object> preparedStatementValues) {
+		
+		StringBuffer selectQuery = new StringBuffer(BASE_QUERY);		
+		addWhereClause(selectQuery, preparedStatementValues, advocateSearchCriteria);
+        addOrderByClause(selectQuery, advocateSearchCriteria);
+        addPagingClause(selectQuery, preparedStatementValues, advocateSearchCriteria);		
+		
+		return selectQuery.toString();
+	}
+
+	private static void addWhereClause(StringBuffer selectQuery, List<Object> preparedStatementValues,
+			AdvocateSearchCriteria advocateSearchCriteria) {
+		
+		if (advocateSearchCriteria.getCode() == null && advocateSearchCriteria.getAdvocateName() == null
+                && advocateSearchCriteria.getOrganizationName() == null && advocateSearchCriteria.getTenantId() == null
+                && advocateSearchCriteria.getIsActive() && advocateSearchCriteria.getIsIndividual() == null)
+            return;
+		
+		selectQuery.append(" WHERE");        
+		if (advocateSearchCriteria.getTenantId() != null) {
+		
+            selectQuery.append(" LOWER(tenantId) =LOWER(?)");
+            preparedStatementValues.add(advocateSearchCriteria.getTenantId());
+		}
+		
+		if (advocateSearchCriteria.getIsIndividual() != null) {
+			
+            selectQuery.append(" AND isindividual = ?");
+            preparedStatementValues.add(advocateSearchCriteria.getIsIndividual());			
+		}
+		
+		if (advocateSearchCriteria.getAdvocateName() != null) {
+		
+            selectQuery.append(" AND LOWER(name) = LOWER(?)");
+            preparedStatementValues.add(advocateSearchCriteria.getAdvocateName());			
+		}
+		
+		if (advocateSearchCriteria.getOrganizationName() != null) {
+			
+            selectQuery.append(" AND organizationname = ?");
+            preparedStatementValues.add(advocateSearchCriteria.getOrganizationName());			
+		}
+		
+		if (advocateSearchCriteria.getIsActive() != null) {
+			
+            selectQuery.append(" isactive = ?");
+            preparedStatementValues.add(advocateSearchCriteria.getIsActive());			
+		}
+		
+		if (advocateSearchCriteria.getCode() != null) {
+			
+			String[] codes = advocateSearchCriteria.getCode();
+			if(codes != null && codes.length > 0 ){
+				int count = 1;
+				String stringIds = "";
+				for(String code : codes){
+					if(count < codes.length ){
+						stringIds = "'" + stringIds + code + "',"; 
+					}else{
+						stringIds = "'" + stringIds + code + "'"; 
+					}
+					count++;
+				}
+				
+	            selectQuery.append(" AND code IN (" + stringIds + ")");
+			}			
+		}		
+	}
+
+	private static void addOrderByClause(StringBuffer selectQuery, AdvocateSearchCriteria advocateSearchCriteria) {
+
+		selectQuery.append(" ORDER BY ");
+
+		if (advocateSearchCriteria.getSort() != null && advocateSearchCriteria.getSort().length > 0) {
+
+			int orderBycount = 1;
+			StringBuffer orderByCondition = new StringBuffer();
+			
+			for (String order : advocateSearchCriteria.getSort()) {
+				if (orderBycount < advocateSearchCriteria.getSort().length)
+					orderByCondition.append(order + ",");
+				else
+					orderByCondition.append(order);
+				orderBycount++;
+			}
+
+			if (orderBycount > 1)
+				orderByCondition.append(" asc");
+
+			selectQuery.append(orderByCondition.toString());
+		}
+		
+		selectQuery.append(" code ");
+	}
+
+	private static void addPagingClause(StringBuffer selectQuery, List<Object> preparedStatementValues,
+			AdvocateSearchCriteria advocateSearchCriteria) {
+		
+		int pageNumber = advocateSearchCriteria.getPageNumber();
+		int pageSize = advocateSearchCriteria.getPageSize();
+		int offset = 0;
+		int limit = 0;		
+		
+		limit = pageNumber * pageSize;
+
+		if (pageNumber <= 1)
+			offset = (limit - pageSize);
+		else
+			offset = (limit - pageSize) + 1;
+
+		selectQuery.append(" offset ?  limit ?");
+		preparedStatementValues.add(offset);
+		preparedStatementValues.add(limit);
+	}
+}

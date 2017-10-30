d7fb48be889aa4582e50dbfe6d25f5496052dcf1
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2017-10-30 12:58:21 +0530

    role action for report service code updated

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171030125311__asset_report_roleaction_update.sql
@@ -0,0 +1,3 @@
+update eg_action set servicecode='Asset' where name='CreateAssetMovable' AND displayname='Create Asset movable' AND url='/app/asset/create-asset-movable.html';
+
+update eg_action set servicecode='AssetRegisterReportsSearchPage' where name='AssetImmovableRegister' AND displayname='Asset Immovable Register'AND url='/report/asset/metadata/_get';

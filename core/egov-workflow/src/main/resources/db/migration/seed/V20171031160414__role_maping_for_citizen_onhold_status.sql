bc164d17af0001704308feae51f24df2e40ff93b
shashi <shashikanth.g@egovernments.org>
2017-10-31 16:50:13 +0530

    [EGSVC-1807]:Added script for status role mapping.

---
+++ core/egov-workflow/src/main/resources/db/migration/seed/V20171031160414__role_maping_for_citizen_onhold_status.sql
@@ -0,0 +1,25 @@
+
+--default tenant
+INSERT INTO egpgr_complaintstatus_mapping (id, role_id, current_status_id, orderno, show_status_id, version, tenantid) 
+VALUES (nextval('seq_egpgr_complaintstatus_mapping'), 4, (select id from service_status where name = 'FORWARDED' and tenantid = 'default'),
+ 5, (select id from service_status where name = 'ONHOLD' and tenantid = 'default'), 0, 'default');
+
+INSERT INTO egpgr_complaintstatus_mapping (id, role_id, current_status_id, orderno, show_status_id, version, tenantid) 
+VALUES (nextval('seq_egpgr_complaintstatus_mapping'), 4, (select id from service_status where name = 'REOPENED' and tenantid = 'default'),
+ 1, (select id from service_status where name = 'ONHOLD' and tenantid = 'default'), 0, 'default');
+
+INSERT INTO egpgr_complaintstatus_mapping (id, role_id, current_status_id, orderno, show_status_id, version, tenantid) 
+VALUES (nextval('seq_egpgr_complaintstatus_mapping'), 5, (select id from service_status where name = 'FORWARDED' and tenantid = 'default'),
+ 5, (select id from service_status where name = 'ONHOLD' and tenantid = 'default'), 0, 'default');
+
+INSERT INTO egpgr_complaintstatus_mapping (id, role_id, current_status_id, orderno, show_status_id, version, tenantid) 
+VALUES (nextval('seq_egpgr_complaintstatus_mapping'), 5, (select id from service_status where name = 'REOPENED' and tenantid = 'default'),
+ 1, (select id from service_status where name = 'ONHOLD' and tenantid = 'default'), 0, 'default');
+
+INSERT INTO egpgr_complaintstatus_mapping (id, role_id, current_status_id, orderno, show_status_id, version, tenantid) 
+VALUES (nextval('seq_egpgr_complaintstatus_mapping'), 6, (select id from service_status where name = 'FORWARDED' and tenantid = 'default'),
+ 5, (select id from service_status where name = 'ONHOLD' and tenantid = 'default'), 0, 'default');
+
+INSERT INTO egpgr_complaintstatus_mapping (id, role_id, current_status_id, orderno, show_status_id, version, tenantid) 
+VALUES (nextval('seq_egpgr_complaintstatus_mapping'), 6, (select id from service_status where name = 'REOPENED' and tenantid = 'default'),
+ 1, (select id from service_status where name = 'ONHOLD' and tenantid = 'default'), 0, 'default');

56f140b77df0691abce342a8ea748f3080671727
Yosadhara <yosadhara.pradhan@walkingtree.in>
2017-10-31 20:10:37 +0530

    Committing Register and Notice models

---
+++ lcms/lcms-services/src/main/java/org/egov/lcms/models/NoticeSearchResponse.java
@@ -0,0 +1,24 @@
+package org.egov.lcms.models;
+
+import java.util.List;
+import org.egov.common.contract.response.ResponseInfo;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class NoticeSearchResponse {
+	
+	@JsonProperty("ResponseInfo")
+	private ResponseInfo responseInfo;
+	
+	@JsonProperty("notices")
+    private List<Notice> notices;
+}

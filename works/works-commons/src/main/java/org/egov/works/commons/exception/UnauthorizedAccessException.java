3269c2a208be5e1e78cddf7945a6fa7df7af9205
ramki <ramakrishna@egovernments.org>
2017-11-01 14:31:18 +0530

    Added latest ScheduleOfRate, common utils and exceptions

---
+++ works/works-commons/src/main/java/org/egov/works/commons/exception/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package org.egov.works.commons.exception;
+
+public class UnauthorizedAccessException extends RuntimeException {
+
+	private static final long serialVersionUID = -7621995485789137725L;
+
+}

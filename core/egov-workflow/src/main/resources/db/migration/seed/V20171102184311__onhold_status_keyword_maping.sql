018951bd9d8d911384eb5a13dadc058148e916df
shashi <shashikanth.g@egovernments.org>
2017-11-02 18:53:17 +0530

    Added keyword status mapping for on hold status

---
+++ core/egov-workflow/src/main/resources/db/migration/seed/V20171102184311__onhold_status_keyword_maping.sql
@@ -0,0 +1,2 @@
+INSERT INTO keyword_service_status (keyword,tenantId,servicestatuscode,version,createdby,createddate,lastmodifiedby,lastmodifieddate)
+values('Complaint','default','ONHOLD',0,0,now(),0,now());

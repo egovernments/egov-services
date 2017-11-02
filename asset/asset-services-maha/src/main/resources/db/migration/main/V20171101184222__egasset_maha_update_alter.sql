289ef96baf0708296076815b2a996c007825f228
Elzan Mathew <elzan.mathew@egovernments.org>
2017-11-02 11:49:22 +0530

    Deleted the script to alter the asset table as build was failing. We need to fix

--- asset/asset-services-maha/src/main/resources/db/migration/main/V20171101184222__egasset_maha_update_alter.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE egasset_asset ADD COLUMN IF NOT EXISTS openingdate bigint,ADD COLUMN IF NOT EXISTS fundsource character varying(250),ADD COLUMN IF NOT EXISTS location character varying(4000); 

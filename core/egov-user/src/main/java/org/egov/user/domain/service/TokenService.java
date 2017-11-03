99815c7f3b15bcc6fd65eae4a559b6ec7943c4f4
kiranmai <kiranmayi.pulluru@tarento.com>
2017-11-03 11:12:14 +0530

    Token changes

--- core/egov-user/src/main/java/org/egov/user/domain/service/TokenService.java
+++ core/egov-user/src/main/java/org/egov/user/domain/service/TokenService.java
@@ -1,21 +1,26 @@
 package org.egov.user.domain.service;
 
 
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.egov.user.domain.exception.InvalidAccessTokenException;
 import org.egov.user.domain.model.Action;
 import org.egov.user.domain.model.SecureUser;
 import org.egov.user.domain.model.UserDetail;
 import org.egov.user.persistence.repository.ActionRestRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class TokenService {
-
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TokenService.class);
+	
 	private TokenStore tokenStore;
 
 	private ActionRestRepository actionRestRepository;
@@ -29,10 +34,16 @@ public class TokenService {
 		if (StringUtils.isEmpty(accessToken)) {
 			throw new InvalidAccessTokenException();
 		}
-
+	
+		LOGGER.info("accessToken: " +accessToken); 
 		OAuth2Authentication authentication = tokenStore.readAuthentication(accessToken);
+		OAuth2RefreshToken oath2RefreshToken= tokenStore.readRefreshToken(accessToken);
+		LOGGER.info("oath2RefreshToken: " +oath2RefreshToken); 
+		OAuth2Authentication refreshAuthentication =tokenStore.readAuthenticationForRefreshToken(oath2RefreshToken);
 
-		if (authentication == null) {
+		LOGGER.info("refreshAuthentication: " +refreshAuthentication); 
+		
+		if (refreshAuthentication == null) {
 			throw new InvalidAccessTokenException();
 		}
 

197c679df046ceeede31f70c042b1abe7e83a4be
venki <venki@egovernments.org>
2017-11-06 12:08:45 +0530

    Actions for sanitation staff target master.

--- core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171106010312__swm_sanitationstafftarget_actions_dml.sql
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171106010312__swm_sanitationstafftarget_actions_dml.sql
@@ -1,15 +1,15 @@
-insert into service (id, code, name, enabled, displayname, ordernumber, parentmodule, tenantId) values (nextval('SEQ_SERVICE'), 'VehicleMaintenance', 'VehicleMaintenance', true, 'VehicleMaintenance', 1, (select id from service where code = 'SWM Masters' and tenantId='default'), 'default');
+insert into service (id, code, name, enabled, displayname, ordernumber, parentmodule, tenantId) values (nextval('SEQ_SERVICE'), 'SanitationStaffTarget', 'Sanitation Staff Target', true, 'Sanitation Staff Target', 1, (select id from service where code = 'SWM Masters' and tenantId='default'), 'default');
 
 insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)
-values(nextval('SEQ_EG_ACTION'),'VehicleMaintenance Create','/swm-services/vehiclemaintenances/_create','VehicleMaintenance',null,(select id from service where code='VehicleMaintenance' and tenantid='default'),0,'Create Vehicle Maintenance',true,1,now(),1,now());
+values(nextval('SEQ_EG_ACTION'),'SanitationStaffTarget Create','/swm-services/sanitationstafftargets/_create','SanitationStaffTarget',null,(select id from service where code='SanitationStaffTarget' and tenantid='default'),0,'Create Sanitation Staff Target',true,1,now(),1,now());
 
 insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)
-values(nextval('SEQ_EG_ACTION'),'VehicleMaintenance Update','/swm-services/vehiclemaintenances/_update','VehicleMaintenance',null,(select id from service where code='VehicleMaintenance' and tenantid='default'),0,'Update Vehicle Maintenance',true,1,now(),1,now());
+values(nextval('SEQ_EG_ACTION'),'SanitationStaffTarget Update','/swm-services/sanitationstafftargets/_update','SanitationStaffTarget',null,(select id from service where code='SanitationStaffTarget' and tenantid='default'),0,'Update Sanitation Staff Target',true,1,now(),1,now());
 
 insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)
-values(nextval('SEQ_EG_ACTION'),'VehicleMaintenance Search','/swm-services/vehiclemaintenances/_search','VehicleMaintenance',null,(select id from service where code='VehicleMaintenance' and tenantid='default'),0,'View Vehicle Maintenance',true,1,now(),1,now());
+values(nextval('SEQ_EG_ACTION'),'SanitationStaffTarget Search','/swm-services/sanitationstafftargets/_search','SanitationStaffTarget',null,(select id from service where code='SanitationStaffTarget' and tenantid='default'),0,'View Sanitation Staff Target',true,1,now(),1,now());
 
 
-insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'VehicleMaintenance Create' ),'default');
-insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'VehicleMaintenance Update' ),'default');
-insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'VehicleMaintenance Search' ),'default');
+insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'SanitationStaffTarget Create' ),'default');
+insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'SanitationStaffTarget Update' ),'default');
+insert into eg_roleaction(roleCode,actionid,tenantid)values('SUPERUSER',(select id from eg_action where name = 'SanitationStaffTarget Search' ),'default');

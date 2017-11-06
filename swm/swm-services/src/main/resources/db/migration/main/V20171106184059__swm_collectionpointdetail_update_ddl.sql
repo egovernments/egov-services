896bd72b8318aa95404d7bd0c0f17522645bf67e
venki <venki@egovernments.org>
2017-11-06 18:42:51 +0530

    Deleted auditable fields from collection point details/.

---
+++ swm/swm-services/src/main/resources/db/migration/main/V20171106184059__swm_collectionpointdetail_update_ddl.sql
@@ -0,0 +1,7 @@
+ALTER TABLE egswm_collectionpointdetails DROP COLUMN createdby RESTRICT;
+
+ALTER TABLE egswm_collectionpointdetails DROP COLUMN createdtime RESTRICT;
+
+ALTER TABLE egswm_collectionpointdetails DROP COLUMN lastmodifiedby RESTRICT;
+
+ALTER TABLE egswm_collectionpointdetails DROP COLUMN lastmodifiedtime RESTRICT;

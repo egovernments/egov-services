53c8b6f58263f145e2f01bfa68c424f66c7efbf1
Darshan Nagesh <darshan.nagesh@tarento.com>
2017-11-06 11:06:29 +0530

    Changing the URL for APIs

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171106110057__perfmanagement_master_api_access.sql
@@ -0,0 +1,40 @@
+INSERT INTO SERVICE (id, code, name, enabled, displayname, ordernumber, tenantid) 
+VALUES (nextval('seq_service'), 'PERF_ASSESSMENT', 'Performance Assessment', true, 'Performance Assessment', 1, 'default');
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiMasterCreate','/perfmanagement/v1/kpimaster/_create',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Create KPI Master',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiMasterCreate'),'default');
+
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiMasterUpdate','/perfmanagement/v1/kpimaster/_update',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Update KPI Master',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiMasterUpdate'),'default');
+
+
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiMasterDelete','/perfmanagement/v1/kpimaster/_delete',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Delete KPI Master',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiMasterDelete'),'default');
+
+
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiMasterSearch','/perfmanagement/v1/kpimaster/_search',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Search KPI Master',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiMasterSearch'),'default');
+
---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171106110111__perfmanagement_value_api_access.sql
@@ -0,0 +1,27 @@
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiValueCreate','/perfmanagement/v1/kpivalue/_create',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Create KPI Value',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiValueCreate'),'default');
+
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiValueUpdate','/perfmanagement/v1/kpivalue/_update',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Update KPI Value',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiValueUpdate'),'default');
+
+
+
+insert into eg_action(id, name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,
+lastmodifiedby,lastmodifieddate)
+values(nextval('SEQ_EG_ACTION'),'PerfAssmtKpiValueSearch','/perfmanagement/v1/kpivalue/_search',
+'Performance Assessment',NULL, (select id from service where code ='PERF_ASSESSMENT' and tenantid='default'),1,
+'Search KPI Value',true,1,now(),1,now());
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('SUPERUSER',(select id from eg_action where name='PerfAssmtKpiValueSearch'),'default');
+

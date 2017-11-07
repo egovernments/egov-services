f8a7b85374782ea8228cbafdfd40f7e0aaea0933
sumanth <sumanth@egovernments.org>
2017-11-07 11:38:39 +0530

    swm action url update for refilling pump station.

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171107113604__swm_action_url_update.sql
@@ -0,0 +1,5 @@
+update eg_action set url = '/swm-services/refillingpumpstations/_create' where url = '/swm-services/refillinpumpstations/_create';
+
+update eg_action set url = '/swm-services/refillingpumpstations/_update' where url = '/swm-services/refillinpumpstations/_update';
+
+update eg_action set url = '/swm-services/refillingpumpstations/_search' where url = '/swm-services/refillinpumpstations/_search';

a7c9e9f488b075495b292d20e0ae18e8c20af4ab
ramki <ramakrishna@egovernments.org>
2017-11-07 12:51:38 +0530

    adding common constants class

---
+++ works/works-commons/src/main/java/org/egov/works/commons/utils/CommonConstants.java
@@ -0,0 +1,19 @@
+package org.egov.works.commons.utils;
+
+/**
+ * Created by ramki on 7/11/17.
+ */
+public class CommonConstants {
+    //Module names
+    public  static final String MODULENAME_WORKS="Works";
+    public  static final String MODULENAME_COMMON="common-masters";
+
+    //Works master names
+    public static final String MASTERNAME_SCHEDULE_CATEGORY="ScheduleCategory";
+    public static final String MASTERNAME_CONTRACTORCLASS="ContractorClass";
+    public static final String MASTERNAME_TYPEOFWORK="TypeOfWork";
+    public static final String MASTERNAME_REFERENCETYPE="ReferenceType";
+
+    //Common master names
+    public static final String MASTERNAME_UOM="Uom";
+}

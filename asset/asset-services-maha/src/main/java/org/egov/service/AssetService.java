051dd5e833804d0f5dd0131c2429ac9c8c7deae8
kusumaRS <kusuma.rs@riflexions.com>
2017-11-08 14:41:05 +0530

    updated assetservice

--- asset/asset-services-maha/src/main/java/org/egov/service/AssetService.java
+++ asset/asset-services-maha/src/main/java/org/egov/service/AssetService.java
@@ -57,8 +57,9 @@ public class AssetService {
 		final Asset asset = assetRequest.getAsset();
 
 		// FIXME put asset code seq per ulb Ghanshyam will update
-		asset.setCode(asset.getTenantId() + asset.getDepartmentCode() + asset.getAssetCategory().getCode()
-				+ assetCommonService.getCode(Sequence.ASSETCODESEQUENCE));
+		asset.setCode(asset.getTenantId() +"/"+ asset.getDepartmentCode() + "/"+asset.getAssetCategory().getCode()
+				+"/"+ assetCommonService.getCode(Sequence.ASSETCODESEQUENCE));
+		System.err.println("asset.getcode"+asset.getCode());
 
 		asset.setId(assetCommonService.getNextId(Sequence.ASSETSEQUENCE));
 
@@ -76,8 +77,10 @@ public class AssetService {
 		List<CurrentValue> assetCurrentValueList = new ArrayList<>();
 		BigDecimal grossValue = asset.getGrossValue();
 		BigDecimal accumulatedDepreciation = asset.getAccumulatedDepreciation();
-		if (grossValue != null && accumulatedDepreciation != null)
+		//Accumulateddepreciation should be less than gross value todo in validator
+		if (grossValue != null && accumulatedDepreciation != null) {
 			currentValue.setCurrentAmount(grossValue.subtract(accumulatedDepreciation));
+		}
 		else
 			currentValue.setCurrentAmount(asset.getOriginalValue());
 
@@ -103,9 +106,19 @@ public class AssetService {
 
 	public AssetResponse getAssets(final AssetCriteria searchAsset, final RequestInfo requestInfo) {
 		log.info("AssetService getAssets");
+		/*if (searchAsset.getAssetCategory()!=null) {
+			Set<Long> assetCategory=new HashSet<>();
+			assetCategory.add(searchAsset.getAssetCategory());
+			Map<Long, AssetCategory> assetCatMap = masterDataService.getAssetCategoryMap(assetCategory, requestInfo,  searchAsset.getTenantId());
+			AssetCategory assetCategoryMdms = assetCatMap.get(searchAsset.getAssetCategory());
+			assetCategoryMdms.get
+		}*/
 		final List<Asset> assets = assetRepository.findForCriteria(searchAsset);
-		if (!assets.isEmpty())
+		Set<Long> idSet=null;
+		if (!assets.isEmpty()) {
+			 idSet = assets.stream().map(asset -> asset.getAssetCategory().getId()).collect(Collectors.toSet());
 			mapAssetCategories(assets, requestInfo, searchAsset.getTenantId());
+		}
 		return getAssetResponse(assets, requestInfo);
 	}
 

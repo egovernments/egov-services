101f23b4ccfe30ba018493b4625829b75e1b145c
saurabh297 <saurabh91verma@gmail.com>
2017-11-08 15:47:12 +0530

    add new reports for movble

--- web/react-pgr-web/src/components/framework/templates/templates/index.js
+++ web/react-pgr-web/src/components/framework/templates/templates/index.js
@@ -1,4 +1,5 @@
 import MyExampleTemplate from './MyExampleTemplate';
 import assetImmovableReport from '../../specs/asset/templates/assetImmovableReport';
+import assetMovableReport from '../../specs/asset/templates/assetMovableReport';
 
-export { MyExampleTemplate, assetImmovableReport };
+export { MyExampleTemplate, assetImmovableReport , assetMovableReport};

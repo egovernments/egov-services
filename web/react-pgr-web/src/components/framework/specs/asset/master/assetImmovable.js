6af5d05a3849c448b5e1da00e350bb46b48570c1
saurabh297 <saurabh91verma@gmail.com>
2017-11-08 16:05:53 +0530

    change url for assetcategory

--- web/react-pgr-web/src/components/framework/specs/asset/master/assetImmovable.js
+++ web/react-pgr-web/src/components/framework/specs/asset/master/assetImmovable.js
@@ -816,7 +816,7 @@ var dat = {
 						"label": "ac.create.Asset.SubCategory.Name",
 						"pattern": "",
 						"type": "singleValueList",
-						"url": "/egov-mdms-service/v1/_get?&moduleName=ASSET&masterName=AssetCategory&filter=%5B%3F(%20%40.isAssetAllow%20%3D%3D%20true%20%26%26%20%40.assetCategoryType%20%3D%3D%20%22IMMOVABLE%22)%5D|$..id|$..name",
+						"url": "/egov-mdms-service/v1/_get?&moduleName=ASSET&masterName=AssetCategory&filter=%5B%3F(%20%40.isAssetAllow%20%3D%3D%20true%20%26%26%20%40.assetCategoryType%20%3D%3D%20%22IMMOVABLE%22)%5D|$.MdmsRes.ASSET.AssetCategory.*.id|$.MdmsRes.ASSET.AssetCategory.*.name",
 						// "url": "/egov-mdms-service/v1/_get?&masterName=AssetCategory&moduleName=ASSET|$.MdmsRes.ASSET.AssetCategory.*.id|$.MdmsRes.ASSET.AssetCategory.*.name",
 						"isRequired": false,
 						"isDisabled": false,

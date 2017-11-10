e9ff09d7b0247ca6f2b6a7359adb8b46d81c2729
kusumaRS <kusuma.rs@riflexions.com>
2017-11-10 11:38:45 +0530

    adding workflowdetails

---
+++ asset/asset-services-maha/src/main/java/org/egov/model/WorkflowDetails.java
@@ -0,0 +1,23 @@
+package org.egov.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class WorkflowDetails {
+	
+  private String department;
+
+  private String designation;
+
+  private Long assignee;
+
+  private String action;
+  
+  private String status;
+  
+  private Long initiatorPosition;
+  
+  private String comments;
+}

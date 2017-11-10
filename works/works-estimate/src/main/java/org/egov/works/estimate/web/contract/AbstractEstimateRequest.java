e9c48fb6e78f1c04a898246bf3f5f79a661ce284
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2017-11-10 13:32:21 +0530

    Updated yaml and genrated updated class

--- works/works-estimate/src/main/java/org/egov/works/estimate/web/contract/AbstractEstimateRequest.java
+++ works/works-estimate/src/main/java/org/egov/works/estimate/web/contract/AbstractEstimateRequest.java
@@ -12,107 +12,109 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 /**
- * Contract class to send response. Array of AbstractEstimate items are used in case of search results, also multiple  AbstractEstimate item is used for create and update
+ * Contract class to send response. Array of AbstractEstimate items are used in
+ * case of search results, also multiple AbstractEstimate item is used for
+ * create and update
  */
 @ApiModel(description = "Contract class to send response. Array of AbstractEstimate items are used in case of search results, also multiple  AbstractEstimate item is used for create and update")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-03T07:36:47.547Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-10T07:43:05.630Z")
 
-public class AbstractEstimateRequest   {
-  @JsonProperty("requestInfo")
-  private RequestInfo requestInfo = null;
+public class AbstractEstimateRequest {
+	@JsonProperty("RequestInfo")
+	private RequestInfo requestInfo = null;
 
-  @JsonProperty("abstractEstimates")
-  private List<AbstractEstimate> abstractEstimates = null;
+	@JsonProperty("abstractEstimates")
+	private List<AbstractEstimate> abstractEstimates = null;
 
-  public AbstractEstimateRequest requestInfo(RequestInfo requestInfo) {
-    this.requestInfo = requestInfo;
-    return this;
-  }
+	public AbstractEstimateRequest requestInfo(RequestInfo requestInfo) {
+		this.requestInfo = requestInfo;
+		return this;
+	}
 
-   /**
-   * Get requestInfo
-   * @return requestInfo
-  **/
-  @ApiModelProperty(value = "")
+	/**
+	 * Get requestInfo
+	 * 
+	 * @return requestInfo
+	 **/
+	@ApiModelProperty(value = "")
 
-  @Valid
+	@Valid
 
-  public RequestInfo getRequestInfo() {
-    return requestInfo;
-  }
+	public RequestInfo getRequestInfo() {
+		return requestInfo;
+	}
 
-  public void setRequestInfo(RequestInfo requestInfo) {
-    this.requestInfo = requestInfo;
-  }
+	public void setRequestInfo(RequestInfo requestInfo) {
+		this.requestInfo = requestInfo;
+	}
 
-  public AbstractEstimateRequest abstractEstimates(List<AbstractEstimate> abstractEstimates) {
-    this.abstractEstimates = abstractEstimates;
-    return this;
-  }
+	public AbstractEstimateRequest abstractEstimates(List<AbstractEstimate> abstractEstimates) {
+		this.abstractEstimates = abstractEstimates;
+		return this;
+	}
 
-  public AbstractEstimateRequest addAbstractEstimatesItem(AbstractEstimate abstractEstimatesItem) {
-    if (this.abstractEstimates == null) {
-      this.abstractEstimates = new ArrayList<AbstractEstimate>();
-    }
-    this.abstractEstimates.add(abstractEstimatesItem);
-    return this;
-  }
+	public AbstractEstimateRequest addAbstractEstimatesItem(AbstractEstimate abstractEstimatesItem) {
+		if (this.abstractEstimates == null) {
+			this.abstractEstimates = new ArrayList<AbstractEstimate>();
+		}
+		this.abstractEstimates.add(abstractEstimatesItem);
+		return this;
+	}
 
-   /**
-   * Used for create and update only
-   * @return abstractEstimates
-  **/
-  @ApiModelProperty(value = "Used for create and update only")
+	/**
+	 * Used for create and update only
+	 * 
+	 * @return abstractEstimates
+	 **/
+	@ApiModelProperty(value = "Used for create and update only")
 
-  @Valid
+	@Valid
 
-  public List<AbstractEstimate> getAbstractEstimates() {
-    return abstractEstimates;
-  }
+	public List<AbstractEstimate> getAbstractEstimates() {
+		return abstractEstimates;
+	}
 
-  public void setAbstractEstimates(List<AbstractEstimate> abstractEstimates) {
-    this.abstractEstimates = abstractEstimates;
-  }
+	public void setAbstractEstimates(List<AbstractEstimate> abstractEstimates) {
+		this.abstractEstimates = abstractEstimates;
+	}
 
+	@Override
+	public boolean equals(java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		AbstractEstimateRequest abstractEstimateRequest = (AbstractEstimateRequest) o;
+		return Objects.equals(this.requestInfo, abstractEstimateRequest.requestInfo)
+				&& Objects.equals(this.abstractEstimates, abstractEstimateRequest.abstractEstimates);
+	}
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    AbstractEstimateRequest abstractEstimateRequest = (AbstractEstimateRequest) o;
-    return Objects.equals(this.requestInfo, abstractEstimateRequest.requestInfo) &&
-        Objects.equals(this.abstractEstimates, abstractEstimateRequest.abstractEstimates);
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(requestInfo, abstractEstimates);
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(requestInfo, abstractEstimates);
-  }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class AbstractEstimateRequest {\n");
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class AbstractEstimateRequest {\n");
-    
-    sb.append("    requestInfo: ").append(toIndentedString(requestInfo)).append("\n");
-    sb.append("    abstractEstimates: ").append(toIndentedString(abstractEstimates)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+		sb.append("    requestInfo: ").append(toIndentedString(requestInfo)).append("\n");
+		sb.append("    abstractEstimates: ").append(toIndentedString(abstractEstimates)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-
--- works/works-estimate/src/main/java/org/egov/works/estimate/web/contract/RevisionEstimateRequest.java
+++ works/works-estimate/src/main/java/org/egov/works/estimate/web/contract/RevisionEstimateRequest.java
@@ -1,115 +1,120 @@
 package org.egov.works.estimate.web.contract;
 
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 import javax.validation.Valid;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
- * Contract class to send response. Array of Revision Estimate items are used in case of search results, also multiple  Revision Estimate item is used for create and update
+ * Contract class to send response. Array of Revision Estimate items are used in
+ * case of search results, also multiple Revision Estimate item is used for
+ * create and update
  */
 @ApiModel(description = "Contract class to send response. Array of Revision Estimate items are used in case of search results, also multiple  Revision Estimate item is used for create and update")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-09T10:32:33.802Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-10T07:43:05.630Z")
 
-public class RevisionEstimateRequest   {
-  @JsonProperty("requestInfo")
-  private RequestInfo requestInfo = null;
+public class RevisionEstimateRequest {
+	@JsonProperty("RequestInfo")
+	private RequestInfo requestInfo = null;
 
-  @JsonProperty("revisionEstimates")
-  private List<RevisionEstimate> revisionEstimates = null;
+	@JsonProperty("revisionEstimates")
+	private List<RevisionEstimate> revisionEstimates = null;
 
-  public RevisionEstimateRequest requestInfo(RequestInfo requestInfo) {
-    this.requestInfo = requestInfo;
-    return this;
-  }
+	public RevisionEstimateRequest requestInfo(RequestInfo requestInfo) {
+		this.requestInfo = requestInfo;
+		return this;
+	}
 
-   /**
-   * Get requestInfo
-   * @return requestInfo
-  **/
-  @ApiModelProperty(value = "")
+	/**
+	 * Get requestInfo
+	 * 
+	 * @return requestInfo
+	 **/
+	@ApiModelProperty(value = "")
 
-  @Valid
+	@Valid
 
-  public RequestInfo getRequestInfo() {
-    return requestInfo;
-  }
+	public RequestInfo getRequestInfo() {
+		return requestInfo;
+	}
 
-  public void setRequestInfo(RequestInfo requestInfo) {
-    this.requestInfo = requestInfo;
-  }
+	public void setRequestInfo(RequestInfo requestInfo) {
+		this.requestInfo = requestInfo;
+	}
 
-  public RevisionEstimateRequest revisionEstimates(List<RevisionEstimate> revisionEstimates) {
-    this.revisionEstimates = revisionEstimates;
-    return this;
-  }
+	public RevisionEstimateRequest revisionEstimates(List<RevisionEstimate> revisionEstimates) {
+		this.revisionEstimates = revisionEstimates;
+		return this;
+	}
 
-  public RevisionEstimateRequest addRevisionEstimatesItem(RevisionEstimate revisionEstimatesItem) {
-    if (this.revisionEstimates == null) {
-      this.revisionEstimates = new ArrayList<RevisionEstimate>();
-    }
-    this.revisionEstimates.add(revisionEstimatesItem);
-    return this;
-  }
+	public RevisionEstimateRequest addRevisionEstimatesItem(RevisionEstimate revisionEstimatesItem) {
+		if (this.revisionEstimates == null) {
+			this.revisionEstimates = new ArrayList<RevisionEstimate>();
+		}
+		this.revisionEstimates.add(revisionEstimatesItem);
+		return this;
+	}
 
-   /**
-   * Used for create and update only
-   * @return revisionEstimates
-  **/
-  @ApiModelProperty(value = "Used for create and update only")
+	/**
+	 * Used for create and update only
+	 * 
+	 * @return revisionEstimates
+	 **/
+	@ApiModelProperty(value = "Used for create and update only")
 
-  @Valid
+	@Valid
 
-  public List<RevisionEstimate> getRevisionEstimates() {
-    return revisionEstimates;
-  }
+	public List<RevisionEstimate> getRevisionEstimates() {
+		return revisionEstimates;
+	}
 
-  public void setRevisionEstimates(List<RevisionEstimate> revisionEstimates) {
-    this.revisionEstimates = revisionEstimates;
-  }
+	public void setRevisionEstimates(List<RevisionEstimate> revisionEstimates) {
+		this.revisionEstimates = revisionEstimates;
+	}
 
+	@Override
+	public boolean equals(java.lang.Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		RevisionEstimateRequest revisionEstimateRequest = (RevisionEstimateRequest) o;
+		return Objects.equals(this.requestInfo, revisionEstimateRequest.requestInfo)
+				&& Objects.equals(this.revisionEstimates, revisionEstimateRequest.revisionEstimates);
+	}
 
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    RevisionEstimateRequest revisionEstimateRequest = (RevisionEstimateRequest) o;
-    return Objects.equals(this.requestInfo, revisionEstimateRequest.requestInfo) &&
-        Objects.equals(this.revisionEstimates, revisionEstimateRequest.revisionEstimates);
-  }
+	@Override
+	public int hashCode() {
+		return Objects.hash(requestInfo, revisionEstimates);
+	}
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(requestInfo, revisionEstimates);
-  }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("class RevisionEstimateRequest {\n");
 
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class RevisionEstimateRequest {\n");
-    
-    sb.append("    requestInfo: ").append(toIndentedString(requestInfo)).append("\n");
-    sb.append("    revisionEstimates: ").append(toIndentedString(revisionEstimates)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
+		sb.append("    requestInfo: ").append(toIndentedString(requestInfo)).append("\n");
+		sb.append("    revisionEstimates: ").append(toIndentedString(revisionEstimates)).append("\n");
+		sb.append("}");
+		return sb.toString();
+	}
 
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+	/**
+	 * Convert the given object to string with each line indented by 4 spaces
+	 * (except the first line).
+	 */
+	private String toIndentedString(java.lang.Object o) {
+		if (o == null) {
+			return "null";
+		}
+		return o.toString().replace("\n", "\n    ");
+	}
 }
-

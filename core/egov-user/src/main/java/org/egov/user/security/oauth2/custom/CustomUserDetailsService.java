5797c7e643d159ad283ffc7cc38c9fde80b58cc5
kiranmai <kiranmayi.pulluru@tarento.com>
2017-11-10 11:20:03 +0530

    Session Expiry changes

--- core/egov-user/src/main/java/org/egov/user/security/oauth2/custom/CustomUserDetailsService.java
+++ core/egov-user/src/main/java/org/egov/user/security/oauth2/custom/CustomUserDetailsService.java
@@ -1,137 +1,45 @@
 package org.egov.user.security.oauth2.custom;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.egov.user.domain.model.SecureUser;
-import org.egov.user.domain.model.User;
 import org.egov.user.domain.service.UserService;
-import org.egov.user.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
-public class CustomUserDetailsService implements UserDetailsService {
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+	private final UserService userService;
 
 	@Autowired
-    private UserService userService;
+	public CustomUserDetailsService(UserService userService) {
+		this.userService = userService;
+	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {	
-		System.out.println("USER NAME: "+username);
-		System.out.println("Tenant: "+CustomAuthenticationProvider.getTenant());
-	    User user;
-	    if (username.contains("@") && username.contains(".")) {
-	        user = userService.getUserByEmailId(username, CustomAuthenticationProvider.getTenant());
-	    } else {
-	        user = userService.getUserByUsername(username, "default");
-	    }
-	    if (null == user) {
-	        throw new UsernameNotFoundException(username);
-	    }
-        final SecureUser secureUser = new SecureUser(getUser(user));
-		System.out.println("USER: "+secureUser.toString());
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-		List<GrantedAuthority> grantedAuths = new ArrayList<>();
-        grantedAuths.add(new SimpleGrantedAuthority("ROLE_" + user.getType()));
-
-		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =  
-				new UsernamePasswordAuthenticationToken(secureUser, user.getPassword(), grantedAuths);
+		org.egov.user.domain.model.User user1=userService.getUserByUsername(username, CustomAuthenticationProvider.getTenantId());
 		
-	    return new UserDetailsImpl(usernamePasswordAuthenticationToken);
+		List<org.egov.user.domain.model.Role> roles = user1.getRoles();
+		
+		List<org.egov.user.web.contract.auth.Role> roleList = new ArrayList<org.egov.user.web.contract.auth.Role>();
+		 
+		for(org.egov.user.domain.model.Role role : roles){
+			org.egov.user.web.contract.auth.Role role1 = new org.egov.user.web.contract.auth.Role(role);
+			roleList.add(role1);
+		}
+		org.egov.user.web.contract.auth.User authuser = org.egov.user.web.contract.auth.User.builder().id(user1.getId()).userName(user1.getUsername()).name(user1.getName()).mobileNumber(user1.getMobileNumber())
+				.emailId(user1.getEmailId()).locale(user1.getLocale()).active(user1.getActive()).roles(roleList).tenantId(user1.getTenantId()).type(user1.getType().toString()).build();
+		
+		SecureUser secureUser = new SecureUser(authuser);
+		
+		return secureUser;
 	}
 	
-	private org.egov.user.web.contract.auth.User getUser(User user) {
-		System.out.println("building secure user");
-        return org.egov.user.web.contract.auth.User.builder()
-                .id(user.getId())
-                .userName(user.getUsername())
-                .name(user.getName())
-                .mobileNumber(user.getMobileNumber())
-                .emailId(user.getEmailId())
-                .locale(user.getLocale())
-                .active(user.getActive())
-                .type(user.getType().name())
-                .roles(toAuthRole(user.getRoles()))
-                .tenantId(user.getTenantId())
-                .build();
-    }
-	
-    private List<org.egov.user.web.contract.auth.Role> toAuthRole(List<org.egov.user.domain.model.Role> domainRoles) {
-		System.out.println("building authroles");
-
-        if (domainRoles == null) 
-        	return new ArrayList<>();
-        
-        return domainRoles.stream().map(org.egov.user.web.contract.auth.Role::new)
-                .collect(Collectors.toList());
-    }
-
-	public static class UserDetailsImpl implements UserDetails {
-
-		private static final long serialVersionUID = 1L;
-		private User user;
-	    private SecureUser secureUser;
-	    private UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken;
-	 
-	    public UserDetailsImpl(User user) {
-	        this.user = user;
-	    }
-	    
-	    public UserDetailsImpl(SecureUser secureUser){
-	    	this.secureUser = secureUser;
-	    }
-
-	    public UserDetailsImpl(UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken){
-	    	this.usernamePasswordAuthenticationToken = usernamePasswordAuthenticationToken;
-	    }
-	    
-		@Override
-		public Collection<? extends GrantedAuthority> getAuthorities() {
-			 List<GrantedAuthority> grantedAuths = new ArrayList<>();
-	            grantedAuths.add(new SimpleGrantedAuthority("ROLE_" + user.getType()));
-			return grantedAuths;
-		}
-
-		@Override
-		public String getPassword() {
-			return user.getPassword();
-		}
-
-		@Override
-		public String getUsername() {
-			return user.getUsername();
-		}
-
-		@Override
-		public boolean isAccountNonExpired() {
-			return true;
-		}
-
-		@Override
-		public boolean isAccountNonLocked() {
-			return true;
-		}
-
-		@Override
-		public boolean isCredentialsNonExpired() {
-			return true;
-		}
-
-		@Override
-		public boolean isEnabled() {
-			return true;
-		}
-	}
-	
-	
-
 }

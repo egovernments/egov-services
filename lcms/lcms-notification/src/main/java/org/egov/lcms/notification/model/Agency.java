5faafd800f862cb5c5fc0c510dbb1b8aa82112a9
Yosadhara <yosadhara.pradhan@walkingtree.in>
2017-11-15 12:26:11 +0530

    Committing notification model changes

--- lcms/lcms-notification/src/main/java/org/egov/lcms/notification/model/Agency.java
+++ lcms/lcms-notification/src/main/java/org/egov/lcms/notification/model/Agency.java
@@ -1,6 +1,9 @@
 package org.egov.lcms.notification.model;
 
+import java.util.List;
+
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -9,55 +12,59 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * Advocate
- */
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-public class Advocate extends PersonDetails {
+@AllArgsConstructor
+public class Agency {
+	
+	@Size(min = 1, max = 100)
+	@JsonProperty("name")
+	private String name ;
 
+	@JsonProperty("code")
+	private String code;
+	
+	@JsonProperty("agencyAddress")
+	private String agencyAddress;
+
+	@NotNull
 	@JsonProperty("isIndividual")
-	private Boolean isIndividual = true;
+	private Boolean isIndividual ;
 
-	@NotNull
 	@JsonProperty("dateOfEmpanelment")
-	private Long dateOfEmpanelment;
+	private Long dateOfEmpanelment ;
 
-	@NotNull
 	@JsonProperty("standingCommitteeDecisionDate")
-	private Long standingCommitteeDecisionDate;
+	private Long standingCommitteeDecisionDate ;
 
-	@NotNull
 	@JsonProperty("empanelmentFromDate")
-	private Long empanelmentFromDate;
+	private Long empanelmentFromDate ;
 
-	@NotNull
 	@JsonProperty("newsPaperAdvertismentDate")
-	private Long newsPaperAdvertismentDate;
+	private Long newsPaperAdvertismentDate ;
 
-	@NotNull
 	@JsonProperty("empanelmentToDate")
-	private Long empanelmentToDate;
+	private Long empanelmentToDate ;
 
-	@NotNull
+	@JsonProperty("personDetails")
+	private List<PersonDetails> personDetails;
+	
+	@JsonProperty("advocates")
+	private List<Advocate> advocates;
+	
 	@JsonProperty("bankName")
 	private String bankName;
 
-	@NotNull
 	@JsonProperty("bankBranch")
 	private String bankBranch;
 
-	@NotNull
 	@JsonProperty("bankAccountNo")
 	private String bankAccountNo;
 
-	@NotNull
 	@JsonProperty("ifscCode")
 	private String ifscCode;
 
-	@NotNull
 	@JsonProperty("micr")
 	private String micr;
 
@@ -78,4 +85,12 @@ public class Advocate extends PersonDetails {
 	
 	@JsonProperty("status")
 	private String status;
+
+	@NotNull
+	@Size(min = 4, max = 128)
+	@JsonProperty("tenantId")
+	private String tenantId;
+
+	@JsonProperty("auditDetails")
+	private AuditDetails auditDetails;
 }

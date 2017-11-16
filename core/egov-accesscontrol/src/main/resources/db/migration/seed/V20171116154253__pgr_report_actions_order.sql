ce3677ab525cab97b5b63f3d54917ee36ea301ee
sumanth <sumanth@egovernments.org>
2017-11-16 15:49:19 +0530

    [EGSVC-1957]: PGR report actions reordering.

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171116154253__pgr_report_actions_order.sql
@@ -0,0 +1,11 @@
+ 
+UPDATE eg_action SET ordernumber =1 WHERE servicecode = 'RPT' AND name='Ageing Report';
+UPDATE eg_action SET ordernumber =2 WHERE servicecode = 'RPT' AND name='Ageing By Department Report' ;
+UPDATE eg_action SET ordernumber =3 WHERE servicecode = 'RPT' AND name='Drill Down Report';
+UPDATE eg_action SET ordernumber =4 WHERE servicecode = 'RPT' AND name='Drill Down By Department Report';
+UPDATE eg_action SET ordernumber =5 WHERE servicecode = 'RPT' AND name='Complaint Summary Register Report';
+UPDATE eg_action SET ordernumber =6 WHERE servicecode = 'RPT' AND name='Grievance Type Wise Report';
+UPDATE eg_action SET ordernumber =7 WHERE servicecode = 'RPT' AND name='Functionary Wise Report';
+UPDATE eg_action SET ordernumber =8 WHERE servicecode = 'RPT' AND name='Router Escalation Report';
+
+

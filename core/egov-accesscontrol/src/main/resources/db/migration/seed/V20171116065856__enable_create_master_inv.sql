a901d27fdf0bd290d9f23e634043086f89bfd3a1
sanjeevnvp <sanjeev.p@egovernments.org>
2017-11-16 07:01:01 +0530

    Enabled create screen for inventory masters

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171116065856__enable_create_master_inv.sql
@@ -0,0 +1,7 @@
+update eg_action set enabled = true where name = 'Store Create' and servicecode = 'STORE MASTER';
+
+update eg_action set enabled = true where name = 'Supplier Create' and servicecode = 'SUPPLIER MASTER';
+
+update eg_action set enabled = true where name = 'PriceList Create' and servicecode = 'PRICE LIST MASTER' ;
+
+update eg_action set enabled = true where name = 'Material Create' and servicecode = 'MATERIAL MASTER' ;

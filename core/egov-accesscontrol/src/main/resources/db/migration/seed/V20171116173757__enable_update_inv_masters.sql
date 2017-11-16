23c5b3669bfd8479b59c808ad970dc854e88376f
sanjeevnvp <sanjeev.p@egovernments.org>
2017-11-16 17:47:06 +0530

    Enabled Update screens for inventory

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171116173757__enable_update_inv_masters.sql
@@ -0,0 +1,7 @@
+update eg_action set enabled = true where name = 'PriceList Update' and servicecode = 'PRICE LIST MASTER' ;
+
+update eg_action set enabled = true where name = 'Material Update' and servicecode = 'MATERIAL MASTER' ;
+
+update eg_action set enabled = true where name = 'Store Update' and servicecode = 'STORE MASTER' ;
+
+update eg_action set enabled = true where name = 'Supplier Update' and servicecode = 'SUPPLIER MASTER' ;

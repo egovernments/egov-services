b4416db661f0ef89b29d1c5a1bce22876c63742f
shashi <shashikanth.g@egovernments.org>
2017-11-16 17:45:32 +0530

    Changed persister yaml  in inventory service,

--- inventory/inventory-services/src/main/java/org/egov/inv/model/OpeningBalanceSearchCriteria.java
+++ inventory/inventory-services/src/main/java/org/egov/inv/model/OpeningBalanceSearchCriteria.java
@@ -11,13 +11,17 @@ import org.egov.common.contract.request.RequestInfo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OpeningBalanceSearchCriteria {
 	@JsonProperty("RequestInfo")
 	private RequestInfo requestInfo = null;

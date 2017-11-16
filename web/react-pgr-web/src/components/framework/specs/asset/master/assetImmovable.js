307f2e97afad1dc532a687bd4853baefee3bba8d
saurabh297 <saurabh91verma@gmail.com>
2017-11-16 17:36:11 +0530

    assetImmovable issue fixed

--- web/react-pgr-web/src/components/framework/specs/asset/master/assetImmovable.js
+++ web/react-pgr-web/src/components/framework/specs/asset/master/assetImmovable.js
@@ -1614,7 +1614,7 @@ var dat = {
   						"label": "ac.create.Asset.SubCategory.Name",
   						"pattern": "",
   						"type": "singleValueList",
-  						"url": "/egov-mdms-service/v1/_get?&masterName=AssetCategory&moduleName=ASSET|$.MdmsRes.ASSET.AssetCategory.*.id|$.MdmsRes.ASSET.AssetCategory.*.name",
+  						// "url": "/egov-mdms-service/v1/_get?&masterName=AssetCategory&moduleName=ASSET|$.MdmsRes.ASSET.AssetCategory.*.id|$.MdmsRes.ASSET.AssetCategory.*.name",
 							"isRequired": true,
   						"isDisabled": false,
   						"requiredErrMsg": "",

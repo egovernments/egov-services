3c70d0261d6488cd9d31a9c4053ca6df6e7f3679
sanjeevnvp <sanjeev.p@egovernments.org>
2017-11-21 21:41:31 +0530

    Enabling Indent screen for inventory

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171121213804__inv_enable_indent_screen.sql
@@ -0,0 +1,5 @@
+update eg_action set enabled = true where name = 'Indent Create' and servicecode = 'INDENT';
+
+update eg_action set enabled = true where name = 'Indent Update' and servicecode = 'INDENT';
+
+update eg_action set enabled = true where name = 'Indent Search' and servicecode = 'INDENT' ;

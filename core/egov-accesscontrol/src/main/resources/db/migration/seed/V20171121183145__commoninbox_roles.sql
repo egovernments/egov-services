52cf2e4a8eccda9578193b860b61856be5cfd005
SenthilKumar <gysenthil@gmail.com>
2017-11-21 18:34:26 +0530

    added existing roles of pgr to common inbox

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171121183145__commoninbox_roles.sql
@@ -0,0 +1,15 @@
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GA',(select id from eg_action where name='Common Inbox Report MetaData'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GO',(select id from eg_action where name='Common Inbox Report MetaData'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('RO',(select id from eg_action where name='Common Inbox Report MetaData'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GRO',(select id from eg_action where name='Common Inbox Report MetaData'),'default');
+
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GA',(select id from eg_action where name='Common Inbox Report'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GO',(select id from eg_action where name='Common Inbox Report'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('RO',(select id from eg_action where name='Common Inbox Report'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GRO',(select id from eg_action where name='Common Inbox Report'),'default');
+
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GA',(select id from eg_action where name='Common Inbox Report Reload'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GO',(select id from eg_action where name='Common Inbox Report Reload'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('RO',(select id from eg_action where name='Common Inbox Report Reload'),'default');
+insert into eg_roleaction(roleCode,actionid,tenantId)values('GRO',(select id from eg_action where name='Common Inbox Report Reload'),'default');

7b1f06d16cc88b449fca41648b1d45ba2ea8672f
kusumaRS <kusuma.rs@riflexions.com>
2017-11-22 18:18:30 +0530

    added transaction date

--- asset/asset-services-maha/src/main/java/org/egov/repository/rowmapper/CurrentValueRowMapper.java
+++ asset/asset-services-maha/src/main/java/org/egov/repository/rowmapper/CurrentValueRowMapper.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import org.egov.model.CurrentValue;
 import org.egov.model.AuditDetails;
 import org.egov.model.enums.TransactionType;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
@@ -13,17 +14,17 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
-public class CurrentValueRowMapper implements RowMapper<CurrentValue> {
+public class CurrentValueRowMapper implements RowMapper<CurrentValue>{
 
-    @Override
+  @Override
     public CurrentValue mapRow(final ResultSet rs, final int rowNum) throws SQLException {
 
-        final AuditDetails auditDetails = AuditDetails.builder().createdBy(rs.getString("createdby"))
+      final AuditDetails auditDetails = AuditDetails.builder().createdBy(rs.getString("createdby"))
                 .createdDate(rs.getLong("createdtime")).lastModifiedBy(rs.getString("lastmodifiedby"))
                 .lastModifiedDate(rs.getLong("lastmodifiedtime")).build();
 
         return CurrentValue.builder().id((Long) rs.getObject("id")).auditDetails(auditDetails)
-                .assetId(rs.getLong("assetid")).assetTranType(TransactionType.fromValue(rs.getString("assetTranType")))
+                .assetId(rs.getLong("assetid")).transactionDate(rs.getLong("transactiondate")).assetTranType(TransactionType.fromValue(rs.getString("assetTranType")))
                 .tenantId(rs.getString("tenantId")).currentAmount(rs.getBigDecimal("currentamount")).build();
     }
 }

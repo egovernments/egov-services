e28246bf168af6b0ed6c32a6def675cf0cbbc906
saurabh297 <saurabh91verma@gmail.com>
2017-11-22 11:30:49 +0530

    pgr/works/done

---
+++ web/ui-web-app/src/development/lcms/lib/specifications/master/eventcalendar.js
@@ -0,0 +1,27 @@
+var dat ={
+  "legal.update": {
+    "numCols": 12,
+    title:"legal.create.group.title.eventDetails",
+    "useTimestamp": true,
+    "objectName": "events",
+    "searchUrl": "/lcms-services/legalcase/event/_get?id={id}",
+    "groups": [
+       {
+        "name": "eventDetails",
+        "label": " ",
+        "fields": [
+          {
+            name: "Calendar", 
+            type: "calendar",
+             jsonPath: "events",
+             isRequired: true,
+             isDisabled: false
+          }
+        ]
+      }
+    ],
+     "url": "/lcms-services/legalcase/hearingdetails/_create",
+    "tenantIdRequired": true
+  }
+}
+   export default dat;

5918bb3b6d0dd15639cb8f2462bee386000aca36
gwl-sourabh <sourabh.debnath@goodworklabs.com>
2017-11-23 12:12:30 +0530

    Modified- Asset template import

--- npm-modules/ui-react-framework/lib/templates/templates/index.js
+++ npm-modules/ui-react-framework/lib/templates/templates/index.js
@@ -4,9 +4,9 @@ var assetMovableReport;
 var VakalatnamaTemplate;
 var landRegisterReport;
 if(process.env.NODE_ENV == "production") {
-  assetImmovableReport = require('egov-asset/templates/assetImmovableReport');
-  assetMovableReport = require('egov-asset/templates/assetMovableReport');
-  landRegisterReport = require('egov-asset/templates/landRegisterReport');
+  assetImmovableReport = require('egov-asset/specifictions/templates/assetImmovableReport');
+  assetMovableReport = require('egov-asset/specifictions/templates/assetMovableReport');
+  landRegisterReport = require('egov-asset/specifictions/templates/landRegisterReport');
 } else {
   assetImmovableReport = require('../../../../asset/lib/specifications/templates/assetImmovableReport');
   assetMovableReport = require('../../../../asset/lib/specifications/templates/assetMovableReport');

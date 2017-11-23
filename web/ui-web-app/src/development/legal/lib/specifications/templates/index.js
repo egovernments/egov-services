efd052668c46386cbc91f374eb557afcb6e9a5b9
gwl-sourabh <sourabh.debnath@goodworklabs.com>
2017-11-23 11:19:44 +0530

    Removed- Development folder from ui-app-web

--- web/ui-web-app/src/development/legal/lib/specifications/templates/index.js
+++
@@ -1,4 +0,0 @@
-import MyExampleTemplate from './MyExampleTemplate';
-import VakalatnamaTemplate from './VakalatnamaTemplate';
-
-export { MyExampleTemplate, VakalatnamaTemplate };
--- web/ui-web-app/src/development/perfManagement/lib/index.js
+++
@@ -1,8 +0,0 @@
-
-
-
-var routes = [];
-
-export {
-  routes
-};

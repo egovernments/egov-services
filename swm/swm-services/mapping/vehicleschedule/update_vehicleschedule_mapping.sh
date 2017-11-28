747ba8639b42bcf3c96822e14cac8292099725bc
sumanth <sumanth@egovernments.org>
2017-11-28 12:15:05 +0530

    vehicle-schedule and vendor indexer.

---
+++ swm/swm-services/mapping/vehicleschedule/update_vehicleschedule_mapping.sh
@@ -0,0 +1,1 @@
+curl -X PUT 'localhost:9200/vehicleschedules/_mapping/vehicleschedules?pretty' -H 'Content-Type: application/json' -d @update_vehicleschedules_mapping.json
---
+++ swm/swm-services/mapping/vendor/update_vendor_mapping.sh
@@ -0,0 +1,1 @@
+curl -X PUT 'localhost:9200/vendor/_mapping/vendor?pretty' -H 'Content-Type: application/json' -d @update_vendor_mapping.json

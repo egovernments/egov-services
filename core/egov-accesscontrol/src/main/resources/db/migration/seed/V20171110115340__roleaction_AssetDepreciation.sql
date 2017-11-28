bb56b9f594d3953617f8dbeed5c2323f20540e10
kusumaRS <kusuma.rs@riflexions.com>
2017-11-28 10:45:21 +0530

    added delete script

--- core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171110115340__roleaction_AssetDepreciation.sql
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171110115340__roleaction_AssetDepreciation.sql
@@ -1,3 +1,8 @@
+delete from eg_action where name='CreateAssetDepreciationServiceMaha' ;
+delete from eg_action where name='ViewAssetDepreciationServiceMaha' ;
+delete from eg_roleaction where actionid in (select id from eg_action where name='CreateAssetDepreciationServiceMaha');
+delete from eg_roleaction where actionid in (select id from eg_action where name='ViewAssetDepreciationServiceMaha');
+
 insert into eg_action(id,name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)values(nextval('SEQ_EG_ACTION'),'CreateAssetDepreciationServiceMaha','/asset-services-maha/assets/depreciations/_create','Asset Service',null,(select id from service where name='Asset Service Maha' and tenantId='default'),1,'Create AssetDepreciation Service Maha',false,1,now(),1,now());
 
 insert into eg_action(id,name,url,servicecode,queryparams,parentmodule,ordernumber,displayname,enabled,createdby,createddate,lastmodifiedby,lastmodifieddate)values(nextval('SEQ_EG_ACTION'),'ViewAssetDepreciationServiceMaha','/asset-services-maha/assets/depreciations/_search','Asset Service',null,(select id from service where name='Asset Service Maha'and tenantId='default'),1,'View AssetDepreciation Service Maha',false,1,now(),1,now());

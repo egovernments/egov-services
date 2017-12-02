f2b477e6251790c928b65ca2ed64ac6a5fd17cd9
venki <venki@egovernments.org>
2017-12-02 10:59:59 +0530

    Added setter for MaintenanceType enum.

--- swm/swm-services/src/main/java/org/egov/swm/domain/enums/MaintenanceType.java
+++ swm/swm-services/src/main/java/org/egov/swm/domain/enums/MaintenanceType.java
@@ -1,11 +1,13 @@
 package org.egov.swm.domain.enums;
 
 import lombok.Getter;
+import lombok.Setter;
 
 public enum MaintenanceType {
     MAINTENANCE("MAINTENANCE"), REPAIR("REPAIR");
 
     @Getter
+    @Setter
     private String code;
 
     MaintenanceType(final String code) {

7bd167692582a6a6e8fb2cc7c51637cc19001417
mansi <mansi.bansal@egovernments.org>
2017-12-04 14:31:26 +0530

    "added materialissue screen enabled as true script"

---
+++ core/egov-accesscontrol/src/main/resources/db/migration/seed/V20171204142853__inv_setmaterialissues_enabledtrue.sql
@@ -0,0 +1,3 @@
+update eg_action set enabled = true where name='Material Issues Create';
+update eg_action set enabled = true where name='Material Issues Update';
+update eg_action set enabled = true where name='Material Issues Search';

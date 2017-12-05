9c71961e6f12afa04af9de64fe2a7ca8641230e6
mansi <mansi.bansal@egovernments.org>
2017-12-05 12:35:00 +0530

    "added supplier db changes script"

---
+++ inventory/inventory-services/src/main/resources/db/migration/main/V20171205123338__changed_supplier_tin_cst_gstsize.sql
@@ -0,0 +1,4 @@
+ALTER TABLE supplier ALTER COLUMN tinno TYPE character varying(11);
+ALTER TABLE supplier ALTER COLUMN cstno TYPE character varying(11);
+ALTER TABLE supplier ALTER COLUMN vatno TYPE character varying(11);
+ALTER TABLE supplier ALTER COLUMN gstno TYPE character varying(15);

f684de1205ba1a3e735e438684ecfe71e8c5d35b
KD <0xbeefdead@gmail.com>
2017-12-06 16:00:01 +0530

    removing test url form api and added error handling when chart cannot be drawn case

--- web/react-pgr-web-v2/src/components/non-framework/perfManagement/dashboard/barchartcard.js
+++ web/react-pgr-web-v2/src/components/non-framework/perfManagement/dashboard/barchartcard.js
@@ -113,26 +113,43 @@ export default class BarChartCard extends Component {
     }
 
     render () {
-        console.log(this.state.data)
+        return (
+            <div>
+            {
+                this.renderChart()
+            }
+            </div>
+        )
+    }
 
+    /**
+     * render
+     * presents chart
+     */
+    renderChart = () => {
+        if (this.state.data.length < 1) {
+            return (
+                <div></div>
+            )
+        }
         return (
             <div style={{"textAlign": "center"}}>
-                <br /><br />
-                <Card className="uiCard">
-                    <CardHeader
-                        title="Chart presented for "
-                    />
-                    <BarChart padding={'20%'} width={600} height={500} data={this.state.data} margin={{top: 20, right: 30, left: 20, bottom: 5}}>
-                        <XAxis dataKey={this.state.dataKey}/>
-                        <YAxis yAxisId="left" orientation="left" stroke="#8884d8"/>
-                        <YAxis yAxisId="right" orientation="right" stroke="#82ca9d"/>
-                        <Tooltip/>
-                        <Legend />
-                        <Bar yAxisId="left" dataKey="target" fill="#8884d8" />
-                        <Bar yAxisId="right" dataKey="value" fill="#82ca9d" />
-                    </BarChart>
-                </Card>
-            </div>
-        );
+            <br /><br />
+            <Card className="uiCard">
+                <CardHeader
+                    title={<strong> Chart presented for</strong>}
+                />
+                <BarChart padding={'20%'} width={600} height={500} data={this.state.data} margin={{top: 20, right: 30, left: 20, bottom: 5}}>
+                    <XAxis dataKey={this.state.dataKey}/>
+                    <YAxis yAxisId="left" orientation="left" stroke="#8884d8"/>
+                    <YAxis yAxisId="right" orientation="right" stroke="#82ca9d"/>
+                    <Tooltip/>
+                    <Legend />
+                    <Bar yAxisId="left" dataKey="target" fill="#8884d8" />
+                    <Bar yAxisId="right" dataKey="value" fill="#82ca9d" />
+                </BarChart>
+            </Card>
+        </div>
+        )
     }
 }

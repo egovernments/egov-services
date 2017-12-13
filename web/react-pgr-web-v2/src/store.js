6d872c4ac5ab705d1905c3554900d88b60890e29
Varun Hegde <varun@zenparent.in>
2017-12-13 16:04:28 +0530

    Added redux logger for debugging redux actions

--- web/react-pgr-web-v2/src/store.js
+++ web/react-pgr-web-v2/src/store.js
@@ -1,11 +1,17 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-// import { autoRehydrate } from 'redux-persist';
 import { persistStore, autoRehydrate } from 'redux-persist';
 import reducers from './reducer';
 
-const store = createStore(reducers, {}, compose(applyMiddleware(thunk), autoRehydrate()));
+const middlewares = [];
 
-// persistStore(store);
+middlewares.push(thunk);
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const store = createStore(reducers, {}, compose(applyMiddleware(...middlewares), autoRehydrate()));
 
 export default store;

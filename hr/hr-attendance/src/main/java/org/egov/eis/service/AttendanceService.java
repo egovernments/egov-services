c2102d3319459f612542f580fcdb8fd892dd0c3c
Seema Nayak <seema.nayak@egovernments.org>
2017-12-16 20:13:17 +0530

    [EGSVC-1285] Changes in attendance report

--- hr/hr-attendance/src/main/java/org/egov/eis/service/AttendanceService.java
+++ hr/hr-attendance/src/main/java/org/egov/eis/service/AttendanceService.java
@@ -40,8 +40,15 @@
 
 package org.egov.eis.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.eis.config.PropertiesManager;
 import org.egov.eis.model.Attendance;
@@ -57,143 +64,145 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.stream.Collectors;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class AttendanceService {
-    public static final Logger logger = LoggerFactory.getLogger(AttendanceService.class);
+	public static final Logger logger = LoggerFactory.getLogger(AttendanceService.class);
 
-    @Autowired
-    private AttendanceRepository attendanceRepository;
+	@Autowired
+	private AttendanceRepository attendanceRepository;
 
-    @Autowired
-    private AttendanceProducer attendanceProducer;
+	@Autowired
+	private AttendanceProducer attendanceProducer;
 
-    @Autowired
-    private EmployeeService employeeService;
+	@Autowired
+	private EmployeeService employeeService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-    @Autowired
-    private HolidayService holidayService;
+	@Autowired
+	private HolidayService holidayService;
 
-    @Autowired
-    private HRConfigurationService hrConfigurationService;
+	@Autowired
+	private HRConfigurationService hrConfigurationService;
 
-    @Autowired
-    private PropertiesManager propertiesManager;
+	@Autowired
+	private PropertiesManager propertiesManager;
 
-    public List<Attendance> getAttendances(final AttendanceGetRequest attendanceGetRequest) throws ParseException {
-        return attendanceRepository.findForCriteria(attendanceGetRequest);
-    }
+	public List<Attendance> getAttendances(final AttendanceGetRequest attendanceGetRequest) throws ParseException {
+		return attendanceRepository.findForCriteria(attendanceGetRequest);
+	}
 
-    public List<Attendance> findAttendanceForHolidays(final AttendanceGetRequest attendanceGetRequest, RequestInfo requestInfo) throws ParseException {
-        List<Date> holidayDates = getHolidayDates(attendanceGetRequest, requestInfo);
-        return attendanceRepository.findByCriteria(attendanceGetRequest, holidayDates);
-    }
+	public List<Attendance> findAttendanceForHolidays(final AttendanceGetRequest attendanceGetRequest,
+			RequestInfo requestInfo) throws ParseException {
+		List<Date> holidayDates = getHolidayDates(attendanceGetRequest, requestInfo);
+		return attendanceRepository.findByCriteria(attendanceGetRequest, holidayDates);
+	}
 
-    private List<Date> getHolidayDates(AttendanceGetRequest attendanceGetRequest, RequestInfo requestInfo) {
-        return holidayService.getHolidaysBetweenDates(attendanceGetRequest.getTenantId(), requestInfo, attendanceGetRequest.getFromDate(), new Date());
-    }
+	private List<Date> getHolidayDates(AttendanceGetRequest attendanceGetRequest, RequestInfo requestInfo) {
+		return holidayService.getHolidaysBetweenDates(attendanceGetRequest.getTenantId(), requestInfo,
+				attendanceGetRequest.getFromDate(), new Date());
+	}
 
-    /*
-     * This method is used to create new attendance
-     * @return Attendance, return the attendance details with current status
-     * @param attendance, hold attendance details
-     */
+	/*
+	 * This method is used to create new attendance
+	 * 
+	 * @return Attendance, return the attendance details with current status
+	 * 
+	 * @param attendance, hold attendance details
+	 */
 
-    public List<Attendance> createAsync(final AttendanceRequest attendanceRequest) {
-        String attendanceValue = null;
-        try {
-            logger.info("createAttendance service::" + attendanceRequest);
-            attendanceValue = objectMapper.writeValueAsString(attendanceRequest);
-            logger.info("attendanceValue::" + attendanceValue);
-        } catch (final JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        try {
-            attendanceProducer.sendMessage("egov-hr-attendance", "save-attendance", attendanceValue);
-        } catch (final Exception ex) {
-            ex.printStackTrace();
-        }
-        return attendanceRequest.getAttendances();
-    }
+	public List<Attendance> createAsync(final AttendanceRequest attendanceRequest) {
+		String attendanceValue = null;
+		try {
+			logger.info("createAttendance service::" + attendanceRequest);
+			attendanceValue = objectMapper.writeValueAsString(attendanceRequest);
+			logger.info("attendanceValue::" + attendanceValue);
+		} catch (final JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		try {
+			attendanceProducer.sendMessage("egov-hr-attendance", "save-attendance", attendanceValue);
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+		return attendanceRequest.getAttendances();
+	}
 
-    public List<Attendance> getAttendanceReport(final AttendanceReportRequest attendanceReportRequest, RequestInfo requestInfo) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        List<Long> ids = null;
+	public List<Attendance> getAttendanceReport(final AttendanceReportRequest attendanceReportRequest,
+			RequestInfo requestInfo) throws ParseException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		List<Long> ids = null;
 
-        final List<EmployeeInfo> employeeInfos = employeeService.getEmployees(attendanceReportRequest, requestInfo);
-        if (employeeInfos.isEmpty())
-            return Collections.EMPTY_LIST;
+		final List<EmployeeInfo> employeeInfos = employeeService.getEmployees(attendanceReportRequest, requestInfo);
+		if (employeeInfos.isEmpty())
+			return Collections.EMPTY_LIST;
 
-        ids = employeeInfos.stream().map(employeeInfo -> employeeInfo.getId()).collect(Collectors.toList());
-        attendanceReportRequest.setEmployeeIds(ids);
-        Long daysInMonth = getNoOfDaysInMonth(attendanceReportRequest.getMonth(), attendanceReportRequest.getYear());
-        return attendanceRepository.findReportQuery(attendanceReportRequest, daysInMonth);
-    }
+		ids = employeeInfos.stream().map(employeeInfo -> employeeInfo.getId()).collect(Collectors.toList());
+		attendanceReportRequest.setEmployeeIds(ids);
+		Long daysInMonth = getNoOfDaysInMonth(attendanceReportRequest.getMonth(), attendanceReportRequest.getYear());
+		return attendanceRepository.findReportQuery(attendanceReportRequest, daysInMonth);
+	}
 
-    public Long getNoOfDaysInMonth(Integer month, String year) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, Integer.parseInt(year));
-        calendar.set(Calendar.MONTH, month);
-        return Long.valueOf(calendar.getActualMaximum(calendar.DAY_OF_MONTH));
-    }
+	public Long getNoOfDaysInMonth(Integer month, String year) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, Integer.parseInt(year));
+		calendar.set(Calendar.MONTH, month - 1);
+		return Long.valueOf(calendar.getActualMaximum(calendar.DAY_OF_MONTH));
+	}
 
-    public Long getNoOfWorkingDays(AttendanceReportRequest attendanceReportRequest, RequestInfo requestInfo) {
+	public Long getNoOfWorkingDays(AttendanceReportRequest attendanceReportRequest, RequestInfo requestInfo) {
 
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, Integer.parseInt(attendanceReportRequest.getYear()));
-        calendar.set(Calendar.MONTH, attendanceReportRequest.getMonth());
-        final Map<String, List<String>> weeklyHolidays = hrConfigurationService.getWeeklyHolidays(attendanceReportRequest.getTenantId(),
-                requestInfo);
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, Integer.parseInt(attendanceReportRequest.getYear()));
+		calendar.set(Calendar.MONTH, attendanceReportRequest.getMonth() - 1);
+		final Map<String, List<String>> weeklyHolidays = hrConfigurationService
+				.getWeeklyHolidays(attendanceReportRequest.getTenantId(), requestInfo);
 
-        int count = 0;
-        if ((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY))
-            count++;
+		int count = 0;
+		if ((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY))
+			count++;
 
-        if (propertiesManager.getHrMastersServiceConfigurationsFiveDayWeek()
-                .equals(weeklyHolidays.get(propertiesManager.getHrMastersServiceConfigurationsKey()).get(0))) {
-            if ((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY))
-                count++;
-        } else if (propertiesManager.getHrMastersServiceConfigurationsFiveDayWithSecondSaturday()
-                .equals(weeklyHolidays.get(propertiesManager.getHrMastersServiceConfigurationsKey()).get(0))) {
-            count += 1;
-        } else if (propertiesManager.getHrMastersServiceConfigurationsFiveDayWithSecondAndFourthSaturday()
-                .equals(weeklyHolidays.get(propertiesManager.getHrMastersServiceConfigurationsKey()).get(0)))
-            count += 2;
+		if (propertiesManager.getHrMastersServiceConfigurationsFiveDayWeek()
+				.equals(weeklyHolidays.get(propertiesManager.getHrMastersServiceConfigurationsKey()).get(0))) {
+			if ((calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY))
+				count++;
+		} else if (propertiesManager.getHrMastersServiceConfigurationsFiveDayWithSecondSaturday()
+				.equals(weeklyHolidays.get(propertiesManager.getHrMastersServiceConfigurationsKey()).get(0))) {
+			count += 1;
+		} else if (propertiesManager.getHrMastersServiceConfigurationsFiveDayWithSecondAndFourthSaturday()
+				.equals(weeklyHolidays.get(propertiesManager.getHrMastersServiceConfigurationsKey()).get(0)))
+			count += 2;
 
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		Date monthStart = calendar.getTime();
+		calendar.add(Calendar.MONTH, 1);
+		calendar.add(Calendar.DAY_OF_MONTH, -1);
+		Date monthEnd = calendar.getTime();
 
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        Date monthStart = calendar.getTime();
-        calendar.add(Calendar.MONTH, 1);
-        calendar.add(Calendar.DAY_OF_MONTH, -1);
-        Date monthEnd = calendar.getTime();
+		List<Date> holidaysBetweenDates = holidayService.getHolidaysBetweenDates(attendanceReportRequest.getTenantId(),
+				requestInfo, monthStart, monthEnd);
+		count += holidaysBetweenDates.size();
+		count = calendar.getActualMaximum(calendar.DAY_OF_MONTH) - count;
+		return Long.valueOf(count);
+	}
 
-        List<Date> holidaysBetweenDates = holidayService.getHolidaysBetweenDates(attendanceReportRequest.getTenantId(), requestInfo, monthStart, monthEnd);
-        count += holidaysBetweenDates.size();
-        count = calendar.getActualMaximum(calendar.DAY_OF_MONTH) - count;
-        return Long.valueOf(count);
-    }
+	public boolean getByEmployeeAndDate(final Long employee, final Date attendanceDate, final String tenantId) {
+		return attendanceRepository.checkAttendanceByEmployeeAndDate(employee, attendanceDate, tenantId);
+	}
 
+	public AttendanceRequest create(final AttendanceRequest attendanceRequest) {
+		return attendanceRepository.saveAttendance(attendanceRequest);
+	}
 
-    public boolean getByEmployeeAndDate(final Long employee, final Date attendanceDate, final String tenantId) {
-        return attendanceRepository.checkAttendanceByEmployeeAndDate(employee, attendanceDate, tenantId);
-    }
+	public AttendanceType getAttendanceTypeByCode(final String code, final String tenantId) {
+		return attendanceRepository.findAttendanceTypeByCode(code, tenantId);
+	}
 
-    public AttendanceRequest create(final AttendanceRequest attendanceRequest) {
-        return attendanceRepository.saveAttendance(attendanceRequest);
-    }
-
-    public AttendanceType getAttendanceTypeByCode(final String code, final String tenantId) {
-        return attendanceRepository.findAttendanceTypeByCode(code, tenantId);
-    }
-
-    public List<AttendanceType> getAllAttendanceTypes(final String tenantId) {
-        return attendanceRepository.findAllAttendanceTypes(tenantId);
-    }
+	public List<AttendanceType> getAllAttendanceTypes(final String tenantId) {
+		return attendanceRepository.findAllAttendanceTypes(tenantId);
+	}
 }

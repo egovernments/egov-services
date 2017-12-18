f2b56a14a79dd6346b28ac6ba6d29dbf68896425
SurabhiDesai <surabhi.desai@goodworklabs.com>
2017-12-18 15:19:48 +0530

    Asset Reports Amount fields reset fix

--- web/react-pgr-web-v2/src/components/contents/reports/showField.js
+++ web/react-pgr-web-v2/src/components/contents/reports/showField.js
@@ -69,6 +69,7 @@ export default class ShowField extends Component {
         return (
           <Col xs={12} sm={4} md={3} lg={3}>
             <TextField
+              value={this.props.value}
               className="custom-form-control-for-textfield"
               id={obj.label.split('.').join('-')}
               fullWidth={true}

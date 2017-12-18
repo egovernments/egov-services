5f31c2de8a3162444743dcb0604842776bea2285
Barath Vutukuri <barath.vutukuri@goodworklabs.com>
2017-12-18 18:38:24 +0530

    Modified the datatype for dates in pricelist and pricelistdetails

---
+++ inventory/inventory-services/src/main/resources/db/migration/main/V20171218183727__alter_pricelistdetailsdates.sql
@@ -0,0 +1,9 @@
+alter table pricelist alter column agreementDate type bigint not null;
+
+alter table pricelist alter column agreementStartDate type bigint not null;
+
+alter table pricelist alter column agreementEndDate type bigint not null;
+
+alter table pricelistdetails alter column fromDate type bigint not null;
+
+alter table pricelistdetails alter column fromDate type bigint not null;

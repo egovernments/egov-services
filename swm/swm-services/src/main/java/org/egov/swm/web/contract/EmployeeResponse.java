37ba9daed2dc1dba2df30b0623039fc4864177cb
venki <venki@egovernments.org>
2017-12-20 10:14:43 +0530

    Not receiving responseinfo from employee service.

--- swm/swm-services/src/main/java/org/egov/swm/web/contract/EmployeeResponse.java
+++ swm/swm-services/src/main/java/org/egov/swm/web/contract/EmployeeResponse.java
@@ -3,17 +3,12 @@ package org.egov.swm.web.contract;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.egov.common.contract.response.ResponseInfo;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 
 @Getter
 public class EmployeeResponse {
-
-    @JsonProperty("ResponseInfo")
-    private final ResponseInfo responseInfo = null;
 
     @JsonProperty("Employee")
     private final List<Employee> employees = new ArrayList<>();

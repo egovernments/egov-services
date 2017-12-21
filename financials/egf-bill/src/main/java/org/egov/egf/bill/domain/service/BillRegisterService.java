bf87a3a2fb78234db1016cbc758d49d18f511e24
venki <venki@egovernments.org>
2017-12-21 15:44:03 +0530

    Fixed the issue while showing error on account detail type not present.

--- financials/egf-bill/src/main/java/org/egov/egf/bill/domain/service/BillRegisterService.java
+++ financials/egf-bill/src/main/java/org/egov/egf/bill/domain/service/BillRegisterService.java
@@ -311,7 +311,7 @@ public class BillRegisterService {
                             if (accountDetailType == null || accountDetailType.getId() == null
                                     || accountDetailType.getId().isEmpty())
                                 throw new CustomException("accountDetailType",
-                                        "Given accountDetailType is Invalid: " + accountDetailType.getId());
+                                        "Given accountDetailType is Invalid: " + detail.getAccountDetailType());
 
                             adtMap.put(detail.getAccountDetailType().getId(), accountDetailType);
                         }
@@ -328,7 +328,7 @@ public class BillRegisterService {
                             if (accountDetailKey == null || accountDetailKey.getId() == null
                                     || accountDetailKey.getId().isEmpty())
                                 throw new CustomException("accountDetailType",
-                                        "Given accountDetailType is Invalid: " + accountDetailKey.getId());
+                                        "Given accountDetailType is Invalid: " + detail.getAccountDetailKey());
 
                             adkMap.put(detail.getAccountDetailKey().getId(), accountDetailKey);
                         }

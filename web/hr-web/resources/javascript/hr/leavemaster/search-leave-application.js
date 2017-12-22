fdd442d751d5b89d6a4caf48cd82dd5f97fd6974
Siva-nalluri <siva.nalluri@riflexions.com>
2017-12-22 13:05:27 +0530

    [APQA-86] Changes to the search leave application page

--- web/hr-web/resources/javascript/hr/leavemaster/search-leave-application.js
+++ web/hr-web/resources/javascript/hr/leavemaster/search-leave-application.js
@@ -2,22 +2,23 @@ var flag = 0;
 class SearchLeaveApplication extends React.Component {
   constructor(props) {
     super(props);
-    this.state={employees:[],employ:{},searchSet:{
-      name:"",
-      code:"",
-      departmentId:"",
-      designationId:""
-    },
-      isSearchClicked:false,
-      assignments_department:[],
-      assignments_designation:[],
+    this.state = {
+      employees: [], employ: {}, searchSet: {
+        name: "",
+        code: "",
+        departmentId: "",
+        designationId: ""
+      },
+      isSearchClicked: false,
+      assignments_department: [],
+      assignments_designation: [],
       modified: false
     }
-    this.handleChange=this.handleChange.bind(this);
-    this.search=this.search.bind(this);
-    this.handleBlur=this.handleBlur.bind(this);
-    this.setInitialState=this.setInitialState.bind(this);
-    this.handleClick=this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.search = this.search.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.setInitialState = this.setInitialState.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
 
@@ -25,27 +26,27 @@ class SearchLeaveApplication extends React.Component {
     this.setState(initState);
   }
 
-  componentDidMount(){
-    if(window.opener && window.opener.document) {
-       var logo_ele = window.opener.document.getElementsByClassName("homepage_logo");
-       if(logo_ele && logo_ele[0]) {
-         document.getElementsByClassName("homepage_logo")[0].src = window.location.origin + logo_ele[0].getAttribute("src");
-       }
-     }
-    if(getUrlVars()["type"]) $('#hp-citizen-title').text(titleCase(getUrlVars()["type"]) + " Leave Application");
+  componentDidMount() {
+    if (window.opener && window.opener.document) {
+      var logo_ele = window.opener.document.getElementsByClassName("homepage_logo");
+      if (logo_ele && logo_ele[0]) {
+        document.getElementsByClassName("homepage_logo")[0].src = window.location.origin + logo_ele[0].getAttribute("src");
+      }
+    }
+    if (getUrlVars()["type"]) $('#hp-citizen-title').text(titleCase(getUrlVars()["type"]) + " Leave Application");
 
     var count = 2, _this = this, _state = {};
-    var checkCountNCall = function(key, res) {
-    count--;
-    _state[key] = res;
-    if(count == 0)
-      _this.setInitialState(_state);
+    var checkCountNCall = function (key, res) {
+      count--;
+      _state[key] = res;
+      if (count == 0)
+        _this.setInitialState(_state);
     }
 
-    getDropdown("assignments_designation", function(res) {
+    getDropdown("assignments_designation", function (res) {
       checkCountNCall("assignments_designation", res);
     });
-    getDropdown("assignments_department", function(res) {
+    getDropdown("assignments_department", function (res) {
       checkCountNCall("assignments_department", res);
     });
   }
@@ -54,246 +55,259 @@ class SearchLeaveApplication extends React.Component {
   search(e) {
     let {
     name,
-    code,
-    departmentId,
-    designationId}=this.state.searchSet, _this = this;
+      code,
+      departmentId,
+      designationId } = this.state.searchSet, _this = this;
     e.preventDefault();
     //call api call
-    var employees=[];
+    var employees = [];
 
-    commonApiPost("hr-employee","employees","_search",{..._this.state.searchSet, tenantId,
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+
+    today = dd + '/' + mm + '/' + yyyy;
+
+
+    commonApiPost("hr-employee", "employees", "_search", {
+      ..._this.state.searchSet, tenantId,
+      isPrimary: true,
+      asOnDate: today,
       departmentId: departmentId || null,
       designationId: designationId || null,
       code: code || null,
-      pageSize: 500}, function(err,res) {
-        if(res){
-          employees=res["Employee"];
-          flag = 1;
-          _this.setState({
-            isSearchClicked:true,
-            employees,
-            modified: true
-          });
-        }
-          setTimeout(function() {
-            _this.setState({
-              modified: false
-            })
-          }, 1200);
+      pageSize: 500
+    }, function (err, res) {
+      if (res) {
+        employees = res["Employee"];
+        flag = 1;
+        _this.setState({
+          isSearchClicked: true,
+          employees,
+          modified: true
+        });
+      }
+      setTimeout(function () {
+        _this.setState({
+          modified: false
+        })
+      }, 1200);
     });
   }
 
   handleChange(e, name) {
-      this.setState({
-          searchSet:{
-              ...this.state.searchSet,
-              [name]:e.target.value
-          }
-      })
+    this.setState({
+      searchSet: {
+        ...this.state.searchSet,
+        [name]: e.target.value
+      }
+    })
   }
 
   componentWillUpdate() {
-    if(flag == 1) {
+    if (flag == 1) {
       flag = 0;
       $('#employeeTable').dataTable().fnDestroy();
     }
   }
 
   handleBlur(e) {
-    setTimeout(function(){
-       if(document.activeElement.id == "sub") {
-          $("#sub").click();
-       }
+    setTimeout(function () {
+      if (document.activeElement.id == "sub") {
+        $("#sub").click();
+      }
     }, 100);
     var _this = this;
-      if (e.target.value) {
-              var code = e.target.value;
-               commonApiPost("hr-employee", "employees", "_search", { code, tenantId },function(err,res){
-                 if(res){
-                   _this.setState({
-                       searchSet: {
-                         ..._this.state.searchSet,
-                          name: res.Employee && res.Employee[0] ? res.Employee[0].name : ""
-                       }
-                   })
-                 }
-               });
-      } else {
-        _this.setState({
-          searchSet: {
-            ..._this.state.searchSet,
-            name: ""
-          }
-        })
-      }
+    if (e.target.value) {
+      var code = e.target.value;
+      commonApiPost("hr-employee", "employees", "_search", { code, tenantId }, function (err, res) {
+        if (res) {
+          _this.setState({
+            searchSet: {
+              ..._this.state.searchSet,
+              name: res.Employee && res.Employee[0] ? res.Employee[0].name : ""
+            }
+          })
+        }
+      });
+    } else {
+      _this.setState({
+        searchSet: {
+          ..._this.state.searchSet,
+          name: ""
+        }
+      })
     }
+  }
 
 
-  close(){
-      // widow.close();
-      open(location, '_self').close();
+  close() {
+    // widow.close();
+    open(location, '_self').close();
   }
 
   handleClick(type, id) {
-    if (type==="create") {
+    if (type === "create") {
       window.open(`app/hr/leavemaster/apply-leave.html?id=${id}&type=${type}`, '_blank', 'location=yes, height=760, width=800, scrollbars=yes, status=yes');
-    }else {
+    } else {
       window.open(`app/hr/leavemaster/view-apply.html?id=${id}&type=${type}`, '_blank', 'location=yes, height=760, width=800, scrollbars=yes, status=yes');
     }
   }
 
 
   componentDidUpdate(prevProps, prevState) {
-      if (this.state.modified) {
-          $('#employeeTable').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                     'copy', 'csv', 'excel', 'pdf', 'print'
-             ],
-             ordering: false,
-             bDestroy: true,
-             language: {
-               "emptyTable": "No Records"
-            }
-          });
-      }
+    if (this.state.modified) {
+      $('#employeeTable').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+          'copy', 'csv', 'excel', 'pdf', 'print'
+        ],
+        ordering: false,
+        bDestroy: true,
+        language: {
+          "emptyTable": "No Records"
+        }
+      });
+    }
   }
 
   render() {
-    let {handleChange,search,handleBlur,handleClick}=this;
-    let {isSearchClicked,employees,assignments_designation,assignments_department}=this.state;
-    let {name,
-    code,
-    departmentId,
-    designationId}=this.state.searchSet;
-    const renderOption=function(list)
-    {
-        if(list)
-        {
-            return list.map((item)=>
-            {
-                return (<option key={item.id} value={item.id}>
-                        {item.name}
-                  </option>)
-            })
-        }
+    let { handleChange, search, handleBlur, handleClick } = this;
+    let { isSearchClicked, employees, assignments_designation, assignments_department } = this.state;
+    let { name,
+      code,
+      departmentId,
+      designationId } = this.state.searchSet;
+    const renderOption = function (list) {
+      if (list) {
+        return list.map((item) => {
+          return (<option key={item.id} value={item.id}>
+            {item.name}
+          </option>)
+        })
+      }
     }
 
 
-    const showTable=function()
-    {
-      if(isSearchClicked)
-      {
-          return (
-            <table id="employeeTable" className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Employee Code</th>
-                        <th>Employee Name</th>
-                        <th>Employee Designation</th>
-                        <th>Employee Department</th>
+    const showTable = function () {
+      if (isSearchClicked) {
+        return (
+          <table id="employeeTable" className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Employee Code</th>
+                <th>Employee Name</th>
+                <th>Employee Designation</th>
+                <th>Employee Department</th>
 
-                    </tr>
-                </thead>
+              </tr>
+            </thead>
 
-                <tbody id="employeeSearchResultTableBody">
-                    {
-                        renderBody()
-                    }
-                </tbody>
+            <tbody id="employeeSearchResultTableBody">
+              {
+                renderBody()
+              }
+            </tbody>
 
-            </table>
+          </table>
 
-          )
+        )
 
 
       }
 
     }
-    const renderBody=function()
-    {
-      if(employees.length>0)
-      {
-      return employees.map((item,index,id)=>
-      {
-            return (<tr key={index} onClick={() => {handleClick(getUrlVars()["type"], item.id)}}>
+    const renderBody = function () {
+      if (employees.length > 0) {
+        return employees.map((item, index, id) => {
+          return (<tr key={index} onClick={() => { handleClick(getUrlVars()["type"], item.id) }}>
 
-                    <td data-label="code">{item.code}</td>
-                    <td data-label="name">{item.name}</td>
-                    <td data-label="designation">{getNameById(assignments_designation,item.assignments[0].designation)}</td>
-                    <td data-label="department">{getNameById(assignments_department,item.assignments[0].department)}</td>
-                </tr>
-            );
-      })
+            <td data-label="code">{item.code}</td>
+            <td data-label="name">{item.name}</td>
+            <td data-label="designation">{getNameById(assignments_designation, item.assignments[0].designation)}</td>
+            <td data-label="department">{getNameById(assignments_department, item.assignments[0].department)}</td>
+          </tr>
+          );
+        })
+      }
     }
-}
     return (
       <div>
         <h3>Search employee to {getUrlVars()["type"]} a leave application</h3>
-          <form onSubmit={(e)=>{search(e)}}>
+        <form onSubmit={(e) => { search(e) }}>
           <fieldset>
-          <div className="row">
-                <div className="col-sm-6">
-                    <div className="row">
-                        <div className="col-sm-6 label-text">
-                          <label for="">Designation  </label>
-                        </div>
-                        <div className="col-sm-6">
-                          <div className="styled-select">
-                            <select id="designationId" name="designationId" value={designationId} onChange={(e)=>{
-                                handleChange(e,"designationId")}}>
-                            <option value= "">Select Designation</option>
-                            {renderOption(this.state.assignments_designation)}
-                           </select>
-                        </div>
-                        </div>
-                    </div>
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="row">
+                  <div className="col-sm-6 label-text">
+                    <label for="">Designation  </label>
                   </div>
                   <div className="col-sm-6">
-                      <div className="row">
-                          <div className="col-sm-6 label-text">
-                            <label for="">Department  </label>
-                          </div>
-                          <div className="col-sm-6">
-                          <div className="styled-select">
-                              <select id="departmentId" name="departmentId" value={departmentId}
-                              onChange={(e)=>{ handleChange(e,"departmentId")}}>
-                                <option  value= "">Select Department</option>
-                                {renderOption(this.state.assignments_department)}
-                             </select>
-                          </div>
-                          </div>
-                      </div>
+                    <div className="styled-select">
+                      <select id="designationId" name="designationId" value={designationId} onChange={(e) => {
+                        handleChange(e, "designationId")
+                      }}>
+                        <option value="">Select Designation</option>
+                        {renderOption(this.state.assignments_designation)}
+                      </select>
                     </div>
+                  </div>
                 </div>
-
-
-              <div className="row">
-                <div className="col-sm-6">
-                    <div className="row">
-                        <div className="col-sm-6 label-text">
-                          <label for="">Employee Code</label>
-                        </div>
-                        <div className="col-sm-6">
-                            <input type="text" id="code" name="code" value={code}
-                              onChange={(e)=>{handleChange(e,"code")}} onBlur={(e)=>{handleBlur(e)}}/>
-                        </div>
-                    </div>
+              </div>
+              <div className="col-sm-6">
+                <div className="row">
+                  <div className="col-sm-6 label-text">
+                    <label for="">Department  </label>
                   </div>
                   <div className="col-sm-6">
-                      <div className="row">
-                          <div className="col-sm-6 label-text">
-                            <label for="">Employee Name</label>
-                          </div>
-                          <div className="col-sm-6">
-                              <input type="text" id="name" name="name" value={name}
-                                onChange={(e)=>{  handleChange(e,"name")}} disabled/>
-                          </div>
-                      </div>
+                    <div className="styled-select">
+                      <select id="departmentId" name="departmentId" value={departmentId}
+                        onChange={(e) => { handleChange(e, "departmentId") }}>
+                        <option value="">Select Department</option>
+                        {renderOption(this.state.assignments_department)}
+                      </select>
                     </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
-                {/*<div className="row">
+
+            <div className="row">
+              <div className="col-sm-6">
+                <div className="row">
+                  <div className="col-sm-6 label-text">
+                    <label for="">Employee Code</label>
+                  </div>
+                  <div className="col-sm-6">
+                    <input type="text" id="code" name="code" value={code}
+                      onChange={(e) => { handleChange(e, "code") }} onBlur={(e) => { handleBlur(e) }} />
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-6">
+                <div className="row">
+                  <div className="col-sm-6 label-text">
+                    <label for="">Employee Name</label>
+                  </div>
+                  <div className="col-sm-6">
+                    <input type="text" id="name" name="name" value={name}
+                      onChange={(e) => { handleChange(e, "name") }} disabled />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/*<div className="row">
                       <div className="col-sm-6">
                           <div className="row">
                               <div className="col-sm-6 label-text">
@@ -351,13 +365,13 @@ class SearchLeaveApplication extends React.Component {
 
 
             <div className="text-center">
-                <button id="sub" type="submit"  className="btn btn-submit">Search</button> &nbsp;&nbsp;
-                <button type="button" className="btn btn-close" onClick={(e)=>{this.close()}}>Close</button>
+              <button id="sub" type="submit" className="btn btn-submit">Search</button> &nbsp;&nbsp;
+                <button type="button" className="btn btn-close" onClick={(e) => { this.close() }}>Close</button>
             </div>
           </fieldset>
-          </form>
-          <br/>
-          {showTable()}
+        </form>
+        <br />
+        {showTable()}
       </div>
     );
   }

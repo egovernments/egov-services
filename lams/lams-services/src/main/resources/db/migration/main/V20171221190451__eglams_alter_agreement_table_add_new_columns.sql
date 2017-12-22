ffe583b1f4fdd6846c30b17b3336010df2a941c9
Rafeek <mohammad.rafeek@egovernments.org>
2017-12-22 12:36:39 +0530

    [EGSVC-2324]-removed not null constraint

--- lams/lams-services/src/main/resources/db/migration/main/V20171221190451__eglams_alter_agreement_table_add_new_columns.sql
+++ lams/lams-services/src/main/resources/db/migration/main/V20171221190451__eglams_alter_agreement_table_add_new_columns.sql
@@ -1,5 +1,5 @@
 ALTER TABLE eglams_agreement ADD COLUMN first_allotment character varying(16) ;
-ALTER TABLE eglams_agreement ADD COLUMN gstin character varying(16) NOT NULL;
+ALTER TABLE eglams_agreement ADD COLUMN gstin character varying(16) ;
 ALTER TABLE eglams_agreement ADD COLUMN municipal_order_no character varying(32);
 ALTER TABLE eglams_agreement ADD COLUMN municipal_order_date timestamp without time zone;
 ALTER TABLE eglams_agreement ADD COLUMN govt_order_no character varying(32);

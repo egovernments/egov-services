f03259db47ad6cae03099a090d6ba3ea05d9276f
kusumaRS <kusuma.rs@riflexions.com>
2017-12-29 11:36:15 +0530

    changes in landregister

--- asset/asset-services-maha/src/main/resources/db/migration/main/V20171228161616__alter_landdetails.sql
+++ asset/asset-services-maha/src/main/resources/db/migration/main/V20171228161616__alter_landdetails.sql
@@ -1,4 +1,3 @@
-
 CREATE SEQUENCE seq_egasset_asset_landetails;
 
 ALTER TABLE egasset_asset_landdetails DROP CONSTRAINT pk_egasset_asset_landdetails;
@@ -6,7 +5,7 @@ ALTER TABLE egasset_asset_landdetails DROP CONSTRAINT pk_egasset_asset_landdetai
 ALTER TABLE egasset_asset_landdetails ADD COLUMN id bigint;
 ALTER TABLE egasset_asset_landdetails ALTER COLUMN id SET NOT NULL;
 
-ALTER TABLE pk_egasset_asset_landdetails ADD PRIMARY KEY (id,tenantid);
+ALTER TABLE egasset_asset_landdetails
+  ADD CONSTRAINT egasset_asset_landdetails_pkey PRIMARY KEY(id, tenantid);
 
-ALTER TABLE egasset_asset_landdetails ADD CONSTRAINT uk_egasset_asset_landdetails UNIQUE(id, assetid, tenantid);
-
+ALTER TABLE egasset_asset_landdetails ADD CONSTRAINT uk_egasset_asset_landdetails UNIQUE(id, assetid, tenantid)

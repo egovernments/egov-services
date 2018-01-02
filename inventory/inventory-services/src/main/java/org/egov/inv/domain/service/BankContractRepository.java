13afa475b42c58280a69e00250374831286ca660
sanjeevnvp <sanjeev.p@egovernments.org>
2018-01-02 14:18:52 +0530

    pointing to egf master bank search

--- inventory/inventory-services/src/main/java/org/egov/inv/domain/service/BankContractRepository.java
+++ inventory/inventory-services/src/main/java/org/egov/inv/domain/service/BankContractRepository.java
@@ -13,7 +13,7 @@ public class BankContractRepository {
 
     private RestTemplate restTemplate;
     private String hostUrl;
-    public static final String SEARCH_URL = "/egf-masters/banks/_search?";
+    public static final String SEARCH_URL = "/egf-master/banks/_search?";
 
     public BankContractRepository(@Value("${egf.master.host.url}") String hostUrl, RestTemplate restTemplate) {
         this.restTemplate = restTemplate;

84739ab4d198d574be87330618263337f122cfa7
shashi <shashikanth.g@egovernments.org>
2018-01-03 12:56:58 +0530

    Added script for scrap service,

---
+++ inventory/inventory-services/src/main/resources/db/migration/main/V20180103121540__inv_scrap_create_ddl.sql
@@ -0,0 +1,40 @@
+CREATE TABLE scrap(
+	id character varying(50) NOT NULL,
+	tenantid varchar(256) NOT NULL,
+	storeCode character varying(50),
+	scrapNumber character varying(50),
+	scrapdate bigint,
+	description character varying(500),
+	status character varying(50),
+	workflowdetailid character varying(50),
+	stateid character varying(50),
+	designation character varying(50),
+        createdby varchar(50),
+        createdtime bigint,
+        lastmodifiedby varchar(50),
+        lastmodifiedtime bigint
+       );
+
+alter table scrap add constraint pk_scrap primary key (id,tenantId);
+create sequence seq_scrap;
+
+CREATE TABLE scrapDetails(
+	id character varying(50) NOT NULL,
+	tenantid varchar(256) NOT NULL,
+	scrapNumber character varying(50),
+	material character varying(50),
+	uom character varying(50),
+	lotnumber character varying(50),
+	expirydate bigint,
+	scrapReason character varying(500),
+	quantity numeric (13,2),
+	disposalQuantity numeric (13,2),
+	scrapValue numeric (13,2),
+        createdby varchar(50),
+        createdtime bigint,
+        lastmodifiedby varchar(50),
+        lastmodifiedtime bigint
+       );
+
+alter table scrapDetails add constraint pk_scrapDetails primary key (id,tenantId);
+create sequence seq_scrapDetails;

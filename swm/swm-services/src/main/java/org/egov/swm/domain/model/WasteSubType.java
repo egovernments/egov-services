b5767b3f5665c641af499788ae4220750a50f1e3
venki <venki@egovernments.org>
2018-01-03 19:41:23 +0530

    Added waste subtype model.

---
+++ swm/swm-services/src/main/java/org/egov/swm/domain/model/WasteSubType.java
@@ -0,0 +1,41 @@
+package org.egov.swm.domain.model;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WasteSubType {
+
+    @NotNull
+    @Length(min = 1, max = 128)
+    @JsonProperty("tenantId")
+    private String tenantId = null;
+
+    @NotNull
+    @Length(min = 1, max = 128)
+    @JsonProperty("name")
+    private String name = null;
+
+    @NotNull
+    @Length(min = 1, max = 128)
+    @JsonProperty("code")
+    private String code = null;
+
+    @NotNull
+    @JsonProperty("wasteType")
+    private WasteType wasteType = null;
+
+}

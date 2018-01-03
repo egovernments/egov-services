5e520d983320b07715e584ee909e8b9a3849c411
Barath Vutukuri <barath.vutukuri@goodworklabs.com>
2018-01-03 16:18:19 +0530

    Added the toDomain and toEntity methods in SupplierAdvanceRequisitionEntity

--- inventory/inventory-services/src/main/java/org/egov/inv/persistence/entity/SupplierAdvanceRequisitionEntity.java
+++ inventory/inventory-services/src/main/java/org/egov/inv/persistence/entity/SupplierAdvanceRequisitionEntity.java
@@ -1,15 +1,16 @@
 package org.egov.inv.persistence.entity;
 
+import java.math.BigDecimal;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import org.egov.inv.model.AuditDetails;
 import org.egov.inv.model.PurchaseOrder;
 import org.egov.inv.model.Supplier;
 import org.egov.inv.model.SupplierAdvanceRequisition;
-
-import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -31,33 +32,49 @@ public class SupplierAdvanceRequisitionEntity {
 
     private String stateId;
 
-    private String status;
+    private String sarStatus;
 
     private String createdBy;
 
     private Long createdTime;
 
-    private String lastmodifiedBy;
+    private String lastModifiedBy;
 
-    private Long lastmodifiedTime;
+    private Long lastModifiedTime;
 
+	public SupplierAdvanceRequisition toDomain() {
+		SupplierAdvanceRequisition supplierAdvanceRequisition = new SupplierAdvanceRequisition();
+		supplierAdvanceRequisition.setId(this.id);
+		supplierAdvanceRequisition.setTenantId(this.tenantId);
+		supplierAdvanceRequisition.setSarStatus(this.sarStatus);
+		supplierAdvanceRequisition.setStateId(this.stateId);
+		supplierAdvanceRequisition.setSupplier(new Supplier().code(supplier));
+		supplierAdvanceRequisition.setPurchaseOrder(new PurchaseOrder().purchaseOrderNumber(purchaseOrder));
+		supplierAdvanceRequisition.setAdvanceAdjustedAmount(this.advanceAdjustedAmount);
+		supplierAdvanceRequisition.setAdvanceFullyAdjustedInBill(this.advanceFullyAdjustedInBill);
+		AuditDetails auditDetail = new AuditDetails()
+				.createdBy(createdBy)
+				.lastModifiedBy(lastModifiedBy)
+				.createdTime(createdTime)
+				.lastModifiedTime(lastModifiedTime);
+		supplierAdvanceRequisition.setAuditDetails(auditDetail);
+		return supplierAdvanceRequisition;
+	}
 
-    public SupplierAdvanceRequisition toDomain() {
-        AuditDetails auditDetails = new AuditDetails();
-        auditDetails.createdBy(createdBy)
-                .createdTime(createdTime)
-                .lastModifiedBy(lastmodifiedBy)
-                .lastModifiedTime(lastmodifiedTime);
-
-        SupplierAdvanceRequisition supplierAdvanceRequisition = new SupplierAdvanceRequisition();
-        return supplierAdvanceRequisition.id(id)
-                .tenantId(tenantId)
-                .supplier(new Supplier().code(supplier))
-                .purchaseOrder(new PurchaseOrder().id(purchaseOrder))
-                .advanceAdjustedAmount(advanceAdjustedAmount)
-                .advanceFullyAdjustedInBill(advanceFullyAdjustedInBill)
-                .stateId(stateId)
-                .sarStatus(status)
-                .auditDetails(auditDetails);
-    }
+	public SupplierAdvanceRequisitionEntity toEntity(SupplierAdvanceRequisition supplierAdvanceRequisition) {
+		this.id = supplierAdvanceRequisition.getId();
+		this.tenantId = supplierAdvanceRequisition.getTenantId();
+		this.sarStatus = supplierAdvanceRequisition.getSarStatus();
+		this.stateId = supplierAdvanceRequisition.getStateId();
+		this.supplier = supplierAdvanceRequisition.getSupplier() != null ? supplierAdvanceRequisition.getSupplier().getCode() : null;
+		this.purchaseOrder = supplierAdvanceRequisition.getPurchaseOrder() != null ? supplierAdvanceRequisition.getPurchaseOrder().toString() : null;
+		this.advanceAdjustedAmount = supplierAdvanceRequisition.getAdvanceAdjustedAmount();
+		this.advanceFullyAdjustedInBill = supplierAdvanceRequisition.getAdvanceFullyAdjustedInBill();
+		this.createdBy=supplierAdvanceRequisition.getAuditDetails().getCreatedBy();
+		this.lastModifiedBy=supplierAdvanceRequisition.getAuditDetails().getLastModifiedBy();
+		this.createdTime=supplierAdvanceRequisition.getAuditDetails().getCreatedTime();
+		this.lastModifiedTime=supplierAdvanceRequisition.getAuditDetails().getLastModifiedTime();
+		
+		return this;
+	}
 }

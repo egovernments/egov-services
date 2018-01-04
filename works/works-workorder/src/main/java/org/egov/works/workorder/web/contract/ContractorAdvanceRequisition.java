b0342086e043e5d0f66945cb681cdf047f3764d1
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2018-01-04 18:52:56 +0530

    Added contracts for contractor advance

--- works/works-workorder/src/main/java/org/egov/works/workorder/web/contract/ContractorAdvanceRequisition.java
+++ works/works-workorder/src/main/java/org/egov/works/workorder/web/contract/ContractorAdvanceRequisition.java
@@ -1,51 +1,208 @@
 package org.egov.works.workorder.web.contract;
 
-import io.swagger.annotations.ApiModel;
-
 import java.util.Objects;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
  * An Object that holds the basic data for Contractor Advance Requisition
  */
 @ApiModel(description = "An Object that holds the basic data for Contractor Advance Requisition")
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-11-30T11:45:24.744Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-01-04T12:59:21.353Z")
 
 public class ContractorAdvanceRequisition extends AdvanceRequisition {
+    @JsonProperty("id")
+    private String id = null;
+
+    @JsonProperty("tenantId")
+    private String tenantId = null;
+
+    @JsonProperty("letterOfAcceptanceEstimate")
+    private LetterOfAcceptanceEstimate letterOfAcceptanceEstimate = null;
+
+    @JsonProperty("workFlowDetails")
+    private WorkFlowDetails workFlowDetails = null;
+
+    @JsonProperty("stateId")
+    private String stateId = null;
+
+    @JsonProperty("auditDetails")
+    private AuditDetails auditDetails = null;
+
+    public ContractorAdvanceRequisition id(String id) {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * Unique Identifier of the Contractor Advance Requisition Form
+     * @return id
+     **/
+    @ApiModelProperty(value = "Unique Identifier of the Contractor Advance Requisition Form")
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public ContractorAdvanceRequisition tenantId(String tenantId) {
+        this.tenantId = tenantId;
+        return this;
+    }
+
+    /**
+     * tenant id of the Contractor Advance Requisition Form
+     * @return tenantId
+     **/
+    @ApiModelProperty(required = true, value = "tenant id of the Contractor Advance Requisition Form")
+    @NotNull
+
+    @Size(min = 2, max = 128)
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public ContractorAdvanceRequisition letterOfAcceptanceEstimate(LetterOfAcceptanceEstimate letterOfAcceptanceEstimate) {
+        this.letterOfAcceptanceEstimate = letterOfAcceptanceEstimate;
+        return this;
+    }
+
+    /**
+     * Letter of acceptance reference
+     * @return letterOfAcceptanceEstimate
+     **/
+    @ApiModelProperty(required = true, value = "Letter of acceptance reference")
+    @NotNull
+
+    @Valid
+
+    public LetterOfAcceptanceEstimate getLetterOfAcceptanceEstimate() {
+        return letterOfAcceptanceEstimate;
+    }
+
+    public void setLetterOfAcceptanceEstimate(LetterOfAcceptanceEstimate letterOfAcceptanceEstimate) {
+        this.letterOfAcceptanceEstimate = letterOfAcceptanceEstimate;
+    }
+
+    public ContractorAdvanceRequisition workFlowDetails(WorkFlowDetails workFlowDetails) {
+        this.workFlowDetails = workFlowDetails;
+        return this;
+    }
+
+    /**
+     * Get workFlowDetails
+     * @return workFlowDetails
+     **/
+    @ApiModelProperty(value = "")
+
+    @Valid
+
+    public WorkFlowDetails getWorkFlowDetails() {
+        return workFlowDetails;
+    }
+
+    public void setWorkFlowDetails(WorkFlowDetails workFlowDetails) {
+        this.workFlowDetails = workFlowDetails;
+    }
+
+    public ContractorAdvanceRequisition stateId(String stateId) {
+        this.stateId = stateId;
+        return this;
+    }
+
+    /**
+     * State id of the workflow
+     * @return stateId
+     **/
+    @ApiModelProperty(value = "State id of the workflow")
+
+    public String getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(String stateId) {
+        this.stateId = stateId;
+    }
+
+    public ContractorAdvanceRequisition auditDetails(AuditDetails auditDetails) {
+        this.auditDetails = auditDetails;
+        return this;
+    }
+
+    /**
+     * Get auditDetails
+     * @return auditDetails
+     **/
+    @ApiModelProperty(value = "")
+
+    @Valid
+
+    public AuditDetails getAuditDetails() {
+        return auditDetails;
+    }
+
+    public void setAuditDetails(AuditDetails auditDetails) {
+        this.auditDetails = auditDetails;
+    }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(java.lang.Object o) {
         if (this == o) {
             return true;
         }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        return true;
+        ContractorAdvanceRequisition contractorAdvanceRequisition = (ContractorAdvanceRequisition) o;
+        return Objects.equals(this.id, contractorAdvanceRequisition.id) &&
+                Objects.equals(this.tenantId, contractorAdvanceRequisition.tenantId) &&
+                Objects.equals(this.letterOfAcceptanceEstimate, contractorAdvanceRequisition.letterOfAcceptanceEstimate) &&
+                Objects.equals(this.workFlowDetails, contractorAdvanceRequisition.workFlowDetails) &&
+                Objects.equals(this.stateId, contractorAdvanceRequisition.stateId) &&
+                Objects.equals(this.auditDetails, contractorAdvanceRequisition.auditDetails);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode());
+        return Objects.hash(id, tenantId, letterOfAcceptanceEstimate, workFlowDetails, stateId, auditDetails);
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("class ContractorAdvanceRequisition {\n");
-        sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+
+        sb.append("    id: ").append(toIndentedString(id)).append("\n");
+        sb.append("    tenantId: ").append(toIndentedString(tenantId)).append("\n");
+        sb.append("    letterOfAcceptanceEstimate: ").append(toIndentedString(letterOfAcceptanceEstimate)).append("\n");
+        sb.append("    workFlowDetails: ").append(toIndentedString(workFlowDetails)).append("\n");
+        sb.append("    stateId: ").append(toIndentedString(stateId)).append("\n");
+        sb.append("    auditDetails: ").append(toIndentedString(auditDetails)).append("\n");
         sb.append("}");
         return sb.toString();
     }
 
     /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
+     * Convert the given object to string with each line indented by 4 spaces (except the first line).
      */
-    private String toIndentedString(Object o) {
+    private String toIndentedString(java.lang.Object o) {
         if (o == null) {
             return "null";
         }
         return o.toString().replace("\n", "\n    ");
     }
 }
-

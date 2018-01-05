43081298f74e134498353d821717156964f0182c
shwetha <shwetha.dc@egovernments.org>
2018-01-05 15:51:36 +0530

    [EGSVC-2614] Added Written Depreciation Method in Asset Category Master

--- asset/asset-services/src/main/java/org/egov/asset/model/enums/DepreciationMethod.java
+++ asset/asset-services/src/main/java/org/egov/asset/model/enums/DepreciationMethod.java
@@ -5,28 +5,26 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum DepreciationMethod {
-	
-    STRAIGHT_LINE_METHOD("STRAIGHT_LINE_METHOD");
+
+    STRAIGHT_LINE_METHOD("STRAIGHT_LINE_METHOD"), WRITTEN_DOWN_METHOD("WRITTEN_DOWN_METHOD");
 
     private String value;
 
-    DepreciationMethod(String value) {
-      this.value = value;
+    DepreciationMethod(final String value) {
+        this.value = value;
     }
 
     @Override
     @JsonValue
     public String toString() {
-      return String.valueOf(value);
+        return String.valueOf(value);
     }
 
     @JsonCreator
-    public static DepreciationMethod fromValue(String text) {
-      for (DepreciationMethod b : DepreciationMethod.values()) {
-        if (String.valueOf(b.value).equals(text)) {
-          return b;
-        }
-      }
-      return null;
+    public static DepreciationMethod fromValue(final String text) {
+        for (final DepreciationMethod b : DepreciationMethod.values())
+            if (String.valueOf(b.value).equals(text))
+                return b;
+        return null;
     }
 }

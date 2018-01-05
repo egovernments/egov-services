ee1f765d7354b9aa671486b226b2609bef2ec450
venki <venki@egovernments.org>
2018-01-05 16:56:25 +0530

    Removed notnull condition for ending dumping ground.

--- swm/swm-services/src/main/resources/db/migration/main/V20180105160335__swm_route_alter_ddl.sql
+++ swm/swm-services/src/main/resources/db/migration/main/V20180105160335__swm_route_alter_ddl.sql
@@ -10,14 +10,14 @@ ALTER TABLE egswm_route DROP COLUMN garbageEstimate RESTRICT;
 
 
 
-ALTER TABLE egswm_routecollectionpointmap ADD COLUMN id varchar(256) NOT NULL;
+ALTER TABLE egswm_routecollectionpointmap ADD COLUMN id varchar(256);
 
 ALTER TABLE egswm_routecollectionpointmap ADD COLUMN isStartingCollectionPoint boolean;
 
 ALTER TABLE egswm_routecollectionpointmap ADD COLUMN isEndingCollectionPoint boolean;
 
-ALTER TABLE egswm_routecollectionpointmap ADD COLUMN endingDumpingGroundPoint varchar(256) NOT NULL;
+ALTER TABLE egswm_routecollectionpointmap ADD COLUMN endingDumpingGroundPoint varchar(256);
 
-ALTER TABLE egswm_routecollectionpointmap ADD COLUMN distance numeric NOT NULL;
+ALTER TABLE egswm_routecollectionpointmap ADD COLUMN distance numeric;
 
-ALTER TABLE egswm_routecollectionpointmap ADD COLUMN garbageEstimate numeric NOT NULL;
+ALTER TABLE egswm_routecollectionpointmap ADD COLUMN garbageEstimate numeric;

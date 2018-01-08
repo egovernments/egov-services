0031dbf53388430da3018a80ff526170b780e1e4
shashi <shashikanth.g@egovernments.org>
2018-01-08 17:55:35 +0530

    Fixed recursive generation of number in inventory service,

--- inventory/inventory-services/src/main/java/org/egov/inv/domain/util/InventoryUtilities.java
+++ inventory/inventory-services/src/main/java/org/egov/inv/domain/util/InventoryUtilities.java
@@ -6,7 +6,7 @@ import java.math.RoundingMode;
 public class InventoryUtilities {
 	
 	public static BigDecimal getQuantityInSelectedUom(BigDecimal quantity, BigDecimal conversionFactor) {
-		return quantity.divide(conversionFactor).setScale(10, RoundingMode.HALF_UP);
+		return quantity.divide(conversionFactor,10, RoundingMode.HALF_UP);
 	}
 	
 	public static BigDecimal getQuantityInBaseUom(BigDecimal quantity, BigDecimal conversionFactor) {

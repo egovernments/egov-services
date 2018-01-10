f34b3ded33a0af76b77c463f732ebbc435844a39
malathi <malathi@egovernments.org>
2018-01-10 16:59:06 +0530

    EGSVC-2574 Create Contractor bill - changed date columns to bigint

--- works/works-measurementbook/src/main/resources/db/migration/main/V20180110164855__works_contractor_bill_workcompletiondate_ddl.sql
+++ works/works-measurementbook/src/main/resources/db/migration/main/V20180110164855__works_contractor_bill_workcompletiondate_ddl.sql
@@ -1,1 +1,3 @@
-Alter table egw_contractorbill add column workcompletiondate timestamp without time zone;
+Alter table egw_contractorbill drop column APPROVEDDATE;
+Alter table egw_contractorbill add column workcompletiondate bigint;
+Alter table egw_contractorbill add column APPROVEDDATE bigint;

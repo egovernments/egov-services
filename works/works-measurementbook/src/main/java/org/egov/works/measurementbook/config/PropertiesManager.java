291ca000d2f7c9d8776e9fdcd5676b2b8ac91c73
malathi <malathi@egovernments.org>
2018-01-11 15:38:57 +0530

    EGSVC-2574 Create Contractor bill - service deployment issue fix

--- works/works-measurementbook/src/main/java/org/egov/works/measurementbook/config/PropertiesManager.java
+++ works/works-measurementbook/src/main/java/org/egov/works/measurementbook/config/PropertiesManager.java
@@ -29,10 +29,10 @@ public class PropertiesManager {
     @Value("${egov.services.works.contractorbill.create.update.topic}")
     private String contractorBillCreateUpdateTopic;
     
-    @Value("${works.billnumber.format}")
+    /*@Value("${works.billnumber.format}")
     private String billNumberFormat;
     
     @Value("${works.billnumber}")
-    private String billNumber;
+    private String billNumber;*/
     
 }

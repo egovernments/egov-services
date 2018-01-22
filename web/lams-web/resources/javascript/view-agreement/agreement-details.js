527fd616a27197a580122abc41c650469fd61df5
Rafeek <mohammad.rafeek@egovernments.org>
2018-01-22 11:26:51 +0530

    [EGSVC-2799]-Corrected label spelling

--- web/lams-web/resources/javascript/view-agreement/agreement-details.js
+++ web/lams-web/resources/javascript/view-agreement/agreement-details.js
@@ -453,7 +453,7 @@ class AgreementDetails extends React.Component {
                             <div className="col-sm-6">
                                 <div className="row">
                                     <div className="col-sm-6 label-text">
-                                        <label htmlFor="securityDeposit">Advace Collection:</label>
+                                        <label htmlFor="securityDeposit">Advance Collection:</label>
                                     </div>
                                     <div className="col-sm-6 label-view-text">
                                         <label id="securityDeposit" name="securityDeposit">

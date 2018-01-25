320cb139c59e429326785d68d96993a5ad413fdf
riteshchikatwar <ritesh.chikatwar@egovernments.org>
2018-01-25 14:20:37 +0530

    Fix for search LOA and WO

--- works/works-workorder/src/main/java/org/egov/works/workorder/web/controller/LetterofacceptancesApiController.java
+++ works/works-workorder/src/main/java/org/egov/works/workorder/web/controller/LetterofacceptancesApiController.java
@@ -41,7 +41,7 @@ public class LetterofacceptancesApiController implements LetterofacceptancesApi 
 
     public ResponseEntity<LetterOfAcceptanceResponse> letterofacceptancesSearchPost(
             @NotNull @ApiParam(value = "Unique id for a tenant.", required = true) @RequestParam(value = "tenantId", required = true) String tenantId,
-            @ApiParam(value = "Parameter to carry Request metadata in the request body") @Valid @RequestBody RequestInfo requestInfo,
+            @ApiParam(value = "Parameter to carry Request metadata in the request body") @RequestBody RequestInfo requestInfo,
             @Min(0) @Max(100) @ApiParam(value = "Number of records returned.", defaultValue = "20") @RequestParam(value = "pageSize", required = false, defaultValue = "20") Integer pageSize,
             @ApiParam(value = "Page number", defaultValue = "1") @RequestParam(value = "pageNumber", required = false, defaultValue = "1") Integer pageNumber,
             @ApiParam(value = "This takes any field from the Object seperated by comma and asc,desc keywords. example name asc,code desc or name,code or name,code desc", defaultValue = "id") @RequestParam(value = "sortBy", required = false, defaultValue = "id") String sortBy,

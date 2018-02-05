52ecc40dcccda8812f6ced9ef4420a3dfd5e0628
parvati <parvati.hiremath@egovernments.org>
2018-02-05 12:50:16 +0530

    Search bill fix

--- works/works-measurementbook/src/main/java/org/egov/works/measurementbook/web/controller/ContractorbillsApiController.java
+++ works/works-measurementbook/src/main/java/org/egov/works/measurementbook/web/controller/ContractorbillsApiController.java
@@ -37,7 +37,7 @@ public class ContractorbillsApiController implements ContractorbillsApi {
 
     public ResponseEntity<ContractorBillResponse> contractorbillsSearchPost(
             @NotNull @ApiParam(value = "Unique id for a tenant.", required = true) @RequestParam(value = "tenantId", required = true) String tenantId,
-            @ApiParam(value = "Parameter to carry Request metadata in the request body") @Valid @RequestBody RequestInfo requestInfo,
+            @ApiParam(value = "Parameter to carry Request metadata in the request body") @RequestBody RequestInfo requestInfo,
             @Min(0) @Max(100) @ApiParam(value = "Number of records returned.", defaultValue = "20") @RequestParam(value = "pageSize", required = false, defaultValue = "20") Integer pageSize,
             @ApiParam(value = "Page number", defaultValue = "1") @RequestParam(value = "pageNumber", required = false, defaultValue = "1") Integer pageNumber,
             @ApiParam(value = "This takes any field from the Object seperated by comma and asc,desc keywords. example name asc,code desc or name,code or name,code desc", defaultValue = "id") @RequestParam(value = "sortBy", required = false, defaultValue = "id") String sortBy,

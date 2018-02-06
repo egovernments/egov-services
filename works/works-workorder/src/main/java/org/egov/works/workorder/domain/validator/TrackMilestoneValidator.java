92f1a0ce458d424287e04001ca712cba97518082
ramki <ramakrishna@egovernments.org>
2018-02-06 19:13:09 +0530

    fix

--- works/works-workorder/src/main/java/org/egov/works/workorder/domain/validator/TrackMilestoneValidator.java
+++ works/works-workorder/src/main/java/org/egov/works/workorder/domain/validator/TrackMilestoneValidator.java
@@ -103,7 +103,7 @@ public class TrackMilestoneValidator {
         LetterOfAcceptance letterOfAcceptance = letterOfAcceptanceRepository.searchLOAs(LetterOfAcceptanceSearchContract.builder().ids(Arrays.asList(letterOfAcceptanceEstimate.getLetterOfAcceptance())).tenantId(trackMilestone.getTenantId()).build(), requestInfo).get(0);
         ContractorBillResponse contractorBillResponse = contractorBillRepository.getByLoaNumbers(ContractorBillSearchContract.builder().letterOfAcceptanceNumbers(Arrays.asList(letterOfAcceptance.getLoaNumber())).billTypes(Arrays.asList("Final")).tenantId(trackMilestone.getTenantId()).build(), trackMilestone.getTenantId(), requestInfo);
         ContractorBill contractorBill = contractorBillResponse.getContractorBills().get(0);
-        if(contractorBill!=null && contractorBill.getStatus()!=null && !contractorBill.getStatus().getCode().equals(CommonConstants.STATUS_CANCELLED)){
+        if(contractorBill!=null && contractorBill.getStatus().getCode()!=null && !contractorBill.getStatus().getCode().equals(CommonConstants.STATUS_CANCELLED)){
             validationMessages.put(Constants.KEY_TRACKMILESTONE_FINAL_BILL_CREATED, Constants.MESSAGE_TRACKMILESTONE_FINAL_BILL_CREATED);
         }
     }

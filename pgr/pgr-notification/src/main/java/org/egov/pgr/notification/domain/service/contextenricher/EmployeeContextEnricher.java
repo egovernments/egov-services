3b288065af58c71f39475fa543d626a433e19c4d
sumanth <sumanth@egovernments.org>
2018-02-06 17:04:29 +0530

    PGR notification fix.

--- pgr/pgr-notification/src/main/java/org/egov/pgr/notification/domain/service/contextenricher/EmployeeContextEnricher.java
+++ pgr/pgr-notification/src/main/java/org/egov/pgr/notification/domain/service/contextenricher/EmployeeContextEnricher.java
@@ -8,6 +8,7 @@ import org.egov.pgr.common.model.Position;
 import org.egov.pgr.common.repository.DesignationRepository;
 import org.egov.pgr.common.repository.EmployeeRepository;
 import org.egov.pgr.common.repository.PositionRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,22 +17,27 @@ public class EmployeeContextEnricher implements NotificationContextEnricher {
     private EmployeeRepository employeeRepository;
     private PositionRepository positionRepository;
     private DesignationRepository designationRepository;
+    private Boolean escalationSmsEnabled;
 
     public EmployeeContextEnricher(EmployeeRepository employeeRepository,
                                    PositionRepository positionRepository,
-                                   DesignationRepository designationRepository) {
+                                   DesignationRepository designationRepository,
+                                   @Value("${sms.escalation.enabled}") final Boolean escalationSmsEnabled) {
         this.employeeRepository = employeeRepository;
         this.positionRepository = positionRepository;
         this.designationRepository = designationRepository;
+        this.escalationSmsEnabled = escalationSmsEnabled;
     }
 
     @Override
     public void enrich(SevaRequest sevaRequest, NotificationContext context) {
-        final Employee employee = this.employeeRepository
-            .getEmployeeByPosition(sevaRequest.getPositionId(), sevaRequest.getTenantId());
-        context.setEmployee(employee);
-        setDesignation(sevaRequest, context);
-        setPosition(sevaRequest, context);
+        if(escalationSmsEnabled){
+            final Employee employee = this.employeeRepository
+                .getEmployeeByPosition(sevaRequest.getPositionId(), sevaRequest.getTenantId());
+            context.setEmployee(employee);
+            setDesignation(sevaRequest, context);
+            setPosition(sevaRequest, context);
+        }
     }
 
     private void setPosition(SevaRequest sevaRequest, NotificationContext context) {

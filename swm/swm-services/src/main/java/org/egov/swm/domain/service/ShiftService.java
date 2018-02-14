d3b1a5d734adbbf241c92991f3f4640e80663a30
venki <venki@egovernments.org>
2018-02-14 15:32:29 +0530

    Showing error message in 12hr format in shift overlap validation.

--- swm/swm-services/src/main/java/org/egov/swm/domain/service/ShiftService.java
+++ swm/swm-services/src/main/java/org/egov/swm/domain/service/ShiftService.java
@@ -95,7 +95,7 @@ public class ShiftService {
         DesignationResponse designationResponse;
         ShiftSearch search;
         Pagination<Shift> shifts;
-        final DateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        final DateFormat dateFormat = new SimpleDateFormat("HH:mm a");
         dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
         for (final Shift shift : shiftRequest.getShifts()) {
 

ab7b066868fb226253d4a05a87d6f1617f06149d
Rafeek <mohammad.rafeek@egovernments.org>
2018-02-26 18:10:26 +0530

    [EGSVC-3141]-showing proper validation message

--- web/lams-web/resources/javascript/dataentry/data-entry.js
+++ web/lams-web/resources/javascript/dataentry/data-entry.js
@@ -962,6 +962,7 @@ $("#createAgreementForm").validate({
                       hideLoading();
                     } else {
                       showError(response["statusText"]);
+                      showError(err);
                       hideLoading();
                     }
                 } else {

6f72088d66ba521a72c94a505735b290f12bc8e1
shwetha <shwetha.dc@egovernments.org>
2018-03-06 14:04:49 +0530

    [EGSVC-3144] removed not null constraints in disposal

---
+++ asset/asset-services/src/main/resources/db/migration/main/V20180306135832__egasset_disposal_drop_not_null.sql
@@ -0,0 +1,7 @@
+alter table egasset_disposal alter column buyername drop NOT NULL;
+
+alter table egasset_disposal alter column buyeraddress drop NOT NULL;
+
+alter table egasset_disposal alter column pancardnumber drop NOT NULL;
+
+alter table egasset_disposal alter column aadharcardnumber drop NOT NULL;

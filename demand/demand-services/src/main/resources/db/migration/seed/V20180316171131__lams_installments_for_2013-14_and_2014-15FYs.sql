e4f129e56e6920e1cb6e65efd6d298762282eee9
Rafeek <mohammad.rafeek@egovernments.org>
2018-03-16 17:19:12 +0530

    [EGSVC-3179]-added installments for FYs 2013-14 and 2014-15

---
+++ demand/demand-services/src/main/resources/db/migration/seed/V20180316171131__lams_installments_for_2013-14_and_2014-15FYs.sql
@@ -0,0 +1,99 @@
+--installments for Jan2013-Mar2013
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),12013,'2013-01-01 00:00:00', '2013-01-01 00:00:00', '2013-01-31 23:59:59','Leases And Agreements',current_timestamp,'LA-JAN/12-13','MONTH', '2012-13', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),22013,'2013-02-01 00:00:00', '2013-02-01 00:00:00', '2013-02-28 23:59:59','Leases And Agreements',current_timestamp,'LA-FEB/12-13','MONTH', '2012-13', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),32013,'2013-03-01 00:00:00', '2013-03-01 00:00:00', '2013-03-31 23:59:59','Leases And Agreements',current_timestamp,'LA-MAR/12-13','MONTH', '2012-13', 'default');
+--Quarterly
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0420122013,'2013-01-01 00:00:00', '2013-01-01 00:00:00', '2013-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-IV/12-13','QUARTER', '2012-13', 'default');
+---Halfyearly
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),02201213,'2012-10-01 00:00:00', '2012-10-01 00:00:00', '2013-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_HYI-II/12-13','HALFYEAR', '2012-13', 'default');
+--Annual
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),20122013,'2012-04-01 00:00:00', '2012-04-01 00:00:00', '2013-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_AI-I/12-13','ANNUAL', '2012-13', 'default');
+--------------------------FY 13-14
+--Monthly
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),42013,'2013-04-01 00:00:00', '2013-04-01 00:00:00', '2013-04-30 23:59:59','Leases And Agreements',current_timestamp,'LA-APR/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),52013,'2013-05-01 00:00:00', '2013-05-01 00:00:00', '2013-05-31 23:59:59','Leases And Agreements',current_timestamp,'LA-MAY/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),62013,'2013-06-01 00:00:00', '2013-06-01 00:00:00', '2013-06-30 23:59:59','Leases And Agreements',current_timestamp,'LA-JUN/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),72013,'2013-07-01 00:00:00', '2013-07-01 00:00:00', '2013-07-31 23:59:59','Leases And Agreements',current_timestamp,'LA-JUL/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),82013,'2013-08-01 00:00:00', '2013-08-01 00:00:00', '2013-08-31 23:59:59','Leases And Agreements',current_timestamp,'LA-AUG/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),92013,'2013-09-01 00:00:00', '2013-09-01 00:00:00', '2013-09-30 23:59:59','Leases And Agreements',current_timestamp,'LA-SEP/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),102013,'2013-10-01 00:00:00', '2013-10-01 00:00:00', '2013-10-31 23:59:59','Leases And Agreements',current_timestamp,'LA-OCT/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),112013,'2013-11-01 00:00:00', '2013-11-01 00:00:00', '2013-11-30 23:59:59','Leases And Agreements',current_timestamp,'LA-NOV/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),122013,'2013-12-01 00:00:00', '2013-12-01 00:00:00', '2013-12-31 23:59:59','Leases And Agreements',current_timestamp,'LA-DEC/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),12014,'2014-01-01 00:00:00', '2014-01-01 00:00:00', '2014-01-31 23:59:59','Leases And Agreements',current_timestamp,'LA-JAN/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),22014,'2014-02-01 00:00:00', '2014-02-01 00:00:00', '2014-02-28 23:59:59','Leases And Agreements',current_timestamp,'LA-FEB/13-14','MONTH', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),32014,'2014-03-01 00:00:00', '2014-03-01 00:00:00', '2014-03-31 23:59:59','Leases And Agreements',current_timestamp,'LA-MAR/13-14','MONTH', '2013-14', 'default');
+
+--Qarterly 2013-14
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0120132014,'2013-04-01 00:00:00', '2013-04-01 00:00:00', '2013-06-30 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-I/13-14','QUARTER', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0220132014,'2013-07-01 00:00:00', '2013-07-01 00:00:00', '2013-09-30 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-II/13-14','QUARTER', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0320132014,'2013-10-01 00:00:00', '2013-10-01 00:00:00', '2013-12-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-III/13-14','QUARTER', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0420132014,'2014-01-01 00:00:00', '2014-01-01 00:00:00', '2014-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-IV/13-14','QUARTER', '2013-14', 'default');
+
+--Halfyear 2013-14
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),01201314,'2013-04-01 00:00:00', '2013-04-01 00:00:00', '2013-09-30 23:59:59','Leases And Agreements',current_timestamp,'LAMS_HYI-I/13-14','HALFYEAR', '2013-14', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),02201314,'2013-10-01 00:00:00', '2013-10-01 00:00:00', '2014-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_HYI-II/13-14','HALFYEAR', '2013-14', 'default');
+
+--Annual 2013-14
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),20132014,'2013-04-01 00:00:00', '2013-04-01 00:00:00', '2014-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_AI-I/13-14','ANNUAL', '2013-14', 'default');
+
+---------------------------FY 14-15
+---Monthly
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),42014,'2014-04-01 00:00:00', '2014-04-01 00:00:00', '2014-04-30 23:59:59','Leases And Agreements',current_timestamp,'LA-APR/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),52014,'2014-05-01 00:00:00', '2014-05-01 00:00:00', '2014-05-31 23:59:59','Leases And Agreements',current_timestamp,'LA-MAY/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),62014,'2014-06-01 00:00:00', '2014-06-01 00:00:00', '2014-06-30 23:59:59','Leases And Agreements',current_timestamp,'LA-JUN/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),72014,'2014-07-01 00:00:00', '2014-07-01 00:00:00', '2014-07-31 23:59:59','Leases And Agreements',current_timestamp,'LA-JUL/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),82014,'2014-08-01 00:00:00', '2014-08-01 00:00:00', '2014-08-31 23:59:59','Leases And Agreements',current_timestamp,'LA-AUG/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),92014,'2014-09-01 00:00:00', '2014-09-01 00:00:00', '2014-09-30 23:59:59','Leases And Agreements',current_timestamp,'LA-SEP/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),102014,'2014-10-01 00:00:00', '2014-10-01 00:00:00', '2014-10-31 23:59:59','Leases And Agreements',current_timestamp,'LA-OCT/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),112014,'2014-11-01 00:00:00', '2014-11-01 00:00:00', '2014-11-30 23:59:59','Leases And Agreements',current_timestamp,'LA-NOV/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),122014,'2014-12-01 00:00:00', '2014-12-01 00:00:00', '2014-12-31 23:59:59','Leases And Agreements',current_timestamp,'LA-DEC/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),12015,'2015-01-01 00:00:00', '2015-01-01 00:00:00', '2015-01-31 23:59:59','Leases And Agreements',current_timestamp,'LA-JAN/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),22015,'2015-02-01 00:00:00', '2015-02-01 00:00:00', '2015-02-28 23:59:59','Leases And Agreements',current_timestamp,'LA-FEB/14-15','MONTH', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),32015,'2015-03-01 00:00:00', '2015-03-01 00:00:00', '2015-03-31 23:59:59','Leases And Agreements',current_timestamp,'LA-MAR/14-15','MONTH', '2014-15', 'default');
+
+--Qarterly 2014-15
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0120142015,'2014-04-01 00:00:00', '2014-04-01 00:00:00', '2014-06-30 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-I/14-15','QUARTER', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0220142015,'2014-07-01 00:00:00', '2014-07-01 00:00:00', '2014-09-30 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-II/14-15','QUARTER', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0320142015,'2014-10-01 00:00:00', '2014-10-01 00:00:00', '2014-12-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-III/14-15','QUARTER', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),0420142015,'2015-01-01 00:00:00', '2015-01-01 00:00:00', '2015-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_QI-IV/14-15','QUARTER', '2014-15', 'default');
+
+--Halfyear 2014-15
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),01201415,'2014-04-01 00:00:00', '2014-04-01 00:00:00', '2014-09-30 23:59:59','Leases And Agreements',current_timestamp,'LAMS_HYI-I/14-15','HALFYEAR', '2014-15', 'default');
+
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),02201415,'2014-10-01 00:00:00', '2014-10-01 00:00:00', '2015-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_HYI-II/14-15','HALFYEAR', '2014-15', 'default');
+
+--Annual 2014-15
+Insert into eg_installment_master (id, installment_num, installment_year, start_date, end_date, module, lastupdatedtimestamp, description, installment_type, financial_year, tenantid) values (nextval('SEQ_EG_INSTALLMENT_MASTER'),20142015,'2014-04-01 00:00:00', '2014-04-01 00:00:00', '2015-03-31 23:59:59','Leases And Agreements',current_timestamp,'LAMS_AI-I/14-15','ANNUAL', '2014-15', 'default');
+
+

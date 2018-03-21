bf80da6144374ba250658cc0972e8c838e57a403
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2018-03-21 12:15:19 +0530

    removed searh methods added

--- rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/controller/ServiceController.java
+++ rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/controller/ServiceController.java
@@ -4,13 +4,17 @@ import java.util.Date;
 
 import javax.validation.Valid;
 
+import org.egov.pgr.contract.RequestInfoWrapper;
+import org.egov.pgr.contract.ServiceReqSearchCriteria;
 import org.egov.pgr.service.GrievanceService;
+import org.egov.pgr.utils.PGRRequestValidator;
 import org.egov.pgr.v2.contract.ServiceRequest;
 import org.egov.pgr.v2.contract.ServiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +29,10 @@ public class ServiceController {
 
 	@Autowired
 	private GrievanceService service;
+	
+
+	@Autowired
+	private PGRRequestValidator pgrRequestValidator;
 
 	/**
 	 * enpoint to create service requests
@@ -60,5 +68,49 @@ public class ServiceController {
 		log.debug(" the time taken for update in ms: {}", endTime - startTime);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
+	
+
+	/**
+	 * Controller endpoint to fetch service requests
+	 * 
+	 * @param requestInfoWrapper
+	 * @param serviceReqSearchCriteria
+	 * @return ResponseEntity<?>
+	 * @author vishal
+	 */
+	@PostMapping("_search")
+	@ResponseBody
+	private ResponseEntity<?> search(@RequestBody @Valid RequestInfoWrapper requestInfoWrapper,
+			@ModelAttribute @Valid ServiceReqSearchCriteria serviceReqSearchCriteria) {
+
+		pgrRequestValidator.validateSearch(serviceReqSearchCriteria, requestInfoWrapper.getRequestInfo());
+		long startTime = new Date().getTime();
+		Object serviceReqResponse = service.getServiceRequests(requestInfoWrapper.getRequestInfo(),
+				serviceReqSearchCriteria);
+		long endTime = new Date().getTime();
+		log.debug(" the time taken for search in ms: {}", endTime - startTime);
+		return new ResponseEntity<>(serviceReqResponse, HttpStatus.OK);
+	}
+
+/*	*//**
+	 * Controller to fetch count of service requests based on a given criteria
+	 * 
+	 * @param requestInfoWrapper
+	 * @param serviceReqSearchCriteria
+	 * @return ResponseEntity<?>
+	 * @author vishal
+	 *//*
+	@PostMapping("_count")
+	@ResponseBody
+	private ResponseEntity<?> count(@RequestBody @Valid RequestInfoWrapper requestInfoWrapper,
+			@ModelAttribute @Valid ServiceReqSearchCriteria serviceReqSearchCriteria) {
+		pgrRequestValidator.validateSearch(serviceReqSearchCriteria, requestInfoWrapper.getRequestInfo());
+		long startTime = new Date().getTime();
+		Object countResponse = service.getCount(requestInfoWrapper.getRequestInfo(), serviceReqSearchCriteria);
+		long endTime = new Date().getTime();
+		log.debug(" the time taken for count in ms: {}", endTime - startTime);
+		return new ResponseEntity<>(countResponse, HttpStatus.OK);
+	}*/
+
 
 }
--- rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/service/GrievanceService.java
+++ rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/service/GrievanceService.java
@@ -1,6 +1,7 @@
 package org.egov.pgr.service;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -8,10 +9,16 @@ import java.util.stream.Collectors;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.common.contract.request.Role;
 import org.egov.common.contract.request.User;
+import org.egov.mdms.model.MdmsCriteriaReq;
 import org.egov.pgr.contract.AuditDetails;
 import org.egov.pgr.contract.IdResponse;
+import org.egov.pgr.contract.SearcherRequest;
+import org.egov.pgr.contract.ServiceReq;
+import org.egov.pgr.contract.ServiceReqResponse;
+import org.egov.pgr.contract.ServiceReqSearchCriteria;
 import org.egov.pgr.producer.PGRProducer;
 import org.egov.pgr.repository.IdGenRepo;
+import org.egov.pgr.repository.ServiceRequestRepository;
 import org.egov.pgr.utils.PGRConstants;
 import org.egov.pgr.utils.PGRUtils;
 import org.egov.pgr.utils.ResponseInfoFactory;
@@ -23,6 +30,12 @@ import org.egov.pgr.v2.contract.ServiceRequest;
 import org.egov.pgr.v2.contract.ServiceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.jayway.jsonpath.JsonPath;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,6 +63,9 @@ public class GrievanceService {
 		
 		@Autowired
 		private PGRProducer pGRProducer;
+		
+		@Autowired
+		private ServiceRequestRepository serviceRequestRepository;
 		
 		private static final String MODULE_NAME = "PGR:";
 
@@ -180,5 +196,72 @@ public class GrievanceService {
 			return ServiceResponse.builder()
 					.responseInfo(factory.createResponseInfoFromRequestInfo(serviceReqRequest.getRequestInfo(), true))
 					.services(serviceReqRequest.getServices()).build();
+		}
+		
+		/**
+		 * Method to return service requests received from the repo to the controller in
+		 * the reqd format
+		 * 
+		 * @param requestInfo
+		 * @param serviceReqSearchCriteria
+		 * @return ServiceReqResponse
+		 * @author vishal
+		 */
+		public Object getServiceRequests(RequestInfo requestInfo,
+				ServiceReqSearchCriteria serviceReqSearchCriteria) {
+			
+			ObjectMapper mapper = new ObjectMapper();
+			mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+			//ServiceReqResponse serviceReqResponse = null;
+			StringBuilder uri = new StringBuilder();
+			SearcherRequest searcherRequest = null;
+			if(null != serviceReqSearchCriteria.getServiceRequestId() && serviceReqSearchCriteria.getServiceRequestId().size() == 1) {
+				searcherRequest = pGRUtils.prepareSearchRequestSpecific(uri, serviceReqSearchCriteria, requestInfo);
+			}else if(null != serviceReqSearchCriteria.getAssignedTo() && !serviceReqSearchCriteria.getAssignedTo().isEmpty()) {
+				searcherRequest = pGRUtils.prepareSearchRequestAssignedTo(uri, serviceReqSearchCriteria, requestInfo);
+			}else {
+				if(null != serviceReqSearchCriteria.getGroup() && !serviceReqSearchCriteria.getGroup().isEmpty()){
+						Object response = fetchServiceCodes(requestInfo, serviceReqSearchCriteria.getTenantId(), serviceReqSearchCriteria.getGroup());
+						List<String> serviceCodes = null;
+						if(null == response)
+							return new ServiceReqResponse(factory.createResponseInfoFromRequestInfo(requestInfo, false),
+									new ArrayList<ServiceReq>());
+						try {
+							serviceCodes = (List<String>) JsonPath.read(response, PGRConstants.JSONPATH_SERVICE_CODES);
+						}catch(Exception e) {
+							return new ServiceReqResponse(factory.createResponseInfoFromRequestInfo(requestInfo, false),
+									new ArrayList<ServiceReq>());
+						}
+						serviceReqSearchCriteria.setServiceCodes(serviceCodes);
+				}
+				searcherRequest = pGRUtils.prepareSearchRequestGeneral(uri, serviceReqSearchCriteria, requestInfo);
+			}
+			Object response = serviceRequestRepository.fetchResult(uri, searcherRequest);
+			log.info("Searcher response: ", response);
+			if (null == response) {
+				return new ServiceReqResponse(factory.createResponseInfoFromRequestInfo(requestInfo, false),
+						new ArrayList<ServiceReq>());
+			}
+			//serviceReqResponse = mapper.convertValue(response, ServiceReqResponse.class);
+			return response;
+		}
+		
+		/**
+		 * method to fetch service codes from mdms based on dept
+		 * 
+		 * @param requestInfo
+		 * @param tenantId
+		 * @param department
+		 * @return Object
+		 * @author vishal
+		 */
+		public Object fetchServiceCodes(RequestInfo requestInfo,
+				String tenantId, String department) {
+			StringBuilder uri = new StringBuilder();
+			MdmsCriteriaReq mdmsCriteriaReq = pGRUtils.prepareSearchRequestForServiceCodes(uri, tenantId, department, requestInfo);
+			return serviceRequestRepository.fetchResult(uri, mdmsCriteriaReq);
+			
 		}
 }

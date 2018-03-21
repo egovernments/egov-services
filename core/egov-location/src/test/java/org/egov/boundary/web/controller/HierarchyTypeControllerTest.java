fc8217be2db6d4d66cb133660686ed333ade1423
Nithin DV <nithin_1992@msn.com>
2018-03-21 12:29:39 +0530

    Tracer addition, test cases fix

--- core/egov-location/src/test/java/org/egov/boundary/web/controller/HierarchyTypeControllerTest.java
+++ core/egov-location/src/test/java/org/egov/boundary/web/controller/HierarchyTypeControllerTest.java
@@ -1,15 +1,7 @@
 package org.egov.boundary.web.controller;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.io.IOException;
-
 import org.apache.commons.io.IOUtils;
+import org.egov.boundary.TestConfiguration;
 import org.egov.boundary.domain.service.HierarchyTypeService;
 import org.egov.boundary.web.contract.HierarchyType;
 import org.egov.boundary.web.contract.factory.ResponseInfoFactory;
@@ -20,12 +12,23 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.io.IOException;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(HierarchyTypeController.class)
+@Import(TestConfiguration.class)
 public class HierarchyTypeControllerTest {
 
 	@Autowired

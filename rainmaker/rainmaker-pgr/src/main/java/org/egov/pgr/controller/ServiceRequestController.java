fe1dd64ed55cfbd8ed59027cee8718ab0573bcb7
vishalmahuli <vishalmahuli8@gmail.com>
2018-03-22 13:33:52 +0530

    updated path

--- rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/controller/ServiceRequestController.java
+++ rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/controller/ServiceRequestController.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
-@RequestMapping(value = "/requests/")
+@RequestMapping(value = "/requests/v1/")
 public class ServiceRequestController {
 		
 	@Autowired

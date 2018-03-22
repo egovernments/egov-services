e780fec54349226ea187a49119fece1e71fd49b1
kiranmai <kiranmayi.pulluru@tarento.com>
2018-03-22 11:21:40 +0530

    Added required jar's

--- user/user-otp/src/main/java/org/egov/persistence/repository/UserRepository.java
+++ user/user-otp/src/main/java/org/egov/persistence/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package org.egov.persistence.repository;
 
+import java.util.Map;
+
 import org.egov.domain.exception.UserNotFoundException;
 import org.egov.domain.model.User;
 import org.egov.persistence.contract.UserSearchRequest;
@@ -7,6 +9,11 @@ import org.egov.persistence.contract.UserSearchResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Service
 public class UserRepository {
@@ -23,8 +30,13 @@ public class UserRepository {
 		final UserSearchRequest request = new UserSearchRequest(mobileNumber, tenantId);
 
 		UserSearchResponse userSearchResponse = null;
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
 		try {
-			userSearchResponse = restTemplate.postForObject(searchUserUrl, request, UserSearchResponse.class);
+			Object response = restTemplate.postForObject(searchUserUrl, request, Map.class);
+			userSearchResponse = mapper.convertValue(response, UserSearchResponse.class);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

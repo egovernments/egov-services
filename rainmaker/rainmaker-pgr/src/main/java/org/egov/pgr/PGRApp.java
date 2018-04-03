45114e2bea426c0bb65cb2f1ae3eb369dc781354
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2018-04-03 14:20:16 +0530

    all keys changed to  upppercase

--- rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/PGRApp.java
+++ rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/PGRApp.java
@@ -64,8 +64,8 @@ public class PGRApp
 		map.put("CITIZEN", Arrays.asList("open", "close", "reopen"));
 		map.put("GRO", Arrays.asList("assign", "reject", "reassign"));
 		map.put("DGRO", Arrays.asList("assign", "reject", "reassign"));
-		map.put("Grievance Routing Officer", Arrays.asList("assign", "reject", "reassign"));
-		map.put("Department Grievance Routing Officer", Arrays.asList("assign", "reject", "reassign"));
+		map.put("GRIEVANCE ROUTING OFFICER", Arrays.asList("assign", "reject", "reassign"));
+		map.put("DEPARTMENT GRIEVANCE ROUTING OFFICER", Arrays.asList("assign", "reject", "reassign"));
 		//TODO remove the name map when roles object strucutre is fixed
 		roleActionMap = map;
 	}

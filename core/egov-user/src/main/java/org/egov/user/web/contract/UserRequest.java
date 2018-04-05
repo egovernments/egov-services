ad9f66206fc7c08c499a3f98ef080c060b4341b2
kiranmai <kiranmayi.pulluru@tarento.com>
2018-04-05 14:46:57 +0530

    corrected date format

--- core/egov-user/src/main/java/org/egov/user/web/contract/UserRequest.java
+++ core/egov-user/src/main/java/org/egov/user/web/contract/UserRequest.java
@@ -59,7 +59,7 @@ public class UserRequest {
 	private Date createdDate;
 	@JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
 	private Date lastModifiedDate;
-	@JsonFormat(pattern = "yyyy-MM-DD")
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dob;
 	@JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
 	private Date pwdExpiryDate;

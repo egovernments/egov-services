4dfabcdc90f9db1a9e16494773051f432970cb78
kiranmai <kiranmayi.pulluru@tarento.com>
2018-04-06 12:00:02 +0530

    login with different tenants issue fixed

--- core/egov-user/src/test/java/org/egov/user/web/controller/UserRequestControllerTest.java
+++ core/egov-user/src/test/java/org/egov/user/web/controller/UserRequestControllerTest.java
@@ -28,6 +28,7 @@ import org.egov.user.domain.model.enums.Gender;
 import org.egov.user.domain.model.enums.UserType;
 import org.egov.user.domain.service.TokenService;
 import org.egov.user.domain.service.UserService;
+import org.egov.user.security.CustomAuthenticationKeyGenerator;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -57,6 +58,9 @@ public class UserRequestControllerTest {
 
     @MockBean
     private TokenService tokenService;
+    
+    @MockBean
+    private CustomAuthenticationKeyGenerator authenticationKeyGenerator;
 
 /*
     @Test

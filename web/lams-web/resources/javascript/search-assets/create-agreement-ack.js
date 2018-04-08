cc4c5d50e1508f31e41336a271e926dfd7852ed0
Rafeek <mohammad.rafeek@egovernments.org>
2018-04-08 19:58:52 +0530

    [EGSVC-3197]-acknowledgement screen changes for history agreements

--- web/lams-web/resources/javascript/search-assets/create-agreement-ack.js
+++ web/lams-web/resources/javascript/search-assets/create-agreement-ack.js
@@ -24,6 +24,25 @@ class CreateAgreement extends React.Component {
     render() {
       let {close,addEditDCB} = this;
 
+      const renderAckMessage = function(){
+        if(getUrlVars()["from"] === "dataEntry" && getUrlVars()["status"]==="HISTORY"){
+        return  ( [<h3> Acknowledgement of History Agreement </h3>,
+          <h3> <center>
+          <font color="ass"> History Agreement is saved successfully in the system.
+          </font> </center></h3>
+       ] );
+
+           }else{
+            return ([<h3> {typeof(getUrlVars()["from"]) == "undefined" ? "Acknowledgement of Create" : "Acknowledgement of Data Entry"}  Agreement </h3>,
+              <h3> <center>
+              <font color="ass">
+              {getUrlVars()["action"] ? (getUrlVars()["action"].toLowerCase() == "approve" ? "Agreement Approved." : getUrlVars()["action"].toLowerCase() == "modify" ? "Agreement Updated." : (getUrlVars()["action"].toLowerCase() == "forward" ? (getUrlVars()["name"] ? "Agreement forwarded to " + decodeURIComponent(getUrlVars()["name"]) : "Agreement forwarded.") : (getUrlVars()["name"] ? "Agreement rejected and forwarded to " + decodeURIComponent(getUrlVars()["name"]) : (getUrlVars()["action"] && getUrlVars()["action"].toLowerCase() == "cancel" ? "Agreement cancelled." : "Agreement rejected.")))) : (getUrlVars()["name"] ? "Agreement created and forwarded to " + decodeURIComponent(getUrlVars()["name"]) : "Successfully Created Agreement for Asset  "+decodeURIComponent(getUrlVars()["asset"]))}
+              </font> </center></h3>,
+              <h4> <center><font color="ass"><strong> {getUrlVars()["ackNo"] ? (getUrlVars()["from"] == "dataEntry" ? "Agreement Number: " : getUrlVars()["action"] && getUrlVars()["action"].toLowerCase() == "approve" ? "Agreement Number: " : "Acknowledgement number: ") + decodeURIComponent(getUrlVars()["ackNo"]) : ""}</strong>  </font> </center></h4>
+           ]);
+        }
+      }
+
       const renderButtons = function(){
         if(getUrlVars()["from"] === "dataEntry" && getUrlVars()["status"]==="ACTIVE"){
         return (
@@ -35,9 +54,10 @@ class CreateAgreement extends React.Component {
           </div>
         );
       }else{
+        return (
         <div className="text-center">
             <button type="button" className="btn btn-close" onClick={(e)=>{close()}}>Close</button>
-        </div>
+        </div>);
       }
       }
 
@@ -45,14 +65,8 @@ class CreateAgreement extends React.Component {
         <div>
           <div className="form-section">
             <form>
-              <h3> {typeof(getUrlVars()["from"]) == "undefined" ? "Acknowledgement of Create" : "Acknowledgement of Data Entry"}  Agreement </h3>
-              <h3> <center>
-                <font color="ass">
-                  {getUrlVars()["action"] ? (getUrlVars()["action"].toLowerCase() == "approve" ? "Agreement Approved." : getUrlVars()["action"].toLowerCase() == "modify" ? "Agreement Updated." : (getUrlVars()["action"].toLowerCase() == "forward" ? (getUrlVars()["name"] ? "Agreement forwarded to " + decodeURIComponent(getUrlVars()["name"]) : "Agreement forwarded.") : (getUrlVars()["name"] ? "Agreement rejected and forwarded to " + decodeURIComponent(getUrlVars()["name"]) : (getUrlVars()["action"] && getUrlVars()["action"].toLowerCase() == "cancel" ? "Agreement cancelled." : "Agreement rejected.")))) : (getUrlVars()["name"] ? "Agreement created and forwarded to " + decodeURIComponent(getUrlVars()["name"]) : "Successfully Created Agreement for Asset  "+decodeURIComponent(getUrlVars()["asset"]))}
-                </font> </center></h3>
-              <h4> <center><font color="ass"><strong> {getUrlVars()["ackNo"] ? (getUrlVars()["from"] == "dataEntry" ? "Agreement Number: " : getUrlVars()["action"] && getUrlVars()["action"].toLowerCase() == "approve" ? "Agreement Number: " : "Acknowledgement number: ") + decodeURIComponent(getUrlVars()["ackNo"]) : ""}</strong>  </font> </center></h4>
-
-               {renderButtons()}
+             {renderAckMessage()}
+             {renderButtons()}
             </form>
           </div>
         </div>

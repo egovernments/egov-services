521d845daf56076417da6028d716675495f04396
Senthil <senthil.kalimuthu@tarento.com>
2018-04-09 16:02:38 +0530

    Adding new relic support for egov-user service

--- core/egov-user/start.sh
+++ core/egov-user/start.sh
@@ -4,4 +4,15 @@ if [[ -z "${JAVA_OPTS}" ]];then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
 
-java ${JAVA_OPTS} -jar /opt/egov/egov-user.jar
+if [ -z "$TARGET_ENV" ]; then
+    export TARGET_ENV=default
+fi
+
+if [[ -z "${NEWRELIC_MONITORING_ENABLED}" ]]; then
+  export APP_NAME=$(echo $HOSTNAME |  rev | cut -d - -f3- | rev)
+  sed -i 's/license_key: LICENSE_KEY/license_key: $TARGET_ENV-$NEWRELIC_LICENSE_KEY/' /opt/egov/newrelic/newrelic.yml
+  sed -i 's/app_name: APP_NAME/app_name: $TARGET_ENV-$APP_NAME/' /opt/egov/newrelic/newrelic.yml
+  java ${JAVA_OPTS} -javaagent:/opt/egov/newrelic/newrelic.jar -jar /opt/egov/egov-user.jar
+else
+  java ${JAVA_OPTS} -jar /opt/egov/egov-user.jar
+fi

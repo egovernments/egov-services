0a4b5ad2620f7fb6783d8915802fe0ab49711855
Senthil <senthil.kalimuthu@tarento.com>
2018-04-09 16:24:29 +0530

    Fixed wrong path for new relic agent

--- core/egov-user/start.sh
+++ core/egov-user/start.sh
@@ -10,8 +10,8 @@ fi
 
 if [[ -z "${NEWRELIC_MONITORING_ENABLED}" ]]; then
   export APP_NAME=$(echo $HOSTNAME |  rev | cut -d - -f3- | rev)
-  sed -i 's/license_key: LICENSE_KEY/license_key: $TARGET_ENV-$NEWRELIC_LICENSE_KEY/' /opt/egov/newrelic.yml
-  sed -i 's/app_name: APP_NAME/app_name: $TARGET_ENV-$APP_NAME/' /opt/egov/newrelic.yml
+  sed -i 's/license_key: LICENSE_KEY/license_key: '"$NEWRELIC_LICENSE_KEY"'/' /opt/egov/newrelic.yml
+  sed -i 's/app_name: APP_NAME/app_name: '"$TARGET_ENV-$APP_NAME"'/' /opt/egov/newrelic.yml
   java ${JAVA_OPTS} -javaagent:/opt/egov/newrelic.jar -jar /opt/egov/egov-user.jar
 else
   java ${JAVA_OPTS} -jar /opt/egov/egov-user.jar

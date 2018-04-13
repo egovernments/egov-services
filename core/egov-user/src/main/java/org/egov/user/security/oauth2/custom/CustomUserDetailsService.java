7db0f48474482e88857299d4b13d10bf1b9f8887
kiranmai <kiranmayi.pulluru@tarento.com>
2018-04-13 11:37:43 +0530

    refresh_token issue got fixed

--- core/egov-user/src/main/java/org/egov/user/security/oauth2/custom/CustomUserDetailsService.java
+++ core/egov-user/src/main/java/org/egov/user/security/oauth2/custom/CustomUserDetailsService.java
@@ -9,10 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService {
 
 	private final UserService userService;
 
@@ -23,23 +24,35 @@ public class CustomUserDetailsService implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		org.egov.user.domain.model.User user1=userService.getUserByUsername(username, CustomAuthenticationProvider.getTenantId());
-		
-		List<org.egov.user.domain.model.Role> roles = user1.getRoles();
-		
+
+		if (null == CustomAuthenticationProvider.getTenantId())
+			throw new OAuth2Exception("User Session not availble (There was a server re-start.) .Please login again");
+
+		org.egov.user.domain.model.User user = userService.getUserByUsername(username,
+				CustomAuthenticationProvider.getTenantId());
+
+		if (null == user)
+			user = userService.getUserByUsernameAndTenantId(username, CustomAuthenticationProvider.getTenantId());
+
+		if (null == user)
+			throw new OAuth2Exception("User Not Found.");
+
+		List<org.egov.user.domain.model.Role> roles = user.getRoles();
+
 		List<org.egov.user.web.contract.auth.Role> roleList = new ArrayList<org.egov.user.web.contract.auth.Role>();
-		 
-		for(org.egov.user.domain.model.Role role : roles){
+
+		for (org.egov.user.domain.model.Role role : roles) {
 			org.egov.user.web.contract.auth.Role role1 = new org.egov.user.web.contract.auth.Role(role);
 			roleList.add(role1);
 		}
-		org.egov.user.web.contract.auth.User authuser = org.egov.user.web.contract.auth.User.builder().id(user1.getId()).userName(user1.getUsername()).name(user1.getName()).mobileNumber(user1.getMobileNumber())
-				.emailId(user1.getEmailId()).locale(user1.getLocale()).active(user1.getActive()).roles(roleList).tenantId(user1.getTenantId()).type(user1.getType().toString()).build();
-		
+		org.egov.user.web.contract.auth.User authuser = org.egov.user.web.contract.auth.User.builder().id(user.getId())
+				.userName(user.getUsername()).name(user.getName()).mobileNumber(user.getMobileNumber())
+				.emailId(user.getEmailId()).locale(user.getLocale()).active(user.getActive()).roles(roleList)
+				.tenantId(user.getTenantId()).type(user.getType().toString()).build();
+
 		SecureUser secureUser = new SecureUser(authuser);
-		
+
 		return secureUser;
 	}
-	
+
 }

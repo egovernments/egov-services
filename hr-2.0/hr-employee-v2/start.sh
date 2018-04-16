cd3d6d8406a4fa3e950bc0ffee5b50e0d17be645
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-04-16 18:41:51 +0530

    Adding build configuration for hr-employee-v2 and hr-masters-v2

--- hr-2.0/hr-employee-v2/start.sh
+++ hr-2.0/hr-employee-v2/start.sh
@@ -4,4 +4,4 @@ if [[ -z "${JAVA_OPTS}" ]];then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
 
-java ${JAVA_OPTS} -jar /opt/egov/hr-employee.jar
+java ${JAVA_OPTS} -jar /opt/egov/hr-employee-v2.jar
--- hr-2.0/hr-masters-v2/start.sh
+++ hr-2.0/hr-masters-v2/start.sh
@@ -4,4 +4,4 @@ if [[ -z "${JAVA_OPTS}" ]];then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
 
-java ${JAVA_OPTS} -jar /opt/egov/hr-masters.jar
+java ${JAVA_OPTS} -jar /opt/egov/hr-masters-v2.jar

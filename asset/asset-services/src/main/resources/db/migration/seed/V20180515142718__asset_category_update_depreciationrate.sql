5a00a4f758ee04b8132b4ff8948a9f8315ecca58
shwetha <shwetha.dc@egovernments.org>
2018-05-15 14:34:19 +0530

    [EGSVC-3224] updated depreciation rate and life of the asset for asset categories.

---
+++ asset/asset-services/src/main/resources/db/migration/seed/V20180515142718__asset_category_update_depreciationrate.sql
@@ -0,0 +1,45 @@
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Office Building';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='School Building';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Hospital/ Urban Health Centre / Maternity building';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Commercial complex';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Auditorium';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Function Hall';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Community Hall';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Stadium';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Library';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Reading room';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Residential Municipal Quarter';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Municipal Guest House';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Museums';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Godown';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Night Shelter';
+
+UPDATE egasset_assetcategory SET depreciationrate=2,lifeoftheasset=50 where name='Ward Office/Muster Office';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Swimming pool';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Building at Water work';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Building at dumping yard';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Building within park';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Building at Burial Ground';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='eSeva centre';
+
+UPDATE egasset_assetcategory SET depreciationrate=5,lifeoftheasset=20 where name='Open air theatre';

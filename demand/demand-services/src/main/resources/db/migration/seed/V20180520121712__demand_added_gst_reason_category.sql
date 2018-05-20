e772f66c3f48a2fd6671de45f010b15a9fdcfb9d
Rafeek <mohammad.rafeek@egovernments.org>
2018-05-20 12:27:51 +0530

    [EGSVC-3235]-added GST,service tax demand reasons

---
+++ demand/demand-services/src/main/resources/db/migration/seed/V20180520121712__demand_added_gst_reason_category.sql
@@ -0,0 +1,3 @@
+INSERT INTO EG_REASON_CATEGORY values (nextval('seq_eg_reason_category'),'GST','GST',7,now(),'default');
+
+INSERT INTO EG_REASON_CATEGORY values (nextval('seq_eg_reason_category'),'Service Tax','SERVICETAX',8,now(),'default');
---
+++ demand/demand-services/src/main/resources/db/migration/seed/V20180520121746__demand_added_gst_reasonmaster_and_reasons.sql
@@ -0,0 +1,13 @@
+INSERT INTO EG_DEMAND_REASON_MASTER ( ID, REASONMASTER, "category", ISDEBIT, module, CODE, "order", create_date, modified_date, isdemand, tenantid) VALUES(nextval('seq_eg_demand_reason_master'), 'CGST', (select id from eg_reason_category where code='GST' and tenantid='default'), 'N', 'Leases And Agreements', 'CENTRAL_GST', 4, current_timestamp, current_timestamp,false, 'default');
+
+INSERT INTO EG_DEMAND_REASON (ID,ID_DEMAND_REASON_MASTER,ID_INSTALLMENT,PERCENTAGE_BASIS,ID_BASE_REASON,create_date,modified_date,GLCODE, tenantid) select nextval('seq_eg_demand_reason'), (select id from eg_demand_reason_master where reasonmaster='CGST' and module='Leases And Agreements' and tenantid='default'), inst.id, null, null, current_timestamp, current_timestamp, '1402010', 'default' from eg_installment_master inst where inst.module='Leases And Agreements' and inst.tenantid='default';
+
+
+INSERT INTO EG_DEMAND_REASON_MASTER ( ID, REASONMASTER, "category", ISDEBIT, module, CODE, "order", create_date, modified_date, isdemand, tenantid) VALUES(nextval('seq_eg_demand_reason_master'), 'SGST', (select id from eg_reason_category where code='GST' and tenantid='default'), 'N', 'Leases And Agreements', 'STATE_GST', 4, current_timestamp, current_timestamp,false, 'default');
+
+INSERT INTO EG_DEMAND_REASON (ID,ID_DEMAND_REASON_MASTER,ID_INSTALLMENT,PERCENTAGE_BASIS,ID_BASE_REASON,create_date,modified_date,GLCODE, tenantid) select nextval('seq_eg_demand_reason'), (select id from eg_demand_reason_master where reasonmaster='SGST' and module='Leases And Agreements' and tenantid='default'), inst.id, null, null, current_timestamp, current_timestamp, '1402010', 'default' from eg_installment_master inst where inst.module='Leases And Agreements' and inst.tenantid='default';
+
+
+INSERT INTO EG_DEMAND_REASON_MASTER ( ID, REASONMASTER, "category", ISDEBIT, module, CODE, "order", create_date, modified_date, isdemand, tenantid) VALUES(nextval('seq_eg_demand_reason_master'), 'Service Tax', (select id from eg_reason_category where code='SERVICETAX' and tenantid='default'), 'N', 'Leases And Agreements', 'SERVICE_TAX', 4, current_timestamp, current_timestamp,false, 'default');
+
+INSERT INTO EG_DEMAND_REASON (ID,ID_DEMAND_REASON_MASTER,ID_INSTALLMENT,PERCENTAGE_BASIS,ID_BASE_REASON,create_date,modified_date,GLCODE, tenantid) select nextval('seq_eg_demand_reason'), (select id from eg_demand_reason_master where reasonmaster='Service Tax' and module='Leases And Agreements' and tenantid='default'), inst.id, null, null, current_timestamp, current_timestamp, '1402010', 'default' from eg_installment_master inst where inst.module='Leases And Agreements' and inst.tenantid='default';

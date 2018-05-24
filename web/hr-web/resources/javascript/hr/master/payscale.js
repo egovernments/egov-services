ac19037dfdfae671169b7e948c93b083d74bebc3
Siva-nalluri <siva.nalluri@riflexions.com>
2018-05-24 16:27:01 +0530

    [EGSVC-3228] Change to payscale

--- web/hr-web/resources/javascript/hr/master/payscale.js
+++ web/hr-web/resources/javascript/hr/master/payscale.js
@@ -2,17 +2,46 @@ class PayScaleMaster extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+
       payscaleSet: {
-        name: "",
-        description: "",
-        orderNo: "",
-        active: ""
+        "paycommission": "",
+        "payscale": "",
+        "amountFrom": "",
+        "amountTo": "",
+        "tenantId": tenantId,
+        "payscaleDetails": [
+          {
+            "basicFrom": "",
+            "basicTo": "",
+            "increment": "",
+            "tenantId": tenantId
+          }
+        ]
       }
+
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleDetailsChange = this.handleDetailsChange.bind(this);
     this.addOrUpdate = this.addOrUpdate.bind(this);
+    this.addPayScaleDetails = this.addPayScaleDetails.bind(this);
+    this.deletePayscaleDetails = this.deletePayscaleDetails.bind(this);
   }
 
+  handleDetailsChange(e, index, name) {
+
+    console.log(index, name);
+
+    let details = Object.assign([], this.state.payscaleSet.payscaleDetails);
+
+    details[index][name] = e.target.value;
+
+    this.setState({
+      payscaleSet: {
+        ...this.state.payscaleSet,
+        payscaleDetails: details
+      }
+    })
+  }
 
   handleChange(e, name) {
     this.setState({
@@ -21,6 +50,36 @@ class PayScaleMaster extends React.Component {
         [name]: e.target.value
       }
     })
+  }
+
+  deletePayscaleDetails() {
+    let details = Object.assign([], this.state.payscaleSet.payscaleDetails);
+    details.splice(details.length - 1, 1);
+
+    this.setState({
+      payscaleSet: {
+        ...this.state.payscaleSet,
+        payscaleDetails: details
+      }
+    })
+
+  }
+
+  addPayScaleDetails(index) {
+
+    let details = Object.assign([], this.state.payscaleSet.payscaleDetails);
+
+    let newDetails = { "basicFrom": "", "basicTo": "", "increment": "", "tenantId": tenantId };
+
+    details.push(newDetails);
+
+    this.setState({
+      payscaleSet: {
+        ...this.state.payscaleSet,
+        payscaleDetails: details
+      }
+    })
+
   }
 
 
@@ -34,7 +93,7 @@ class PayScaleMaster extends React.Component {
       }
     }
 
-    if (getUrlVars()["type"]) $('#hp-citizen-title').text(titleCase(getUrlVars()["type"]) + " Grade");
+    if (getUrlVars()["type"]) $('#hp-citizen-title').text(titleCase(getUrlVars()["type"]) + " Pay Scale");
 
     if (getUrlVars()["type"] === "view") {
       for (var variable in this.state.payscaleSet)
@@ -42,7 +101,7 @@ class PayScaleMaster extends React.Component {
     }
 
     if (type === "view" || type === "update") {
-      getCommonMasterById("hr-masters", "grades", id, function (err, res) {
+      getCommonMasterById("hr-employee", "payscale/_search", id, function (err, res) {
         if (res) {
           var payscaleSet = res["Grade"][0];
           _this.setState({
@@ -59,32 +118,100 @@ class PayScaleMaster extends React.Component {
   }
 
 
-  addOrUpdate(e, mode) {
+  addOrUpdate(e) {
     e.preventDefault();
-    console.log(this.state.payscaleSet);
-    this.setState({
-      payscaleSet: {
-        name: "",
-        description: "",
-        orderNo: "",
-        active: ""
-      }
-    })
-  }
+    var tempInfo = Object.assign({}, this.state.payscaleSet), type = getUrlVars()["type"];
+    var body = {
+      "RequestInfo": requestInfo,
+      "PayscaleHeader": tempInfo
+    }, _this = this;
 
+    let url = "";
+    getUrlVars()["type"] == "update" ? "hr-employee/payscale/_update?tenantId=" : "hr-employee/payscale/_create?tenantId=" 
+
+      $.ajax({    
+        url: baseUrl + url + tenantId,
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(body),
+        contentType: 'application/json',
+        headers: {
+          'auth-token': authToken
+        },
+        success: function (res) {
+          showSuccess("PayScale Created successfully.");
+          _this.setState({
+            payscaleSet: {
+              "paycommission": "",
+              "payscale": "",
+              "amountFrom": "",
+              "amountTo": "",
+              "tenantId": tenantId,
+              "payscaleDetails": [
+                {
+                  "basicFrom": "",
+                  "basicTo": "",
+                  "increment": "",
+                  "tenantId": tenantId
+                }
+              ]
+            }
+          })
+        },
+        error: function (err) {
+          showError(err);
+        }
+      });
+    
+  }
 
 
   render() {
 
-    let { handleChange, addOrUpdate } = this;
+    let { handleChange, addOrUpdate, handleDetailsChange, deletePayscaleDetails } = this;
     let mode = getUrlVars()["type"];
-    let { name, description, orderNo, active } = this.state.payscaleSet;
+    let { paycommission, payscale, amountFrom, amountTo, payscaleDetails } = this.state.payscaleSet;
 
     const showActionButton = function () {
       if ((!mode) || mode === "update") {
         return (<button type="submit" className="btn btn-submit">{mode ? "Update" : "Add"}</button>);
       }
     };
+
+    const renderDelBtn = function (ind) {
+      if (ind != 0 && ind == payscaleDetails.length - 1)
+        return (
+          <button type="button" className="btn btn-close" style={{ "color": "#000000" }} onClick={() => deletePayscaleDetails()}>Delete</button>
+        )
+    }
+
+    const renderPayScaleDetails = function () {
+
+      if (payscaleDetails && payscaleDetails.length) {
+
+        return payscaleDetails.map(function (payscaleDetail, ind) {
+          return (
+
+            <tr key={ind} id={ind} >
+              <td>
+                <input type="number" name="basicFrom" id="basicFrom" value={payscaleDetail["basicFrom"]}
+                  onChange={(e) => { handleDetailsChange(e, ind, "basicFrom") }} required />
+              </td>
+              <td>
+                <input type="number" name="basicTo" id="basicTo" value={payscaleDetail["basicTo"]}
+                  onChange={(e) => { handleDetailsChange(e, ind, "basicTo") }} required />
+              </td>
+              <td>
+                <input type="number" name="increment" id="increment" value={payscaleDetail["increment"]}
+                  onChange={(e) => { handleDetailsChange(e, ind, "increment") }} required />
+              </td>
+              <td>{renderDelBtn(ind)}</td>
+            </tr>
+
+          );
+        })
+      }
+    }
 
     return (<div>
       <h3>{getUrlVars()["type"] ? titleCase(getUrlVars()["type"]) : "Create"} Pay Scale</h3>
@@ -97,22 +224,22 @@ class PayScaleMaster extends React.Component {
                 <div className="col-sm-6">
                   <div className="row">
                     <div className="col-sm-6 label-text">
-                      <label for="Grade">Pay commission <span>* </span></label>
+                      <label htmlFor="Grade">Pay commission <span>* </span></label>
                     </div>
                     <div className="col-sm-6">
-                      <input type="text" name="name" id="name" value={name}
-                        onChange={(e) => { handleChange(e, "name") }} required />
+                      <input type="text" name="paycommission" id="paycommission" value={paycommission}
+                        onChange={(e) => { handleChange(e, "paycommission") }} required />
                     </div>
                   </div>
                 </div>
                 <div className="col-sm-6">
                   <div className="row">
                     <div className="col-sm-6 label-text">
-                      <label for="">Pay scale<span> *</span> </label>
+                      <label htmlFor="">Pay scale<span> *</span> </label>
                     </div>
                     <div className="col-sm-6">
-                      <input type="text" name="description" id="description" value={description}
-                        onChange={(e) => { handleChange(e, "description") }} required />
+                      <input type="text" name="payscale" id="payscale" value={payscale}
+                        onChange={(e) => { handleChange(e, "payscale") }} required />
                     </div>
                   </div>
                 </div>
@@ -121,22 +248,22 @@ class PayScaleMaster extends React.Component {
                 <div className="col-sm-6">
                   <div className="row">
                     <div className="col-sm-6 label-text">
-                      <label for="">From <span>* </span></label>
+                      <label htmlFor="">From <span>* </span></label>
                     </div>
                     <div className="col-sm-6 label-view-text">
-                      <input type="number" name="orderNo" id="orderNo" min="0" value={orderNo}
-                        onChange={(e) => { handleChange(e, "orderNo") }} required />
+                      <input type="number" name="amountFrom" id="amountFrom" min="0" value={amountFrom}
+                        onChange={(e) => { handleChange(e, "amountFrom") }} required />
                     </div>
                   </div>
                 </div>
                 <div className="col-sm-6">
                   <div className="row">
                     <div className="col-sm-6 label-text">
-                      <label for="">To <span>* </span></label>
+                      <label htmlFor="">To <span>* </span></label>
                     </div>
                     <div className="col-sm-6 label-view-text">
-                      <input type="number" name="orderNo" id="orderNo" min="0" value={orderNo}
-                        onChange={(e) => { handleChange(e, "orderNo") }} required />
+                      <input type="number" name="amountTo" id="amountTo" min="0" value={amountTo}
+                        onChange={(e) => { handleChange(e, "amountTo") }} required />
                     </div>
                   </div>
                 </div>
@@ -147,44 +274,26 @@ class PayScaleMaster extends React.Component {
           </div>
           <br />
           <div className="form-section">
+
             <div className="form-section-inner">
 
-              <div className="row">
-                <div className="col-sm-6">
-                  <div className="row">
-                    <div className="col-sm-6 label-text">
-                      <label for="Grade">From Basic <span>* </span></label>
-                    </div>
-                    <div className="col-sm-6">
-                      <input type="text" name="name" id="name" value={name}
-                        onChange={(e) => { handleChange(e, "name") }} required />
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-6">
-                  <div className="row">
-                    <div className="col-sm-6 label-text">
-                      <label for="">To Basic<span> *</span> </label>
-                    </div>
-                    <div className="col-sm-6">
-                      <input type="text" name="description" id="description" value={description}
-                        onChange={(e) => { handleChange(e, "description") }} required />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-sm-6">
-                  <div className="row">
-                    <div className="col-sm-6 label-text">
-                      <label for="">Increment <span> *</span></label>
-                    </div>
-                    <div className="col-sm-6">
-                      <input type="text" name="description" id="description" value={description}
-                        onChange={(e) => { handleChange(e, "description") }} required />
-                    </div>
-                  </div>
-                </div>
+              <table id="fileTable" className="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>From Basic<span>* </span></th>
+                    <th>To Basic<span>* </span></th>
+                    <th>Increment<span>* </span></th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody id="tableBody">
+                  {
+                    renderPayScaleDetails()
+                  }
+                </tbody>
+              </table>
+              <div className="text-right">
+                <button type="button" className="btn btn-submit" onClick={(e) => { this.addPayScaleDetails(payscaleDetails.length) }}>Add Payscale Details</button>
               </div>
             </div>
           </div>

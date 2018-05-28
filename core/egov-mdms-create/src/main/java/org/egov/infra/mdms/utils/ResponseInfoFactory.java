b1adf1d573f2e569afa52e54c4f610c5aa8dafd3
vishalmahuli <vishalmahuli8@gmail.com>
2018-05-28 15:26:37 +0530

    commented out ts for time being

--- core/egov-mdms-create/src/main/java/org/egov/infra/mdms/utils/ResponseInfoFactory.java
+++ core/egov-mdms-create/src/main/java/org/egov/infra/mdms/utils/ResponseInfoFactory.java
@@ -10,7 +10,7 @@ public class ResponseInfoFactory {
 	public ResponseInfo createResponseInfoFromRequestInfo(final RequestInfo requestInfo, final Boolean success) {
 		final String apiId = requestInfo != null ? requestInfo.getApiId() : "";
 		final String ver = requestInfo != null ? requestInfo.getVer() : "";
-		final Long ts = requestInfo != null ? requestInfo.getTs() : 0L;
+		//final Long ts = requestInfo != null ? requestInfo.getTs() : 0L;
 		final String resMsgId = "uief87324"; // FIXME : Hard-coded
 		final String msgId = requestInfo != null ? requestInfo.getMsgId() : "";
 		final String responseStatus = success ? "successful" : "failed";

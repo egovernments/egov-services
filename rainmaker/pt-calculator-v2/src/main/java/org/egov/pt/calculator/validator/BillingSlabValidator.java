baec15302f468b633f875c3e9668318f206fd26f
Vishal Mahuli <vishal.mahuli@tarento.com>
2018-06-07 16:56:49 +0530

    change in error message

--- rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/validator/BillingSlabValidator.java
+++ rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/validator/BillingSlabValidator.java
@@ -167,18 +167,21 @@ public class BillingSlabValidator {
 				List<String> allowedOccupancyTypes = JsonPath.read(occupancyType, BillingSlabConstants.MDMS_CODE_JSONPATH);
 				log.info("allowedOccupancyTypes: "+allowedOccupancyTypes);
 				if(!allowedOccupancyTypes.contains(billingSlab.getOccupancyType()))
-					errorMap.put("INVALID_OCCUPANCY_TYPE","Allowed Occupancy Types are: "+allowedOccupancyTypes);
+					//errorMap.put("INVALID_OCCUPANCY_TYPE","Allowed Occupancy Types are: "+allowedOccupancyTypes);
+					errorMap.put("INVALID_OCCUPANCY_TYPE","Occupancy Type provided is invalid");
 			}
 			if(!StringUtils.isEmpty(billingSlab.getPropertyType())) {
 				List<String> allowedPropertyTypes = JsonPath.read(propertyTypes,BillingSlabConstants.MDMS_CODE_JSONPATH);
 				if(!allowedPropertyTypes.contains(billingSlab.getPropertyType())) {
-					errorMap.put("INVALID_PROPERTY_TYPE","Allowed Property Type are: "+allowedPropertyTypes);
+					//errorMap.put("INVALID_PROPERTY_TYPE","Allowed Property Type are: "+allowedPropertyTypes);
+					errorMap.put("INVALID_PROPERTY_TYPE","Property Type provided is invalid");
 				}else {
 					if (!StringUtils.isEmpty(billingSlab.getPropertySubType())) {
 						if (!CollectionUtils.isEmpty(propertySubtypes)) {
 							List<String> allowedPropertySubTypes = JsonPath.read(propertySubtypes,"$.*.[?(@.propertyType=='" + billingSlab.getPropertyType() + "')].code");
 							if (!allowedPropertySubTypes.contains(billingSlab.getPropertySubType())) {
-								errorMap.put("INVALID_PROPERTY_SUBTYPE", "Allowed property subtype for this property type: "+ billingSlab.getPropertyType() + " are: " + allowedPropertySubTypes);
+								//errorMap.put("INVALID_PROPERTY_SUBTYPE", "Allowed property subtype for this property type: "+ billingSlab.getPropertyType() + " are: " + allowedPropertySubTypes);
+								errorMap.put("INVALID_PROPERTY_SUBTYPE", "Property Subtype provided is invalid");
 							}
 						}
 					}
@@ -187,12 +190,16 @@ public class BillingSlabValidator {
 			if(!StringUtils.isEmpty(billingSlab.getOwnerShipCategory())) {
 				List<String> allowedOwnerShipCategories = JsonPath.read(ownerShipCategory,BillingSlabConstants.MDMS_CODE_JSONPATH);
 				if(!allowedOwnerShipCategories.contains(billingSlab.getOwnerShipCategory())) {
-					errorMap.put("INVALID_OWNERSHIP_CATEGORY","Allowed Ownership categories are: "+allowedOwnerShipCategories);
+					//errorMap.put("INVALID_OWNERSHIP_CATEGORY","Allowed Ownership categories are: "+allowedOwnerShipCategories);
+					errorMap.put("INVALID_OWNERSHIP_CATEGORY","Ownership category is invalid");
+
 				}else {
 					if (!CollectionUtils.isEmpty(subOwnerShipCategory)) {
 						List<String> allowedsubOwnerShipCategories = JsonPath.read(subOwnerShipCategory,"$.*.[?(@.ownerShipCategoryCode=='" + billingSlab.getOwnerShipCategory() + "')].code");
 						if (!allowedsubOwnerShipCategories.contains(billingSlab.getSubOwnerShipCategory())) {
-							errorMap.put("INVALID_SUBOWNERSHIP_CATEGORY","Allowed subownership category for this ownership category: "+ billingSlab.getOwnerShipCategory() + " are: " + allowedsubOwnerShipCategories);
+							//errorMap.put("INVALID_SUBOWNERSHIP_CATEGORY","Allowed subownership category for this ownership category: "+ billingSlab.getOwnerShipCategory() + " are: " + allowedsubOwnerShipCategories);
+							errorMap.put("INVALID_SUBOWNERSHIP_CATEGORY","Subownership category is invalid");
+
 						}
 					}
 				}
@@ -200,17 +207,20 @@ public class BillingSlabValidator {
 			if(!StringUtils.isEmpty(billingSlab.getUsageCategoryMajor())) {
 				List<String> allowedUsageCategoryMajor = JsonPath.read(usageCategoryMajor, BillingSlabConstants.MDMS_CODE_JSONPATH);
 				if(!allowedUsageCategoryMajor.contains(billingSlab.getUsageCategoryMajor())) {
-					errorMap.put("INVALID_USAGECATEGORY_MAJOR","Allowed Usage category major are: "+allowedUsageCategoryMajor);
+					//errorMap.put("INVALID_USAGECATEGORY_MAJOR","Allowed Usage category major are: "+allowedUsageCategoryMajor);
+					errorMap.put("INVALID_USAGECATEGORY_MAJOR","Usage category Major is invalid");
 				}else {
 					if (!CollectionUtils.isEmpty(usageCategoryMinors)) {
 						List<String> allowedUsageCategoryMinors = JsonPath.read(usageCategoryMinors, "$.*.[?(@.usageCategoryMajor=='" + billingSlab.getUsageCategoryMajor() + "')].code");
 						if (!allowedUsageCategoryMinors.contains(billingSlab.getUsageCategoryMinor())) {
-							errorMap.put("INVALID_USAGE_CATEGORY_MINOR","Allowed Usage category minor for this usage category major: "+ billingSlab.getUsageCategoryMajor() + " are: " + allowedUsageCategoryMinors);
+							//errorMap.put("INVALID_USAGE_CATEGORY_MINOR","Allowed Usage category minor for this usage category major: "+ billingSlab.getUsageCategoryMajor() + " are: " + allowedUsageCategoryMinors);
+							errorMap.put("INVALID_USAGE_CATEGORY_MINOR","Usage category minor is invalid");
 						} else {
 							if (!CollectionUtils.isEmpty(allowedUsageCategoryMinors)) {
 								List<String> usageCategorySubMinors = JsonPath.read(allowedUsageCategoryMinors,"$.*.[?(@.usageCategoryMinor=='" + billingSlab.getUsageCategoryMinor() + "')].code");
 								if (!usageCategorySubMinors.contains(billingSlab.getUsageCategorySubMinor())) {
-									errorMap.put("INVALID_USAGE_CATEGORY_SUBMINOR","Allowed Usage category sub minor for this usage category minor: "+ billingSlab.getUsageCategoryMinor() + " are: " + usageCategorySubMinors);
+									//errorMap.put("INVALID_USAGE_CATEGORY_SUBMINOR","Allowed Usage category sub minor for this usage category minor: "+ billingSlab.getUsageCategoryMinor() + " are: " + usageCategorySubMinors);
+									errorMap.put("INVALID_USAGE_CATEGORY_SUBMINOR","Provided Usage category sub minor is invalid");
 								}
 							}
 						}

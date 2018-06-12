77e6a9a12300abf68cea0706ffab534a54f06b2f
Vishal Mahuli <vishal.mahuli@tarento.com>
2018-06-12 18:08:28 +0530

    added support for All value in billing slab

--- rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/util/BillingSlabConstants.java
+++ rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/util/BillingSlabConstants.java
@@ -10,6 +10,7 @@ public class BillingSlabConstants {
 	public static final String MDMS_PROPERTYTYPE_MASTER_NAME = "PropertyType";
 	public static final String MDMS_USAGEMINOR_MASTER_NAME = "UsageCategoryMinor";
 	public static final String MDMS_USAGESUBMINOR_MASTER_NAME = "UsageCategorySubMinor";
+	public static final String MDMS_USAGEDETAIL_MASTER_NAME = "UsageCategoryDetail";
 	public static final String MDMS_USAGEMAJOR_MASTER_NAME = "UsageCategoryMajor";
 	public static final String MDMS_SUBOWNERSHIP_MASTER_NAME = "SubOwnerShipCategory";
 	public static final String MDMS_OWNERSHIP_MASTER_NAME = "OwnerShipCategory";
@@ -18,5 +19,8 @@ public class BillingSlabConstants {
 	
 	public static final String MDMS_PROPERTYTAX_JSONPATH = "$.MdmsRes.PropertyTax.";
 	public static final String MDMS_CODE_JSONPATH = "$.*.code";
+	
+	public static final String ALL_PLACEHOLDER_BILLING_SLAB = "All";
+
 
 }

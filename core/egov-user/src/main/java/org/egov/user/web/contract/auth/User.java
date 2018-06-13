6d127bf4592f454d3672333992cc05f8db122a05
Vishal Mahuli <vishal.mahuli@tarento.com>
2018-06-13 14:32:24 +0530

    uuid in RI

--- core/egov-user/src/main/java/org/egov/user/web/contract/auth/User.java
+++ core/egov-user/src/main/java/org/egov/user/web/contract/auth/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class User implements Serializable {
 	private static final long serialVersionUID = -1053170163821651014L;
 	private Long id;
+	private String uuid;
 	private String userName;
 	private String name;
 	private String mobileNumber;

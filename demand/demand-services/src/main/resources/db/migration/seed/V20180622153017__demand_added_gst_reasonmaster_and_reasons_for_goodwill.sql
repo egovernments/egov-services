59eef7bdb0c6b946abe102934d8685e173a3af05
Rafeek <mohammad.rafeek@egovernments.org>
2018-06-22 16:06:48 +0530

    [EGSVC-3265]-GST demand reasons for goodwill amount

---
+++ demand/demand-services/src/main/resources/db/migration/seed/V20180622153017__demand_added_gst_reasonmaster_and_reasons_for_goodwill.sql
@@ -0,0 +1,8 @@
+INSERT INTO EG_DEMAND_REASON_MASTER ( ID, REASONMASTER, "category", ISDEBIT, module, CODE, "order", create_date, modified_date, isdemand, tenantid) VALUES(nextval('seq_eg_demand_reason_master'), 'CGST On GoodWill', (select id from eg_reason_category where code='GST' and tenantid='default'), 'N', 'Leases And Agreements', 'GW_CGST', 5, current_timestamp, current_timestamp,false, 'default');
+
+INSERT INTO EG_DEMAND_REASON (ID,ID_DEMAND_REASON_MASTER,ID_INSTALLMENT,PERCENTAGE_BASIS,ID_BASE_REASON,create_date,modified_date,GLCODE, tenantid) select nextval('seq_eg_demand_reason'), (select id from eg_demand_reason_master where code='GW_CGST' and module='Leases And Agreements' and tenantid='default'), inst.id, null, null, current_timestamp, current_timestamp, '3502034', 'default' from eg_installment_master inst where inst.module='Leases And Agreements' and inst.tenantid='default';
+
+INSERT INTO EG_DEMAND_REASON_MASTER ( ID, REASONMASTER, "category", ISDEBIT, module, CODE, "order", create_date, modified_date, isdemand, tenantid) VALUES(nextval('seq_eg_demand_reason_master'), 'SGST On GoodWill', (select id from eg_reason_category where code='GST' and tenantid='default'), 'N', 'Leases And Agreements', 'GW_SGST', 5, current_timestamp, current_timestamp,false, 'default');
+
+INSERT INTO EG_DEMAND_REASON (ID,ID_DEMAND_REASON_MASTER,ID_INSTALLMENT,PERCENTAGE_BASIS,ID_BASE_REASON,create_date,modified_date,GLCODE, tenantid) select nextval('seq_eg_demand_reason'), (select id from eg_demand_reason_master where code='GW_SGST' and module='Leases And Agreements' and tenantid='default'), inst.id, null, null, current_timestamp, current_timestamp, '3502034', 'default' from eg_installment_master inst where inst.module='Leases And Agreements' and inst.tenantid='default';
+

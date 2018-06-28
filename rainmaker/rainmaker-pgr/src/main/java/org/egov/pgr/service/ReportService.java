b36a3727e1b2cd3794891f99e7549de50025ce73
Vishal Mahuli <vishal.mahuli@tarento.com>
2018-06-28 21:26:27 +0530

    functionary report fix

--- rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/service/ReportService.java
+++ rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/service/ReportService.java
@@ -1,5 +1,6 @@
 package org.egov.pgr.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -78,13 +79,12 @@ public class ReportService {
 		List<Long> GROids = dbResponse.parallelStream().map(obj -> {
 			return Long.valueOf(obj.get("ao_name").toString().split("[:]")[0]);
 		}).collect(Collectors.toList());
-		
+
 		Map<Long, String> mapOfIdAndName = getEmployeeDetails(reportRequest, GROids);
-		
+
 		for (Map<String, Object> tuple : dbResponse) {
 			String name = mapOfIdAndName.get(Long.valueOf(tuple.get("ao_name").toString().split("[:]")[0]));
-			tuple.put("ao_name", 
-				 !StringUtils.isEmpty(name) ? name : tuple.get("ao_name").toString().split("[:]")[0]);
+			tuple.put("ao_name", !StringUtils.isEmpty(name) ? name : tuple.get("ao_name").toString().split("[:]")[0]);
 			tuple.put("complaints_unassigned",
 					((Long.valueOf(tuple.get("total_complaints_received").toString()))
 							- ((Long.valueOf(tuple.get("complaints_assigned").toString()))
@@ -92,65 +92,72 @@ public class ReportService {
 		}
 
 	}
-	
+
 	public void enrichDepartmentWiseReport(ReportRequest reportRequest, List<Map<String, Object>> dbResponse) {
 		Map<String, String> mapOfServiceCodesAndDepts = getServiceDefsData(reportRequest, false);
 		for (Map<String, Object> tuple : dbResponse) {
 			tuple.put("department_name", mapOfServiceCodesAndDepts.get(tuple.get("department_name")));
 		}
 	}
-	
+
 	public void enrichSourceWiseReport(ReportRequest reportRequest, List<Map<String, Object>> dbResponse) {
-		//do nothing for now, later if there's a requirement, this method can be used.
+		// do nothing for now, later if there's a requirement, this method can be used.
 	}
-	
+
 	public void enrichFunctionaryWiseReport(ReportRequest reportRequest, List<Map<String, Object>> dbResponse) {
-		List<Long> employeeIds = dbResponse.parallelStream().map(obj -> {
-			return Long.valueOf(StringUtils.isEmpty(obj.get("employee_name").toString()) ? "0" : obj.get("employee_name").toString());
-		}).collect(Collectors.toList());
-		
-		Map<Long, String> mapOfIdAndName = getEmployeeDetails(reportRequest, employeeIds);
-		
+		List<Long> employeeIds = new ArrayList<>();
 		for (Map<String, Object> tuple : dbResponse) {
-			String name = mapOfIdAndName.get(Long.valueOf(tuple.get("employee_name").toString()));
-			tuple.put("employee_name", 
-				 !StringUtils.isEmpty(name) ? name : tuple.get("employee_name").toString());
+			employeeIds.add(
+					Long.valueOf((null == tuple.get("employee_name")) ? "0" : tuple.get("employee_name").toString()));
+		}
+
+		Map<Long, String> mapOfIdAndName = getEmployeeDetails(reportRequest, employeeIds);
+
+		for (Map<String, Object> tuple : dbResponse) {
+			String name = mapOfIdAndName.get(
+					Long.valueOf((null == tuple.get("employee_name")) ? "0" : tuple.get("employee_name").toString()));
+			tuple.put("employee_name", name);
 		}
 
 	}
 
-	public Map<Long, String> getEmployeeDetails(ReportRequest reportRequest, List<Long> employeeIds){
+	public Map<Long, String> getEmployeeDetails(ReportRequest reportRequest, List<Long> employeeIds) {
 		Map<Long, String> mapOfIdAndName = new HashMap<>();
 		ObjectMapper mapper = pgrUtils.getObjectMapper();
 		StringBuilder uri = new StringBuilder();
 		Object request = reportUtils.getGROSearchRequest(uri, employeeIds, reportRequest);
 		Object response = serviceRequestRepository.fetchResult(uri, request);
-		if(null != response) {
-			List<Map<String, Object>> resultCast = mapper.convertValue(JsonPath.read(response, "$.Employee"), List.class);
-			for(Map<String, Object> employee: resultCast) {
+		if (null != response) {
+			List<Map<String, Object>> resultCast = mapper.convertValue(JsonPath.read(response, "$.Employee"),
+					List.class);
+			for (Map<String, Object> employee : resultCast) {
 				mapOfIdAndName.put(Long.parseLong(employee.get("id").toString()), employee.get("name").toString());
 			}
 		}
-		log.info("mapOfIdAndName: "+mapOfIdAndName);
+		log.info("mapOfIdAndName: " + mapOfIdAndName);
 		return mapOfIdAndName;
 	}
-	
-	public Map<String, String> getServiceDefsData(ReportRequest reportRequest, Boolean iWantSlahours){
+
+	public Map<String, String> getServiceDefsData(ReportRequest reportRequest, Boolean iWantSlahours) {
 		Map<String, String> mapOfServiceCodesAndDepts = new HashMap<>();
 		Map<String, String> mapOfServiceCodesAndSLA = new HashMap<>();
 		ObjectMapper mapper = pgrUtils.getObjectMapper();
 		StringBuilder uri = new StringBuilder();
-		Object request = reportUtils.getRequestForServiceDefsSearch(uri, reportRequest.getTenantId(), reportRequest.getRequestInfo());
+		Object request = reportUtils.getRequestForServiceDefsSearch(uri, reportRequest.getTenantId(),
+				reportRequest.getRequestInfo());
 		Object response = serviceRequestRepository.fetchResult(uri, request);
-		if(null != response) {
-			List<Map<String, Object>> resultCast = mapper.convertValue(JsonPath.read(response, "$.MdmsRes.RAINMAKER-PGR.ServiceDefs"), List.class);
-			for(Map<String, Object> serviceDef: resultCast) {
-				mapOfServiceCodesAndDepts.put(serviceDef.get("serviceCode").toString(), serviceDef.get("department").toString());
-				mapOfServiceCodesAndSLA.put(serviceDef.get("serviceCode").toString(), serviceDef.get("slaHours").toString());
+		if (null != response) {
+			List<Map<String, Object>> resultCast = mapper
+					.convertValue(JsonPath.read(response, "$.MdmsRes.RAINMAKER-PGR.ServiceDefs"), List.class);
+			for (Map<String, Object> serviceDef : resultCast) {
+				mapOfServiceCodesAndDepts.put(serviceDef.get("serviceCode").toString(),
+						serviceDef.get("department").toString());
+				mapOfServiceCodesAndSLA.put(serviceDef.get("serviceCode").toString(),
+						serviceDef.get("slaHours").toString());
 			}
 		}
-		log.info("mapOfServiceCodesAndDepts: "+mapOfServiceCodesAndDepts);
-		if(iWantSlahours)
+		log.info("mapOfServiceCodesAndDepts: " + mapOfServiceCodesAndDepts);
+		if (iWantSlahours)
 			return mapOfServiceCodesAndSLA;
 		else
 			return mapOfServiceCodesAndDepts;

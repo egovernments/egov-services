48b5f337e663d212d139232b7eb70dc3286daf98
Rafeek <mohammad.rafeek@egovernments.org>
2018-07-05 17:43:44 +0530

    [EGSVC-3291]-throwing exception

--- lams/lams-services/src/main/java/org/egov/lams/repository/NoticeRepository.java
+++ lams/lams-services/src/main/java/org/egov/lams/repository/NoticeRepository.java
@@ -1,5 +1,10 @@
 package org.egov.lams.repository;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.egov.lams.model.Notice;
 import org.egov.lams.model.NoticeCriteria;
 import org.egov.lams.repository.builder.NoticeQueryBuilder;
@@ -10,15 +15,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Repository
 public class NoticeRepository {
@@ -31,10 +30,11 @@ public class NoticeRepository {
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    public Notice createNotice(NoticeRequest noticeRequest) {
 
-        RequestInfo requestInfo = noticeRequest.getRequestInfo();
-        Notice notice = noticeRequest.getNotice();
+	public Notice createNotice(NoticeRequest noticeRequest) {
+
+		RequestInfo requestInfo = noticeRequest.getRequestInfo();
+		Notice notice = noticeRequest.getNotice();
 
 		Object[] obj = new Object[] { notice.getNoticeNo(), notice.getNoticeDate(), notice.getAgreementNumber(),
 				notice.getAssetCategory(), notice.getAcknowledgementNumber(), notice.getAssetNo(),
@@ -43,16 +43,17 @@ public class NoticeRepository {
 				notice.getExpiryDate(), notice.getRent(), notice.getSecurityDeposit(), notice.getCommissionerName(),
 				notice.getZone(), notice.getWard(), notice.getStreet(), notice.getElectionward(), notice.getLocality(),
 				notice.getBlock(), requestInfo.getRequesterId(), new Date(), requestInfo.getRequesterId(), new Date(),
-				notice.getTenantId(), notice.getFileStore() ,notice.getNoticeType()};
+				notice.getTenantId(), notice.getFileStore(), notice.getNoticeType() };
 
-        try {
-            jdbcTemplate.update(NoticeQueryBuilder.INSERT_NOTICE_QUERY, obj);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+		try {
+			jdbcTemplate.update(NoticeQueryBuilder.INSERT_NOTICE_QUERY, obj);
+		} catch (Exception ex) {
+			LOGGER.info("the exception from notice repo query :: " + ex);
+			throw new RuntimeException(ex.getMessage());
+		}
 
-        return notice;
-    }
+		return notice;
+	}
 
     public List<Notice> getNotices(NoticeCriteria noticeCriteria) {
 

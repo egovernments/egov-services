6116b2c5567f08a9f0edcc70983f067207ba33d5
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-07-13 22:00:23 +0530

    Updating mdms build config for fetching data

--- core/egov-mdms-service/start.sh
+++ core/egov-mdms-service/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd /opt && wget "https://codeload.github.com/egovernments/egov-mdms-data/zip/master" -O master.zip && unzip master.zip && rm -rf master.zip mdms && mv egov-mdms-data-master mdms 
+
 if [[ -z "${JAVA_OPTS}" ]];then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi

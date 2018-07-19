8df1ebcc93d27ba89bfd0a39bfe85e844e28accb
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-07-19 17:00:46 +0530

    Adding library path and updating postgres version in build script

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 ci_image=egovio/ci:0.0.4
-db=`sudo docker run -d postgres:9.4`
+db=`sudo docker run -d postgres:9.6`
 echo "Started db container: $db"
 
 pwd=`pwd`
-ci=`sudo docker run -d -v $pwd:/var/lib/egov-services:rw -w /var/lib/egov-services --net=container:${db} ${ci_image} /bin/bash -c "cd $1;mvn clean verify package -s /var/lib/egov-services/settings.xml; cp ../../newrelic/newrelic.* target/."`
+ci=`sudo docker run -d -v $HOME/.m2:/root/.m2 -v $pwd:/var/lib/egov-services:rw -w /var/lib/egov-services --net=container:${db} ${ci_image} /bin/bash -c "cd $1;mvn clean verify package -s /var/lib/egov-services/settings.xml; cp ../../newrelic/newrelic.* target/."`
 echo "Started ci container to run build: $ci"
 
 echo "Attaching to ci container: $ci"

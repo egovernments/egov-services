12707732ea83084d52c4391d50519ea9b3c9e798
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2018-07-31 16:48:13 +0530

    tax head code changed for adhoc rebate estimate

--- rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/EstimationService.java
+++ rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/EstimationService.java
@@ -199,9 +199,9 @@ public class EstimationService {
 		if (null != detail.getAdhocPenalty())
 			estimates.add(TaxHeadEstimate.builder().taxHeadCode(CalculatorConstants.PT_ADHOC_PENALTY)
 					.estimateAmount(detail.getAdhocPenalty()).build());
-		if (null != detail.getAdhocExemption() && detail.getAdhocExemption().compareTo(taxAmt) < 0) {
+		if (null != detail.getAdhocExemption() && detail.getAdhocExemption().compareTo(taxAmt) > 0) {
 			estimates.add(
-					TaxHeadEstimate.builder().taxHeadCode(CalculatorConstants.PT_TAX).estimateAmount(taxAmt).build());
+					TaxHeadEstimate.builder().taxHeadCode(CalculatorConstants.PT_ADHOC_REBATE).estimateAmount(taxAmt).build());
 			payableTax = payableTax.subtract(detail.getAdhocPenalty());
 		} else
 			throw new CustomException(CalculatorConstants.PT_ADHOC_REBATE_INVALID_AMOUNT,

fb6f147509ffca6f5a1eaeee28b750caeb93ee4f
Tarun Lalwani <tarunlalwani@gmail.com>
2018-08-01 15:23:34 +0530

    delete node_modules commited for legal in repo

--- web/ui-web-app/src/development/legal/node_modules/react-display-name/lib/getDisplayName.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-var getDisplayName = function getDisplayName(Component) {
-  return Component.displayName || Component.name || (typeof Component === 'string' ? Component : 'Component');
-};
-
-exports.default = getDisplayName;

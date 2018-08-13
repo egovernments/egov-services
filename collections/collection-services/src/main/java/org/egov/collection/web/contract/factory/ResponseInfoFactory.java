bfb1bbb8600b5f146056c230faf4dfb66a6ea16c
Nithin DV <nithin_1992@msn.com>
2018-08-13 18:46:53 +0530

    Handle NPE in ResponseInfoFactory
    Signed-off-by: Nithin DV <nithin_1992@msn.com>

--- collections/collection-services/src/main/java/org/egov/collection/web/contract/factory/ResponseInfoFactory.java
+++ collections/collection-services/src/main/java/org/egov/collection/web/contract/factory/ResponseInfoFactory.java
@@ -47,13 +47,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class ResponseInfoFactory {
 
-	public ResponseInfo createResponseInfoFromRequestInfo(final RequestInfo requestInfo, final Boolean success) {
+	public static ResponseInfo createResponseInfoFromRequestInfo(final RequestInfo requestInfo, final Boolean success) {
+
 		final String apiId = requestInfo != null ? requestInfo.getApiId() : "";
 		final String ver = requestInfo != null ? requestInfo.getVer() : "";
-        final long ts = requestInfo != null ? requestInfo.getTs() : 0L;
+		Long ts = null;
+		if (requestInfo != null)
+			ts = requestInfo.getTs();
 		final String resMsgId = "uief87324"; // FIXME : Hard-coded
 		final String msgId = requestInfo != null ? requestInfo.getMsgId() : "";
-		final String responseStatus = success ? "successful" : "failed";
-		return new ResponseInfo(apiId, ver, ts, resMsgId, msgId, responseStatus);
+
+
+		return ResponseInfo.builder().apiId(apiId).ver(ver).ts(ts).resMsgId(resMsgId).msgId(msgId).resMsgId(resMsgId)
+				.build();
 	}
 }

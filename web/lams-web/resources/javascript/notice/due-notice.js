a6f2533576d6c0b958072d48cbcd5b45010d950e
vijeeshp <vijeesh.padmanabhan@tarento.com>
2018-08-31 17:58:14 +0530

    EGSVC-3154 Fix for date validation

--- web/lams-web/resources/javascript/notice/due-notice.js
+++ web/lams-web/resources/javascript/notice/due-notice.js
@@ -180,7 +180,7 @@ class DueNotice extends React.Component {
 
 
     doc.fromHTML("Ref: 1. Lease agreement No <b>" + agreement.agreementNumber +"</b> dt <b>" + agreement.commencementDate +"</b>",20,90);
-    doc.fromHTML("2. Roc No........................................dt.......................of this office",29,95);
+    doc.fromHTML("2. Roc No .......................................dt.......................of this office",29,95);
     doc.fromHTML("3. Resolution No.................................dt.......................of Municipal Council/Standing Committee ",29,100);
 
     doc.text(100, 120, "><><><", 'center');

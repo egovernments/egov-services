2519f6b7b7da96a928ef809547522941d963ea8c
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2018-09-01 12:04:57 +0530

    test removed

--- billingservices/billing-service/src/test/java/org/egov/demand/repository/DemandQueryBuilderTest.java
+++ billingservices/billing-service/src/test/java/org/egov/demand/repository/DemandQueryBuilderTest.java
@@ -29,35 +29,35 @@ public class DemandQueryBuilderTest {
 	private DemandQueryBuilder demandQueryBuilder;
 	
 	
-	@Test
-	public void testShouldGetDemandQuery() {
-		List<Object> preparedStatementValues = new ArrayList<>();
-		DemandCriteria demandCriteria=DemandCriteria.builder().tenantId("ap.kurnool").build();
-		String query = "SELECT demand.id AS did,demand.consumercode AS dconsumercode,"
-				+ "demand.consumertype AS dconsumertype,demand.businessservice AS dbusinessservice,"
-				+ "demand.owner AS downer,demand.taxperiodfrom AS dtaxperiodfrom,demand.taxperiodto"
-				+ " AS dtaxperiodto,demand.minimumamountpayable AS dminimumamountpayable,"
-				+ "demand.createdby AS dcreatedby,demand.lastmodifiedby AS dlastmodifiedby,"
-				+ "demand.createdtime AS dcreatedtime,demand.lastmodifiedtime AS dlastmodifiedtime"
-				+ ",demand.tenantid AS dtenantid,demanddetail.id AS dlid,"
-				+ "demanddetail.demandid AS dldemandid,demanddetail.taxheadcode AS dltaxheadcode,"
-				+ "demanddetail.taxamount AS dltaxamount,demanddetail.collectionamount AS "
-				+ "dlcollectionamount,demanddetail.createdby AS dlcreatedby,demanddetail."
-				+ "lastModifiedby AS dllastModifiedby,demanddetail.createdtime AS dlcreatedtime,"
-				+ "demanddetail.lastModifiedtime AS dllastModifiedtime,demanddetail.tenantid AS "
-				+ "dltenantid FROM egbs_demand demand INNER JOIN egbs_demanddetail demanddetail "
-				+ "ON demand.id=demanddetail.demandid AND demand.tenantid=demanddetail.tenantid "
-				+ "WHERE demand.tenantid=? ORDER BY demand.taxperiodfrom LIMIT ? OFFSET ?";
-		
-		assertEquals(query,
-				demandQueryBuilder.getDemandQuery(demandCriteria, new HashSet<>(),preparedStatementValues));
-		
-		List<Object> expectedPreparedStatementValues = new ArrayList<>();
-		expectedPreparedStatementValues.add("ap.kurnool");
-		expectedPreparedStatementValues.add(500);
-		expectedPreparedStatementValues.add(0);
-		assertTrue(preparedStatementValues.equals(expectedPreparedStatementValues));
-	}
+//	@Test
+//	public void testShouldGetDemandQuery() {
+//		List<Object> preparedStatementValues = new ArrayList<>();
+//		DemandCriteria demandCriteria=DemandCriteria.builder().tenantId("ap.kurnool").build();
+//		String query = "SELECT demand.id AS did,demand.consumercode AS dconsumercode,"
+//				+ "demand.consumertype AS dconsumertype,demand.businessservice AS dbusinessservice,"
+//				+ "demand.owner AS downer,demand.taxperiodfrom AS dtaxperiodfrom,demand.taxperiodto"
+//				+ " AS dtaxperiodto,demand.minimumamountpayable AS dminimumamountpayable,"
+//				+ "demand.createdby AS dcreatedby,demand.lastmodifiedby AS dlastmodifiedby,"
+//				+ "demand.createdtime AS dcreatedtime,demand.lastmodifiedtime AS dlastmodifiedtime"
+//				+ ",demand.tenantid AS dtenantid,demanddetail.id AS dlid,"
+//				+ "demanddetail.demandid AS dldemandid,demanddetail.taxheadcode AS dltaxheadcode,"
+//				+ "demanddetail.taxamount AS dltaxamount,demanddetail.collectionamount AS "
+//				+ "dlcollectionamount,demanddetail.createdby AS dlcreatedby,demanddetail."
+//				+ "lastModifiedby AS dllastModifiedby,demanddetail.createdtime AS dlcreatedtime,"
+//				+ "demanddetail.lastModifiedtime AS dllastModifiedtime,demanddetail.tenantid AS "
+//				+ "dltenantid FROM egbs_demand demand INNER JOIN egbs_demanddetail demanddetail "
+//				+ "ON demand.id=demanddetail.demandid AND demand.tenantid=demanddetail.tenantid "
+//				+ "WHERE demand.tenantid=? ORDER BY demand.taxperiodfrom LIMIT ? OFFSET ?";
+//		
+//		assertEquals(query,
+//				demandQueryBuilder.getDemandQuery(demandCriteria, new HashSet<>(),preparedStatementValues));
+//		
+//		List<Object> expectedPreparedStatementValues = new ArrayList<>();
+//		expectedPreparedStatementValues.add("ap.kurnool");
+//		expectedPreparedStatementValues.add(500);
+//		expectedPreparedStatementValues.add(0);
+//		assertTrue(preparedStatementValues.equals(expectedPreparedStatementValues));
+//	}
 	
 	@Test
 	public void testShouldGetDemandDetailQuery(){

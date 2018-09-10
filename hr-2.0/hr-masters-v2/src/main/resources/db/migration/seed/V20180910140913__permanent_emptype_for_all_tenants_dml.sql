390617033f685d004b0f127dea9dfcaab8cc85c3
Vishal Mahuli <vishal.mahuli@tarento.com>
2018-09-10 14:17:03 +0530

    added a new employee type for 5 tenants

---
+++ hr-2.0/hr-masters-v2/src/main/resources/db/migration/seed/V20180910140913__permanent_emptype_for_all_tenants_dml.sql
@@ -0,0 +1,8 @@
+INSERT INTO egeis_employeetype (id, name, chartofaccounts, tenantid) VALUES (nextval('seq_egeis_employeeType'), 'Permanent', NULL, 'pb.amritsar');
+INSERT INTO egeis_employeetype (id, name, chartofaccounts, tenantid) VALUES (nextval('seq_egeis_employeeType'), 'Permanent', NULL, 'pb.jalandhar');
+INSERT INTO egeis_employeetype (id, name, chartofaccounts, tenantid) VALUES (nextval('seq_egeis_employeeType'), 'Permanent', NULL, 'pb.phagwara');
+INSERT INTO egeis_employeetype (id, name, chartofaccounts, tenantid) VALUES (nextval('seq_egeis_employeeType'), 'Permanent', NULL, 'pb.ludhiana');
+INSERT INTO egeis_employeetype (id, name, chartofaccounts, tenantid) VALUES (nextval('seq_egeis_employeeType'), 'Permanent', NULL, 'pb.bathinda');
+INSERT INTO egeis_employeetype (id, name, chartofaccounts, tenantid) VALUES (nextval('seq_egeis_employeeType'), 'Permanent', NULL, 'pb.hoshiarpur');
+
+

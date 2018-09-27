d606bb726d03c648f7ee0f65ab25a6cff1806fcd
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-09-27 21:09:27 +0530

    Adding ulb js config for pb nginx

--- gateway/nginx-pb-uat/start.sh
+++ gateway/nginx-pb-uat/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sed "s/DOMAIN/$DOMAIN/g" /tmp/default_ssl.conf > /etc/nginx/conf.d/default_ssl.conf
+sed "s#ULB_JS_URL#$ULB_JS_URL#g" /tmp/sub_filter.conf > /etc/nginx/conf.d/sub_filter.conf
 echo "$KIBANA_CREDENTIALS" > /etc/nginx/.htpasswd
 nginx -g "daemon off;"

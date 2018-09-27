161036659d8a6cc1bd4d5fdb24703ce005da28ae
Aniket Talele <talele.aniket@gmail.com>
2018-09-27 11:48:16 +0530

    correction in migration TL

--- rainmaker/tl-services/src/main/resources/db/migration/main/V20180926182432__tl_alter_tradelicensedetail_ddl.sql
+++ rainmaker/tl-services/src/main/resources/db/migration/main/V20180926182432__tl_alter_tradelicensedetail_ddl.sql
@@ -8,20 +8,4 @@ ADD COLUMN adhocExemptionReason character varying(1024),
 ADD COLUMN adhocPenaltyReason character varying(1024);
 
 
-CREATE TABLE eg_tl_institution (
-  tenantId character varying(256),
-  id character varying(64),
-  tradelicenseId character varying(64),
-  name character varying(64),
-  type character varying(64),
-  designation character varying(64),
-  createdby character varying(64),
-  createdtime bigint,
-  lastmodifiedby character varying(64),
-  lastmodifiedtime bigint,
 
-  CONSTRAINT pk_eg_pt_institution PRIMARY KEY (id),
-  CONSTRAINT fk_eg_pt_institution FOREIGN KEY (tradelicenseId) REFERENCES eg_tl_TradeLicense (id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE
-);

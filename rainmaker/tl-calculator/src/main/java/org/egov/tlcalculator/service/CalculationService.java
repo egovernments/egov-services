f5cfc1c5a0ff03d5885d3c2bb11bdf4f18cb208f
Aniket Talele <talele.aniket@gmail.com>
2018-10-04 21:59:56 +0530

    correction to adhocPenalty

--- rainmaker/tl-calculator/src/main/java/org/egov/tlcalculator/service/CalculationService.java
+++ rainmaker/tl-calculator/src/main/java/org/egov/tlcalculator/service/CalculationService.java
@@ -96,7 +96,7 @@ public class CalculationService {
       if(license.getTradeLicenseDetail().getAdhocExemption()!=null)
           totalTax = totalTax.subtract(license.getTradeLicenseDetail().getAdhocExemption());
       if(license.getTradeLicenseDetail().getAdhocPenalty()!=null)
-          totalTax = totalTax.subtract(license.getTradeLicenseDetail().getAdhocPenalty());
+          totalTax = totalTax.add(license.getTradeLicenseDetail().getAdhocPenalty());
       estimate.setEstimateAmount(totalTax);
       estimate.setCategory(Category.TAX);
       estimate.setTaxHeadCode(config.getBaseTaxHead());

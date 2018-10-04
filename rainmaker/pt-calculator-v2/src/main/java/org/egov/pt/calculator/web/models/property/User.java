e47ba407158ef9d7d4b81070222a70affbd5160d
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2018-10-04 16:25:20 +0530

    @size added for name

--- rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/web/models/property/User.java
+++ rainmaker/pt-calculator-v2/src/main/java/org/egov/pt/calculator/web/models/property/User.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.springframework.validation.annotation.Validated;
 

f22fcac78a5fc72319bba4f15aa3b13d04f22327
Aniket Talele <talele.aniket@gmail.com>
2018-10-08 15:32:14 +0530

    fix to propertyValidator in TL

--- rainmaker/tl-services/src/main/java/org/egov/tl/validator/PropertyValidator.java
+++ rainmaker/tl-services/src/main/java/org/egov/tl/validator/PropertyValidator.java
@@ -38,22 +38,23 @@ public class PropertyValidator {
         RequestInfo requestInfo = request.getRequestInfo();
 
         request.getLicenses().forEach(license -> {
-            String url = util.getPropertySearchURL();
-            url = url.replace("{1}",license.getTenantId());
-            url = url.replace("{2}",license.getPropertyId());
+            if(license.getPropertyId()!=null) {
+                String url = util.getPropertySearchURL();
+                url = url.replace("{1}", license.getTenantId());
+                url = url.replace("{2}", license.getPropertyId());
 
-            try{
-                Object obj = serviceRequestRepository.fetchResult(new StringBuilder(url),
-                        RequestInfoWrapper.builder().requestInfo(requestInfo).build());
-                HashMap<String, Object> result = (HashMap<String, Object>) obj;
-                String jsonString = new JSONObject(result).toString();
-                DocumentContext documentContext = JsonPath.parse(jsonString);
-                String propertyIdFromSearch = documentContext.read(PROPERTY_JSONPATH);
-                if(propertyIdFromSearch==null)
-                    throw new CustomException("INVALID PROPERTY"," The propertyId "+license.getPropertyId()+" does not exist");
-            }
-            catch (Exception e){
-                throw new CustomException("INVALID PROPERTY"," Failed to parse the response from property search on id "+license.getPropertyId());
+                try {
+                    Object obj = serviceRequestRepository.fetchResult(new StringBuilder(url),
+                            RequestInfoWrapper.builder().requestInfo(requestInfo).build());
+                    HashMap<String, Object> result = (HashMap<String, Object>) obj;
+                    String jsonString = new JSONObject(result).toString();
+                    DocumentContext documentContext = JsonPath.parse(jsonString);
+                    String propertyIdFromSearch = documentContext.read(PROPERTY_JSONPATH);
+                    if (propertyIdFromSearch == null)
+                        throw new CustomException("INVALID PROPERTY", " The propertyId " + license.getPropertyId() + " does not exist");
+                } catch (Exception e) {
+                    throw new CustomException("INVALID PROPERTY", " Failed to parse the response from property search on id " + license.getPropertyId());
+                }
             }
         });
     }

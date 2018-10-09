4f605771e74e1e6b1122f78ddcec46b102a6d9e9
Aniket Talele <talele.aniket@gmail.com>
2018-10-09 20:12:37 +0530

    receipt consumer in TL autowiring correction

--- rainmaker/tl-services/src/main/java/org/egov/tl/consumer/ReceiptConsumer.java
+++ rainmaker/tl-services/src/main/java/org/egov/tl/consumer/ReceiptConsumer.java
@@ -2,10 +2,12 @@ package org.egov.tl.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.egov.tl.service.PaymentUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
@@ -14,8 +16,13 @@ public class ReceiptConsumer {
 
     private PaymentUpdateService paymentUpdateService;
 
+    @Autowired
+    public ReceiptConsumer(PaymentUpdateService paymentUpdateService) {
+        this.paymentUpdateService = paymentUpdateService;
+    }
+
     @KafkaListener(topics = {"${kafka.topics.receipt.create}"})
-    public void listenPayments(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+    public void listenPayments(final HashMap<String, Object> record) {
         paymentUpdateService.process(record);
     }
 }

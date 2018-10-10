115a687bd4001f0710071e12271aba0fe35b59ac
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-10-10 22:02:48 +0530

    Adding condition for startup script subfilter

--- gateway/nginx-pb-uat/start.sh
+++ gateway/nginx-pb-uat/start.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 sed "s/DOMAIN/$DOMAIN/g" /tmp/default_ssl.conf > /etc/nginx/conf.d/default_ssl.conf
-sed "s#ULB_JS_URL#$ULB_JS_URL#g" /tmp/sub_filter.conf > /etc/nginx/conf.d/sub_filter.conf
+if [[ "$SUBFILTER" = "YES" ]]; then
+echo "sub_filter  '<head>' '" >> /etc/nginx/conf.d/sub_filter.conf
+  if [[ -n "$ULB_JS_URL" ]]; then
+    echo "<script src="$ULB_JS_URL" type="text/javascript"></script>" >> /etc/nginx/conf.d/sub_filter.conf
+  fi
+
+  if [[ -n "$TELEMETRY_JS_URL" ]]; then
+    echo "<script src="$TELEMETRY_JS_URL" type="text/javascript"></script>" >> /etc/nginx/conf.d/sub_filter.conf
+  fi
+echo "<"/"head>';" >> /etc/nginx/conf.d/sub_filter.conf
+echo "sub_filter_once on;" >> /etc/nginx/conf.d/sub_filter.conf
+fi
+#sed "s#ULB_JS_URL#$ULB_JS_URL#g";"s#TELEMETRY_JS_URL#$ULB_JS_URL#g" /tmp/sub_filter.conf > /etc/nginx/conf.d/sub_filter.conf
 echo "$KIBANA_CREDENTIALS" > /etc/nginx/.htpasswd
 nginx -g "daemon off;"
--- gateway/nginx/start.sh
+++ gateway/nginx/start.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 acme-client -a https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf -Nnmv $DOMAIN && renew=1
 sed "s/DOMAIN/$DOMAIN/g" /tmp/default_ssl.conf > /etc/nginx/conf.d/default_ssl.conf
+if [[ "$SUBFILTER" = "YES" ]]; then
+echo "sub_filter  '<head>' '" >> /etc/nginx/conf.d/sub_filter.conf
+  if [[ -n "$ULB_JS_URL" ]]; then
+    echo "<script src="$ULB_JS_URL" type="text/javascript"></script>" >> /etc/nginx/conf.d/sub_filter.conf
+  fi
+
+  if [[ -n "$TELEMETRY_JS_URL" ]]; then
+    echo "<script src="$TELEMETRY_JS_URL" type="text/javascript"></script>" >> /etc/nginx/conf.d/sub_filter.conf
+  fi
+echo "<"/"head>';" >> /etc/nginx/conf.d/sub_filter.conf
+echo "sub_filter_once on;" >> /etc/nginx/conf.d/sub_filter.conf
+fi
 nginx -g "daemon off;"

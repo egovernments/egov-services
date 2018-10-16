78fddfac20497c8491990e5443f972847657ab07
venki <venki@egovernments.org>
2018-10-16 08:46:36 +0530

    [PBERP-450] - Fixed script issue of bank account service mapping.

--- collections/collection-services/src/main/resources/db/migration/main/V20181013113120__collection_bank_account_ser_mapping_add_column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE egcl_bankaccountservicemapping ADD COLUMN bank character varying(12) ,ADD COLUMN bankbranch character varying(12);
--- collections/collection-services/src/main/resources/db/migration/main/V20181016084259__collection_bankaccount_service_mapping_corr.sql
+++ collections/collection-services/src/main/resources/db/migration/main/V20181016084259__collection_bankaccount_service_mapping_corr.sql
@@ -1,12 +1,14 @@
-DROP TABLE egcl_bankaccountservicemapping;
+DROP TABLE IF EXISTS egcl_bankaccountservicemapping;
 
-DROP SEQUENCE seq_egcl_bankaccountservicemapping;
+DROP SEQUENCE IF EXISTS seq_egcl_bankaccountservicemapping;
 
 CREATE TABLE egcl_bankaccountservicemapping
 (
   id bigint NOT NULL,
   businessdetails character varying(12) NOT NULL,
   bankaccount character varying(12) NOT NULL,
+  bank character varying(12) ,
+  bankbranch character varying(12),
   active boolean,
   version bigint NOT NULL DEFAULT 1,
   createdby bigint NOT NULL,

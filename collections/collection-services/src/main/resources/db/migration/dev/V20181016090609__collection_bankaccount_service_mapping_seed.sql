7614851b67639cf0c37db13fe22c17589fcb680a
venki <venki@egovernments.org>
2018-10-16 09:07:03 +0530

    [PBERP-450] - Fixed script issue of bank account service mapping.

--- collections/collection-services/src/main/resources/db/migration/dev/V20181016090609__collection_bankaccount_service_mapping_seed.sql
+++ collections/collection-services/src/main/resources/db/migration/dev/V20181016090609__collection_bankaccount_service_mapping_seed.sql
@@ -1,4 +1,2 @@
-delete from egcl_bankaccountservicemapping;
-
 insert into egcl_bankaccountservicemapping(id,businessdetails,bankaccount,bank,bankbranch,active,createdby,lastmodifiedby,tenantid,version)
 values(nextval('seq_egcl_bankaccountservicemapping'),'ATH','000000000001','SBI','BC001',true,1,1,'pb.jalandhar',0);

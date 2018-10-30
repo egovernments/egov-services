e3887e8ba38740016cc3009215b62e7e9e10ffd5
Nithin DV <nithin_1992@msn.com>
2018-10-30 12:11:53 +0530

    Telemetry - Fix IE 11 compatibility

--- core/egov-telemetry/telemetry-client/src/egov-telemetry.js
+++ core/egov-telemetry/telemetry-client/src/egov-telemetry.js
@@ -1,127 +1,154 @@
-import "./array_find";
-global.jQuery = require('jquery');
-import exec from 'script-loader!../vendor/telemetryLibrary.js';
-import TelemetryManager from './telemetry-class'
+global.jQuery = require("jquery");
+import exec from "script-loader!../vendor/telemetryLibrary.js";
+import TelemetryManager from "./telemetry-class";
+
+(function() {
+  if (typeof window.CustomEvent === "function") return false; //If not IE
+
+  function CustomEvent(event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent("CustomEvent");
+    evt.initCustomEvent(
+      event,
+      params.bubbles,
+      params.cancelable,
+      params.detail
+    );
+    return evt;
+  }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
 
 function findParent(element, elementType) {
-    if(element === null) {
-        return undefined;
-    }
-    if(element.nodeName === elementType) {
-        return element;
-    }
-    return findParent(element.parentElement, elementType);
+  if (element === null) {
+    return undefined;
+  }
+  if (element.nodeName === elementType) {
+    return element;
+  }
+  return findParent(element.parentElement, elementType);
 }
-
 
 var telemetryManager = new TelemetryManager();
 
-
 function urlChanged(URL) {
-    telemetryManager.resetTelemetries(URL);
+  telemetryManager.resetTelemetries(URL);
 }
 
 function getFieldKey(inputElement) {
-    let fieldKey = inputElement.id;
-    if(inputElement.type === "radio") {
-        fieldKey = inputElement.name + "-radio";
-    } 
-    return fieldKey;
+  let fieldKey = inputElement.id;
+  if (inputElement.type === "radio") {
+    fieldKey = inputElement.name + "-radio";
+  }
+  return fieldKey;
 }
 
 function getFormKey(inputElement) {
-    let formElement = findParent(inputElement, "FORM");
-    if(formElement) {
-        let formKey = (formElement.id  || "FormKeyNotSpecified") + "-form";
-        return formKey;
-    }
-    return undefined
+  let formElement = findParent(inputElement, "FORM");
+  if (formElement) {
+    let formKey = (formElement.id || "FormKeyNotSpecified") + "-form";
+    return formKey;
+  }
+  return undefined;
 }
 
 function getFormTelemetryObject(formKey) {
-    let formTelemetryObject = telemetryManager.getTelemetryObject(formKey)
-    if(formTelemetryObject === undefined) {
-        formTelemetryObject = telemetryManager.createFormTelemetry(formKey, window.location.href);
-    }
-    return formTelemetryObject;
+  let formTelemetryObject = telemetryManager.getTelemetryObject(formKey);
+  if (formTelemetryObject === undefined) {
+    formTelemetryObject = telemetryManager.createFormTelemetry(
+      formKey,
+      window.location.href
+    );
+  }
+  return formTelemetryObject;
 }
 
 // Field focus and blur events
-window.document.addEventListener("focus", (event) => { 
+window.document.addEventListener(
+  "focus",
+  event => {
     telemetryManager.pauseBackgroundForms();
-    let inputElement = findParent(event.target, "INPUT")
-    if(inputElement) {
-        let fieldKey = getFieldKey(inputElement);
-        let formKey = getFormKey(inputElement);
-        if(formKey) {
-            let formTelemetryObject = getFormTelemetryObject(formKey);
-            formTelemetryObject.fieldInteractionStart(fieldKey);
-        }
+    let inputElement = findParent(event.target, "INPUT");
+    if (inputElement) {
+      let fieldKey = getFieldKey(inputElement);
+      let formKey = getFormKey(inputElement);
+      if (formKey) {
+        let formTelemetryObject = getFormTelemetryObject(formKey);
+        formTelemetryObject.fieldInteractionStart(fieldKey);
+      }
     }
-}, true);
+  },
+  true
+);
 
-window.document.addEventListener("blur", (event) => { 
-    let inputElement = findParent(event.target, "INPUT")
-    if(inputElement) {
-        let fieldKey = getFieldKey(inputElement);
-        let formKey = getFormKey(inputElement);
-        if(formKey) {
-            let formTelemetryObject = telemetryManager.getTelemetryObject(formKey)
-            if(formTelemetryObject) {
-                formTelemetryObject.fieldInteractionEnded(fieldKey, inputElement.value.length);
-            }
+window.document.addEventListener(
+  "blur",
+  event => {
+    let inputElement = findParent(event.target, "INPUT");
+    if (inputElement) {
+      let fieldKey = getFieldKey(inputElement);
+      let formKey = getFormKey(inputElement);
+      if (formKey) {
+        let formTelemetryObject = telemetryManager.getTelemetryObject(formKey);
+        if (formTelemetryObject) {
+          formTelemetryObject.fieldInteractionEnded(
+            fieldKey,
+            inputElement.value.length
+          );
         }
+      }
     }
-}, true);
+  },
+  true
+);
 
-window.addEventListener("submit", (event) => {
-    let formKey = getFormKey(event.target);
-    let formTelemetryObject = telemetryManager.getTelemetryObject(formKey);
-    if(formTelemetryObject) {
-        formTelemetryObject.submit();
-        telemetryManager.deleteTelemetry(formKey);
-    }
+window.addEventListener("submit", event => {
+  let formKey = getFormKey(event.target);
+  let formTelemetryObject = telemetryManager.getTelemetryObject(formKey);
+  if (formTelemetryObject) {
+    formTelemetryObject.submit();
+    telemetryManager.deleteTelemetry(formKey);
+  }
 });
-
 
 // Page load and unload events
-window.addEventListener("load", (event) => {
-    urlChanged(window.location.href);
+window.addEventListener("load", event => {
+  urlChanged(window.location.href);
 });
-window.addEventListener("beforeunload", (event) => {
-    urlChanged();
+window.addEventListener("beforeunload", event => {
+  urlChanged(_, true); //end = true
 });
-
 
 // Window background and foreground event
-window.addEventListener("focus", (event) => { 
-    telemetryManager.resumeAll();
+window.addEventListener("focus", event => {
+  telemetryManager.resumeAll();
 });
-window.addEventListener("blur", (event) => {
-    telemetryManager.pauseAll(); 
+window.addEventListener("blur", event => {
+  telemetryManager.pauseAll();
 });
-
-
 
 //Updated url methods after removing '?.....' and 'hostname'
 
 // handle URL change on pressing back and forward buttons
-window.addEventListener('popstate', function(event) {
-    let fullURL = event.currentTarget.location.href
-    urlChanged(fullURL);
+window.addEventListener("popstate", function(event) {
+  let fullURL = event.currentTarget.location.href;
+  urlChanged(fullURL);
 });
 
 // handle URL change on user clicks inside pages
-(function(history){
-    const pushState = history.pushState;
-    history.pushState = function(state) {
-        if (typeof history.onpushstate === "function") {
-            history.onpushstate({state: state});
-        }
-        var fullURL = arguments[2];
-
-        urlChanged("http://" + window.location.host + fullURL);
-
-        return pushState.apply(history, arguments);
+(function(history) {
+  const pushState = history.pushState;
+  history.pushState = function(state) {
+    if (typeof history.onpushstate === "function") {
+      history.onpushstate({ state: state });
     }
+    var fullURL = arguments[2];
+
+    urlChanged("http://" + window.location.host + fullURL);
+
+    return pushState.apply(history, arguments);
+  };
 })(window.history);
--- core/egov-telemetry/telemetry-client/webpack.config.js
+++ core/egov-telemetry/telemetry-client/webpack.config.js
@@ -1,62 +1,24 @@
-// const path = require('path');
-
-// module.exports = {
-  
-//   entry: './src/egov-telemetry.js',
-//   output: {
-//     filename: 'egov-telemetry-1540795215.js',
-//     path: path.resolve(__dirname, 'build')
-//   },
-//   devtool: 'source-map',
-//   module: {
-//     rules: [
-//       {
-//         test: /\.js?/,
-//         include: '/src',
-//         use: {
-//           loader: 'babel-loader',
-//           options: {
-//             presets: ['es2015', 'react', 'stage-0'],
-//             plugins: [
-//               [
-//                 "transform-runtime",
-//                 {
-//                   "polyfill": false,
-//                   "regenerator": true
-//                 }
-//               ]
-//             ]
-//           }
-//         }
-//       }
-//     ]
-//   }
-// };
-
-
 const path = require("path");
-const webpack = require("webpack");
-const webpack_rules = [];
-const webpackOption = {
-    entry: "./src/egov-telemetry.js",
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "egov-telemetry-1540795215.js",
-    },
-    module: {
-        rules: webpack_rules
-    }
-};
-let babelLoader = {
-    test: /\.js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-        loader: "babel-loader",
-        options: {
-            presets: ["@babel/preset-env"]
-        }
-    }
-};
-webpack_rules.push(babelLoader);
-module.exports = webpackOption;
 
+module.exports = {
+  entry: "./src/egov-telemetry.js",
+  output: {
+    filename: "egov-telemetry-1540294236.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
+      }
+    ]
+  }
+};

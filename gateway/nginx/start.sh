8cb31c910bd1200e928e5ee17104e528a8aa8376
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-10-30 18:54:37 +0530

    Adding asset bucket url to nginx

--- gateway/nginx/start.sh
+++ gateway/nginx/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-acme-client -a https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf -Nnmv $DOMAIN && renew=1
-sed "s/DOMAIN/$DOMAIN/g" /tmp/default_ssl.conf > /etc/nginx/conf.d/default_ssl.conf
+#acme-client -a https://letsencrypt.org/documents/LE-SA-v1.2-November-15-2017.pdf -Nnmv $DOMAIN && renew=1
+sed -e "s/DOMAIN/$DOMAIN/g" -e "s#ASSET_BUCKET_URL#$ASSET_BUCKET_URL#g" /tmp/default_ssl.conf > /etc/nginx/conf.d/default_ssl.conf
 if [[ "$SUBFILTER" = "YES" ]]; then
 echo "sub_filter  '<head>' '" >> /etc/nginx/conf.d/sub_filter.conf
   if [[ -n "$ULB_JS_URL" ]]; then

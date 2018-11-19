911be915df88f297d3cd8d6bd26ba60db2650249
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-11-19 17:13:01 +0530

    Updating egf-account-details-consumer dockerfile

--- financials/egf-account-details-consumer/start.sh
+++ financials/egf-account-details-consumer/start.sh
@@ -8,4 +8,4 @@ if [ x"${JAVA_ENABLE_DEBUG}" != x ] && [ "${JAVA_ENABLE_DEBUG}" != "false" ]; th
     java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-5005}"
 fi
 
-java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS} /opt/egov/egf-account-details-consumer.jar
+java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS} -jar /opt/egov/egf-account-details-consumer.jar

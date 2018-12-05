63af1b372f8401e467268fb68f165ceb3b8f0c1b
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-12-05 17:31:59 +0530

    Updating playground script

--- dev-utils/playground.sh
+++ dev-utils/playground.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pod_name=$(kubectl get pods --namespace=playground | grep -v NAME | awk {'print $1'})
+pod_name=$(kubectl get pods --namespace=playground | grep -v NAME | grep playground | awk {'print $1'})
 kubectl exec -it --namespace=playground ${pod_name} -- bash

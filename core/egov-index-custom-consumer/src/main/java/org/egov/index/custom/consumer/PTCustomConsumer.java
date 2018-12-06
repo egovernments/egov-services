c14e8f6bcfc6b5d707e017176aaeaaca9c6e9e7b
Vishal Mahuli <vishal.mahuli@tarento.com>
2018-12-06 17:42:27 +0530

    producer changed

--- core/egov-index-custom-consumer/src/main/java/org/egov/index/custom/consumer/PTCustomConsumer.java
+++ core/egov-index-custom-consumer/src/main/java/org/egov/index/custom/consumer/PTCustomConsumer.java
@@ -2,26 +2,27 @@ package org.egov.index.custom.consumer;
 
 import java.util.HashMap;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.egov.index.custom.models.pt.PropertyRequest;
+import org.egov.index.custom.service.PTCustomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.listener.MessageListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Service
-@Slf4j
 public class PTCustomConsumer {
+	
+	@Autowired
+	private PTCustomService service;
 
 	@KafkaListener(topics = {"${kafka.topics.pt.update.custom}"})
 
 	public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
-		
+		ObjectMapper mapper = new ObjectMapper();
+		PropertyRequest propertyRequest = mapper.convertValue(record, PropertyRequest.class);
+		service.dataTransformForPTUpdate(propertyRequest);	
 	}
 }

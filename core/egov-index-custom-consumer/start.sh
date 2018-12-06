fcc8983d19f11bc5195181571945b87245e001d4
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-12-06 12:48:27 +0530

    Adding build config for egov-index-custom-consumer

--- core/egov-index-custom-consumer/start.sh
+++ core/egov-index-custom-consumer/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ -z "${JAVA_OPTS}" ]];then
-    export JAVA_OPTS="-Xmx64m -Xms64m"
+    export JAVA_OPTS="-Xmx192m -Xms192m"
 fi
 
 java ${JAVA_OPTS} -jar /opt/egov/egov-indexer.jar

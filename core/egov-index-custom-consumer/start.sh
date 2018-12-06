1020c99e7b3474e1880ce6d6239a84ec483cdea4
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-12-06 15:16:27 +0530

    Updating artifact name in start script

--- core/egov-index-custom-consumer/start.sh
+++ core/egov-index-custom-consumer/start.sh
@@ -4,4 +4,4 @@ if [[ -z "${JAVA_OPTS}" ]];then
     export JAVA_OPTS="-Xmx192m -Xms192m"
 fi
 
-java ${JAVA_OPTS} -jar /opt/egov/egov-indexer.jar
+java ${JAVA_OPTS} -jar /opt/egov/egov-index-custom-consumer.jar

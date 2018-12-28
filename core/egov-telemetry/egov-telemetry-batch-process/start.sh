fbbc0a02fbb60280c28144aca5f28de48bca6c7d
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-12-28 17:01:20 +0530

    Adding dates for batch process

--- core/egov-telemetry/egov-telemetry-batch-process/start.sh
+++ core/egov-telemetry/egov-telemetry-batch-process/start.sh
@@ -2,4 +2,8 @@
  if [ -z "${JAVA_OPTS}" ];then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
+
+DATE=`cat datefile`
+for i in $DATE; do
  java ${JAVA_OPTS} -jar /opt/egov/egov-telemetry-batch-process.jar $DATE
+done

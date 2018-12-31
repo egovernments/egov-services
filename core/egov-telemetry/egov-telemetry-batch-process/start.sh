e86fad71a033ff135cdcda76dde3d5af5adf06c5
sivaprakash123 <sivaprakash.ramasamy@tarento.com>
2018-12-31 12:09:07 +0530

    Updating batch process script for empty variables

--- core/egov-telemetry/egov-telemetry-batch-process/start.sh
+++ core/egov-telemetry/egov-telemetry-batch-process/start.sh
@@ -3,7 +3,11 @@
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
 
+if [ -z $DATE ]; then
+	java ${JAVA_OPTS} -jar /opt/egov/egov-telemetry-batch-process.jar
+else
 for i in $DATE; do
  java ${JAVA_OPTS} -jar /opt/egov/egov-telemetry-batch-process.jar $i
  sleep 30;
 done
+fi

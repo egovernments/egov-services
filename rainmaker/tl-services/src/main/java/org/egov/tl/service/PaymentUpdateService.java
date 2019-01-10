0845e63e6b4d0f5781d14c3a4ade24a9af955291
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2019-01-10 19:41:11 +0530

    cast issue for requestinfo fixed

--- rainmaker/tl-services/src/main/java/org/egov/tl/service/PaymentUpdateService.java
+++ rainmaker/tl-services/src/main/java/org/egov/tl/service/PaymentUpdateService.java
@@ -1,5 +1,6 @@
 package org.egov.tl.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import org.egov.common.contract.request.RequestInfo;
@@ -21,18 +22,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 @Service
 public class PaymentUpdateService {
 
-    private TradeLicenseService tradeLicenseService;
+	private TradeLicenseService tradeLicenseService;
 
-    private TLConfiguration config;
+	private TLConfiguration config;
 
-    private TLRepository repository;
-    
-    private WorkflowIntegrator wfIntegrator;
+	private TLRepository repository;
 
+	private WorkflowIntegrator wfIntegrator;
+
+	@Autowired
+	private ObjectMapper mapper;
 
 	@Autowired
 	public PaymentUpdateService(TradeLicenseService tradeLicenseService, TLConfiguration config,
@@ -43,84 +45,80 @@ public class PaymentUpdateService {
 		this.wfIntegrator = wfIntegrator;
 	}
 
+	final String tenantId = "tenantId";
 
+	final String businessService = "businessService";
 
-    final String tenantId = "tenantId";
+	final String consumerCode = "consumerCode";
 
-    final String businessService = "businessService";
+	/**
+	 * Process the message from kafka and updates the status to paid
+	 * 
+	 * @param record The incoming message from receipt create consumer
+	 */
+	public void process(HashMap<String, Object> record) {
 
-    final String consumerCode = "consumerCode";
+		try {
+			String jsonString = new JSONObject(record).toString();
+			DocumentContext documentContext = JsonPath.parse(jsonString);
+			Map<String, String> valMap = enrichValMap(documentContext);
 
+			Map<String, Object> info = documentContext.read("$.RequestInfo");
+			RequestInfo requestInfo = mapper.convertValue(info, RequestInfo.class);
 
-    /**
-     * Process the message from kafka and updates the status to paid
-     * @param record The incoming message from receipt create consumer
-     */
-    public void process(HashMap<String, Object> record){
+			if (valMap.get(businessService).equalsIgnoreCase(config.getBusinessService())) {
+				TradeLicenseSearchCriteria searchCriteria = new TradeLicenseSearchCriteria();
+				searchCriteria.setTenantId(valMap.get(tenantId));
+				searchCriteria.setApplicationNumber(valMap.get(consumerCode));
+				List<TradeLicense> licenses = tradeLicenseService.getLicensesWithOwnerInfo(searchCriteria, requestInfo);
 
-        try{
-            String jsonString = new JSONObject(record).toString();
-            DocumentContext documentContext = JsonPath.parse(jsonString);
-            Map<String,String> valMap = enrichValMap(documentContext);
+				if (CollectionUtils.isEmpty(licenses))
+					throw new CustomException("INVALID RECEIPT",
+							"No tradeLicense found for the comsumerCode " + searchCriteria.getApplicationNumber());
 
-            if(valMap.get(businessService).equalsIgnoreCase(config.getBusinessService())){
-                RequestInfo requestInfo = new RequestInfo();
-                TradeLicenseSearchCriteria searchCriteria = new TradeLicenseSearchCriteria();
-                searchCriteria.setTenantId(valMap.get(tenantId));
-                searchCriteria.setApplicationNumber(valMap.get(consumerCode));
-                List<TradeLicense> licenses = tradeLicenseService.getLicensesWithOwnerInfo(searchCriteria,requestInfo);
+				licenses.forEach(license -> license.setAction(ActionEnum.PAY));
 
-                if(CollectionUtils.isEmpty(licenses))
-                    throw new CustomException("INVALID RECEIPT","No tradeLicense found for the comsumerCode "+searchCriteria.getApplicationNumber());
+				// FIXME check if the update call to repository can be avoided
+				// FIXME check why aniket is not using request info from consumer
+				// REMOVE SYSTEM HARDCODING AFTER ALTERING THE CONFIG IN WF FOR TL
 
-                
-                licenses.forEach(license -> license.setAction(ActionEnum.PAY));
+				Role role = Role.builder().code("SYSTEM_PAYMENT").build();
+				requestInfo.getUserInfo().getRoles().add(role);
+				TradeLicenseRequest updateRequest = TradeLicenseRequest.builder().requestInfo(requestInfo)
+						.licenses(licenses).build();
 
-                // FIXME check if the update call to repository can be avoided
-                // FIXME check why aniket is not using request info from consumer 
-                // REMOVE SYSTEM HARDCODING AFTER ALTERING THE CONFIG IN WF FOR TL 
-                
-                RequestInfo info = documentContext.read("$.RequestInfo");
-                Role role = Role.builder().code("SYSTEM_PAYMENT").build();
-                info.getUserInfo().getRoles().add(role);
-                TradeLicenseRequest updateRequest = TradeLicenseRequest.builder().requestInfo(info)
-                .licenses(licenses).build();
-               
-                /*
-                 * calling workflow to update status
-                 */
-                wfIntegrator.callWorkFlow(updateRequest);
-                
-                /*
-                 * calling repository to update the object in TL tables
-                 */
-                repository.update(updateRequest);
-            }
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+				/*
+				 * calling workflow to update status
+				 */
+				wfIntegrator.callWorkFlow(updateRequest);
 
+				/*
+				 * calling repository to update the object in TL tables
+				 */
+				repository.update(updateRequest);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Extracts the required fields as map
-     * @param context The documentcontext of the incoming receipt
-     * @return Map containing values of required fields
-     */
-    private Map<String,String> enrichValMap(DocumentContext context){
-        Map<String,String> valMap = new HashMap<>();
-        try{
-            valMap.put(businessService,context.read("$.Receipt[0].Bill[0].billDetails[0].businessService"));
-            valMap.put(consumerCode,context.read("$.Receipt[0].Bill[0].billDetails[0].consumerCode"));
-            valMap.put(tenantId,context.read("$.Receipt[0].tenantId"));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            throw new CustomException("RECEIPT ERROR","Unable to fetch values from receipt");
-        }
-        return valMap;
-    }
-
+	/**
+	 * Extracts the required fields as map
+	 * 
+	 * @param context The documentcontext of the incoming receipt
+	 * @return Map containing values of required fields
+	 */
+	private Map<String, String> enrichValMap(DocumentContext context) {
+		Map<String, String> valMap = new HashMap<>();
+		try {
+			valMap.put(businessService, context.read("$.Receipt[0].Bill[0].billDetails[0].businessService"));
+			valMap.put(consumerCode, context.read("$.Receipt[0].Bill[0].billDetails[0].consumerCode"));
+			valMap.put(tenantId, context.read("$.Receipt[0].tenantId"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new CustomException("RECEIPT ERROR", "Unable to fetch values from receipt");
+		}
+		return valMap;
+	}
 
 }

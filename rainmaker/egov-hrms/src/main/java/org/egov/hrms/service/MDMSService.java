400cc86af3c1a4e9c89244955d85878cc5011823
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-01-29 13:11:48 +0530

    added role master

--- rainmaker/egov-hrms/src/main/java/org/egov/hrms/service/MDMSService.java
+++ rainmaker/egov-hrms/src/main/java/org/egov/hrms/service/MDMSService.java
@@ -45,7 +45,6 @@ public class MDMSService {
 					eachMasterMap = (Map) response.getMdmsRes().get(HRMSConstants.HRMS_MDMS_COMMON_MASTERS_CODE);
 					masterData.put(HRMSConstants.HRMS_MDMS_DEPT_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_DEPT_CODE));
 					masterData.put(HRMSConstants.HRMS_MDMS_DESG_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_DESG_CODE));
-					masterData.put(HRMSConstants.HRMS_MDMS_ROLES_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_ROLES_CODE));
 					masterData.put(HRMSConstants.HRMS_MDMS_YEAR_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_YEAR_CODE));
 				}else if(null != response.getMdmsRes().get(HRMSConstants.HRMS_MDMS_HR_MASTERS_CODE)) {
 					eachMasterMap = (Map) response.getMdmsRes().get(HRMSConstants.HRMS_MDMS_HR_MASTERS_CODE);
@@ -54,6 +53,9 @@ public class MDMSService {
 					masterData.put(HRMSConstants.HRMS_MDMS_QUALIFICATION_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_QUALIFICATION_CODE));
 					masterData.put(HRMSConstants.HRMS_MDMS_SERVICE_STATUS_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_SERVICE_STATUS_CODE));
 					masterData.put(HRMSConstants.HRMS_MDMS_STREAMS_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_STREAMS_CODE));
+				}else if(null != response.getMdmsRes().get(HRMSConstants.HRMS_AC_ROLES_MASTERS_CODE)) {
+					eachMasterMap = (Map) response.getMdmsRes().get(HRMSConstants.HRMS_AC_ROLES_MASTERS_CODE);
+					masterData.put(HRMSConstants.HRMS_MDMS_ROLES_CODE, eachMasterMap.get(HRMSConstants.HRMS_MDMS_ROLES_CODE));
 				}
 			}
 		}
@@ -80,9 +82,11 @@ public class MDMSService {
 		Map<String, List<String>> mapOfModulesAndMasters = new HashMap<>();
 		String[] hrMasters = {HRMSConstants.HRMS_MDMS_EMP_STATUS_CODE, HRMSConstants.HRMS_MDMS_EMP_TYPE_CODE, HRMSConstants.HRMS_MDMS_QUALIFICATION_CODE,
 				HRMSConstants.HRMS_MDMS_SERVICE_STATUS_CODE, HRMSConstants.HRMS_MDMS_STREAMS_CODE};
-		String[] commonMasters = {HRMSConstants.HRMS_MDMS_DEPT_CODE, HRMSConstants.HRMS_MDMS_DESG_CODE, HRMSConstants.HRMS_MDMS_ROLES_CODE, HRMSConstants.HRMS_MDMS_YEAR_CODE};
+		String[] commonMasters = {HRMSConstants.HRMS_MDMS_DEPT_CODE, HRMSConstants.HRMS_MDMS_DESG_CODE, HRMSConstants.HRMS_MDMS_YEAR_CODE};
+		String[] accessControlRoles = {HRMSConstants.HRMS_MDMS_ROLES_CODE};
 		mapOfModulesAndMasters.put(HRMSConstants.HRMS_MDMS_COMMON_MASTERS_CODE, Arrays.asList(commonMasters));
 		mapOfModulesAndMasters.put(HRMSConstants.HRMS_MDMS_HR_MASTERS_CODE, Arrays.asList(hrMasters));
+		mapOfModulesAndMasters.put(HRMSConstants.HRMS_AC_ROLES_MASTERS_CODE, Arrays.asList(accessControlRoles));
 		List<ModuleDetail> moduleDetails = new ArrayList<>();
 		for(String module: mapOfModulesAndMasters.keySet()) {
 			ModuleDetail moduleDetail = new ModuleDetail();

d3ceaa5883f10526b66ae70a2a0631299ed34210
Aniket Talele <talele.aniket@gmail.com>
2019-02-14 14:28:30 +0530

    added indexes in pt

---
+++ rainmaker/pt-services-v2/src/main/resources/db/migration/main/V20190129111734__eg_pt_add_index_ddl.sql
@@ -0,0 +1,3 @@
+CREATE INDEX propertyid_eg_pt_property_v2_idx ON eg_pt_property_v2 (PropertyId,tenantid);
+CREATE INDEX financialYear_eg_pt_propertydetail_v2_idx ON eg_pt_propertydetail_v2 (financialYear);
+CREATE INDEX tenantid_eg_pt_property_v2_idx ON eg_pt_property_v2 (tenantid);

95dd4a16e390674a9456ef436593f3c39cb7ba78
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-02-19 16:58:29 +0530

    changed role name

--- rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/utils/PGRConstants.java
+++ rainmaker/rainmaker-pgr/src/main/java/org/egov/pgr/utils/PGRConstants.java
@@ -120,7 +120,7 @@ public class PGRConstants {
 	public static final String ROLE_NAME_EMPLOYEE = "Employee";
 	public static final String ROLE_NAME_GRO = "Grievance Routing Officer";
 	public static final String ROLE_NAME_DGRO = "Department Grievance Routing Officer";
-	public static final String ROLE_NAME_CSR = "Citizen Service Representative";
+	public static final String ROLE_NAME_CSR = "Customer Support Representative";
 	
 
 	

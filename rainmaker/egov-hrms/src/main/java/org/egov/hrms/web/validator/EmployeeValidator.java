ccce6cebf25946e32513ebe4362b844dbdfd1610
Sudhanshu Deshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-02-20 18:04:54 +0530

    hrms: handled non null values from db for  DOA

--- rainmaker/egov-hrms/src/main/java/org/egov/hrms/web/validator/EmployeeValidator.java
+++ rainmaker/egov-hrms/src/main/java/org/egov/hrms/web/validator/EmployeeValidator.java
@@ -229,11 +229,13 @@ public class EmployeeValidator {
 			errorMap.put(ErrorConstants.HRMS_INVALID_EMP_STATUS_CODE, ErrorConstants.HRMS_INVALID_EMP_STATUS_MSG);
 		if(!mdmsData.get(HRMSConstants.HRMS_MDMS_EMP_TYPE_CODE).contains(employee.getEmployeeType()))
 			errorMap.put(ErrorConstants.HRMS_INVALID_EMP_TYPE_CODE, ErrorConstants.HRMS_INVALID_EMP_TYPE_MSG);
-		if(employee.getDateOfAppointment() > new Date().getTime())
+		if(null != employee.getDateOfAppointment() && employee.getDateOfAppointment() > new Date().getTime())
 			errorMap.put(ErrorConstants.HRMS_INVALID_DATE_OF_APPOINTMENT_CODE, ErrorConstants.HRMS_INVALID_DATE_OF_APPOINTMENT_MSG);
 		if(null != employee.getUser().getDob()) {
 			if(employee.getUser().getDob() >= new Date().getTime())
 				errorMap.put(ErrorConstants.HRMS_INVALID_DOB_CODE, ErrorConstants.HRMS_INVALID_DOB_MSG);
+			if(null != employee.getDateOfAppointment() && employee.getDateOfAppointment() < employee.getUser().getDob())
+				errorMap.put(ErrorConstants.HRMS_INVALID_DATE_OF_APPOINTMENT_DOB_CODE, ErrorConstants.HRMS_INVALID_DATE_OF_APPOINTMENT_DOB_MSG);
 		}
 	}
 	
@@ -279,7 +281,7 @@ public class EmployeeValidator {
 			if(employee.getUser().getDob()!=null )
 				if(assignment.getFromDate() < employee.getUser().getDob() || assignment.getToDate() < employee.getUser().getDob())
                 	errorMap.put(ErrorConstants.HRMS_INVALID_ASSIGNMENT_DATES_CODE, ErrorConstants.HRMS_INVALID_ASSIGNMENT_DATES_MSG);
-			if((assignment.getFromDate() <	 employee.getDateOfAppointment()))
+			if(null != employee.getDateOfAppointment() && assignment.getFromDate() <	 employee.getDateOfAppointment())
 				errorMap.put(ErrorConstants.HRMS_INVALID_ASSIGNMENT_DATES_APPOINTMENT_CODE, ErrorConstants.HRMS_INVALID_ASSIGNMENT_DATES_APPOINTMENT_MSG);
 
         }

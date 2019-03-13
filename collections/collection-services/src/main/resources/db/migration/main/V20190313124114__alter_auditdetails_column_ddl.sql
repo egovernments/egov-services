e3489f923f45b8e7002cf290acd47c7addaf9225
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-03-13 12:43:03 +0530

    modified auditdetail clm type

---
+++ collections/collection-services/src/main/resources/db/migration/main/V20190313124114__alter_auditdetails_column_ddl.sql
@@ -0,0 +1,6 @@
+ALTER TABLE egcl_receiptheader ALTER COLUMN createdby TYPE varchar(256);
+ALTER TABLE egcl_receiptheader ALTER COLUMN lastmodifiedby TYPE varchar(256);
+
+ALTER TABLE egcl_receiptheader_history ALTER COLUMN createdby TYPE varchar(256);
+ALTER TABLE egcl_receiptheader_history ALTER COLUMN lastmodifiedby TYPE varchar(256);
+

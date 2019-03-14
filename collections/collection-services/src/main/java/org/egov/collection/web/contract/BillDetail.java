63c6f1f141ee5e781d44eeaff714bfa39df6a2d5
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-03-14 13:29:32 +0530

    collectiontype not null

--- collections/collection-services/src/main/java/org/egov/collection/web/contract/BillDetail.java
+++ collections/collection-services/src/main/java/org/egov/collection/web/contract/BillDetail.java
@@ -167,6 +167,7 @@ public class BillDetail {
 	  @JsonProperty("status")
 	  private String status = null;
 	  
+	  @NotNull
 	  @JsonProperty("collectionType")
 	  private CollectionType collectionType = null;
 

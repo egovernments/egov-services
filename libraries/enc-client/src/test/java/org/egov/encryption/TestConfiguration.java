ff8bbf3284bd3a84998178f444194ccd0e12440d
rushang7 <rushangdhanesha@gmail.com>
2019-03-19 11:43:52 +0530

    Enc-client : Declare seperate Interface for EncryptionService
    Signed-off-by: rushang7 <rushangdhanesha@gmail.com>

--- libraries/enc-client/src/test/java/org/egov/encryption/TestConfiguration.java
+++ libraries/enc-client/src/test/java/org/egov/encryption/TestConfiguration.java
@@ -2,16 +2,13 @@ package org.egov.encryption;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaTemplate;
-
-import static org.mockito.Mockito.mock;
 
 @Configuration
 public class TestConfiguration {
 
     @Bean
-    public EncryptionService encryptionService() {
-        return new EncryptionService();
+    public EncryptionServiceImpl encryptionService() {
+        return new EncryptionServiceImpl();
     }
 
 }

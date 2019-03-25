5b79cf963746fa7afcf55dc1c3b68c4f5bc9b058
rushang7 <rushangdhanesha@gmail.com>
2019-03-25 11:23:56 +0530

    Report : Enc-Audit - written purpose = Report
    Signed-off-by: rushang7 <rushangdhanesha@gmail.com>

--- reportinfra/report/src/main/java/org/egov/report/service/ReportService.java
+++ reportinfra/report/src/main/java/org/egov/report/service/ReportService.java
@@ -20,7 +20,6 @@ import org.egov.tracer.model.CustomException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -236,7 +235,7 @@ public class ReportService {
 	}
 
 	private void auditDecryptRequest(List<Map<String, Object>> maps, String decryptionPathId, User userInfo) {
-		String purpose = "";
+		String purpose = "Report";
 
 		ObjectNode abacParams = objectMapper.createObjectNode();
 		abacParams.set("key", TextNode.valueOf(decryptionPathId));

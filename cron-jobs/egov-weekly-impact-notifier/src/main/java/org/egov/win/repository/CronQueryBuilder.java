c486b7b14394803dab1037baf01c53bb5673833a
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-04-01 18:20:38 +0530

    email template and data mapping

--- cron-jobs/egov-weekly-impact-notifier/src/main/java/org/egov/win/repository/CronQueryBuilder.java
+++ cron-jobs/egov-weekly-impact-notifier/src/main/java/org/egov/win/repository/CronQueryBuilder.java
@@ -1,5 +1,49 @@
 package org.egov.win.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Repository
+@Slf4j
 public class CronQueryBuilder {
+	
+	public String getQuery(String module) {
+		String query = null;
+		switch(module) {
+		    case "STATEWIDE":
+				query = CronQueries.GET_STATEWIDE_DATA;
+				break;
+			case "PGR":
+				query = CronQueries.GET_PGR_DATA;
+				break;
+			case "PGR_CW":
+				query = CronQueries.GET_PGR_CHANNELWISE_DATA;
+				break;
+			case "PT":
+				query = CronQueries.GET_PT__DATA;
+				break;
+			case "TL":
+				query = CronQueries.GET_TL__DATA;
+				break;
+			default:
+				log.info("No Queries found!");
+				break;
+
+		}
+		return query;
+	}
+	
+	public Map<String, Object> getParameterMap(Long time){
+		Map<String, Object> parameterMap = new HashMap<>();
+		parameterMap.put(":time", time);
+		
+		return parameterMap;
+		
+	}
+	
 
 }
--- cron-jobs/egov-weekly-impact-notifier/src/main/java/org/egov/win/repository/CronRepository.java
+++ cron-jobs/egov-weekly-impact-notifier/src/main/java/org/egov/win/repository/CronRepository.java
@@ -44,5 +44,6 @@ public class CronRepository {
 		return Optional.ofNullable(response);
 		
 	}
-}
 	
+
+}

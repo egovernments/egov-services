11e28e055776d92d4bff1085baf965a2693485d1
muralimgwl <murali.m@goodworklabs.com>
2019-05-03 15:59:35 +0530

    added db connection instance

--- rainmaker/firenoc-services/src/db.js
+++ rainmaker/firenoc-services/src/db.js
@@ -1,5 +1,12 @@
-export default callback => {
-	// connect to a database if needed, then pass it to `callback`:
+const { Client } = require("pg");
 
-	callback();
-}
+export default callback => {
+  // connect to a database if needed, then pass it to `callback`:
+  const pg = new Client({
+    connectionString:process.env.DATABASE_URL,
+    ssl: true
+  });
+
+  pg.connect();
+  callback({pg});
+};

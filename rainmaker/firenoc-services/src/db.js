4afaa10cc0009a36fa5c934cceafe5f2da0965e4
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-05-03 17:49:48 +0530

    firenoc_calc: updated yaml

--- rainmaker/firenoc-services/src/db.js
+++ rainmaker/firenoc-services/src/db.js
@@ -3,10 +3,10 @@ const { Client } = require("pg");
 export default callback => {
   // connect to a database if needed, then pass it to `callback`:
   const pg = new Client({
-    connectionString:process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   });
 
   pg.connect();
-  callback({pg});
+  callback({ pg });
 };

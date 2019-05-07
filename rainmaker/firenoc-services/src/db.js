74996bc238854fc8bc5657dd05de8b325cd5dcfd
muralimgwl <murali.m@goodworklabs.com>
2019-05-07 13:31:30 +0530

    updated yaml and db config

--- rainmaker/firenoc-services/src/db.js
+++ rainmaker/firenoc-services/src/db.js
@@ -3,7 +3,8 @@ const { Client } = require("pg");
 export default callback => {
   // connect to a database if needed, then pass it to `callback`:
   const pg = new Client({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://thpqnqhvqfbvqw:46ad4cd2bd2d8d2f3a6cb567482b6c703473dc0eaf150baea5a92909131302b5@ec2-54-225-68-133.compute-1.amazonaws.com:5432/d9jf2v9doprlot',
+    // process.env.DATABASE_URL,
     ssl: true
   });
 

53a467694cff75b2ace477d09ef81f8c686aaf62
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-05-08 13:17:35 +0530

    added reqinfo to resinfo util function

---
+++ rainmaker/firenoc-calculator/src/utils/index.js
@@ -0,0 +1,20 @@
+export const requestInfoToResponseInfo = (requestinfo, success) => {
+  let ResponseInfo = {
+    apiId: "",
+    ver: "",
+    ts: 0,
+    resMsgId: "",
+    msgId: "",
+    status: ""
+  };
+  ResponseInfo.apiId =
+    requestinfo && requestinfo.apiId ? requestinfo.apiId : "";
+  ResponseInfo.ver = requestinfo && requestinfo.ver ? requestinfo.ver : "";
+  ResponseInfo.ts = requestinfo && requestinfo.ts ? requestinfo.ts : null;
+  ResponseInfo.resMsgId = "uief87324";
+  ResponseInfo.msgId =
+    requestinfo && requestinfo.msgId ? requestinfo.msgId : "";
+  ResponseInfo.status = success ? "successful" : "failed";
+
+  return ResponseInfo;
+};

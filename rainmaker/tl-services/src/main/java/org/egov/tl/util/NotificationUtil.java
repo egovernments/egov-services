318b502d7587d0f7cac68571bb8e6289d462cbba
Aniket Talele <talele.aniket@gmail.com>
2019-05-09 13:02:14 +0530

    correction to fetching locale from requestInfo

--- rainmaker/tl-services/src/main/java/org/egov/tl/util/NotificationUtil.java
+++ rainmaker/tl-services/src/main/java/org/egov/tl/util/NotificationUtil.java
@@ -135,7 +135,7 @@ public class NotificationUtil {
 
         String locale = NOTIFICATION_LOCALE;
         if(!StringUtils.isEmpty(requestInfo.getMsgId()) && requestInfo.getMsgId().split("|").length>=2)
-            locale = requestInfo.getMsgId().split("|")[1];
+            locale = requestInfo.getMsgId().split("\\|")[1];
 
         StringBuilder uri = new StringBuilder();
         uri.append(config.getLocalizationHost()).append(config.getLocalizationContextPath())

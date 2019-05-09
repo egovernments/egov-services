097def2c6032462c468154d1a3cc1a1d2fa4af08
Shreya <shreya.b@egovernments.org>
2019-05-09 16:31:55 +0530

    Updated search API

--- rainmaker/firenoc-services/src/api/search.js
+++ rainmaker/firenoc-services/src/api/search.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { mergeSearchResults } from "../utils";
 
 export default ({ config, db }) => {
   let api = Router();
@@ -14,16 +15,25 @@ export default ({ config, db }) => {
       },
       FireNOCs: []
     };
-    // "FireNOCs": [
-    const text =
-      "SELECT * FROM eg_fn_firenoc FN JOIN eg_fn_firenocdetail FD ON (FN.uuid = FD.firenocuuid) JOIN eg_fn_owner FO ON (FD.uuid = FO.firenocdetailsuuid) where";
+    const actions = {
+      INITIATED: "INITIATE",
+      APPROVED: "APPROVE"
+    };
     const queryObj = request.query;
+    queryObj.action = actions[queryObj.action];
+
+    const text = `SELECT FN.uuid as FID,FN.tenantid,FN.fireNOCNumber,FN.dateOfApplied,FD.uuid as firenocdetailsid,FD.applicationnumber,FD.fireNOCType,FD.applicationdate,FD.financialYear,FD.issuedDate,FD.validFrom,FD.validTo,FD.action,FD.channel,FB.uuid as buildingid ,FB.name,FB.noOfFloors,FB.noOfBasements,FO.uuid as ownerid,FO.ownertype,FO.relationship FROM eg_fn_firenoc FN JOIN eg_fn_firenocdetail FD ON (FN.uuid = FD.firenocuuid) JOIN eg_fn_owner FO ON (FD.uuid = FO.firenocdetailsuuid) JOIN eg_fn_buidlings FB ON (FD.uuid = FB.firenocdetailsuuid) where FN.tenantid = '${
+      queryObj.tenantId
+    }' AND`;
+
     const queryKeys = Object.keys(queryObj);
     let sqlQuery = text;
+
     if (queryKeys) {
       queryKeys.forEach(item => {
-        if (item != "fromDate" && item != "toDate")
+        if (item != "fromDate" && item != "toDate" && item != "tenantId") {
           sqlQuery = `${sqlQuery} ${item}='${queryObj[item]}' AND`;
+        }
       });
     }
     if (
@@ -32,22 +42,26 @@ export default ({ config, db }) => {
     ) {
       sqlQuery = `${sqlQuery} FN.createdtime >= ${
         queryObj.fromDate
-      } AND FN.lastmodifiedtime <= ${queryObj.toDate}`;
+      } AND FN.lastmodifiedtime <= ${queryObj.toDate} ORDER BY FN.uuid`;
     } else if (
       queryObj.hasOwnProperty("fromDate") &&
       !queryObj.hasOwnProperty("toDate")
     ) {
-      sqlQuery = `${sqlQuery} FN.createdtime >= ${queryObj.fromDate}`;
+      sqlQuery = `${sqlQuery} FN.createdtime >= ${
+        queryObj.fromDate
+      } ORDER BY FN.uuid`;
     } else {
       console.log("Select From Date");
-      sqlQuery = sqlQuery.substring(0, sqlQuery.length - 3);
+      sqlQuery = `${sqlQuery.substring(
+        0,
+        sqlQuery.length - 3
+      )} ORDER BY FN.uuid`;
     }
     db.query(sqlQuery, (err, res) => {
       if (err) {
         console.log(err.stack);
       } else {
-        console.log(res.rows[0]);
-        response.FireNOCs = res.rows;
+        response.FireNOCs = mergeSearchResults(res.rows);
         apiRes.json(response);
       }
     });
--- rainmaker/firenoc-services/src/utils/index.js
+++ rainmaker/firenoc-services/src/utils/index.js
@@ -1,3 +1,5 @@
+import uniqBy from "lodash/uniqBy";
+
 export const requestInfoToResponseInfo = (requestinfo, success) => {
   let ResponseInfo = {
     apiId: "",
@@ -17,4 +19,84 @@ export const requestInfoToResponseInfo = (requestinfo, success) => {
   ResponseInfo.status = success ? "successful" : "failed";
 
   return ResponseInfo;
+};
+
+export const mergeSearchResults = response => {
+  const responseArray = [];
+  response.forEach(element => {
+    const buildings = [];
+    const owners = [];
+    if (responseArray.findIndex(item => item.id === element.FID) === -1) {
+      const fireNocByIDArray = response.filter(obj => obj.id === element.FID);
+      fireNocByIDArray.forEach((eachItem, index) => {
+        buildings.push({
+          id: eachItem.buildingid,
+          tenantId: eachItem.buildingtenantid,
+          name: eachItem.buildingname,
+          usageType: eachItem.usagetype,
+          noOfFloors: eachItem.nooffloors,
+          noOfBasements: eachItem.noofbasements,
+          plotsize: eachItem.plotsize,
+          builtupArea: eachItem.builtuparea,
+          heightOfBuilding: eachItem.heightOfBuilding,
+          applicationDocuments: []
+        });
+        owners.push({
+          id: eachItem.ownerid,
+          userName: eachItem.username,
+          password: "",
+          salutation: "",
+          name: "",
+          gender: "",
+          mobileNumber: "",
+          emailId: "",
+          altContactNumber: "",
+          pan: "",
+          aadhaarNumber: "",
+          permanentAddress: "",
+          permanentCity: "",
+          permanentPincode: "",
+          correspondenceCity: "",
+          correspondencePincode: "",
+          correspondenceAddress: "",
+          active: eachItem.active,
+          dob: eachItem.dob,
+          ownerType: eachItem.ownertype,
+          relationship: eachItem.relationship,
+          tenantId: eachItem.tenantId,
+          fatherOrHusbandName: ""
+        });
+
+        if (index === fireNocByIDArray.length - 1) {
+          responseArray.push({
+            ...element,
+            fireNOCDetails: {
+              id: element.firenocdetailsid,
+              applicationNumberresponseArray: element.applicationNumber,
+              fireNOCType: element.firenoctype,
+              firestationId: "",
+              applicationDate: element.applicationdate,
+              financialYear: element.financialyear,
+              issuedDate: element.issueddate,
+              validFrom: element.validfrom,
+              validTo: element.validto,
+              action: element.action,
+              channel: element.channel,
+              noOfBuildings: "",
+              buildings: buildings && uniqBy(buildings, "id"),
+              propertyDetails: {},
+              applicantDetails: {
+                ownerShipType: element.ownerType,
+                owners: owners && uniqBy(owners, "id"),
+                additionalDetail: {}
+              },
+              additionalDetail: {},
+              auditDetails: {}
+            }
+          });
+        }
+      });
+    }
+  });
+  return responseArray;
 };

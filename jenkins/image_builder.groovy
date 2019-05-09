4db5d49be16a104ddd38b15bc8eebfd81cb97496
kavi_elrey@1993 <25226238+kaviyarasan1993@users.noreply.github.com>
2019-05-09 17:19:32 +0530

    test 7

--- jenkins/image_builder.groovy
+++ jenkins/image_builder.groovy
@@ -1,15 +1,13 @@
 def build(module_name, service_name, commit_id){    stage("Build docker image") {
 
     build_image("${module_name}", "egovio/${service_name}", commit_id)
-    String[] migration_locs = [ "${module_name}/src/main/resources/db/Dockerfile", "${module_name}/migration/Dockerfile"]    
+    String[] migration_locs = [ "${module_name}/src/main/resources/db", "${module_name}/migration"]    
     
-    for(int i in migration_locs){
+    for(i=0; i<migration_locs.length; i++){
 
-        println(" in loop : " + i)
-        def loc = migration_locs[i]
+        def loc = migration_locs[i] + "/Dockerfile"
         println(" the loc : " + loc)
-        def file_exists = fileExists loc
-        if(file_exists)
+        if(fileExists loc)
             build_image( migration_locs[i], "egovio/${service_name}-db", commit_id)
     }
     }

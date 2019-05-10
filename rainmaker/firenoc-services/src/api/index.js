efed7e5cf886758f314a6755c1a05cf34303fe28
Nithin <nithin_1992@msn.com>
2019-05-10 22:59:41 +0530

    Update urls

--- rainmaker/firenoc-services/src/api/index.js
+++ rainmaker/firenoc-services/src/api/index.js
@@ -8,9 +8,9 @@ import update from "./update";
 export default ({ config, db }) => {
 	let api = Router();
 
-	api.use('/firenoc-service/v1',create({config,db}));
-	api.use('/firenoc-service/v1',search({config,db}));
-	api.use('/firenoc-service/v1',update({config,db}));
+	api.use('/firenoc-services/v1',create({config,db}));
+	api.use('/firenoc-services/v1',search({config,db}));
+	api.use('/firenoc-services/v1',update({config,db}));
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version});

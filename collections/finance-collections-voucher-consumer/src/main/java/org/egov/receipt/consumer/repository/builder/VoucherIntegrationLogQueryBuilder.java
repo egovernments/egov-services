866063080df345725292740077086f97d1b30fab
atique2992 <mohammad.atiqurrahaman@walkingtree.in>
2019-05-16 12:55:13 +0530

    Removing the uses of PGObject class

--- collections/finance-collections-voucher-consumer/src/main/java/org/egov/receipt/consumer/repository/builder/VoucherIntegrationLogQueryBuilder.java
+++ collections/finance-collections-voucher-consumer/src/main/java/org/egov/receipt/consumer/repository/builder/VoucherIntegrationLogQueryBuilder.java
@@ -1,17 +1,14 @@
 package org.egov.receipt.consumer.repository.builder;
 
-import java.sql.SQLException;
-import java.util.Objects;
 import java.util.UUID;
 
 import org.egov.receipt.consumer.entity.VoucherIntegrationLog;
-import org.postgresql.util.PGobject;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 
 public class VoucherIntegrationLogQueryBuilder {
 	
 	public static final String INSERT_VOUCHER_LOG_SQL = "INSERT INTO egf_voucher_integration_log(id, referencenumber, status, vouchernumber, TYPE, description, requestjson, tenantid)"
-			+ "VALUES (:id, :referencenumber, :status, :vouchernumber, :TYPE, :description, :requestjson, :tenantid)";
+			+ "VALUES (:id, :referencenumber, :status, :vouchernumber, :TYPE, :description, cast(:requestjson as jsonb), :tenantid)";
 	
 	public static MapSqlParameterSource getParametersForVoucherIntegartionLog(VoucherIntegrationLog log){
 		MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource();
@@ -21,23 +18,8 @@ public class VoucherIntegrationLogQueryBuilder {
 		sqlParameterSource.addValue("vouchernumber", log.getVoucherNumber());
 		sqlParameterSource.addValue("TYPE", log.getType());
 		sqlParameterSource.addValue("description", log.getDescription());
-		sqlParameterSource.addValue("requestjson", getJsonb(log.getRequestJson()));
+		sqlParameterSource.addValue("requestjson", log.getRequestJson());
 		sqlParameterSource.addValue("tenantid", log.getTenantId());
 		return sqlParameterSource;
 	}
-	
-	private static PGobject getJsonb(String node) {
-        if (Objects.isNull(node))
-            return null;
-
-        PGobject pgObject = new PGobject();
-        pgObject.setType("jsonb");
-        try {
-            pgObject.setValue(node);
-            return pgObject;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }

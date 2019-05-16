532cffb6f8ceac603c539c5ed14bde6c45217e19
Nithin <nithin_1992@msn.com>
2019-05-16 23:53:43 +0530

    Update kafka producer configs for higher throughput

--- rainmaker/pt-services-v2/src/main/java/org/egov/pt/config/KafkaConfigBatch.java
+++ rainmaker/pt-services-v2/src/main/java/org/egov/pt/config/KafkaConfigBatch.java
@@ -1,13 +1,13 @@
 package org.egov.pt.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.AbstractMessageListenerContainer;
 
 import java.util.HashMap;
@@ -47,6 +47,29 @@ public class KafkaConfigBatch {
         factory.getContainerProperties().setAckMode(AbstractMessageListenerContainer.AckMode.BATCH);
 
         return factory;
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                org.apache.kafka.common.serialization.StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                org.springframework.kafka.support.serializer.JsonSerializer.class);
+        props.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 2080075);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 500);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 1000);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> kafkaProducerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(kafkaProducerFactory());
     }
 
 }

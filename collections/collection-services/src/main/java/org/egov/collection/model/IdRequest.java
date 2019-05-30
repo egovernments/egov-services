3bb29ae49e332c089c3916c3bd0eb3e6ae1f7397
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-05-30 16:01:22 +0530

    minor changes

--- collections/collection-services/src/main/java/org/egov/collection/model/IdRequest.java
+++ collections/collection-services/src/main/java/org/egov/collection/model/IdRequest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class IdRequest {
 
     @JsonProperty("idName")

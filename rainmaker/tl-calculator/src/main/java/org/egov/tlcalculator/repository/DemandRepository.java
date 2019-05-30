528697a7e169699cee75dfd122a48fa60ce4a376
kavi_elrey@1993 <25226238+kaviyarasan1993@users.noreply.github.com>
2019-05-30 19:03:47 +0530

    Tl billing integration (#267)
    * Initial Commit
    
    * synched TL Billing and Collection object with v1
    
    * added CollectionType enum in TL and made changes to demand search
    
    * code refactor
    
    * correction to demand search
    
    * correction to demand search-2
    
    * changed billing v1 endpoint
    
    * correction to tl calculation during forward
    
    * added estimate flag in calculate
    
    * Revert "added estimate flag in calculate"
    
    This reverts commit cf5f46c2fd25d4dc70cc62151b4a42f056f611f9.

--- rainmaker/tl-calculator/src/main/java/org/egov/tlcalculator/repository/DemandRepository.java
+++ rainmaker/tl-calculator/src/main/java/org/egov/tlcalculator/repository/DemandRepository.java
@@ -3,7 +3,6 @@ package org.egov.tlcalculator.repository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.tlcalculator.config.TLCalculatorConfigs;
-import org.egov.tlcalculator.web.models.Calculation;
 import org.egov.tlcalculator.web.models.demand.Demand;
 import org.egov.tlcalculator.web.models.demand.DemandRequest;
 import org.egov.tlcalculator.web.models.demand.DemandResponse;
@@ -11,7 +10,6 @@ import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
 import java.util.List;
 
 
@@ -35,7 +33,7 @@ public class DemandRepository {
      * @param demands The demands to be created
      * @return The list of demand created
      */
-    public List<Demand> saveDemand(RequestInfo requestInfo,List<Demand> demands){
+    public List<Demand> saveDemand(RequestInfo requestInfo, List<Demand> demands){
         StringBuilder url = new StringBuilder(config.getBillingHost());
         url.append(config.getDemandCreateEndpoint());
         DemandRequest request = new DemandRequest(requestInfo,demands);

47de9ce20996df37c4d271f3efca96ee4eb1ad6d
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-06-11 16:49:51 +0530

    mdms integration for business service

--- collections/collection-services/src/main/java/org/egov/collection/notification/consumer/NotificationConsumer.java
+++ collections/collection-services/src/main/java/org/egov/collection/notification/consumer/NotificationConsumer.java
@@ -13,12 +13,17 @@ import org.egov.collection.web.contract.BillDetail;
 import org.egov.collection.web.contract.Receipt;
 import org.egov.collection.web.contract.ReceiptReq;
 import org.egov.common.contract.request.RequestInfo;
+import org.egov.mdms.model.MasterDetail;
+import org.egov.mdms.model.MdmsCriteria;
+import org.egov.mdms.model.MdmsCriteriaReq;
+import org.egov.mdms.model.ModuleDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,6 +36,15 @@ import net.logstash.logback.encoder.org.apache.commons.lang.StringUtils;
 @Service
 @Slf4j
 @Data
+/**
+ * Methods with different responsibilites are written in the same class because we're not sure on where this consumer has to go.
+ * Ideally, we will have one consumer that handles notifications on every payment belonging to all the modules. 
+ * The reason for having all methods in the same class is to make it easy for migration when the common-consumer design is finalized.
+ * 
+ *  
+ * @author vishal
+ *
+ */
 public class NotificationConsumer {
 	
 	@Value("${coll.notification.ui.host}")
@@ -38,6 +52,13 @@ public class NotificationConsumer {
 	
 	@Value("${coll.notification.ui.redirect.url}")
 	private String uiRedirectUrl;
+	
+	
+	@Value("${egov.mdms.host}")
+	private String mdmsHost;
+	
+	@Value("${egov.mdms.search.endpoint}")
+	private String mdmsUrl;
 	
 	@Value("${egov.localization.host}")
 	private String localizationHost;
@@ -65,15 +86,24 @@ public class NotificationConsumer {
 	
 	private static final String COLLECTION_LOCALIZATION_MODULE = "collection-services";
 	public static final String PAYMENT_MSG_LOCALIZATION_CODE = "coll.notif.payment.receipt.link";
-
 	private static final String BUSINESSSERVICE_LOCALIZATION_MODULE = "rainmaker-uc";
 	public static final String BUSINESSSERVICELOCALIZATION_CODE_PREFIX = "BILLINGSERVICE_BUSINESSSERVICE_";
-
-	
 	public static final String LOCALIZATION_CODES_JSONPATH = "$.messages.*.code";
 	public static final String LOCALIZATION_MSGS_JSONPATH = "$.messages.*.message";
-
 	
+	private static final String BUSINESSSERVICE_MDMS_MODULE = "BillingService";
+	public static final String BUSINESSSERVICE_MDMS_MASTER = "BusinessService";
+	public static final String BUSINESSSERVICE_CODES_FILTER = "$.*.code";
+	public static final String BUSINESSSERVICE_CODES_JSONPATH = "$.MdmsRes.BillingService.BusinessService";
+	
+	
+
+	/**
+	 * Kafka consumer
+	 * 
+	 * @param record
+	 * @param topic
+	 */
     @KafkaListener(topics = { "${kafka.topics.payment.receiptlink.name}" })
 	public void listen(HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
 		try {
@@ -84,27 +114,50 @@ public class NotificationConsumer {
 		}
 	}
 	
-	
-	private void sendNotification(ReceiptReq receiptReq) {
+	/**
+	 * Method to send notifications.
+	 * 
+	 * @param receiptReq
+	 * @throws Exception 
+	 */
+	private void sendNotification(ReceiptReq receiptReq){
 		Receipt receipt = receiptReq.getReceipt().get(0); //currently we dont support mutliple receipts.
-		for(Bill bill: receipt.getBill()) {
-			for(BillDetail detail: bill.getBillDetails()) {
-				String phNo = bill.getMobileNumber();
-				String message = buildSmsBody(receipt.getInstrument(), bill, detail, receiptReq.getRequestInfo());
-				if(!StringUtils.isEmpty(message)) {
-					Map<String, Object> request = new HashMap<>();
-					request.put("mobileNumber", phNo);
-					request.put("message", message);
-					
-					producer.producer(smsTopic, smsTopickey, request);
-				}else {
-					log.error("No message configured!");
+		List<String> businessServiceAllowed = fetchBusinessServiceFromMDMS(receiptReq.getRequestInfo());
+		if(!CollectionUtils.isEmpty(businessServiceAllowed)) {
+			for(Bill bill: receipt.getBill()) {
+				for(BillDetail detail: bill.getBillDetails()) {
+					if(businessServiceAllowed.contains(detail.getBusinessService())) {
+						String phNo = bill.getMobileNumber();
+						String message = buildSmsBody(receipt.getInstrument(), bill, detail, receiptReq.getRequestInfo());
+						if(!StringUtils.isEmpty(message)) {
+							Map<String, Object> request = new HashMap<>();
+							request.put("mobileNumber", phNo);
+							request.put("message", message);
+							
+							producer.producer(smsTopic, smsTopickey, request);
+						}else {
+							log.error("No message configured! Notification will not be sent.");
+						}	
+					}else {
+						log.info("Notification not configured for this business service!");
+					}
 				}
 			}
+		}else {
+			log.info("Business services to which notifs are to be sent, couldn't be retrieved! Notification will not be sent.");
 		}
-		
 	}
 	
+	
+	/**
+	 * Prepares sms body based on the configuration
+	 * 
+	 * @param instrument
+	 * @param bill
+	 * @param billDetail
+	 * @param requestInfo
+	 * @return
+	 */
 	private String buildSmsBody(Instrument instrument, Bill bill, BillDetail billDetail, RequestInfo requestInfo) {
 		String content = fetchContentFromLocalization(requestInfo, billDetail.getTenantId(), COLLECTION_LOCALIZATION_MODULE, PAYMENT_MSG_LOCALIZATION_CODE);
 		String message = null;
@@ -114,8 +167,8 @@ public class NotificationConsumer {
 					.append(uiRedirectUrl).append("&params=").append(billDetail.getTenantId() + "," + billDetail.getReceiptNumber());
 			
 			content = content.replaceAll("<rcpt_link>", link.toString());
-			String code = BUSINESSSERVICELOCALIZATION_CODE_PREFIX + formatCodes(billDetail.getBusinessService());
-			String taxName = fetchContentFromLocalization(requestInfo, billDetail.getTenantId(), BUSINESSSERVICE_LOCALIZATION_MODULE, code);
+			String taxName = fetchContentFromLocalization(requestInfo, billDetail.getTenantId(), 
+					BUSINESSSERVICE_LOCALIZATION_MODULE, formatCodes(billDetail.getBusinessService()));
 			if(StringUtils.isEmpty(taxName))
 				taxName = "Adhoc Tax";
 			content = content.replaceAll("<tax_name>", taxName);
@@ -128,6 +181,15 @@ public class NotificationConsumer {
 		return message;
 	}
 	
+	/**
+	 * Generic method to fetch data from localization.
+	 * 
+	 * @param requestInfo
+	 * @param tenantId
+	 * @param module
+	 * @param code
+	 * @return
+	 */
 	private String fetchContentFromLocalization(RequestInfo requestInfo, String tenantId, String module, String code) {
 		String message = null;
 		List<String> codes = new ArrayList<>();
@@ -157,6 +219,55 @@ public class NotificationConsumer {
 	}
 	
 	
+	/**
+	 * Method to fetch business service from MDMS
+	 * 
+	 * @param requestInfo
+	 * @return
+	 */
+	private List<String> fetchBusinessServiceFromMDMS(RequestInfo requestInfo){
+		List<String> masterData = new ArrayList<>();
+		StringBuilder uri = new StringBuilder();
+		uri.append(mdmsHost).append(mdmsUrl);
+		MdmsCriteriaReq request = getRequestForEvents(requestInfo, "pb");
+		try {
+			Object response = restTemplate.postForObject(uri.toString(), request, Map.class);
+			masterData = JsonPath.read(response, BUSINESSSERVICE_CODES_JSONPATH);
+		}catch(Exception e) {
+			log.error("Exception while fetching business service codes: ",e);
+		}
+		
+		return masterData;
+	}
+	
+	/**
+	 * Util method to build MDMS request to search businessservices
+	 * 
+	 * @param requestInfo
+	 * @param tenantId
+	 * @return
+	 */
+	private MdmsCriteriaReq getRequestForEvents(RequestInfo requestInfo, String tenantId) {
+		MasterDetail masterDetail = org.egov.mdms.model.MasterDetail.builder()
+				.name(BUSINESSSERVICE_MDMS_MASTER).filter(BUSINESSSERVICE_CODES_FILTER).build();
+		List<MasterDetail> masterDetails = new ArrayList<>();
+		masterDetails.add(masterDetail);
+		ModuleDetail moduleDetail = ModuleDetail.builder().moduleName(BUSINESSSERVICE_MDMS_MODULE)
+				.masterDetails(masterDetails).build();
+		List<ModuleDetail> moduleDetails = new ArrayList<>();
+		moduleDetails.add(moduleDetail);
+		MdmsCriteria mdmsCriteria = MdmsCriteria.builder().tenantId(tenantId).moduleDetails(moduleDetails).build();
+		return MdmsCriteriaReq.builder().requestInfo(requestInfo).mdmsCriteria(mdmsCriteria).build();
+	}
+	
+	
+	/**
+	 * Method to fetch year format from from and to period of the bill
+	 * 
+	 * @param fromPeriod
+	 * @param toPeriod
+	 * @return
+	 */
 	private String fetchFinYear(Long fromPeriod, Long toPeriod) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis(fromPeriod);
@@ -169,12 +280,18 @@ public class NotificationConsumer {
 
 	}
 	
+	/**
+	 * Method to format the code as per localization keys
+	 * 
+	 * @param code
+	 * @return
+	 */
 	private String formatCodes(String code) {
 		String regexForSpecialCharacters = "[$&+,:;=?@#|'<>.-^*()%!]";
 		code = code.replaceAll(regexForSpecialCharacters, "_");
 		code = code.replaceAll(" ", "_");
 		
-		return code.toUpperCase();
+		return BUSINESSSERVICELOCALIZATION_CODE_PREFIX + code.toUpperCase();
 	}
 	
 

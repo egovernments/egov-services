4145e0e81fb62196fed52f632e577e0f3b3f3b0e
sudhanshudeshmukh <sudhanshu.s.deshmukh@gmail.com>
2019-06-11 14:37:26 +0530

    fn calc added error handler middleware

--- rainmaker/firenoc-calculator/src/controller/index.js
+++ rainmaker/firenoc-calculator/src/controller/index.js
@@ -5,15 +5,29 @@ import create from "./create";
 import search from "./search";
 import calculate from "./calculate";
 import getbill from "./getbill";
+const asyncHandler = require("express-async-handler");
 
 export default pool => {
   let api = Router();
 
-  api.post("/billingslab/_create", (req, res) => create(req, res));
-  api.post("/billingslab/_search", (req, res) => search(req, res, pool));
-  api.post("/billingslab/_update", (req, res) => update(req, res));
-  api.post("/v1/_calculate", (req, res) => calculate(req, res, pool));
-  api.post("/v1/_getbill", (req, res) => getbill(req, res));
+  api.post(
+    "/firenoc-calculator/billingslab/_create",
+    asyncHandler(async (req, res, next) => create(req, res))
+  );
+  api.post("/firenoc-calculator/billingslab/_search", (req, res) =>
+    search(req, res, pool)
+  );
+  api.post("/firenoc-calculator/billingslab/_update", (req, res) =>
+    update(req, res)
+  );
+  api.post(
+    "/firenoc-calculator/v1/_calculate",
+    asyncHandler(async (req, res) => calculate(req, res, pool))
+  );
+  api.post(
+    "/firenoc-calculator/v1/_getbill",
+    asyncHandler(async (req, res) => getbill(req, res))
+  );
 
   // perhaps expose some API metadata at the root
   api.get("/", (req, res) => {

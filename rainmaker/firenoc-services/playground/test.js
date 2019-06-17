851de248c3f5fbd1ae42b9f027df0c8a6031397f
muralimgwl <murali.m@goodworklabs.com>
2019-06-17 21:42:16 +0530

    updated test js

--- rainmaker/firenoc-services/playground/test.js
+++ rainmaker/firenoc-services/playground/test.js
@@ -1,30 +1,35 @@
 const Ajv = require("Ajv");
-const ajv = new Ajv({ loadSchema: loadSchema });
-const schema = require("../src/swagger.json");
-const axios = require("axios");
+const ajv = new Ajv({ allErrors:true });
+const schema = require("../src/model/fireNOC");
+console.log(schema);
+// const axios = require("axios");
 
 const data = require("./fireNOCRequest.json");
 
 ajv.addSchema(schema, "swagger.json");
 
-ajv
-  .compileAsync({ $ref: "swagger.json#/definitions/FireNOCRequest" })
-  .then(function(validate) {
-    var valid = validate(data);
-    if (!valid) console.log(validate.errors);
-  });
-
-function loadSchema(uri) {
-  return axios
-    .get(uri)
-    .then(function(res) {
-      return res.body;
-    })
-    .catch(function(error) {
-      throw new Error("Loading error: " + error.statusCode);
-    });
-}
-
-// let validate=ajv.compile({ $ref: 'swagger.json#/definitions/FireNOCRequest' });
+// ajv
+//   .compileAsync({ $ref: "swagger.json#/definitions/FireNOCRequest" })
+//   .then(function(validate) {
+//     var valid = validate(data);
+//     if (!valid) console.log(validate.errors);
+//   });
 //
-// if (validate(data)) console.log(validate.errors);
+// function loadSchema(uri) {
+//   return axios
+//     .get(uri)
+//     .then(function(res) {
+//       return res.body;
+//     })
+//     .catch(function(error) {
+//       throw new Error("Loading error: " + error.statusCode);
+//     });
+// }
+//
+
+let validate=ajv.compile(schema);
+
+if (validate(data)) console.log(validate.errors);
+
+
+console.log('test');

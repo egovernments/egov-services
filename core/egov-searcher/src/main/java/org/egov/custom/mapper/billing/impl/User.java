399b7f89d0a60142dea196834d178a5714c161b7
kaviyarasan1993 <kaviyarasan.p@riflexions.com>
2019-06-20 11:48:01 +0530

    equals added for user

--- core/egov-searcher/src/main/java/org/egov/custom/mapper/billing/impl/User.java
+++ core/egov-searcher/src/main/java/org/egov/custom/mapper/billing/impl/User.java
@@ -2,6 +2,7 @@ package org.egov.custom.mapper.billing.impl;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class User {
 	
 	private String id;

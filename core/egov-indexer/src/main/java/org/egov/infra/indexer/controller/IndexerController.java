6386473275deec14790dc18d223dd2f7b45fb8a2
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-06-26 14:34:18 +0530

    index api endpoint

--- core/egov-indexer/src/main/java/org/egov/infra/indexer/controller/IndexerController.java
+++ core/egov-indexer/src/main/java/org/egov/infra/indexer/controller/IndexerController.java
@@ -22,10 +22,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -59,18 +59,19 @@ public class IndexerController {
 	@Autowired
 	private Validator validator;
 	
-	//This is testing API 
-    @PostMapping("/_index")
-    @ResponseBody
-    private ResponseEntity<?> produceIndexJson(@RequestParam(name = "topic") String topic, @RequestBody Object indexJson){
-    	try{
-    		indexerProducer.producer(topic, indexJson);
-    	}catch(Exception e){
-    		return new ResponseEntity<>(indexJson ,HttpStatus.INTERNAL_SERVER_ERROR);
-    	}
-		return new ResponseEntity<>(indexJson ,HttpStatus.OK);
+	
+	@PostMapping("/{key}/_index")
+	@ResponseBody
+	private ResponseEntity<?> produceIndexJson(@PathVariable("key") String topic,
+			@RequestBody Object indexJson) {
+		try {
+			indexerProducer.producer(topic, indexJson);
+		} catch (Exception e) {
+			return new ResponseEntity<>(indexJson, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<>(indexJson, HttpStatus.OK);
 
-    }
+	}
 
     @GetMapping("/_reload")
     @ResponseBody

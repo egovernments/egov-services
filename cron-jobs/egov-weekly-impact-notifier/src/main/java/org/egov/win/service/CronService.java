554469cdfbd1a90638106ff5319769afa1237ef8
Vishal Mahuli <vishal.mahuli@tarento.com>
2019-07-12 10:58:14 +0530

    testing

--- cron-jobs/egov-weekly-impact-notifier/src/main/java/org/egov/win/service/CronService.java
+++ cron-jobs/egov-weekly-impact-notifier/src/main/java/org/egov/win/service/CronService.java
@@ -116,9 +116,7 @@ public class CronService {
 			for (int week = 0; week < noOfWeeks; week++) {
 				if (record.get("day").equals(prefix + week)) {
 					ulbCoveredPerWeek.put("w" + week + "ulbc", record.get("ulbcovered")); //ws not added because we need a union logic.
-					revenueCollectedPerWeek.put("w" + week + "revcoll", 
-							(Double.valueOf(record.get("revenuecollected").toString()) + 
-									Math.floor(((Double.valueOf(wsData.get(wsIndex).get("revenueCollected").toString())) / 10000000) * 1000) / 1000));
+					revenueCollectedPerWeek.put("w" + week + "revcoll", (Double.valueOf(record.get("revenuecollected").toString())));
 					servicesAppliedPerWeek.put("w" + week + "serapp", 
 							(Double.valueOf(record.get("servicesapplied").toString()) + Double.valueOf(wsData.get(wsIndex).get("servicesApplied").toString())));
 

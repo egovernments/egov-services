9ae9900236027ea72d92bed080af50c948d0afd2
gopesh <gopesh.yadav@egovernments.org>
2019-08-08 15:48:03 +0530

    corrected formatting of pdfs

--- rainmaker/pdf-service/src/api/api.js
+++ rainmaker/pdf-service/src/api/api.js
@@ -31,8 +31,8 @@ try {
     }
 } catch (error) {
   var errorResponse = error.response;
-  console.log(errorResponse) ; 
-  throw {message:"error occured while making request to "+endPoint+": response returned by call :"+(errorResponse && parseInt(errorResponse.status, 10))};  
+  console.log(error) ; 
+  throw {message:"error occured while making request to "+endPoint+": response returned by call :"+(errorResponse ? parseInt(errorResponse.status, 10):error.message)};  
 }
     
 }

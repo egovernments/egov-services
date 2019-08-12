8bf65b32d7d299205680127641b4671c3bc41b6d
gopesh <gopesh.yadav@egovernments.org>
2019-08-12 20:34:02 +0530

    console condition

--- rainmaker/pdf-service/src/index.js
+++ rainmaker/pdf-service/src/index.js
@@ -180,6 +180,7 @@ app.post("/pdf/v1/_create", asyncHandler(async (req, res)=> {
           formatObjectArrayObject.push(formatObject["content"]);
           //putting formatconfig in a file to check docdefinition on pdfmake playground online
           countOfObjectsInCurrentFile++;
+          console.log(countOfObjectsInCurrentFile+" "+maxPagesAllowed,(countOfObjectsInCurrentFile===maxPagesAllowed)||((i+1)==len));
           if((countOfObjectsInCurrentFile===maxPagesAllowed)||((i+1)==len))
           {
             let formatconfigCopy=JSON.parse(JSON.stringify(formatconfig));

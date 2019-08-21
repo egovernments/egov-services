3785ca4f2727b0f061bb78ac2229f767a85e54f0
Nithin <nithin_1992@msn.com>
2019-08-21 16:47:38 +0530

    Fix user otp service

--- user/user-otp/src/main/java/org/egov/UserOtpApplication.java
+++ user/user-otp/src/main/java/org/egov/UserOtpApplication.java
@@ -1,36 +1,16 @@
 package org.egov;
 
-import org.egov.persistence.LogAwareRestTemplate;
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(TracerConfiguration.class)
 public class UserOtpApplication {
-
-	@Bean
-	public RestTemplate restTemplate() {
-		return new LogAwareRestTemplate();
-	}
-
-	@Bean
-	public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
-		return new WebMvcConfigurerAdapter() {
-
-			@Override
-			public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-				configurer.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
-			}
-
-		};
-	}
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserOtpApplication.class, args);
 	}
+
 }

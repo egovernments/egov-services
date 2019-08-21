8c837484bc74574d04eef5aeaf09d9cbac0d4715
Aniket Talele <talele.aniket@gmail.com>
2019-08-21 12:57:44 +0530

    fetching search params from @RequestParam

--- rainmaker/egov-workflow-v2/src/main/java/org/egov/wf/web/models/BusinessServiceSearchCriteria.java
+++ rainmaker/egov-workflow-v2/src/main/java/org/egov/wf/web/models/BusinessServiceSearchCriteria.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;

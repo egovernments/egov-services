c25f16ade8312708046d40c5998928d968cecf5c
gopesh <gopesh.yadav@egovernments.org>
2019-08-26 12:22:22 +0530

    deleted redundant migration file

--- rainmaker/pdf-service/src/migration/ddl/createTable-egov-gen.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE egov-pdf-gen
-(
-  jobid character varying(50) NOT NULL,
-  tenantid character varying(20),
-  createdtime character varying(20),
-  filestoreids json,
-  endtime character varying(20),
-  CONSTRAINT egov-pdf-gen-pkey PRIMARY KEY (jobid)
-)
